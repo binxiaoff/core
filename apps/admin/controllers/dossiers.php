16dbd0ef7cf5682439dc286e58be4cb62b394e58
Damien <Damien@pc-damien>
2015-08-05 15:09:34 +0200

    Encodage email "preteur-regularisation-remboursement"  + condition prod/dev (v4)

--- apps/admin/controllers/dossiers.php
+++ apps/admin/controllers/dossiers.php
@@ -3402,7 +3402,7 @@ if ($lEcheances == false)
                                                     $timeAdd = strtotime($dateDernierStatut);
                                                     $month = $this->dates->tableauMois['fr'][date('n', $timeAdd)];
 
-
+                                                    // envi Prod
                                                     if ($this->Config['env'] == 'prod')
                                                     {
                                                         // Variables du mailing
@@ -3420,7 +3420,7 @@ if ($lEcheances == false)
                                                             'lien_fb' => $lien_fb,
                                                             'lien_tw' => $lien_tw);
                                                     }
-
+                                                    // envi Dev
                                                     else{
                                                        // Variables du mailing
                                                         $varMail = array(
