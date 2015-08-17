ded7bd90df3df72d948e2b4d3e4b4ac5b090aa98
Kevin <Kevin@192.168.0.175>
2015-08-17 09:45:48 +0200

    Merge branch RIB - Resolution unmerge 2/2

--- apps/default/controllers/universign.php
+++ apps/default/controllers/universign.php
@@ -1,5 +1,5 @@
 <?php
-// commit test
+
 class universignController extends bootstrap
 {
 	var $Command;
@@ -80,7 +80,7 @@ class universignController extends bootstrap
                                         
                                         $ligne_historique = "<tr><td><b>Signature du mandat</b> pour l’IBAN ".$compagnie_histo->iban." / ".$compagnie_histo->bic." le ".date('d/m/Y')." &agrave; ".date('H:i')."</td></tr>";                                        
                                         
-                                        $clients_histo->history = $clients_histo->history.' '.$ligne_historique;
+                                        $clients_histo->history = $clients_histo->history." ".$ligne_historique;
                                         $clients_histo->update();
                                         
                                         
@@ -690,4 +690,5 @@ class universignController extends bootstrap
 			die;	
 		}
 	}
-}	
+	
+}
