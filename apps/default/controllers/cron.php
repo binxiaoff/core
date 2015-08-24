85a8aa0d1ce90381cb11f4916075ed47c279a292
Kevin <Kevin@192.168.0.175>
2015-08-24 11:01:49 +0200

    Mise à jour de l'écheancier pour BDC.
    Date + 30 jours

--- apps/default/controllers/cron.php
+++ apps/default/controllers/cron.php
@@ -1221,20 +1221,34 @@ class cronController extends bootstrap
                 foreach ($lEcheanciers as $k => $e)
                 {
                     // on prend le nombre de jours dans le mois au lieu du mois
-                    $nbjourstemp = mktime(0, 0, 0, date("m") + $k, 1, date("Y"));
-                    $nbjoursMois += date('t', $nbjourstemp);
+//                    $nbjourstemp = mktime(0, 0, 0, date("m") + $k, 1, date("Y"));
+//                    $nbjoursMois += date('t', $nbjourstemp);
+//
+//                    // Date d'echeance preteur
+//                    $dateEcheance = $this->dates->dateAddMoisJours($this->projects->date_fin, 0, $nb_jours + $nbjoursMois);
+//                    $dateEcheance = date('Y-m-d H:i', $dateEcheance) . ':00';
+//
+//                    // Date d'echeance emprunteur
+//                    $dateEcheance_emprunteur = $this->dates->dateAddMoisJours($this->projects->date_fin, 0, $nbjoursMois);
+//                    // on retire 6 jours ouvrés
+//                    $dateEcheance_emprunteur = $jo->display_jours_ouvres($dateEcheance_emprunteur, 6);
+//
+//                    $dateEcheance_emprunteur = date('Y-m-d H:i', $dateEcheance_emprunteur) . ':00';
 
+                    // on prend le nombre de jours dans le mois au lieu du mois
+                    //$nbjourstemp = mktime (0,0,0,date("m")+$k ,1,date("Y"));
+                    //$nbjoursMois += date('t',$nbjourstemp);
+                    
                     // Date d'echeance preteur
-                    $dateEcheance = $this->dates->dateAddMoisJours($this->projects->date_fin, 0, $nb_jours + $nbjoursMois);
-                    $dateEcheance = date('Y-m-d H:i', $dateEcheance) . ':00';
-
+                    $dateEcheance = $this->dates->dateAddMoisJoursV3($this->projects->date_fin,$k,$nb_jours);
+                    $dateEcheance = date('Y-m-d h:i',$dateEcheance).':00';
+                    
                     // Date d'echeance emprunteur
-                    $dateEcheance_emprunteur = $this->dates->dateAddMoisJours($this->projects->date_fin, 0, $nbjoursMois);
+                    $dateEcheance_emprunteur = $this->dates->dateAddMoisJoursV3($this->projects->date_fin,$k);
+                    
                     // on retire 6 jours ouvrés
-                    $dateEcheance_emprunteur = $jo->display_jours_ouvres($dateEcheance_emprunteur, 6);
-
-                    $dateEcheance_emprunteur = date('Y-m-d H:i', $dateEcheance_emprunteur) . ':00';
-
+                    $dateEcheance_emprunteur = $jo->display_jours_ouvres($dateEcheance_emprunteur,6);
+                    $dateEcheance_emprunteur = date('Y-m-d H:i',$dateEcheance_emprunteur).':00';
 
 
                     // particulier
