f65fa79ac4a60bf4d00e4fc6e6e171d6c56b30e9
David RAUX <david.raux@unilend.fr>
2015-08-27 12:21:24 +0200

    correction suite MEP sur dashboard

--- apps/admin/controllers/root.php
+++ apps/admin/controllers/root.php
@@ -217,10 +217,13 @@ class rootController extends bootstrap
 		
 		
 		// Les CA pour les typrd Partenaires
-		foreach($this->lTypes as $part)
+		/*foreach($this->lTypes as $part)
 		{
 			$lCaParMoisPart[$part['id_type']] = $this->transactions->recupCAByMonthForAYearType($this->year,$part['id_type']);
-		}
+		}*/
+		
+		$this->caParmoisPart = $this->transactions->recupMonthlyPartnershipTurnoverByYear($this->year);
+		
 		
 		for($i=1; $i<=12; $i++)		
 		{
@@ -231,21 +234,24 @@ class rootController extends bootstrap
 			
 			$this->RembEmprParMois[$i] = number_format(($lRembParMois[$i] != ''?$lRembParMois[$i]:0),2,'.','');
 			
-			
+			/* suppression / optimisation
 			foreach($this->lTypes as $part)
 			{
 				$this->caParmoisPart[$part['id_type']][$i] = number_format(($lCaParMoisPart[$part['id_type']][$i] != ''?$lCaParMoisPart[$part['id_type']][$i]:0),2,'.','');
-			}
+			}*/
 		}
 		
 		
 		$this->month = date('m');
-		$this->year = date('Y');
 		
 		//////////////////
 		
 		// nb preteurs connect
 		$this->nbPreteurLogin = $this->clients_history->getNb($this->month,$this->year,'type = 1 AND status = 1',1);
+		// nb inscription preteur
+		$this->nbInscriptionPreteur = $this->clients_history->getNb($this->month,$this->year,'type = 1 AND status = 2',1);
+		
+		/* Pas de donnée collectée en base sur les métriques ci dessous;
 		
 		// nb emprunteur connect
 		$this->nbEmprunteurLogin = $this->clients_history->getNb($this->month,$this->year,'type > 1 AND status = 1',1);
@@ -253,11 +259,10 @@ class rootController extends bootstrap
 		// nb depot dossier
 		$this->nbDepotDossier = $this->clients_history->getNb($this->month,$this->year,'type > 1 AND status = 3');
 		
-		// nb inscription preteur
-		$this->nbInscriptionPreteur = $this->clients_history->getNb($this->month,$this->year,'type = 1 AND status = 2',1);
 		
 		// nb inscription emprunteur
 		$this->nbInscriptionEmprunteur = $this->clients_history->getNb($this->month,$this->year,'type > 1 AND status = 2',1);
+		*/
 		
 		// fonds deposés
 		$this->nbFondsDeposes = $this->caParmois[$this->month];
