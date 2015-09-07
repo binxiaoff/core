68735f4deba016777cb93a27254ce4b7cf3555c1
Bin XIAO <bin.xiao@unilend.fr>
2015-09-07 17:53:10 +0200

    0018775: Court terme sur LP

--- apps/default/controllers/create_project_emprunteur.php
+++ apps/default/controllers/create_project_emprunteur.php
@@ -47,7 +47,7 @@ class create_project_emprunteurController extends bootstrap
 
 		$this->settings->get('Durée des prêts autorisées','type');
 		$this->dureePossible = explode(',',$this->settings->value);
-		if (empty($this->dureePossible)) {
+		if (empty($this->settings->value)) {
 			$this->dureePossible = array(24,36,48,60);
 		}
 
