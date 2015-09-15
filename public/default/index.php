ff94fa1be6874dfec7bc274db0d7282be222ffb5
Damien Courtier <d.courtier@equinoa.com>
2015-09-15 11:29:51 +0200

    debug

--- public/default/index.php
+++ public/default/index.php
@@ -23,7 +23,6 @@ ini_set('session.gc_maxlifetime',3600); // 1h la session
 //if($_SERVER['REMOTE_ADDR'] == '93.26.42.99' || $_SERVER['REMOTE_ADDR'] == '90.62.110.115' )
 //if($_SERVER['REMOTE_ADDR'] != '93.26.42.99')
 
-
 //require('prepend.php');
 
 
