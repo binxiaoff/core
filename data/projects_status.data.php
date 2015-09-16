9cac6d0295a2be47fabee2b74dc4404bb670f7e1
annabreyer <anna.breyer@unilend.fr>
2015-09-16 16:08:08 +0200

    add projetc status constant PROBLEME_J_PLUS_X

--- data/projects_status.data.php
+++ data/projects_status.data.php
@@ -4,26 +4,26 @@
 // **************************************************************************************************** //
 //
 // Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-// associated documentation files (the "Software"), to deal in the Software without restriction, 
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies 
+// The above copyright notice and this permission notice shall be included in all copies
 // or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but 
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. 
-// In no event shall the authors or copyright holders equinoa be liable for any claim, 
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from, 
+// The Software is provided "as is", without warranty of any kind, express or implied, including but
+// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders equinoa be liable for any claim,
+// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
 // out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising 
-// or otherwise to promote the sale, use or other dealings in this Software without 
+// Except as contained in this notice, the name of equinoa shall not be used in advertising
+// or otherwise to promote the sale, use or other dealings in this Software without
 // prior written authorization from equinoa.
 //
 //  Version : 2.4.0
 //  Date : 21/03/2011
 //  Coupable : CM
-//                                                                                   
+//
 // **************************************************************************************************** //
 
 class projects_status extends projects_status_crud
@@ -51,34 +51,35 @@ class projects_status extends projects_status_crud
 	const PAS_3_BILANS = 6;
 	const ABANDON_ETAPE_2 = 7;
 	const REMBOURSEMENT_ANTICIPE = 130;
+	const PROBLEME_J_plus_X = 140;
 
 
 	function projects_status($bdd,$params='')
     {
         parent::projects_status($bdd,$params);
     }
-    
+
     function get($id,$field='id_project_status')
     {
         return parent::get($id,$field);
     }
-    
+
     function update($cs='')
     {
         parent::update($cs);
     }
-    
+
     function delete($id,$field='id_project_status')
     {
     	parent::delete($id,$field);
     }
-    
+
     function create($cs='')
     {
         $id = parent::create($cs);
         return $id;
     }
-	
+
 	function select($where='',$order='',$start='',$nb='')
 	{
 		if($where != '')
@@ -94,52 +95,52 @@ class projects_status extends projects_status_crud
 			$result[] = $record;
 		}
 		return $result;
-	} 
-	
+	}
+
 	function counter($where='')
 	{
 		if($where != '')
 			$where = ' WHERE '.$where;
-			
+
 		$sql='SELECT count(*) FROM `projects_status` '.$where;
 
 		$result = $this->bdd->query($sql);
 		return (int)($this->bdd->result($result,0,0));
 	}
-	
+
 	function exist($id,$field='id_project_status')
 	{
 		$sql = 'SELECT * FROM `projects_status` WHERE '.$field.'="'.$id.'"';
 		$result = $this->bdd->query($sql);
 		return ($this->bdd->fetch_array($result,0,0)>0);
 	}
-	
+
 	function getIdStatus($status)
 	{
 		$sql = 'SELECT id_project_status
-				FROM `projects_status` 
+				FROM `projects_status`
 				WHERE status = "'.$status.'"
 				';
 
 		$result = $this->bdd->query($sql);
 		return (int)($this->bdd->result($result,0,0));
 	}
-	
+
 	function getLabel($status)
 	{
 		$sql = 'SELECT label
-				FROM `projects_status` 
+				FROM `projects_status`
 				WHERE status = "'.$status.'"
 				';
 
 		$result = $this->bdd->query($sql);
 		return ($this->bdd->result($result,0,0));
 	}
-	
+
 	function getLastStatut($id_project)
 	{
-		$sql = 'SELECT id_project_status 
-				FROM `projects_status_history` 
+		$sql = 'SELECT id_project_status
+				FROM `projects_status_history`
 				WHERE id_project = '.$id_project.'
 				ORDER BY added DESC
 				LIMIT 1
@@ -147,14 +148,14 @@ class projects_status extends projects_status_crud
 
 		$result = $this->bdd->query($sql);
 		$id_project_statut = (int)($this->bdd->result($result,0,0));
-		
+
 		return parent::get($id_project_statut,'id_project_status');
 	}
-	
+
 	function getLastStatutByMonth($id_project,$month,$year)
 	{
-		$sql = 'SELECT id_project_status 
-				FROM `projects_status_history` 
+		$sql = 'SELECT id_project_status
+				FROM `projects_status_history`
 				WHERE id_project = '.$id_project.'
 				AND MONTH(added) = '.$month.' AND YEAR(added) = '.$year.'
 				ORDER BY added DESC
@@ -163,9 +164,9 @@ class projects_status extends projects_status_crud
 
 		$result = $this->bdd->query($sql);
 		$id_project_statut = (int)($this->bdd->result($result,0,0));
-		
+
 		return parent::get($id_project_statut,'id_project_status');
 	}
-	
-	
+
+
 }
