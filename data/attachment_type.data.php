6340086d3f5502c547c7e7465813284de4cba215
annabreyer <anna.breyer@unilend.fr>
2015-09-22 18:23:21 +0200

    add new constants for projects

--- data/attachment_type.data.php
+++ data/attachment_type.data.php
@@ -4,26 +4,26 @@
 // **************************************************************************************************** //
 //
 // Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-// associated documentation files (the "Software"), to deal in the Software without restriction, 
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies 
+// The above copyright notice and this permission notice shall be included in all copies
 // or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but 
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. 
-// In no event shall the authors or copyright holders equinoa be liable for any claim, 
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from, 
+// The Software is provided "as is", without warranty of any kind, express or implied, including but
+// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders equinoa be liable for any claim,
+// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
 // out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising 
-// or otherwise to promote the sale, use or other dealings in this Software without 
+// Except as contained in this notice, the name of equinoa shall not be used in advertising
+// or otherwise to promote the sale, use or other dealings in this Software without
 // prior written authorization from equinoa.
 //
 //  Version : 2.4.0
 //  Date : 21/03/2011
 //  Coupable : CM
-//                                                                                   
+//
 // **************************************************************************************************** //
 
 class attachment_type extends attachment_type_crud
@@ -46,35 +46,47 @@ class attachment_type extends attachment_type_crud
 	const DISPENSE_PRELEVEMENT_2015 = 26;
 	const DISPENSE_PRELEVEMENT_2016 = 27;
 	const DISPENSE_PRELEVEMENT_2017 = 28;
+	const DERNIERE_LIASSE_FISCAL = 15;
+    const RELEVE_BANCAIRE_MOIS_N = 30;
+    const RELEVE_BANCAIRE_MOIS_N_1 = 31;
+    const RELEVE_BANCAIRE_MOIS_N_2 = 32;
+    const PRESENTATION_ENTRERPISE = 33;
+    const ETAT_ENDETTEMENT = 34;
+    const LIASSE_FISCAL_N_1 = 35;
+    const LIASSE_FISCAL_N_2 = 36;
+    const RAPPORT_CAC = 37;
+    const PREVISIONNEL = 38;
+    const BALANCE_CLIENT = 39;
+    const BALANCE_FOURNISSEUR = 40;
+    const ETAT_PRIVILEGES_NANTISSEMENTS = 41;
 
 
-
-	function attachment_type($bdd,$params='')
+    function attachment_type($bdd,$params='')
     {
         parent::attachment_type($bdd,$params);
     }
-    
+
     function get($id,$field='id')
     {
         return parent::get($id,$field);
     }
-    
+
     function update($cs='')
     {
         parent::update($cs);
     }
-    
+
     function delete($id,$field='id')
     {
     	parent::delete($id,$field);
     }
-    
+
     function create($cs='')
     {
         $id = parent::create($cs);
         return $id;
     }
-	
+
 	function select($where='',$order='',$start='',$nb='')
 	{
 		if($where != '')
@@ -90,23 +102,35 @@ class attachment_type extends attachment_type_crud
 			$result[] = $record;
 		}
 		return $result;
-	} 
-	
+	}
+
 	function counter($where='')
 	{
 		if($where != '')
 			$where = ' WHERE '.$where;
-			
+
 		$sql='SELECT count(*) FROM `attachment_type` '.$where;
 
 		$result = $this->bdd->query($sql);
 		return (int)($this->bdd->result($result,0,0));
 	}
-	
+
 	function exist($id,$field='id')
 	{
 		$sql = 'SELECT * FROM `attachment_type` WHERE '.$field.'="'.$id.'"';
 		$result = $this->bdd->query($sql);
 		return ($this->bdd->fetch_array($result,0,0)>0);
+	}
+
+	public function getAll(){
+
+		$sql = 'SELECT * FROM `attachment_type`';
+		$resultat = $this->bdd->query($sql);
+		$result = array();
+		while($record = $this->bdd->fetch_array($resultat))
+		{
+			$result[] = $record;
+		}
+		return $result;
 	}
 }
