00b92fd5766a0e3def63a52c2c305f98beecba93
annabreyer <anna.breyer@unilend.fr>
2015-09-22 09:43:34 +0200

    add constant PRESCRIPTEUR

--- data/attachment.data.php
+++ data/attachment.data.php
@@ -4,48 +4,49 @@
 // **************************************************************************************************** //
 //
 // Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-// associated documentation files (the "Software"), to deal in the Software without restriction, 
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies 
+// The above copyright notice and this permission notice shall be included in all copies
 // or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but 
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. 
-// In no event shall the authors or copyright holders equinoa be liable for any claim, 
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from, 
+// The Software is provided "as is", without warranty of any kind, express or implied, including but
+// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders equinoa be liable for any claim,
+// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
 // out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising 
-// or otherwise to promote the sale, use or other dealings in this Software without 
+// Except as contained in this notice, the name of equinoa shall not be used in advertising
+// or otherwise to promote the sale, use or other dealings in this Software without
 // prior written authorization from equinoa.
 //
 //  Version : 2.4.0
 //  Date : 21/03/2011
 //  Coupable : CM
-//                                                                                   
+//
 // **************************************************************************************************** //
 
 class attachment extends attachment_crud
 {
 	const LENDER = 'lenders_accounts';
 	const COMPANY = 'companies';
+	const PRESCRIPTEUR = 'prescripteurs';
 
 	function attachment($bdd,$params='')
     {
         parent::attachment($bdd,$params);
     }
-    
+
     function get($id,$field='id')
     {
         return parent::get($id,$field);
     }
-    
+
     function update($cs='')
     {
         parent::update($cs);
     }
-    
+
     function delete($id,$field='id')
     {
     	parent::delete($id,$field);
@@ -56,7 +57,7 @@ class attachment extends attachment_crud
         $id = parent::create($cs);
         return $id;
     }
-	
+
 	function select($where='',$order='',$start='',$nb='')
 	{
 		if($where != '')
@@ -72,19 +73,19 @@ class attachment extends attachment_crud
 			$result[] = $record;
 		}
 		return $result;
-	} 
-	
+	}
+
 	function counter($where='')
 	{
 		if($where != '')
 			$where = ' WHERE '.$where;
-			
+
 		$sql='SELECT count(*) FROM `attachment` '.$where;
 
 		$result = $this->bdd->query($sql);
 		return (int)($this->bdd->result($result,0,0));
 	}
-	
+
 	function exist($id,$field='id')
 	{
 		$sql = 'SELECT * FROM `attachment` WHERE '.$field.'="'.$id.'"';
