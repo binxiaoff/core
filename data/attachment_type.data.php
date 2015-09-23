0680f7aad4cdca2373e685a9842fa8457cf6b332
Antoine Préveaux <antoine.preveaux@unilend.fr>
2015-09-23 17:44:18 +0200

    [Tunnel emprunteur] Clean attachment_type and projects_status classes

--- data/attachment_type.data.php
+++ data/attachment_type.data.php
@@ -28,103 +28,91 @@
 
 class attachment_type extends attachment_type_crud
 {
-	const CNI_PASSPORTE = 1;
-	const JUSTIFICATIF_DOMICILE = 2;
-	const RIB = 3;
-	const ATTESTATION_HEBERGEMENT_TIERS = 4;
-	const CNI_PASSPORT_TIERS_HEBERGEANT = 5;
-	const JUSTIFICATIF_FISCAL = 6;
-	const CNI_PASSPORTE_DIRIGEANT = 7;
-	const KBIS = 8;
-	const DELEGATION_POUVOIR = 9;
-	const STATUTS = 10;
-	const CNI_PASSPORTE_VERSO = 11;
-	const AUTRE1 = 22;
-	const AUTRE2 = 23;
-	const AUTRE3 = 24;
-	const DISPENSE_PRELEVEMENT_2014 = 25;
-	const DISPENSE_PRELEVEMENT_2015 = 26;
-	const DISPENSE_PRELEVEMENT_2016 = 27;
-	const DISPENSE_PRELEVEMENT_2017 = 28;
-	const DERNIERE_LIASSE_FISCAL = 15;
-    const RELEVE_BANCAIRE_MOIS_N = 30;
-    const RELEVE_BANCAIRE_MOIS_N_1 = 31;
-    const RELEVE_BANCAIRE_MOIS_N_2 = 32;
-    const PRESENTATION_ENTRERPISE = 33;
-    const ETAT_ENDETTEMENT = 34;
-    const LIASSE_FISCAL_N_1 = 35;
-    const LIASSE_FISCAL_N_2 = 36;
-    const RAPPORT_CAC = 37;
-    const PREVISIONNEL = 38;
-    const BALANCE_CLIENT = 39;
-    const BALANCE_FOURNISSEUR = 40;
+    const CNI_PASSPORTE                 = 1;
+    const JUSTIFICATIF_DOMICILE         = 2;
+    const RIB                           = 3;
+    const ATTESTATION_HEBERGEMENT_TIERS = 4;
+    const CNI_PASSPORT_TIERS_HEBERGEANT = 5;
+    const JUSTIFICATIF_FISCAL           = 6;
+    const CNI_PASSPORTE_DIRIGEANT       = 7;
+    const KBIS                          = 8;
+    const DELEGATION_POUVOIR            = 9;
+    const STATUTS                       = 10;
+    const CNI_PASSPORTE_VERSO           = 11;
+    const AUTRE1                        = 22;
+    const AUTRE2                        = 23;
+    const AUTRE3                        = 24;
+    const DISPENSE_PRELEVEMENT_2014     = 25;
+    const DISPENSE_PRELEVEMENT_2015     = 26;
+    const DISPENSE_PRELEVEMENT_2016     = 27;
+    const DISPENSE_PRELEVEMENT_2017     = 28;
+    const DERNIERE_LIASSE_FISCAL        = 15;
+    const RELEVE_BANCAIRE_MOIS_N        = 30;
+    const RELEVE_BANCAIRE_MOIS_N_1      = 31;
+    const RELEVE_BANCAIRE_MOIS_N_2      = 32;
+    const PRESENTATION_ENTRERPISE       = 33;
+    const ETAT_ENDETTEMENT              = 34;
+    const LIASSE_FISCAL_N_1             = 35;
+    const LIASSE_FISCAL_N_2             = 36;
+    const RAPPORT_CAC                   = 37;
+    const PREVISIONNEL                  = 38;
+    const BALANCE_CLIENT                = 39;
+    const BALANCE_FOURNISSEUR           = 40;
     const ETAT_PRIVILEGES_NANTISSEMENTS = 41;
 
-
-    function attachment_type($bdd,$params='')
+    public function __construct($bdd, $params = '')
     {
-        parent::attachment_type($bdd,$params);
+        parent::attachment_type($bdd, $params);
     }
 
-    function get($id,$field='id')
+    public function get($id, $field = 'id')
     {
-        return parent::get($id,$field);
+        return parent::get($id, $field);
     }
 
-    function update($cs='')
+    public function delete($id, $field = 'id')
     {
-        parent::update($cs);
+        parent::delete($id, $field);
     }
 
-    function delete($id,$field='id')
+    public function select($where = '', $order = '', $start = '', $nb = '')
     {
-    	parent::delete($id,$field);
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+        if ($order != '') {
+            $order = ' ORDER BY ' . $order;
+        }
+        $sql = 'SELECT * FROM `attachment_type`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+
+        $resultat = $this->bdd->query($sql);
+        $result   = array();
+        while ($record = $this->bdd->fetch_array($resultat)) {
+            $result[] = $record;
+        }
+        return $result;
     }
 
-    function create($cs='')
+    public function counter($where = '')
     {
-        $id = parent::create($cs);
-        return $id;
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+
+        $result = $this->bdd->query('SELECT count(*) FROM `attachment_type` ' . $where);
+        return (int) ($this->bdd->result($result, 0, 0));
     }
 
-	function select($where='',$order='',$start='',$nb='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
-		if($order != '')
-			$order = ' ORDER BY '.$order;
-		$sql = 'SELECT * FROM `attachment_type`'.$where.$order.($nb!='' && $start !=''?' LIMIT '.$start.','.$nb:($nb!=''?' LIMIT '.$nb:''));
+    public function exist($id, $field = 'id')
+    {
+        $result = $this->bdd->query('SELECT * FROM `attachment_type` WHERE ' . $field . ' = "' . $id . '"');
+        return ($this->bdd->fetch_array($result, 0, 0) > 0);
+    }
 
-		$resultat = $this->bdd->query($sql);
-		$result = array();
-		while($record = $this->bdd->fetch_array($resultat))
-		{
-			$result[] = $record;
-		}
-		return $result;
-	}
-
-	function counter($where='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
-
-		$sql='SELECT count(*) FROM `attachment_type` '.$where;
-
-		$result = $this->bdd->query($sql);
-		return (int)($this->bdd->result($result,0,0));
-	}
-
-	function exist($id,$field='id')
-	{
-		$sql = 'SELECT * FROM `attachment_type` WHERE '.$field.'="'.$id.'"';
-		$result = $this->bdd->query($sql);
-		return ($this->bdd->fetch_array($result,0,0)>0);
-	}
-
-	public function getAllTypesForProjects(){
-
-        $aTypes = array(self::RELEVE_BANCAIRE_MOIS_N,
+    public function getAllTypesForProjects()
+    {
+        $aTypes = array(
+            self::RELEVE_BANCAIRE_MOIS_N,
             self::RELEVE_BANCAIRE_MOIS_N_1,
             self::RELEVE_BANCAIRE_MOIS_N_2,
             self::KBIS,
@@ -144,16 +132,13 @@ class attachment_type extends attachment_type_crud
             self::AUTRE3,
             self::CNI_PASSPORTE_DIRIGEANT,
             self::CNI_PASSPORTE_VERSO
-            );
+        );
 
-
-		$sql = 'SELECT * FROM `attachment_type` WHERE `id` IN ('.$aTypes.');';
-		$resultat = $this->bdd->query($sql);
-		$result = array();
-		while($record = $this->bdd->fetch_array($resultat))
-		{
-			$result[] = $record;
-		}
-		return $result;
-	}
+        $resultat = $this->bdd->query('SELECT * FROM `attachment_type` WHERE `id` IN (' . $aTypes . ')');
+        $result   = array();
+        while ($record = $this->bdd->fetch_array($resultat)) {
+            $result[] = $record;
+        }
+        return $result;
+    }
 }
--- data/projects_status.data.php
+++ data/projects_status.data.php
@@ -28,149 +28,117 @@
 
 class projects_status extends projects_status_crud
 {
+    const NOTE_EXTERNE_FAIBLE    = 5;
+    const PAS_3_BILANS           = 6;
+    const ABANDON_ETAPE_2        = 7;
+    const COMPLETUDE_ETAPE_2     = 8;
+    const ABANDON                = 9;
+    const EN_ATTENTE_PIECES      = 11;
+    const A_TRAITER              = 10;
+    const A_LETUDE               = 20;
+    const A_FUNDER               = 40;
+    const EN_FUNDING             = 50;
+    const FUNDE                  = 60;
+    const FUNDING_KO             = 70;
+    const REJETE                 = 30;
+    const REMBOURSEMENT          = 80;
+    const PROBLEME               = 100;
+    const RECOUVREMENT           = 110;
+    const DEFAULT_STATUS         = 120;
+    const REMBOURSE              = 90;
+    const PRET_REFUSE            = 75;
+    const REVUE_ANALYSTE         = 31;
+    const REJET_ANALYSTE         = 32;
+    const COMITE                 = 33;
+    const REJET_COMITE           = 34;
+    const PREP_FUNDING           = 35;
+    const REMBOURSEMENT_ANTICIPE = 130;
+    const PROBLEME_J_PLUS_X      = 140;
 
-	const NOTE_EXTERNE_FAIBLE = 5;
-	const PAS_3_BILANS = 6;
-	const ABANDON_ETAPE_2 = 7;
-	const COMPLETUDE_ETAPE_2 = 8;
-	const ABANDON = 9;
-	const EN_ATTENTE_PIECES = 11;
-	const A_TRAITER = 10;
-	const A_LETUDE = 20;
-	const A_FUNDER = 40;
-	const EN_FUNDING = 50;
-	const FUNDE = 60;
-	const FUNDING_KO = 70;
-	const REJETE = 30;
-	const REMBOURSEMENT = 80;
-	const PROBLEME = 100;
-	const RECOUVREMENT = 110;
-	const DEFAULT_STATUS = 120;
-	const REMBOURSE = 90;
-	const PRET_REFUSE = 75;
-	const REVUE_ANALYSTE = 31;
-	const REJET_ANALYSTE = 32;
-	const COMITE = 33;
-	const REJET_COMITE = 34;
-	const PREP_FUNDING = 35;
-	const REMBOURSEMENT_ANTICIPE = 130;
-	const PROBLEME_J_PLUS_X = 140;
-
-
-
-	function projects_status($bdd,$params='')
+    public function __construct($bdd, $params = '')
     {
-        parent::projects_status($bdd,$params);
+        parent::projects_status($bdd, $params);
     }
 
-    function get($id,$field='id_project_status')
+    public function get($id, $field = 'id_project_status')
     {
-        return parent::get($id,$field);
+        return parent::get($id, $field);
     }
 
-    function update($cs='')
+    public function delete($id, $field = 'id_project_status')
     {
-        parent::update($cs);
+        parent::delete($id, $field);
     }
 
-    function delete($id,$field='id_project_status')
+    public function select($where = '', $order = '', $start = '', $nb = '')
     {
-    	parent::delete($id,$field);
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+        if ($order != '') {
+            $order = ' ORDER BY ' . $order;
+        }
+        $sql = 'SELECT * FROM `projects_status`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+
+        $resultat = $this->bdd->query($sql);
+        $result   = array();
+        while ($record = $this->bdd->fetch_array($resultat)) {
+            $result[] = $record;
+        }
+        return $result;
     }
 
-    function create($cs='')
+    public function counter($where = '')
     {
-        $id = parent::create($cs);
-        return $id;
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+
+        $sql = 'SELECT count(*) FROM `projects_status` ' . $where;
+
+        $result = $this->bdd->query($sql);
+        return (int)($this->bdd->result($result, 0, 0));
     }
 
-	function select($where='',$order='',$start='',$nb='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
-		if($order != '')
-			$order = ' ORDER BY '.$order;
-		$sql = 'SELECT * FROM `projects_status`'.$where.$order.($nb!='' && $start !=''?' LIMIT '.$start.','.$nb:($nb!=''?' LIMIT '.$nb:''));
+    public function exist($id, $field = 'id_project_status')
+    {
+        $result = $this->bdd->query('SELECT * FROM `projects_status` WHERE ' . $field . ' = "' . $id . '"');
+        return ($this->bdd->fetch_array($result, 0, 0) > 0);
+    }
 
-		$resultat = $this->bdd->query($sql);
-		$result = array();
-		while($record = $this->bdd->fetch_array($resultat))
-		{
-			$result[] = $record;
-		}
-		return $result;
-	}
+    public function getIdStatus($status)
+    {
+        $result = $this->bdd->query('SELECT id_project_status FROM `projects_status` WHERE status = "' . $status . '"');
+        return (int)($this->bdd->result($result, 0, 0));
+    }
 
-	function counter($where='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
+    public function getLabel($status)
+    {
+        $result = $this->bdd->query('SELECT label FROM `projects_status` WHERE status = "' . $status . '"');
+        return ($this->bdd->result($result, 0, 0));
+    }
 
-		$sql='SELECT count(*) FROM `projects_status` '.$where;
+    public function getLastStatut($id_project)
+    {
+        $result            = $this->bdd->query('SELECT id_project_status FROM `projects_status_history` WHERE id_project = ' . $id_project . ' ORDER BY added DESC LIMIT 1');
+        $id_project_statut = (int)($this->bdd->result($result, 0, 0));
 
-		$result = $this->bdd->query($sql);
-		return (int)($this->bdd->result($result,0,0));
-	}
+        return parent::get($id_project_statut, 'id_project_status');
+    }
 
-	function exist($id,$field='id_project_status')
-	{
-		$sql = 'SELECT * FROM `projects_status` WHERE '.$field.'="'.$id.'"';
-		$result = $this->bdd->query($sql);
-		return ($this->bdd->fetch_array($result,0,0)>0);
-	}
-
-	function getIdStatus($status)
-	{
-		$sql = 'SELECT id_project_status
-				FROM `projects_status`
-				WHERE status = "'.$status.'"
-				';
-
-		$result = $this->bdd->query($sql);
-		return (int)($this->bdd->result($result,0,0));
-	}
-
-	function getLabel($status)
-	{
-		$sql = 'SELECT label
-				FROM `projects_status`
-				WHERE status = "'.$status.'"
-				';
-
-		$result = $this->bdd->query($sql);
-		return ($this->bdd->result($result,0,0));
-	}
-
-	function getLastStatut($id_project)
-	{
-		$sql = 'SELECT id_project_status
+    public function getLastStatutByMonth($id_project, $month, $year)
+    {
+        $sql = 'SELECT id_project_status
 				FROM `projects_status_history`
-				WHERE id_project = '.$id_project.'
+				WHERE id_project = ' . $id_project . '
+				AND MONTH(added) = ' . $month . ' AND YEAR(added) = ' . $year . '
 				ORDER BY added DESC
 				LIMIT 1
 				';
 
-		$result = $this->bdd->query($sql);
-		$id_project_statut = (int)($this->bdd->result($result,0,0));
+        $result            = $this->bdd->query($sql);
+        $id_project_statut = (int)($this->bdd->result($result, 0, 0));
 
-		return parent::get($id_project_statut,'id_project_status');
-	}
-
-	function getLastStatutByMonth($id_project,$month,$year)
-	{
-		$sql = 'SELECT id_project_status
-				FROM `projects_status_history`
-				WHERE id_project = '.$id_project.'
-				AND MONTH(added) = '.$month.' AND YEAR(added) = '.$year.'
-				ORDER BY added DESC
-				LIMIT 1
-				';
-
-		$result = $this->bdd->query($sql);
-		$id_project_statut = (int)($this->bdd->result($result,0,0));
-
-		return parent::get($id_project_statut,'id_project_status');
-	}
-
-
+        return parent::get($id_project_statut, 'id_project_status');
+    }
 }
