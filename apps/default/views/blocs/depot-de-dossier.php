dca31e11ac6d68ccf29784531d46c3fc4ec05bb1
annabreyer <anna.breyer@unilend.fr>
2015-09-30 17:09:43 +0200

    add divs from header so it can serve instead of header.php

--- apps/default/views/blocs/depot-de-dossier.php
+++ apps/default/views/blocs/depot-de-dossier.php
@@ -1,19 +1,25 @@
-<ul style="display:inline; padding-left:0px;">
-	<li style="display:inline;color:#A1A5A7;"><a style="color:#A1A5A7;" href="<?=$this->lurl?>"><?=$this->lng['depot-de-dossier-header']['accueil']?></a> ></li>
-    
-	<li style="display:inline;color:#6B6E70"><?=$this->lng['depot-de-dossier-header']['titre']?></li>
+<div class="wrapper">
+    <div class="header">
+        <div class="shell clearfix">
+            <div class="logo">
+                <a href="<?= $this->lurl ?>">Unilend</a>
+            </div>
+            <!-- /.logo -->
+        </div>
+    </div>
+    <!--/.header>-->
 
-</ul>
-<h1><?=$this->lng['depot-de-dossier-header']['titre'.($this->page==2?'-etape-2':'')]?></h1>
+    <div class="main">
+        <div class="shell">
 
-<?php /*?><div class="proccess">
-    <?=($this->page > 1 ?'<a href="#">'.$this->lng['depot-de-dossier-header']['etape'].' 1</a>':'<span>'.$this->lng['depot-de-dossier-header']['etape-1'].'</span>')?>
-    <i class="divider icon-arrow-medium-thin-next"></i>
-    <?=($this->page > 2?'<a href="'.$this->lurl.'/depot_de_dossier/etape2/'.$this->clients->hash.'">'.$this->lng['depot-de-dossier-header']['etape'].' 2</a>':'<span>'.$this->lng['depot-de-dossier-header']['etape-2'].'</span>')?>
-    <i class="divider icon-arrow-medium-thin-next"></i>
-    <?=($this->page > 3?'<a href="'.$this->lurl.'/depot_de_dossier/etape3/'.$this->clients->hash.'">'.$this->lng['depot-de-dossier-header']['etape'].' 3</a>':'<span>'.$this->lng['depot-de-dossier-header']['etape-3'].'</span>')?>
-    <i class="divider icon-arrow-medium-thin-next"></i>
-    <?=($this->page > 4?'<a href="'.$this->lurl.'/depot_de_dossier/etape4/'.$this->clients->hash.'">'.$this->lng['depot-de-dossier-header']['etape'].' 4</a>':'<span>'.$this->lng['depot-de-dossier-header']['etape-4'].'</span>')?>
-    <i class="divider icon-arrow-medium-thin-next"></i>
-    <span><?=$this->lng['depot-de-dossier-header']['etape-5']?></span>
-</div><!-- /.proccess --><?php */?>
+            <ul style="display:inline; padding-left:0px;">
+                <li style="display:inline;color:#A1A5A7;">
+                    <a style="color:#A1A5A7;"
+                       href="<?= $this->lurl ?>"><?= $this->lng['depot-de-dossier-header']['accueil'] ?>></a>
+
+                </li>
+
+                <li style="display:inline;color:#6B6E70"><?= $this->lng['depot-de-dossier-header']['titre'] ?></li>
+
+            </ul>
+            <h1><?= $this->lng['depot-de-dossier-header']['titre' . ($this->page == 2 ? '-etape-2' : '')] ?></h1>
