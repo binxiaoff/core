3304b2256d40b4caa6cb734eaf5aeb0955e11457
Antoine Préveaux <antoine.preveaux@unilend.fr>
2015-10-02 11:36:30 +0200

    Limit dynamic images generation to a set of predefined sizes and send 404 if image does not match expected requirements

--- public/default/images.php
+++ public/default/images.php
@@ -6,18 +6,17 @@
  * Resulting image is saved in order to avoid regenerating it next time
  *
  * Current restrictions are
- * - Image format is JPEG (source and generated images)
- * - File extension is '.jpg'
- * - Compression is 90%
- * - Fallback image is always the same, even if we know expected size
+ * - Input file extension is 'jpg, jpeg, JPG, JPEG, png, PNG'
+ * - Output format is JPEG
+ * - Output compression is 90%
  */
-include __DIR__ . '/../../core/errorhandler.class.php';
-include __DIR__ . '/../../config.php';
+include_once __DIR__ . '/../../core/errorhandler.class.php';
+include_once __DIR__ . '/../../config.php';
 
 /**
  * @var array $config
  */
-$handler = new ErrorHandler(
+$handler = new \ErrorHandler(
     $config['error_handler'][$config['env']]['file'],
     $config['error_handler'][$config['env']]['allow_display'],
     $config['error_handler'][$config['env']]['allow_log'],
@@ -26,12 +25,11 @@ $handler = new ErrorHandler(
 
 try {
     if (1 !== preg_match('#images/dyn/([^/]+)/([0-9]+)/(.+\.(jpg|jpeg|png))#i', $_SERVER['REQUEST_URI'], $aMatches)) {
-        throw new \ResizableImageException('URL does not match pattern');
+        throw new ResizableImageException('URL does not match pattern');
     }
 
     if (! isset($config['images'][$aMatches[1]])) {
-        $oImagick = new \Imagick($config['static_path'][$config['env']] . 'images/dyn/default.jpg');
-        throw new \ResizableImageException('Unknown image type');
+        throw new ResizableImageException('Unknown image type');
     }
 
     $sImageType       = $aMatches[1];
@@ -41,10 +39,15 @@ try {
     $sTypeRootPath    = $config['static_path'][$config['env']] . 'images/dyn/' . $sImageType;
     $sSourceImagePath = $sTypeRootPath . '/source/' . $sFileName;
 
+    if (false === in_array($iImageHeight, $config['images'][$sImageType]['authorized'])) {
+        throw new ResizableImageException('Unauthorized image size');
+    }
+
     if (false === file_exists($sSourceImagePath)) {
         $oImagick = new \Imagick($config['static_path'][$config['env']] . 'images/dyn/default.jpg');
         $oImagick->scaleImage($iImageWidth, $iImageHeight);
-        throw new \ResizableImageException('Unable to find source image');
+
+        throw new ResizableImageException('Unable to find source image');
     }
 
     if (false === is_dir($sTypeRootPath . '/' . $iImageHeight)) {
@@ -54,13 +57,15 @@ try {
     $oImagick = new \Imagick($sSourceImagePath);
     $oImagick->scaleImage($iImageWidth, $iImageHeight);
     $oImagick->setImageFormat('jpg');
-    $oImagick->setImageCompression(Imagick::COMPRESSION_JPEG);
+    $oImagick->setImageCompression(\Imagick::COMPRESSION_JPEG);
     $oImagick->setImageCompressionQuality(90);
     $oImagick->stripImage();
     $oImagick->writeImage($sTypeRootPath . '/' . $iImageHeight . '/' . $sFileName);
 } catch (\Exception $oException) {
     if (! isset($oImagick)) {
-        $oImagick = new \Imagick($config['static_path'][$config['env']] . 'images/dyn/default.jpg');
+        header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+        header('Status: 404 Not Found');
+        exit;
     }
 }
 
