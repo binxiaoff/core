b88560e9d02e95b3b314e9d4422dc172b029404a
Antoine Préveaux <antoine.preveaux@unilend.fr>
2015-10-09 15:45:17 +0200

    Altares WS management class

---
+++ librairies/Altares.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Unilend\librairies;
+
+class Altares
+{
+    /**
+     * @var string
+     */
+    private $sIdentification;
+
+    /**
+     * @var \bdd
+     */
+    private $oDatabase;
+
+    /**
+     * @var \settings
+     */
+    private $oSettings;
+
+    /**
+     * @param \bdd $oDatabase
+     */
+    public function __construct(\bdd $oDatabase)
+    {
+        ini_set('default_socket_timeout', 60);
+
+        require_once __DIR__ . '/../data/settings.data.php';
+
+        $this->oDatabase = $oDatabase;
+        $this->oSettings = new \settings($oDatabase);
+
+        $this->oSettings->get('Altares login', 'type');
+
+        $this->sIdentification = $this->oSettings->value;
+
+        $this->oSettings->get('Altares mot de passe', 'type');
+        $this->sIdentification .= '|' . $this->oSettings->value;
+    }
+
+    /**
+     * Retrieve getEligibility WS data
+     * @param int $iSIREN
+     * @return mixed
+     */
+    public function getEligibility($iSIREN)
+    {
+        $this->oSettings->get('Altares WSDL Eligibility', 'type');
+
+        return $this->soapCall($this->oSettings->value, 'getEligibility', array('siren' => $iSIREN));
+    }
+
+    /**
+     * Retrieve getDerniersBilans WS data
+     * @param int $iSIREN
+     * @param int $iSheetsCount
+     * @return mixed
+     */
+    public function getBalanceSheets($iSIREN, $iSheetsCount = 3)
+    {
+        $this->oSettings->get('Altares WSDL CallistoIdentite', 'type');
+
+        return $this->soapCall($this->oSettings->value, 'getDerniersBilans', array('siren' => $iSIREN, 'nbBilans' => $iSheetsCount));
+    }
+
+    /**
+     * Make SOAP call to Altares WS
+     * @param string $sWSDLUrl
+     * @param string $sWSName
+     * @param array $aParameters
+     * @return mixed
+     */
+    private function soapCall($sWSDLUrl, $sWSName, array $aParameters = array())
+    {
+        $oClient = new \SoapClient($sWSDLUrl, array('trace' => 1, 'exception' => true));
+        return $oClient->__soapCall(
+            $sWSName,
+            array(array('identification' => $this->sIdentification, 'refClient' => 'sffpme') + $aParameters)
+        );
+    }
+}
