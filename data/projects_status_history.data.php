8fc5a00880fbf6500c650dc1d8cb00768f75147d
Antoine Préveaux <antoine.preveaux@unilend.fr>
2015-10-22 14:26:31 +0200

    Add reminder index and reminder email main content when adding project status history

--- data/projects_status_history.data.php
+++ data/projects_status_history.data.php
@@ -4,116 +4,107 @@
 // **************************************************************************************************** //
 //
 // Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-// associated documentation files (the "Software"), to deal in the Software without restriction, 
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 // subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies 
+// The above copyright notice and this permission notice shall be included in all copies
 // or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but 
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. 
-// In no event shall the authors or copyright holders equinoa be liable for any claim, 
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from, 
+// The Software is provided "as is", without warranty of any kind, express or implied, including but
+// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
+// In no event shall the authors or copyright holders equinoa be liable for any claim,
+// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
 // out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising 
-// or otherwise to promote the sale, use or other dealings in this Software without 
+// Except as contained in this notice, the name of equinoa shall not be used in advertising
+// or otherwise to promote the sale, use or other dealings in this Software without
 // prior written authorization from equinoa.
 //
 //  Version : 2.4.0
 //  Date : 21/03/2011
 //  Coupable : CM
-//                                                                                   
+//
 // **************************************************************************************************** //
 
 class projects_status_history extends projects_status_history_crud
 {
+    public function __construct($bdd, $params = '')
+    {
+        parent::projects_status_history($bdd, $params);
+    }
 
-	function projects_status_history($bdd,$params='')
+    public function get($id, $field = 'id_project_status_history')
     {
-        parent::projects_status_history($bdd,$params);
+        return parent::get($id, $field);
     }
-    
-    function get($id,$field='id_project_status_history')
-    {
-        return parent::get($id,$field);
-    }
-    
-    function update($cs='')
+
+    public function update($cs = '')
     {
         parent::update($cs);
     }
-    
-    function delete($id,$field='id_project_status_history')
+
+    public function delete($id, $field = 'id_project_status_history')
     {
-    	parent::delete($id,$field);
+        parent::delete($id, $field);
     }
-    
-    function create($cs='')
+
+    public function select($where = '', $order = '', $start = '', $nb = '')
     {
-        $id = parent::create($cs);
-        return $id;
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+        if ($order != '') {
+            $order = ' ORDER BY ' . $order;
+        }
+        $sql = 'SELECT * FROM `projects_status_history`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+
+        $resultat = $this->bdd->query($sql);
+        $result   = array();
+        while ($record = $this->bdd->fetch_array($resultat)) {
+            $result[] = $record;
+        }
+        return $result;
     }
-	
-	function select($where='',$order='',$start='',$nb='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
-		if($order != '')
-			$order = ' ORDER BY '.$order;
-		$sql = 'SELECT * FROM `projects_status_history`'.$where.$order.($nb!='' && $start !=''?' LIMIT '.$start.','.$nb:($nb!=''?' LIMIT '.$nb:''));
 
-		$resultat = $this->bdd->query($sql);
-		$result = array();
-		while($record = $this->bdd->fetch_array($resultat))
-		{
-			$result[] = $record;
-		}
-		return $result;
-	} 
-	
-	function counter($where='')
-	{
-		if($where != '')
-			$where = ' WHERE '.$where;
-			
-		$sql='SELECT count(*) FROM `projects_status_history` '.$where;
+    public function counter($where = '')
+    {
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
 
-		$result = $this->bdd->query($sql);
-		return (int)($this->bdd->result($result,0,0));
-	}
-	
-	function exist($id,$field='id_project_status_history')
-	{
-		$sql = 'SELECT * FROM `projects_status_history` WHERE '.$field.'="'.$id.'"';
-		$result = $this->bdd->query($sql);
-		return ($this->bdd->fetch_array($result,0,0)>0);
-	} 
-	
-	function addStatus($id_user,$status,$id_project)
-	{
-		$sql='SELECT id_project_status FROM `projects_status` WHERE status = '.$status.' ';
+        $sql = 'SELECT count(*) FROM `projects_status_history` ' . $where;
 
-		$result = $this->bdd->query($sql);
-		$id_project_status = (int)($this->bdd->result($result,0,0));
-		
-		
-		$this->id_project = $id_project;
-		$this->id_project_status = $id_project_status;
-		$this->id_user = $id_user;
-		
-		$this->create();
-	}
+        $result = $this->bdd->query($sql);
+        return (int) ($this->bdd->result($result, 0, 0));
+    }
 
-	public function getBeforeLastStatut($iProjectId)
-	{
-		$result = $this->select('id_project='.$iProjectId, 'added DESC', 1, 1);
+    public function exist($id, $field = 'id_project_status_history')
+    {
+        $sql    = 'SELECT * FROM `projects_status_history` WHERE ' . $field . '="' . $id . '"';
+        $result = $this->bdd->query($sql);
+        return ($this->bdd->fetch_array($result, 0, 0) > 0);
+    }
 
-		if(isset($result[0]) && false === empty($result[0])) {
+    public function addStatus($id_user, $status, $id_project, $numero_relance = 0, $content = '')
+    {
+        $result = $this->bdd->query('SELECT id_project_status FROM projects_status WHERE status = ' . $status);
 
-			return $result[0]['id_project_status'];
-		}
+        $this->id_project        = $id_project;
+        $this->id_project_status = (int) $this->bdd->result($result, 0, 0);
+        $this->id_user           = $id_user;
+        $this->numero_relance    = $numero_relance;
+        $this->content           = $content;
+        $this->create();
+    }
 
-		return false;
-	}
+    public function getBeforeLastStatut($iProjectId)
+    {
+        $result = $this->select('id_project=' . $iProjectId, 'added DESC', 1, 1);
+
+        if (isset($result[0]) && false === empty($result[0])) {
+            return $result[0]['id_project_status'];
+        }
+
+        return false;
+    }
 }
