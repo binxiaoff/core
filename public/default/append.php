7f333d2af93c9a283ee79333a488c0dcc0c6e4f8
Sébastien Laizeau <sebastienlaizeau@Mac-mini-de-Sebastien.local>
2015-10-22 13:11:59 +0200

    Deleting var_dump

--- public/default/append.php
+++ public/default/append.php
@@ -5,7 +5,6 @@ if(!$nocache)
     $cont = ob_get_contents();
     ob_end_clean();
     // Cache pour 5 minutes, sans compression gzip
-    var_dump('ici');
     $oCache->set($cacheKey,$cont,false,300);
     echo $cont;
     echo '<!-- Unilend cache / write to key : "'.$cacheKey.'" -->';
