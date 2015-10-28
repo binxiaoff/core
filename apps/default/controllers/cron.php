9fbbbc70aec0d2d75a6aea2de95661fbd2a08a54
Antoine Préveaux <antoine.preveaux@unilend.fr>
2015-10-28 16:58:11 +0100

    [BT19328] Remove cron that check crons

--- apps/default/controllers/cron.php
+++ apps/default/controllers/cron.php
@@ -5401,69 +5401,6 @@ class cronController extends bootstrap
         }
     }
 
-    // cron journalier (00h00  0 0 * * *) new => 0 * * * * (toutes les heures)
-    public function _checkControles()
-    {
-        if (true === $this->startCron('checkControles', 5)) {
-
-            $settings = $this->loadData('settings');
-
-            $settings->get('Controle cron checkBids', 'type');
-            $ctrlCheckBids   = $settings->value;
-            $updateCheckBids = $settings->updated;
-
-            $settings->get('Controle cron checkEmailBidKO', 'type');
-            $ctrlCheckEmailBidKO   = $settings->value;
-            $updateCheckEmailBidKO = $settings->updated;
-
-            $settings->get('Controle cron check_projet_en_funding', 'type');
-            $ctrlCheck_projet_en_funding   = $settings->value;
-            $updateCheck_projet_en_funding = $settings->updated;
-
-            $settings->get('Controle statut remboursement', 'type');
-            $ctrlRemb = $settings->value;
-
-            $settings->get('Controle remboursements', 'type');
-            $ctrlRembPreteurs = $settings->value;
-
-            if ($ctrlCheckBids == 0 || $ctrlCheckEmailBidKO == 0 || $ctrlCheck_projet_en_funding == 0 || $ctrlRemb == 0) {
-                $todayMoins1h = mktime(date("H") - 1, date("i"), date("s"), date("m"), date("d"), date("Y"));
-                // Si la valeur est a zero et que la derniere mise a jour date de plus d'une heure
-                if ($ctrlCheckBids == 0 && strtotime($updateCheckBids) < $todayMoins1h) {
-                    $this->oLogger->addRecord(ULogger::ERROR,
-                        'Cron : ' . __METHOD__ . ' Controle cron checkBids value 0 in settings at date : ' . date('Y-m-d H:i:s'),
-                        array($this->Config['env']));
-                }
-                // Si la valeur est a zero et que la derniere mise a jour date de plus d'une heure
-                if ($ctrlCheckEmailBidKO == 0 && strtotime($updateCheckEmailBidKO) < $todayMoins1h) {
-                    $this->oLogger->addRecord(ULogger::ERROR,
-                        'Cron : ' . __METHOD__ . ' Controle cron checkEmailBidKO value 0 in settings at date : ' . date('Y-m-d H:i:s'),
-                        array($this->Config['env']));
-                }
-                // si la valeur est a zero
-                if ($ctrlCheck_projet_en_funding == 0 && strtotime($updateCheck_projet_en_funding) < $todayMoins1h) {
-                    $this->oLogger->addRecord(ULogger::ERROR,
-                        'Cron : ' . __METHOD__ . ' Controle cron check_projet_en_fundings value 0 in settings at date : ' . date('Y-m-d H:i:s'),
-                        array($this->Config['env']));
-                }
-                if ($ctrlRemb == 0) {
-                    $this->oLogger->addRecord(ULogger::ERROR,
-                        'Cron : ' . __METHOD__ . ' Controle cron remboursement value 0 in settings at date : ' . date('Y-m-d H:i:s'),
-                        array($this->Config['env']));
-                }
-                if ($ctrlRembPreteurs == 0) {
-                    $this->oLogger->addRecord(ULogger::ERROR,
-                        'Cron : ' . __METHOD__ . ' Controle cron statut remboursements des prêteurs value 0 in settings at date : ' . date('Y-m-d H:i:s'),
-                        array($this->Config['env']));
-                }
-            } else {
-                echo 'OK';
-            }
-            $this->stopCron();
-        }
-        die;
-    }
-
     // relance une completude a j+8 (add le 22/07/2014)
     // Passe tous les jours (tous les matin à 6h du matin) 0  6  *  *  *
     public function _relance_completude()
