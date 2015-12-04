42c75a35b2b6c52df62853875a7223771cc8d932
annabreyer <anna.breyer@unilend.fr>
2015-12-04 11:42:48 +0100

    initial commit

---
+++ apps/default/views/ajax/operations_emprunteur.php
@@ -0,0 +1,52 @@
+<?php
+
+ob_start();
+
+$bOdd = true;
+
+foreach ($this->aBorrowerOperations as $aOperation) {
+?>
+    <tr class="<?= $bOdd ? 'odd' : 'even' ?>">
+        <td><?= $this->lng['espace-emprunteur'][ 'operations-type-' . $aOperation['type'] ] ?></td>
+        <td><?= $aOperation['id_project'] ?></td>
+        <td><?= $this->dates->formatDateMysqltoShortFR($aOperation['date']) ?></td>
+        <td><?= $this->ficelle->formatnumber($aOperation['montant']) ?> &euro;</td>
+        <?php if ($aOperation['type'] === 'affectation-preteurs'): ?>
+            <td>
+                <a href="<?= $this->lurl ?>/espace_emprunteur/getCSVWithLenderDetails/e/<?= $aOperation['id_project'] ?>">
+                    <img class="xls small" src="<?= $this->surl ?>/images/default/xls_hd.png"/>
+                </a>
+            </td>
+        <?php elseif ($aOperation['type'] === 'financement'): ?>
+            <td>
+                <a href="<?= $this->lurl ?>/espace_emprunteur/getCSVWithLenderDetails/l/<?= $aOperation['id_project'] ?>">
+                     <img class="xls small" src="<?= $this->surl ?>/images/default/xls_hd.png"/>
+                </a>
+            </td>
+        <?php else: ?>
+            <td></td>
+        <?php endif; ?>
+    </tr>
+    <?php if ($aOperation['type'] === 'commission-mensuelle'): ?>
+        <tr class="<?= $bOdd ? 'odd' : 'even' ?>">
+            <td style="text-align: right"><?= $this->lng['espace-emprunteur']['operations-type-commission-ht'] ?></td>
+            <td></td>
+            <td></td>
+            <td><?= $this->ficelle->formatnumber($aOperation['commission']) ?> &euro;</td>
+            <td></td>
+        </tr>
+        <tr class="<?= $bOdd ? 'odd' : 'even' ?>">
+            <td style="text-align: right"><?= $this->lng['espace-emprunteur']['operations-type-tva'] ?></td>
+            <td></td>
+            <td></td>
+            <td><?= $this->ficelle->formatnumber($aOperation['tva']) ?> &euro;</td>
+            <td></td>
+        </tr>
+    <?php endif; ?>
+    <?php
+    $bOdd = $bOdd ? false : true;
+}
+
+$sHtmlBody = ob_get_contents();
+ob_clean();
+echo json_encode(array('html' => $sHtmlBody, 'debut' => $this->sDisplayDateTimeStart, 'fin' => $this->sDisplayDateTimeEnd));
