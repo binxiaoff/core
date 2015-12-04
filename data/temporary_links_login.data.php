9ea539493ff052123f13cef3323783c396ebf48d
annabreyer <anna.breyer@unilend.fr>
2015-12-04 14:46:11 +0100

    initial commit

---
+++ data/temporary_links_login.data.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class temporary_links_login extends temporary_links_login_crud
+{
+
+    public function __construct($bdd, $params = '')
+    {
+        parent::temporary_links_login($bdd, $params);
+    }
+
+
+    public function select($where = '', $order = '', $start = '', $nb = '')
+    {
+        if ($where != '') {
+            $where = ' WHERE ' . $where;
+        }
+        if ($order != '') {
+            $order = ' ORDER BY ' . $order;
+        }
+        $sql = 'SELECT * FROM `temporary_links_login`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+
+        $resultat = $this->bdd->query($sql);
+        $result   = array();
+        while ($record = $this->bdd->fetch_array($resultat)) {
+            $result[] = $record;
+        }
+
+        return $result;
+    }
+
+
+}
