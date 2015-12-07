27c7b1164cde5ffbccdcc6b4e8e67bd6edc83249
annabreyer <anna.breyer@unilend.fr>
2015-12-07 14:21:09 +0100

    first commit

---
+++ apps/default/views/espace_emprunteur/faq.php
@@ -0,0 +1,30 @@
+<!--#include virtual="ssi-header.shtml"  -->
+<div class="main">
+    <div class="shell">
+        <h1><?=$this->tree->title?></h1>
+        <?=$this->content['description-page']?>
+
+        <h3><?=$this->content['titre-rubrique-1']?></h3>
+        <?=$this->content['contenu-rubrique-1']?>
+
+        <h3><?=$this->content['titre-rubrique-2']?></h3>
+        <?=$this->content['contenu-rubrique-2']?>
+
+        <h3><?=$this->content['titre-rubrique-3']?></h3>
+        <?=$this->content['contenu-rubrique-3']?>
+
+        <h3><?=$this->content['titre-rubrique-4']?></h3>
+        <?=$this->content['contenu-rubrique-4']?>
+
+        <h3><?=$this->content['titre-rubrique-5']?></h3>
+        <?=$this->content['contenu-rubrique-5']?>
+
+        <h3><?=$this->content['titre-rubrique-6']?></h3>
+        <?=$this->content['contenu-rubrique-6']?>
+
+        <h3><?=$this->content['titre-rubrique-7']?></h3>
+        <?=$this->content['contenu-rubrique-7']?>
+
+    </div><!-- /.shell -->
+</div><!-- /.main -->
+<!--#include virtual="ssi-footer.shtml"  -->
