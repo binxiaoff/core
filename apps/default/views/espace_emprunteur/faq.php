f824ce25f2a12e0ed814d746a123cd3c5f3ea487
annabreyer <anna.breyer@unilend.fr>
2015-12-18 15:02:05 +0100

    DEV -140 add anchors for titles

--- apps/default/views/espace_emprunteur/faq.php
+++ apps/default/views/espace_emprunteur/faq.php
@@ -2,29 +2,29 @@
 <div class="main">
     <div class="shell">
         <h1><?=$this->tree->title?></h1>
+        <a name="top">
         <?=$this->content['description-page']?>
 
-        <h3><?=$this->content['titre-rubrique-1']?></h3>
-        <?=$this->content['contenu-rubrique-1']?>
+        <h3><a name="titre-rubrique-1"><?= $this->content['titre-rubrique-1'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-1'] ?>
 
-        <h3><?=$this->content['titre-rubrique-2']?></h3>
-        <?=$this->content['contenu-rubrique-2']?>
+        <h3><a name="titre-rubrique-2"><?= $this->content['titre-rubrique-2'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-2'] ?>
 
-        <h3><?=$this->content['titre-rubrique-3']?></h3>
-        <?=$this->content['contenu-rubrique-3']?>
+        <h3><a name="titre-rubrique-3"><?= $this->content['titre-rubrique-3'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-3'] ?>
 
-        <h3><?=$this->content['titre-rubrique-4']?></h3>
-        <?=$this->content['contenu-rubrique-4']?>
+        <h3><a name="titre-rubrique-4"><?= $this->content['titre-rubrique-4'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-4'] ?>
 
-        <h3><?=$this->content['titre-rubrique-5']?></h3>
-        <?=$this->content['contenu-rubrique-5']?>
+        <h3><a name="titre-rubrique-5"><?= $this->content['titre-rubrique-5'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-5'] ?>
 
-        <h3><?=$this->content['titre-rubrique-6']?></h3>
-        <?=$this->content['contenu-rubrique-6']?>
+        <h3><a name="titre-rubrique-6"><?= $this->content['titre-rubrique-6'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-6'] ?>
 
-        <h3><?=$this->content['titre-rubrique-7']?></h3>
-        <?=$this->content['contenu-rubrique-7']?>
-
+        <h3><a name="titre-rubrique-7"><?= $this->content['titre-rubrique-7'] ?></a></h3>
+        <?= $this->content['contenu-rubrique-7'] ?>
     </div><!-- /.shell -->
 </div><!-- /.main -->
 <!--#include virtual="ssi-footer.shtml"  -->
