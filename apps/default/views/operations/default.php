d59ea05226e34512e173f300476d3164e3b5cd32
clementdeniau <clement.deniau@unilend.fr>
2016-01-06 11:23:45 +0100

    TMA-84 Removed hash without reloading page on 'vos operations' tab

--- apps/default/views/operations/default.php
+++ apps/default/views/operations/default.php
@@ -25,6 +25,7 @@
 <script type="text/javascript">
     $('#operations').click(function () {
         location.hash = '';
+        history.pushState('', '', location.pathname);
         $("#operations").scrollTop();
     });
     $('#prets').click(function () {
