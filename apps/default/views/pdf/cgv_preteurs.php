708d35a809c7756fc87a92899c2bcee37d84416d
clementdeniau <clement.deniau@unilend.fr>
2016-01-14 12:07:34 +0100

    TMA-125 Removing landing pages tags

--- apps/default/views/pdf/cgv_preteurs.php
+++ apps/default/views/pdf/cgv_preteurs.php
@@ -5,6 +5,16 @@
     <link rel="stylesheet" href="<?=$this->surl?>/styles/default/pdf/style.css" type="text/css" media="all" />
 </head>
 <body>
+<script>var dataLayer = [<?= json_encode($this->aDataLayer) ?>];</script>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2WQKW"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W2WQKW');</script>
+<!-- End Google Tag Manager -->
 <div class="cgu-wrapper">
     <div class="main" style="padding-bottom: 0px;">
         <div class="shell" style="width:750px;">
