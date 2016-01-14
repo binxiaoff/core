94523e4e031324ddd10438f9e92ea17c088913e1
clementdeniau <clement.deniau@unilend.fr>
2016-01-14 12:11:49 +0100

    TMA-125 Change DEV to PROD GTM environnement

--- apps/default/views/root/figaro.php
+++ apps/default/views/root/figaro.php
@@ -108,13 +108,13 @@
 <body>
 <script>var dataLayer = [<?= json_encode($this->aDataLayer) ?>];</script>
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2WQKW"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MB66VL"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W2WQKW');</script>
+    })(window,document,'script','dataLayer','GTM-MB66VL');</script>
 <!-- End Google Tag Manager -->
 	<script type="text/javascript">
 	window.ffw_param = {
