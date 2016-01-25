d857c42a05cc0c82c1f78ed554dbe663d6594a3d
annabreyer <anna.breyer@unilend.fr>
2016-01-25 09:24:31 +0100

    Change log for IRR calculation

--- data/lenders_account_stats.data.php
+++ data/lenders_account_stats.data.php
@@ -175,12 +175,16 @@ class lenders_account_stats extends lenders_account_stats_crud
      */
     public function calculateIRR($iLendersAccountId)
     {
-        try {
-            $aValuesIRR = $this->getValuesForIRR($iLendersAccountId);
-        } catch (Exception $e){
-            $oLoggerIRR = new ULogger('Calculate IRR', $this->logPath, 'IRR.log');
-            $oLoggerIRR->addRecord(ULogger::WARNING, 'Caught Exception: '.$e->getMessage(). ' '. $e->getTraceAsString());
-        }
+        /* @var array $config */
+        include __DIR__ . '/../config.php';
+
+        $oLoggerIRR = new ULogger('Calculate IRR', $config['log_path'][ENVIRONMENT], 'IRR.log');
+        $fGuess     = 0.1;
+        $oLoggerIRR->addRecord(ULogger::INFO, 'Guess : ' . $fGuess . ' MAX_INTERATIONS : '. 100);
+
+        $fStartSQL  = microtime(true);
+        $aValuesIRR = $this->getValuesForIRR($iLendersAccountId);
+        $oLoggerIRR->addRecord(ULogger::INFO, 'Lender ' . $iLendersAccountId . ' - SQL Time : ' . (round(microtime(true) - $fStartSQL, 2)) . ' for ' . count($aValuesIRR). ' lines ');
 
         foreach ($aValuesIRR as $aValues) {
             foreach ($aValues as $date => $value) {
@@ -189,12 +193,16 @@ class lenders_account_stats extends lenders_account_stats_crud
             }
         }
 
+        $fStartXIRR = microtime(true);
         $oFinancial = new \PHPExcel_Calculation_Financial();
-        $fXIRR      = round($oFinancial->XIRR($aSums, $aDates) * 100, 2);
+        $fXIRR      = round($oFinancial->XIRR($aSums, $aDates, $fGuess) * 100, 2);
+        $oLoggerIRR->addRecord(ULogger::INFO, 'Lender ' . $iLendersAccountId . ' - XIRR Time : ' . (round(microtime(true) - $fStartXIRR, 2)));
 
         if (abs($fXIRR) > 100) {
-            throw new Exception('IRR not in range for id_lender '.$iLendersAccountId. ' IRR : '. $fXIRR);
+            throw new Exception('IRR not in range for id_lender ' . $iLendersAccountId . ' IRR : ' . $fXIRR);
         }
+        $oLoggerIRR->addRecord(ULogger::INFO, 'Lender ' . $iLendersAccountId . ' - Total time : ' . (round(microtime(true) - $fStartSQL, 2)));
+
         return $fXIRR;
     }
 
