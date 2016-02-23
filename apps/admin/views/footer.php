380e7d61db8370120c4b758103356bd497fce064
clementdeniau <clement.deniau@unilend.fr>
2016-02-23 16:13:12 +0100

    TMA-230 Indent html

--- apps/admin/views/footer.php
+++ apps/admin/views/footer.php
@@ -1,9 +1,7 @@
-<div id="footer">
-    <?= $this->cms ?> 1.1 -
-    <a href="http://www.equinoa.com" title="Agence Web Equinoa">Equinoa</a> &copy;<?= date('Y') ?> -
-    <a href="<?= $this->urlfront ?>" title="Retourner sur le site" target="_blank">Retourner sur le site</a>
-</div>
-</div>
+        <div id="footer">
+            <?= $this->cms ?> 1.1 - <a href="http://www.equinoa.com" title="Agence Web Equinoa">Equinoa</a> &copy;<?= date('Y') ?> - <a href="<?= $this->urlfront ?>" title="Retourner sur le site" target="_blank">Retourner sur le site</a>
+        </div>
+    </div>
 </body>
 <?php if ($this->Config['env'] != 'prod'): ?>
 <script type="text/javascript">
