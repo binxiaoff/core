55a8e672249b38f710ac438ce02f63409193a2e1
clementdeniau <clement.deniau@unilend.fr>
2016-02-23 09:49:40 +0100

    TECH-54 jquery ui includes (see TMA-227)

--- apps/admin/views/dossiers/default.php
+++ apps/admin/views/dossiers/default.php
@@ -1,6 +1,5 @@
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="<?= $this->surl ?>/scripts/admin/jquery-ui.css">
+<script src="<?= $this->surl ?>/scripts/admin/jquery-ui.js"></script>
 <script>
     var nbPages = <?= ceil($this->iCountProjects / $this->nb_lignes) ?>;
     $(function () {
