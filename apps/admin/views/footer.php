4f8f4d7eaf380283cc299eb18c6f070fbb818169
clementdeniau <clement.deniau@unilend.fr>
2016-02-23 16:01:44 +0100

    TMA-230 Set Jira issue collector with pre filled email

--- apps/admin/views/footer.php
+++ apps/admin/views/footer.php
@@ -1,6 +1,18 @@
-        <div id="footer">
-            <?=$this->cms?> 1.1 - <a href="http://www.equinoa.com" title="Agence Web Equinoa">Equinoa</a> &copy;<?=date('Y')?> - <a href="<?=$this->urlfront?>" title="Retourner sur le site" target="_blank">Retourner sur le site</a>
-        </div>  
-    </div>     
+<div id="footer">
+    <?= $this->cms ?> 1.1 -
+    <a href="http://www.equinoa.com" title="Agence Web Equinoa">Equinoa</a> &copy;<?= date('Y') ?> -
+    <a href="<?= $this->urlfront ?>" title="Retourner sur le site" target="_blank">Retourner sur le site</a>
+</div>
+</div>
 </body>
+<?php if ($this->Config['env'] != 'prod'): ?>
+<script type="text/javascript">
+    window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
+        // ==== default field values ====
+        fieldValues: {
+            email: '<?php echo $_SESSION['user']['email'] ?>'
+        }
+    });
+</script>
+<?php endif; ?>
 </html>
