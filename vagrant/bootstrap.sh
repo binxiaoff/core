da4fb896d7aa6e7773e8bc213ed274a3d126b935
Bin XIAO <bin.xiao@unilend.fr>
2016-03-30 14:39:33 +0200

    DEV-317 Create new vagrant VM compliant to new infra

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,13 +4,12 @@ locale-gen fr_FR.UTF-8
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 
 # install percona (MySQL)
-apt-key adv --keyserver 213.133.103.71 --recv-keys 1C4CBDCDCD2EFD2A #keys.gnupg.net
-add-apt-repository "deb http://repo.percona.com/apt precise main"
-add-apt-repository "deb-src http://repo.percona.com/apt precise main"
-apt-get update
-debconf-set-selections <<< 'percona-server-server-5.5 percona-server-server/root_password password ROOTPASSWORD'
-debconf-set-selections <<< 'percona-server-server-5.5 percona-server-server/root_password_again password ROOTPASSWORD'
-apt-get install -y percona-server-server-5.5
+wget https://repo.percona.com/apt/percona-release_0.1-3.$(lsb_release -sc)_all.deb
+dpkg -i percona-release_0.1-3.$(lsb_release -sc)_all.deb
+apt-get update > /dev/null
+debconf-set-selections <<< 'percona-server-server-5.6 percona-server-server/root_password password ROOTPASSWORD'
+debconf-set-selections <<< 'percona-server-server-5.6 percona-server-server/root_password_again password ROOTPASSWORD'
+apt-get install -y percona-server-server-5.6
 
 # install lftp for download database
 apt-get install -y lftp
@@ -32,16 +31,17 @@ then
     cat /vagrant/fixture/*.sql | mysql -uroot -pROOTPASSWORD unilend
 fi
 rm -rf /vagrant/database
+rm -rf /vagrant/fixture
 
-# install phpmyadmin 4.4.15 (last version compatible php 5.3)
+# install phpmyadmin
 mkdir /vagrant/phpmyadmin/
 #wget -O /vagrant/phpmyadmin/index.html http://www.phpmyadmin.net/
 #awk 'BEGIN{ RS="<a *href *= *\""} NR>2 {sub(/".*/,"");print; }' /vagrant/phpmyadmin/index.html >> /vagrant/phpmyadmin/url-list.txt
 #grep "https://files.phpmyadmin.net/phpMyAdmin/" /vagrant/phpmyadmin/url-list.txt > /vagrant/phpmyadmin/phpmyadmin.url
 #sed -i 's/.zip/.tar.bz2/' /vagrant/phpmyadmin/phpmyadmin.url
-wget --no-check-certificate --output-document=/vagrant/phpmyadmin/phpMyAdmin.tar.bz2 https://files.phpmyadmin.net/phpMyAdmin/4.4.15/phpMyAdmin-4.4.15-all-languages.tar.bz2
+wget --no-check-certificate --output-document=/vagrant/phpmyadmin/phpMyAdmin.tar.bz2 https://files.phpmyadmin.net/phpMyAdmin/4.6.0/phpMyAdmin-4.6.0-all-languages.tar.bz2 > /dev/null
 mkdir /srv/sites/phpmyadmin
-tar jxvf /vagrant/phpmyadmin/phpMyAdmin.tar.bz2 -C /srv/sites/phpmyadmin --strip 1
+tar jxvf /vagrant/phpmyadmin/phpMyAdmin.tar.bz2 -C /srv/sites/phpmyadmin --strip 1 > /dev/null
 rm -rf /vagrant/phpmyadmin
 
 # configure phpmyadmin
@@ -57,33 +57,23 @@ mysql -uroot -pROOTPASSWORD -e "CREATE USER 'external'@'%' IDENTIFIED BY 'EXTERN
 mysql -uroot -pROOTPASSWORD -e "GRANT ALL ON unilend.* TO 'external'@'%'"
 mysql -uroot -pROOTPASSWORD -e "flush privileges"
 
-# install apache2
-apt-get install -y apache2
-a2enmod deflate
-a2enmod filter
-a2enmod ssl
-a2enmod rewrite
-ln -fs /vagrant/conf/vhosts/admin.unilend.fr.conf /etc/apache2/sites-enabled/admin.unilend.fr.conf
-ln -fs /vagrant/conf/vhosts/www.unilend.fr.conf /etc/apache2/sites-enabled/www.unilend.fr.conf
-ln -fs /vagrant/conf/vhosts/phpmyadmin.conf /etc/apache2/sites-enabled/phpmyadmin.conf
-sed -i '/Listen 443/c Listen 443\n    NameVirtualHost *:443' /etc/apache2/ports.conf
-echo "ServerName localhost" >> /etc/apache2/httpd.conf
-service apache2 restart
-
 # install php
-apt-get install -y php5 libapache2-mod-php5 php5-mcrypt php5-mysql php5-cli php5-gd php5-curl php5-memcache php5-intl php5-geoip memcached php5-xdebug php5-imagick
+add-apt-repository -y ppa:ondrej/php5-5.6
+apt-get update > /dev/null
+apt-get install -y php5-fpm php5-mcrypt php5-mysql php5-cli php5-gd php5-curl php5-memcache php5-intl php5-geoip memcached php5-xdebug php5-imagick
 
 # modify php.ini
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/;session.save_path = .*/session.save_path = \/tmp/" /etc/php5/apache2/php.ini
-sed -i "s/session.gc_maxlifetime = .*/session.gc_maxlifetime = 3600/" /etc/php5/apache2/php.ini
-sed -i '/;date.timezone =/c date.timezone = "Europe/Paris"' /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sed -i "s/html_errors = .*/html_errors = On/" /etc/php5/apache2/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 64M/" /etc/php5/apache2/php.ini
-sed -i "s/post_max_size = .*/post_max_size = 64M/" /etc/php5/apache2/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+sed -i "s/;session.save_path = .*/session.save_path = \/tmp/" /etc/php5/fpm/php.ini
+sed -i "s/session.gc_maxlifetime = .*/session.gc_maxlifetime = 3600/" /etc/php5/fpm/php.ini
+sed -i '/;date.timezone =/c date.timezone = "Europe/Paris"' /etc/php5/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
+sed -i "s/html_errors = .*/html_errors = On/" /etc/php5/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 64M/" /etc/php5/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 64M/" /etc/php5/fpm/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/fpm/php.ini
 printf '
-[xdebug]zend_extension=/usr/lib/php5/20090626/xdebug.so
+[xdebug]zend_extension=/usr/lib/php5/20131226/xdebug.so
 xdebug.remote_enable=1
 xdebug.remote_handler=dbgp
 xdebug.remote_mode=req
@@ -95,8 +85,13 @@ xdebug.profiler_output_name=callgrind.%%t.%%R.cachegrind
 xdebug.var_display_max_data=262144
 xdebug.var_display_max_depth=10
 xdebug.var_display_max_children=1024' >> /etc/php5/apache2/php.ini
+restart php5-fpm
 
-service apache2 restart
+# install Nginx
+apt-get install -y nginx
+ln -fs /vagrant/conf/vhosts/admin.unilend.fr.nginx.conf /etc/nginx/sites-enabled/admin.unilend.fr.conf
+ln -fs /vagrant/conf/vhosts/www.unilend.fr.nginx.conf /etc/nginx/sites-enabled/www.unilend.fr.conf
+ln -fs /vagrant/conf/vhosts/phpmyadmin.nginx.conf /etc/nginx/sites-enabled/phpmyadmin.conf
 
 # copy unversioned files
 lftp -e 'set ssl:verify-certificate no; mirror /TechTeam/vagrant/files_outside_git  /srv/sites/unilend; bye' -u vagrantftp,X9d\@\$nsa -p 21 synology.corp.unilend.fr
@@ -105,10 +100,13 @@ chmod -R u+w /srv/sites/unilend
 # install composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/
 
-# install java, maven et dataloader
+# install git
+apt-get install -y git
+
+# install java, maven and dataloader
 add-apt-repository -y ppa:openjdk-r/ppa
-apt-get update
-apt-get install -y openjdk-8-jdk maven git
+apt-get update > /dev/null
+apt-get install -y openjdk-8-jdk maven
 
 yes | git clone https://www.github.com/forcedotcom/dataloader.git /srv/dataloader
 cd /srv/dataloader
@@ -119,7 +117,7 @@ mvn clean package -DskipTests
 
 #instal wkhtmltopdf for snappy
 apt-get install xfonts-75dpi
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb > /dev/null
 dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 ln -s /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 rm -rf wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
@@ -134,11 +132,12 @@ printf "\nalias composer=\"/usr/bin/composer.phar\"" >> /home/vagrant/.zshrc
 chsh -s /bin/zsh vagrant
 
 # install mailcatcher
-apt-get install -y software-properties-common
-apt-get remove -y ruby1.8
-apt-get install -y ruby1.9.3 libsqlite3-dev
+apt-get install -y build-essential
+apt-add-repository -y ppa:brightbox/ruby-ng
+apt-get update > /dev/null
+apt-get install -y ruby-all-dev ruby-switch libsqlite3-dev
+ruby-switch --set ruby2.0
 gem install mailcatcher
-sed -i '/;sendmail_path =/c sendmail_path = /usr/bin/env catchmail' /etc/php5/apache2/php.ini
-a2enmod proxy proxy_http
-ln -fs /vagrant/conf/vhosts/mailcatcher.conf /etc/apache2/sites-enabled/mailcatcher.conf
+sed -i '/;sendmail_path =/c sendmail_path = /usr/bin/env catchmail' /etc/php5/fpm/php.ini
+ln -fs /vagrant/conf/vhosts/mailcatcher.nginx.conf /etc/nginx/sites-enabled/mailcatcher.conf
 cp /vagrant/conf/mailcatcher.conf /etc/init/mailcatcher.conf
