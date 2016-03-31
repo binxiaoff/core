81adb858263a7286f1285082938cfd4982f2657e
Clément Déniau <clement.deniau@unilend.fr>
2016-03-31 11:30:31 +0200

    TECH-54 Remove useless includes to set datepicker language = fr

--- apps/admin/views/dossiers/default.php
+++ apps/admin/views/dossiers/default.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="<?= $this->surl ?>/scripts/admin/external/jquery/plugin/jquery-ui/jquery-ui.theme.min.css">
-<script src="<?= $this->surl ?>/scripts/admin/external/jquery/plugin/jquery-ui/jquery-ui.min.js"></script>
 <script>
     var nbPages = <?= ceil($this->iCountProjects / $this->nb_lignes) ?>;
     $(function () {
