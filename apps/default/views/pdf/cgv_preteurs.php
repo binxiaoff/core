4261af03e3196b5ce19334e1bf7affdd211e0a39
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-04-15 11:47:45 +0200

    TMA-525 Autolend Terms of Service

--- apps/default/views/pdf/cgv_preteurs.php
+++ apps/default/views/pdf/cgv_preteurs.php
@@ -8,10 +8,11 @@
 <div class="cgu-wrapper">
     <div class="main" style="padding-bottom: 0px;">
         <div class="shell" style="width:750px;">
-                <div class="logo"></div>
-                <?= $this->content['contenu-cgu']?>
-                <div style="page-break-after: always;"></div>
-                <?= utf8_decode($this->content['mandatRecouvrement']) ?>
+            <div class="logo"></div>
+            <?= $this->content['contenu-cgu']?>
+            <div style="page-break-after: always;"></div>
+            <?= utf8_decode($this->content['mandatRecouvrement']) ?>
+            <?= utf8_decode($this->content['mandatRecouvrementAvecPret']) ?>
         </div>
     </div>
 </div>
