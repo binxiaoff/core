9b2ae04afa257b47242a878b3e49d7e8351dcdd2
Bin XIAO <bin.xiao@unilend.fr>
2016-05-02 14:52:01 +0200

    Replace the MailjetTransportEventDispatcher by default SwiftMail default event dispatcher Reorganise the config files

--- Library/Bridge/SwiftMailer/MailjetTransportEventDispatcher.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Unilend\Bridge\SwiftMailer;
-
-class MailjetTransportEventDispatcher extends \Swift_Events_SimpleEventDispatcher
-{
-    //Parent service is a abstract service (not abstract class), so we need extended it in order to create a service.
-    //DO NOT delete it.
-}
