86f9ba33cb51f240817bdb09900211749a1ae02f
Clément Déniau <clement.deniau@unilend.fr>
2016-05-23 14:29:15 +0200

    TMA-643 Fix post issue on project rejection

--- apps/admin/views/dossiers/ajax_rejection.php
+++ apps/admin/views/dossiers/ajax_rejection.php
@@ -11,12 +11,22 @@
         <div class="right">
             <button type="button" onclick="parent.$.fn.colorbox.close();" class="btn btnDisabled">Annuler</button>
             <?php if (0 == $this->iStep) : ?>
-                <input type="submit" class="btn_link" value="Valider"/>
-            <?php elseif (1 == $this->iStep) : ?>
-                <button onclick="check_status_dossier(<?= \projects_status::REJETE ?>, <?= $this->iProjectId ?>);" class="btn">Rejeter</button>
+                <input type="submit" class="btn_link" value="Valider">
             <?php else : ?>
-                <button onclick="valid_rejete_etape<?= $this->iStep ?>(2, <?= $this->iProjectId ?>);" class="btn">Rejeter</button>
+                <button type="submit" class="btn">Rejeter</button>
             <?php endif; ?>
         </div>
     </form>
 </div>
+
+<script>
+    $('#rejection_reason_form').submit(function(e) {
+        e.preventDefault();
+
+        <?php if (1 == $this->iStep) : ?>
+            check_status_dossier(<?= \projects_status::REJETE ?>, <?= $this->iProjectId ?>);
+        <?php elseif (0 < $this->iStep) : ?>
+            valid_rejete_etape<?= $this->iStep ?>(2, <?= $this->iProjectId ?>);
+        <?php endif; ?>
+    });
+</script>
