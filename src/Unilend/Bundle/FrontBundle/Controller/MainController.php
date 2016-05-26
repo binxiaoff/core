83264993ae373eb4b98f244eda4bde6968b4b749
Anna Breyer <anna.breyer@unilend.fr>
2016-05-26 12:10:50 +0200

    Add testimonials loaded from db

--- src/Unilend/Bundle/FrontBundle/Controller/MainController.php
+++ src/Unilend/Bundle/FrontBundle/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace Unilend\Bundle\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Unilend\Bundle\FrontBundle\Service\TestimonialManager;
 
 class MainController extends Controller
 {
@@ -16,51 +17,21 @@ class MainController extends Controller
     public function homeAction()
     {
         //TODO replace mock data par stats from StatsService
-        //TODO add Battenberg people
-        //TODO add VideoHeroes
+
+        /** @var TestimonialManager $testimonialService */
+        $testimonialService = $this->get('unilend.service.testimonial');
+        $aBattenbergPeople = $testimonialService->getActiveBattenbergTestimonials();
+        $aVideoHeroesLenders = $testimonialService->getActiveVideoHeroes('preter');
+        $aVideoHeroesBorrowers = $testimonialService->getActiveVideoHeroes('emprunter');
 
         return $this->render('UnilendFrontBundle:pages:homepage_acquisition.html.twig', array(
             'stats' => array(
-                'numberProjects' => 233,
-                'numberLenders' => 22548,
+                'numberProjects'           => 233,
+                'numberLenders'            => 22548,
                 'amountBorrowedInMillions' => 16
             ),
-            'testimonialPeople' => array(
-                array(
-                    'type'           => 'emprunter',
-                    'image'          => 'frontbundle/media/promos/homeacq-promo-emprunter-600x400.jpg',
-                    'name'           => 'Jean-Marc',
-                    'location'       => 'Marseille',
-                    'quote'          => '"J&quot;ai pu emprunter 15&nbsp;000€ en quelques semaines et relancer la croissance de mon entreprise."',
-                    'urlTestimonial' => ('emprunter/testimonials'),
-                    'urlProject'     => ('project/12345'),
-                    'labelCta'       => 'Présenter un projet',
-                    'urlCta'         => ('projects/new')
-                ),
-                array(
-                    'type'           => 'preter',
-                    'image'          => 'frontbundle/media/promos/homeacq-promo-preter-600x400.jpg',
-                    'name'           => 'Jane',
-                    'location'       => 'Avignon',
-                    'quote'          => '"En 2 ans, j&#39;ai multiplié par 3 mon portefeuille de prêt. Et mes intérêts sont à +8%."',
-                    'urlTestimonial' => 'preter/testimonials',
-                    'urlProject'     => false,
-                    'labelCta'       => 'Devenir prêteur',
-                    'urlCta'         => 'preter'
-                )
-            ),
-            'videoHeroes' => array(
-                'Lenders' => array(
-                    array('name' => 'Julie', 'age' => '36 ans', 'info' => '540€ prêtes, 5% de bénefices', 'urlTestimonial' => ''),
-                    array('name' => 'Celeste', 'age' => '28 ans', 'info' => '100 prêtes, 5% de bénefices', 'urlTestimonial' => ''),
-                    array('name' => 'Julien', 'age' => '40 ans', 'info' => '300 prêtes, 6% de bénefices', 'urlTestimonial' => '')
-                ),
-                'Borrowers' => array(
-                    array('name' => 'Didier', 'age' => '43 ans', 'info' => '10&nbsp;000€ empruntés, une entreprise relancée', 'urlTestimonial' => ''),
-                    array('name' => 'Patrick', 'age' => '38 ans', 'info' => '20&nbsp;000€ empruntés, une entreprise relancée', 'urlTestimonial' => ''),
-                    array('name' => 'Jean-Marc', 'age' => '55 ans', 'info' => '20&nbsp;000€ empruntés, une entreprise relancée', 'urlTestimonial' => '')
-                )
-            )
+            'testimonialPeople' => $aBattenbergPeople,
+            'videoHeroes' => array('Lenders' => $aVideoHeroesLenders, 'Borrowers' => $aVideoHeroesBorrowers)
         ));
     }
 
--- src/Unilend/Bundle/FrontBundle/Service/TestimonialManager.php
+++ src/Unilend/Bundle/FrontBundle/Service/TestimonialManager.php
@@ -2,6 +2,7 @@
 
 namespace Unilend\Bundle\FrontBundle\Service;
 
+use Unilend\Bundle\TranslationBundle\Service\TranslationManager;
 use Unilend\Service\Simulator\EntityManager;
 
 class TestimonialManager
@@ -9,14 +10,68 @@ class TestimonialManager
     /** @var  EntityManager */
     private $entityManager;
 
-    public function __construct(EntityManager $entityManager)
+    /** @var TranslationManager  */
+    private $translationManager;
+
+    public function __construct(EntityManager $entityManager, TranslationManager $translationManager)
     {
         $this->entityManager = $entityManager;
+        $this->translationManager = $translationManager;
     }
 
-    public function getActiveTestimonials()
+    /**
+     * @return array
+     */
+    public function getActiveBattenbergTestimonials()
     {
+        /** @var \testimonial $testimonial */
+        $testimonial             = $this->entityManager->getRepository('testimonial');
+        $aActiveBattenbergPeople = $testimonial->getActiveBattenbergTestimonials();
+        $aActiveBattenbergPeople = $this->addCtaElements($aActiveBattenbergPeople);
 
+        return  $aActiveBattenbergPeople;
+    }
+
+    private function addCtaElements(array $aActiveBattenbergPeople)
+    {
+        foreach ($aActiveBattenbergPeople as $iKey => $aBattenbergPerson) {
+            if ('emprunter' === $aBattenbergPerson['type']){
+                $aActiveBattenbergPeople[$iKey]['labelCta'] = $this->translationManager->selectTranslation('site-general', 'testimonial-section-link-on-borrower');
+                $aActiveBattenbergPeople[$iKey]['urlCta']   = 'projects/new'; //TODO add real URL once routes are finished
+            }
+
+            if ('preter' === $aBattenbergPerson['type']){
+                $aActiveBattenbergPeople[$iKey]['labelCta'] = $this->translationManager->selectTranslation('site-general', 'testimonial-section-link-on-lender');
+                $aActiveBattenbergPeople[$iKey]['urlCta']   = 'preter'; //TODO add real URL once routes are finishedd
+            }
+        }
+
+        return $aActiveBattenbergPeople;
+    }
+
+    /**
+     * @param string $sTypeClient
+     * @return array
+     */
+    public function getActiveVideoHeroes($sTypeClient)
+    {
+        /** @var \testimonial $testimonial */
+        $testimonial  = $this->entityManager->getRepository('testimonial');
+        $aVideoHeroes = $testimonial->getVideoHeroes($sTypeClient);
+
+        return $aVideoHeroes;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllActiveTestimonials()
+    {
+        /** @var \testimonial $testimonial */
+        $testimonial = $this->entityManager->getRepository('testimonial');
+        $aActiveTestimonials = $testimonial->getActiveTestimonials();
+
+        return  $aActiveTestimonials;
     }
 
 }
