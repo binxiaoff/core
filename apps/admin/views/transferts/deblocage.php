67dca610cc5822afb62eccff973247501d8163c5
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-05-31 15:45:50 +0200

    TMA-524 Fund credit refinements
    - check status from DB data instead of using data coming from the interface
    - do not show unsigned mandates and proxy
    - show error message when funds could not be credited
    - use class constants

--- apps/admin/views/transferts/deblocage.php
+++ apps/admin/views/transferts/deblocage.php
@@ -37,52 +37,50 @@
         </thead>
         <tbody>
         <?php foreach ($this->aProjects as $aProject) : ?>
-                <tr>
-                    <td><?= $aProject['id_project'] ?></td>
-                    <td><?= $aProject['title'] ?></td>
-                    <td><?= $this->ficelle->formatNumber($aProject['amount']) . '&nbsp€' ?></td>
-                    <td><?= isset($aProject['bic']) ? $aProject['bic'] : '' ?></td>
-                    <td><?= isset($aProject['iban']) ? $aProject['iban'] : '' ?></td>
-                    <td>
-                        <?php if (false === empty($aProject['rib'])) : ?>
-                            <a href="<?= $this->url ?>/attachment/download/id/<?= $aProject['id_rib'] ?>/file/<?= urlencode($aProject['rib']) ?>">
-                                <img src="<?= $this->surl ?>/images/admin/modif.png" alt="RIB"/>
-                            </a>
+            <tr>
+                <td><?= $aProject['id_project'] ?></td>
+                <td><?= $aProject['title'] ?></td>
+                <td><?= $this->ficelle->formatNumber($aProject['amount']) . '&nbsp€' ?></td>
+                <td><?= isset($aProject['bic']) ? $aProject['bic'] : '' ?></td>
+                <td><?= isset($aProject['iban']) ? $aProject['iban'] : '' ?></td>
+                <td>
+                    <?php if (false === empty($aProject['rib'])) : ?>
+                        <a href="<?= $this->url ?>/attachment/download/id/<?= $aProject['id_rib'] ?>/file/<?= urlencode($aProject['rib']) ?>">
+                            <img src="<?= $this->surl ?>/images/admin/modif.png" alt="RIB"/>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if (false === empty($aProject['kbis'])) : ?>
+                        <a href="<?= $this->url ?>/attachment/download/id/<?= $aProject['id_kbis'] ?>/file/<?= urlencode($aProject['kbis']) ?>">
+                            <img src="<?= $this->surl ?>/images/admin/modif.png" alt="KBIS"/>
+                        </a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if (false === empty($aProject['url_pdf'])) : ?>
+                        <a href="<?= $this->lurl ?>/protected/pouvoir_project/<?= $aProject['url_pdf'] ?>"><img src="<?= $this->surl ?>/images/admin/modif.png" alt="POUVOIR"/></a>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if (false === empty($aProject['mandat'])) : ?>
+                        <a href="<?= $this->lurl ?>/protected/mandat_preteur/<?= $aProject['mandat'] ?>"><img src="<?= $this->surl ?>/images/admin/modif.png" alt="MANDAT"/></a></td>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <form method="post" name="deblocage" onsubmit="return confirm('Voulez-vous vraiment débloquer les fonds pour le projet <?= $aProject['id_project'] ?> ?');">
+                        <?php if (
+                            isset($aProject['status_remb'], $aProject['status_mandat'], $aProject['authority_status'])
+                            && $aProject['status_remb'] == \projects_pouvoir::STATUS_PENDING_VALIDATION
+                            && $aProject['status_mandat'] == \clients_mandats::STATUS_SIGNED
+                            && $aProject['authority_status'] == \projects_pouvoir::STATUS_SIGNED
+                        ) : ?>
+                            <input type="submit" name="validateProxy" class="btn" value="Débloquer les fonds" />
+                            <input type="hidden" name="id_project" value="<?= $aProject['id_project'] ?>"/>
                         <?php endif; ?>
-                    </td>
-                    <td>
-                        <?php if (false === empty($aProject['kbis'])) : ?>
-                            <a href="<?= $this->url ?>/attachment/download/id/<?= $aProject['id_kbis'] ?>/file/<?= urlencode($aProject['kbis']) ?>">
-                                <img src="<?= $this->surl ?>/images/admin/modif.png" alt="KBIS"/>
-                            </a>
-                        <?php endif; ?>
-                    </td>
-                    <td>
-                        <?php if (false === empty($aProject['url_pdf'])) : ?>
-                            <a href="<?= $this->lurl ?>/protected/pouvoir_project/<?= $aProject['url_pdf'] ?>"><img src="<?= $this->surl ?>/images/admin/modif.png" alt="POUVOIR"/></a>
-                        <?php endif; ?>
-                    </td>
-                    <td>
-                        <?php if (false === empty($aProject['mandat'])) : ?>
-                            <a href="<?= $this->lurl ?>/protected/mandat_preteur/<?= $aProject['mandat'] ?>"><img src="<?= $this->surl ?>/images/admin/modif.png" alt="MANDAT"/></a></td>
-                        <?php endif; ?>
-                    </td>
-                    <td>
-                        <form method="post" id="statut_pouvoir" name="deblocage" onsubmit="return confirm('Voulez-vous vraiment débloquer les fonds pour le projet <?= $aProject['id_project'] ?> ?');">
-                            <?php if (
-                                isset($aProject['status_remb'])
-                                && $aProject['status_remb'] == \projects_pouvoir::STATUS_PENDING_VALIDATION
-                                && $aProject['status_mandat'] == \clients_mandats::STATUS_SIGNED
-                                && $aProject['authority_status'] == \projects_pouvoir::STATUS_SIGNED
-                            ) : ?>
-                                <input type="hidden" name="status_remb" value="<?= $aProject['status_remb'] ?>"/>
-                                <input type="hidden" name="statut_pouvoir" class="btn" value="1" />
-                                <input type="submit" class="btn" value="Débloquer les fonds" />
-                                <input type="hidden" name="id_project" value="<?= $aProject['id_project'] ?>"/>
-                            <?php endif; ?>
-                        </form>
-                    </td>
-                </tr>
+                    </form>
+                </td>
+            </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
