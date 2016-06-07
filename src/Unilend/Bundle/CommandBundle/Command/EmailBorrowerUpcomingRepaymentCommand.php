1f6f3c9f570afa892b9abef5fa2c4efc1242fc17
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-06-07 12:35:30 +0200

    DEV-624 Migrate emprunteur_impaye_avant_echeance cron

--- src/Unilend/Bundle/CommandBundle/Command/EmailBorrowerUpcomingRepaymentCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/EmailBorrowerUpcomingRepaymentCommand.php
@@ -18,7 +18,7 @@ class EmailBorrowerUpcomingRepaymentCommand extends ContainerAwareCommand
     {
         $this
             ->setName('email:borrower:upcoming_repayment')
-            ->setDescription('Greet someone');
+            ->setDescription('Send emails to borrower and lenders when an early repayment is done');
     }
 
     /**
