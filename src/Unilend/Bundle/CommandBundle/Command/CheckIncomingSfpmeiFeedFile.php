c3faa0ce7beea67980e4889e1a641db35a582ced
Bin XIAO <bin.xiao@unilend.fr>
2016-06-08 17:08:21 +0200

    Fix the bugs caused by miss-typing for the commands

--- src/Unilend/Bundle/CommandBundle/Command/CheckIncomingSfpmeiFeedFile.php
+++ src/Unilend/Bundle/CommandBundle/Command/CheckIncomingSfpmeiFeedFile.php
@@ -14,10 +14,8 @@ class CheckIncomingSfpmeiFeedFile extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('check-incoming-sfpmei-file')
+            ->setName('check:incoming_sfpmei_file')
             ->setDescription('Check if incoming feeds file exists, otherwise send an alert email')
-            ->addOption('message-limit', 0, InputOption::VALUE_OPTIONAL, 'The maximum number of messages to send.')
-            ->addOption('time-limit', 0, InputOption::VALUE_OPTIONAL, 'The time limit for sending messages (in seconds).')
             ->setHelp(<<<EOF
 The <info>check-incoming-sfpmei-file</info> command sends an email if the incoming feeds file does not exists. Usually the file must be available at 10:00 AM.
 In order to be useful, this command could run once a day (from monday to friday) at 10:20 AM
