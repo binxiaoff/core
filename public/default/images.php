5b67a54a5811fcc2a174ee5846059fda24aec037
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-06-20 14:07:05 +0200

    DEV-650 Change MonoLog configuration

--- public/default/images.php
+++ public/default/images.php
@@ -15,7 +15,8 @@ require_once __DIR__ . '/../../app/AppKernel.php';
 include_once __DIR__ . '/../../config.php';
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('display_errors', 1);
+
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
 $kernel = new AppKernel('prod', false);
