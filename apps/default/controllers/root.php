69710dabcc0f6603e9facfeb6c2ec988e480645c
Anna Breyer <anna.breyer@unilend.fr>
2016-06-20 16:19:44 +0200

    [DEV-711][DEV-716] Fix use of pdfController Object

--- apps/default/controllers/root.php
+++ apps/default/controllers/root.php
@@ -1061,7 +1061,9 @@ class rootController extends bootstrap
         if ($this->clients->checkAccess() || isset($this->params[0]) && $this->clients->get($this->params[0], 'hash')) {
             $this->clients->checkAccessLender();
 
-            $listeAccept = array_shift($this->acceptations_legal_docs->select('id_client = ' . $this->clients->id_client, 'added DESC', 0, 1));
+            $listeAccept = $this->acceptations_legal_docs->select('id_client = ' . $this->clients->id_client, 'added DESC', 0, 1);
+            $listeAccept = array_shift($listeAccept);
+
             $id_tree_cgu = $listeAccept['id_legal_doc'];
 
             $contenu = $this->tree_elements->select('id_tree = "' . $id_tree_cgu . '" AND id_langue = "' . $this->language . '"');
@@ -1077,8 +1079,10 @@ class rootController extends bootstrap
                 header("Content-Type: application/force-download");
                 @readfile($this->surl . '/var/fichiers/' . $this->content['pdf-cgu']);
             } else {
-                $oCommandPdf    = new Command('pdf', 'cgv_preteurs', array($this->clients->hash), $this->language);
-                $oPdf           = new pdfController($oCommandPdf, $this->Config, 'default');
+                $oCommandPdf    = new \Command('pdf', 'cgv_preteurs', array($this->clients->hash), $this->language);
+                $oPdf           = new \pdfController($oCommandPdf, $this->Config, 'default');
+                $oPdf->setContainer($this->container);
+                $oPdf->initialize();
                 $path           = $this->path . 'protected/pdf/cgv_preteurs/' . $this->clients->id_client . '/';
                 $sNamePdf       = 'cgv_preteurs-' . $this->clients->hash . '-' . $id_tree_cgu;
                 $sNamePdfClient = 'CGV-UNILEND-PRETEUR-' . $this->clients->id_client . '-' . $id_tree_cgu;
@@ -1122,7 +1126,9 @@ class rootController extends bootstrap
             if (isset($this->params[0]) && $this->params[0] == 'nosign') {
                 $dateAccept = '';
             } else {
-                $listeAccept = array_shift($this->acceptations_legal_docs->select('id_client = ' . $this->clients->id_client, 'added DESC', 0, 1));
+                $listeAccept = $this->acceptations_legal_docs->select('id_client = ' . $this->clients->id_client, 'added DESC', 0, 1);
+                $listeAccept = array_shift($listeAccept);
+
                 $dateAccept  = 'Sign&eacute; &eacute;lectroniquement le ' . date('d/m/Y', strtotime($listeAccept['added']));
             }
 
