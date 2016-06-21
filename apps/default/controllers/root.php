0f13a1f1323e10d042102ca074ac08875606638b
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-06-21 10:22:59 +0200

    Fix a bug with dynamic stat queries using old mysql_real_escape_string

--- apps/default/controllers/root.php
+++ apps/default/controllers/root.php
@@ -611,7 +611,7 @@ class rootController extends bootstrap
 
         $this->users = $this->loadData('users');
 
-        if ($this->params[0] != '' && $this->params[1] != '') {
+        if (false === empty($this->params[0]) && false === empty($this->params[1])) {
             $this->users->handleLoginFront($this->params[0], $this->params[1]);
         } else {
             $this->users->handleLogoutFront();
