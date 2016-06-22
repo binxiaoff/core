332f409c0f083750574c8980f05881284fc60dd7
Bin XIAO <bin.xiao@unilend.fr>
2016-06-22 12:22:40 +0200

    DEV-734 Serveral issues on PGRFileManager  - Change cache dir  - Fix UT8 issue on json_encode  - Passe URL to https

--- public/admin/ckeditor/plugins/pgrfilemanager/PGRThumb/php/UrlThumb.php
+++ public/admin/ckeditor/plugins/pgrfilemanager/PGRThumb/php/UrlThumb.php
@@ -29,134 +29,136 @@ class PGRThumb_UrlThumb
 {
     /**
      * Full path to file
-     * 
+     *
      * @var string
      */
     static private $_file = null;
     /**
      * Width
-     * 
+     *
      * @var int
      */
     static private $_width = null;
     /**
      * Height
-     * 
+     *
      * @var int
      */
     static private $_height = null;
     /**
      * Fixed aspect ratio
-     * 
+     *
      * @var bool
      */
     static private $_aspectRatio = true;
-    
+
     static private $_watermarkText = null;
-    
+
     static private function _getParamsFromUrl()
     {
         if (isset($_GET['src'])) {
             $relPath = strip_tags(htmlspecialchars($_GET['src']));
-            self::$_file = realpath($relPath); 
+            self::$_file = realpath($relPath);
         }
         if (isset($_GET['w']) && is_numeric($_GET['w'])) {
-            self::$_width = strip_tags(htmlspecialchars($_GET['w'])); 
+            self::$_width = strip_tags(htmlspecialchars($_GET['w']));
         }
         if (isset($_GET['h']) && is_numeric($_GET['h'])) {
-            self::$_height = strip_tags(htmlspecialchars($_GET['h'])); 
+            self::$_height = strip_tags(htmlspecialchars($_GET['h']));
         }
         if (isset($_GET['iar'])) {
-            self::$_aspectRatio =  !(strtolower($_GET['iar']) == 'true'); 
+            self::$_aspectRatio =  !(strtolower($_GET['iar']) == 'true');
         }
         if (isset($_GET['iar'])) {
-            self::$_aspectRatio =  !(strtolower($_GET['iar']) == 'true'); 
+            self::$_aspectRatio =  !(strtolower($_GET['iar']) == 'true');
         }
         if (isset($_GET['wt'])) {
-            self::$_watermarkText =  strip_tags(htmlspecialchars($_GET['wt'])); 
+            self::$_watermarkText =  strip_tags(htmlspecialchars($_GET['wt']));
         }
     }
-    
+
     static public function error($msg)
     {
         header("HTTP/1.0 404 Not Found");
             echo '<h1>Not Found</h1>';
             echo '<p>The image you requested could not be found.</p>';
             echo "<p>An error was triggered: <b>$msg</b></p>";
-        exit();        
+        exit();
     }
-    
+
     /**
      * Generate thumb
-     * 
+     *
      * @return false|image
      */
     static public function generateThumb()
     {
-        //check pass        
-        if (!isset($_GET['hash']) || 
+        //check pass
+        if (!isset($_GET['hash']) ||
             ($_GET['hash'] != md5(str_replace('&hash='.$_GET['hash'], '', $_SERVER['QUERY_STRING']) . PGRThumb_Config::$pass))) {
-            self::error("can't do this");            
+            self::error("can't do this");
         }
-        
+
         self::_getParamsFromUrl();
-        
+
         //check if file is set
         if (!self::$_file) return false;
-        
+
         //generate cached filename
-        $cachedFile = realpath(dirname(__FILE__) . '/../cache') . PGRThumb_Cache::generateFilename(self::$_file, $_SERVER['QUERY_STRING']);
-        
+
+        $currentDir = realpath(dirname(__FILE__));
+        $rootDir = substr($currentDir, 0, strpos($currentDir, 'public/admin/ckeditor'));
+        $cachedFile = $rootDir . 'var/cache/pgrfilemanager' . PGRThumb_Cache::generateFilename(self::$_file, $_SERVER['QUERY_STRING']);
         //check if cached file exist
         if (file_exists($cachedFile)) {
             PGRThumb_Cache::outputCache($cachedFile);
         }
-        
+
         //file is not cached, so I will generate thumb
-        
+
         //check if file exist
         if (!file_exists(self::$_file)) {
             self::error("can't find file");
         }
-        
-        //Get image library 
+
+        //Get image library
         include_once 'Image.php';
         $image = PGRThumb_Image::factory(self::$_file);
-        
+
         if (!$image) {
             self::error("image library doesn't exist");
         }
-        
+
         //watermark
         if (self::$_watermarkText) {
-            $image->resize(355, 0, true);                
+            $image->resize(355, 0, true);
             $image->watermark(
                 self::$_watermarkText,
-                realpath(dirname(__FILE__) . '/ttf/Parkvane.ttf'), 
+                realpath(dirname(__FILE__) . '/ttf/Parkvane.ttf'),
                 28,
                 array(210,210,210),
                 80,
                 'CB'
             );
         }
-        
+
         //resize image
         $res = null;
         if ((self::$_width > 0) && (self::$_height > 0)) {
             if (self::$_aspectRatio) {
                 $res = $image->maxSize(self::$_width, self::$_height);
             } else {
-                $res = $image->resize(self::$_width, self::$_height, false);                
+                $res = $image->resize(self::$_width, self::$_height, false);
             }
         }
-                
-        //save thumb 
+
+        //save thumb
         if ($res) $res = PGRThumb_Cache::saveImage($cachedFile, $image);
-        
+
         if ($res) PGRThumb_Cache::outputFile($cachedFile);
 
         if (!$res) {
             self::error("Can't perform thumb");
-        }        
+        }
     }
 }
