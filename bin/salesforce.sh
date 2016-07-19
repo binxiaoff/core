98c8bd2bdb14af64293a68f056093f99c45b4a7f
Bin XIAO <bin.xiao@unilend.fr>
2016-07-19 17:11:01 +0200

    Use Dataloader v37 Remove the double double-quote CSV files should use CR-LF (RFC 4180)

--- bin/salesforce.sh
+++ bin/salesforce.sh
@@ -7,7 +7,7 @@ php /data/sites/unilend/bin/console salesforce:extraction projects --env=prod
 php /data/sites/unilend/bin/console salesforce:extraction lenders --env=prod
 
 # send the extractions via Dataloader
-java -cp /data/dataloader/target/dataloader-26.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=preteurs
-java -cp /data/dataloader/target/dataloader-26.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=companies
-java -cp /data/dataloader/target/dataloader-26.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=emprunteurs
-java -cp /data/dataloader/target/dataloader-26.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=projects
+java -cp /data/dataloader/target/dataloader-37.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=preteurs
+java -cp /data/dataloader/target/dataloader-37.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=companies
+java -cp /data/dataloader/target/dataloader-37.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=emprunteurs
+java -cp /data/dataloader/target/dataloader-37.0.0-uber.jar -Dsalesforce.config.dir=/data/sites/unilend/app/dataloader/conf/ com.salesforce.dataloader.process.ProcessRunner process.name=projects
