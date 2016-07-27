167eb5e1e662d28cc8dadbffd860cd715a7ff40d
David Tessier <david.tessier@unilend.fr>
2016-07-27 14:48:30 +0200

    DEV-585 Carnet d'ordre - ajax on detail - style - structure

--- src/Unilend/Bundle/FrontBundle/Resources/src/js/app/components/bidsDetail.js
+++ src/Unilend/Bundle/FrontBundle/Resources/src/js/app/components/bidsDetail.js
@@ -3,13 +3,53 @@ var $ = require('jquery')
 
 
 
+var AjaxCall = function(elem, rate, project) {
+
+    $.ajax({
+        type: 'POST',
+        url: '/projects/bids/'+project+'/'+rate,
+        success: function(response) {
+            $(response).insertAfter(elem);
+        },
+        error: function() {
+            console.log("error retrieving datas");
+        }
+    });
+
+}
+
 $(document).on('click', '.bids-row', function() {
-    var el = $(this);
-    console.log(el.attr('data-sortable-rate'));
-    if(el.hasClass('active-row')) {
-        el.removeClass('active-row');
+
+    var ClickedElement = $(this);
+    var CurrentProject = $('.table-alloffersoverview').attr('data-current-project');
+    var ClickedRate = ClickedElement.attr('data-bid-rate');
+    var CurrentDetail = $('.detail-table-content');
+
+    if ( $('.detail-table-content').length && ! ClickedElement.hasClass('active-row') ) {
+        ClickedElement.addClass('active-row');
+        CurrentDetail.prev().removeClass('active-row');
+        CurrentDetail.remove();
+        AjaxCall(ClickedElement, ClickedRate, CurrentProject);
     }
+
+    else if ( ClickedElement.hasClass('active-row') ) {
+        ClickedElement.removeClass('active-row');
+        CurrentDetail.remove();
+    }
+
     else {
-        el.addClass('active-row');
+        $('.active-row').removeClass('active-row');
+        $('.detail-table-content').remove();
+        ClickedElement.addClass('active-row');
+        AjaxCall(ClickedElement, ClickedRate, CurrentProject);
     }
+
+
+
+});
+
+
+$(document).on('click', '.rejected-offers', function() {
+    $('.rejected-row').show();
+    $('.rejected-offers').remove();
 });
