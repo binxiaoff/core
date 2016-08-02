9e9df33a47f5f5e998d81004e6d2ddbab91d666b
Clément Déniau <clement.deniau@unilend.fr>
2016-08-02 17:03:46 +0200

    TMA-690 Use str_replace to remove forbidden characters

--- src/Unilend/Bundle/CommandBundle/Command/FeedsDirectDebitCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/FeedsDirectDebitCommand.php
@@ -103,8 +103,8 @@ class FeedsDirectDebitCommand extends ContainerAwareCommand
                 'mandateDate'      => date('Y-m-d', strtotime($mandate->updated)),
                 'debitBic'         => $borrowerDirectDebit['bic'],
                 'debitIban'        => $borrowerDirectDebit['iban'],
-                'lastname'         => preg_replace("^[\"'\\\\<>&]^", '', $client->nom),
-                'firstname'        => preg_replace("^[\"'\\\\<>&]^", '', $client->prenom),
+                'lastname'         => str_replace(array('"', '\'', '\\', '>', '<', '&'), '', $client->nom),
+                'firstname'        => str_replace(array('"', '\'', '\\', '>', '<', '&'), '', $client->prenom),
                 'reference'        => $borrowerDirectDebit['motif']
             ));
 
