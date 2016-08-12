cad7cf2ffbae7fc458b11452604a5c0c1114ddbe
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2016-08-12 09:22:54 +0200

    Change indentation from 2 white spaces to 4 white spaces

--- src/Unilend/Bundle/FrontBundle/Resources/src/js/app/components/DashboardPanel.js
+++ src/Unilend/Bundle/FrontBundle/Resources/src/js/app/components/DashboardPanel.js
@@ -11,36 +11,36 @@ var ElementAttrsObject = require('ElementAttrsObject')
 // @param {Mixed} elem Either a {String} selector, {HTMLElement} or a {jQueryObject} representing an element in the view
 // @param {Object} options
 var DashboardPanel = function (elem, options) {
-  var self = this
-  self.$elem = $(elem)
+    var self = this
+    self.$elem = $(elem)
 
-  // Error
-  if (self.$elem.length === 0 || elem.hasOwnProperty('DashboardPanel')) return false
+    // Error
+    if (self.$elem.length === 0 || elem.hasOwnProperty('DashboardPanel')) return false
 
-  // Needs an ID number
-  self.id = self.$elem.attr('id') || randomString()
-  if (!self.$elem.attr('id')) self.$elem.attr('id', self.id)
-  self.title = self.$elem.find('.dashboard-panel-title').text()
+    // Needs an ID number
+    self.id = self.$elem.attr('id') || randomString()
+    if (!self.$elem.attr('id')) self.$elem.attr('id', self.id)
+    self.title = self.$elem.find('.dashboard-panel-title').text()
 
-  // Settings
-  self.settings = $.extend({
-    draggable: true
-  }, ElementAttrsObject(elem, {
-    draggable: 'data-draggable'
-  }), options)
+    // Settings
+    self.settings = $.extend({
+        draggable: true
+    }, ElementAttrsObject(elem, {
+        draggable: 'data-draggable'
+    }), options)
 
-  // Assign the class to show the UI functionality has been applied
-  self.$elem.addClass('ui-dashboard-panel')
+    // Assign the class to show the UI functionality has been applied
+    self.$elem.addClass('ui-dashboard-panel')
 
-  // Trigger hide
-  if (self.$elem.is('.ui-dashboard-panel-hidden')) {
-    self.hide()
-  } else {
-    self.show()
-  }
+    // Trigger hide
+    if (self.$elem.is('.ui-dashboard-panel-hidden')) {
+        self.hide()
+    } else {
+        self.show()
+    }
 
-  self.$elem[0].DashboardPanel = self
-  return self
+    self.$elem[0].DashboardPanel = self
+    return self
 }
 
 /*
@@ -48,80 +48,80 @@ var DashboardPanel = function (elem, options) {
  */
 // Show the panel
 DashboardPanel.prototype.show = function () {
-  var self = this
-  self.$elem.removeClass('ui-dashboard-panel-hidden')
-  self.getToggles().removeClass('ui-dashboard-panel-hidden')
-  self.refreshLayout()
+    var self = this
+    self.$elem.removeClass('ui-dashboard-panel-hidden')
+    self.getToggles().removeClass('ui-dashboard-panel-hidden')
+    self.refreshLayout()
 }
 
 // Hide the panel
 DashboardPanel.prototype.hide = function () {
-  var self = this
-  self.$elem.addClass('ui-dashboard-panel-hidden')
-  self.getToggles().addClass('ui-dashboard-panel-hidden')
-  self.refreshLayout()
+    var self = this
+    self.$elem.addClass('ui-dashboard-panel-hidden')
+    self.getToggles().addClass('ui-dashboard-panel-hidden')
+    self.refreshLayout()
 }
 
 // Toggle the panel
 DashboardPanel.prototype.toggle = function () {
-  var self = this
-  if (self.$elem.is('.ui-dashboard-panel-hidden')) {
-    self.show()
-  } else {
-    self.hide()
-  }
+    var self = this
+    if (self.$elem.is('.ui-dashboard-panel-hidden')) {
+        self.show()
+    } else {
+        self.hide()
+    }
 }
 
 // Refresh any layout modules
 DashboardPanel.prototype.refreshLayout = function () {
-  var self = this
+    var self = this
 
-  // Refresh packery
-  // @note no more packery, doesn't suit the use-case
-  //       using jquery-ui draggable now
-  // if (self.$elem.parents('[data-packery]').length > 0) {
-  //   self.$elem.parents('[data-packery]').packery()
-  // }
+    // Refresh packery
+    // @note no more packery, doesn't suit the use-case
+    //       using jquery-ui draggable now
+    // if (self.$elem.parents('[data-packery]').length > 0) {
+    //   self.$elem.parents('[data-packery]').packery()
+    // }
 }
 
 // Get any item which toggles this dashboard panel
 DashboardPanel.prototype.getToggles = function () {
-  var self = this
-  return $('[href="#' + self.id + '"].dashboard-panel-toggle')
+    var self = this
+    return $('[href="#' + self.id + '"].dashboard-panel-toggle')
 }
 
 /*
  * jQuery Plugin
  */
 $.fn.uiDashboardPanel = function (op) {
-  return this.each(function (i, elem) {
-    if (!elem.hasOwnProperty('DashboardPanel')) {
-      new DashboardPanel(elem, op)
-    }
-  })
+    return this.each(function (i, elem) {
+        if (!elem.hasOwnProperty('DashboardPanel')) {
+            new DashboardPanel(elem, op)
+        }
+    })
 }
 
 /*
  * jQuery Events
  */
 $(document)
-  // Auto-init component behaviours on document ready, or when parent element (or self) is made visible with `UI:visible` custom event
-  .on('ready UI:visible', function (event) {
-    $(event.target).find('.dashboard-panel, [data-dashboardpanel]').not('.ui-dashboard-panel').uiDashboardPanel()
-  })
+// Auto-init component behaviours on document ready, or when parent element (or self) is made visible with `UI:visible` custom event
+    .on('ready UI:visible', function (event) {
+        $(event.target).find('.dashboard-panel, [data-dashboardpanel]').not('.ui-dashboard-panel').uiDashboardPanel()
+    })
 
-  // Toggle the panel via the toggle option
-  .on(Utility.clickEvent, '.dashboard-panel-toggle', function (event) {
-    event.preventDefault()
-    var $panel = $($(this).attr('href'))
-    if ($panel.length > 0) $panel[0].DashboardPanel.toggle()
-  })
+    // Toggle the panel via the toggle option
+    .on(Utility.clickEvent, '.dashboard-panel-toggle', function (event) {
+        event.preventDefault()
+        var $panel = $($(this).attr('href'))
+        if ($panel.length > 0) $panel[0].DashboardPanel.toggle()
+    })
 
-  // Hide the panel via the close button
-  .on(Utility.clickEvent, '.dashboard-panel .btn-close', function (event) {
-    event.preventDefault()
-    var $panel = $(this).parents('.dashboard-panel')
-    $panel[0].DashboardPanel.hide()
-  })
+    // Hide the panel via the close button
+    .on(Utility.clickEvent, '.dashboard-panel .btn-close', function (event) {
+        event.preventDefault()
+        var $panel = $(this).parents('.dashboard-panel')
+        $panel[0].DashboardPanel.hide()
+    })
 
 module.exports = DashboardPanel
