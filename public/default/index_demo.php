1fe3beec5f286ed6c7bf7b0ca2c358b9502d15e8
Bin XIAO <bin.xiao@unilend.fr>
2016-08-30 14:17:09 +0200

    DEV-919 Création environnement de démo

--- public/default/index_demo.php
+++ public/default/index_demo.php
@@ -8,8 +8,8 @@ include __DIR__ . '/../../core/command.class.php';
 include __DIR__ . '/../../config.php';
 require_once __DIR__.'/../../app/AppKernel.php';
 
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
-ini_set('display_errors', 0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 
 if (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
@@ -24,10 +24,7 @@ if (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVE
     );
 }
 
-session_start();
-ini_set('session.gc_maxlifetime', 3600); // 1h la session
-
-$kernel = new AppKernel('preprod', false);
+$kernel = new AppKernel('demo', false);
 $request  = Request::createFromGlobals();
 
 try {
