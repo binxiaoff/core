00345fe9ae45e147ed13fdd4ae93a1f127144789
Bin XIAO <bin.xiao@unilend.fr>
2016-09-01 12:44:17 +0200

    DEV-767 Acceptation CGV

--- src/Unilend/Bundle/CoreBusinessBundle/Service/ClientManager.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Service/ClientManager.php
@@ -1,22 +1,32 @@
 <?php
 namespace Unilend\Bundle\CoreBusinessBundle\Service;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Unilend\Bundle\CoreBusinessBundle\Service\Simulator\EntityManager;
+use Unilend\Bundle\FrontBundle\Security\User\UserLender;
 
 /**
  * Class ClientManager
  * @package Unilend\Bundle\CoreBusinessBundle\Service
  */
-
 class ClientManager
 {
+    const SESSION_KEY_TOS_ACCEPTED = 'user_legal_doc_accepted';
+
     /** @var ClientSettingsManager */
     private $oClientSettingsManager;
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+    /** @var RequestStack */
+    private $requestStack;
 
-    public function __construct(EntityManager $oEntityManager, ClientSettingsManager $oClientSettingsManager)
+    public function __construct(EntityManager $oEntityManager, ClientSettingsManager $oClientSettingsManager, TokenStorageInterface $tokenStorage, RequestStack $requestStack)
     {
         $this->oEntityManager         = $oEntityManager;
         $this->oClientSettingsManager = $oClientSettingsManager;
+        $this->tokenStorage           = $tokenStorage;
+        $this->requestStack           = $requestStack;
     }
 
 
@@ -41,6 +51,61 @@ class ClientManager
         /** @var \acceptations_legal_docs $oAcceptationLegalDocs */
         $oAcceptationLegalDocs = $this->oEntityManager->getRepository('acceptations_legal_docs');
         return $oAcceptationLegalDocs->exist($oClient->id_client, 'id_legal_doc = ' . $iLegalDocId . ' AND id_client ');
+    }
+
+    /**
+     * If the lender has accepted the last TOS, the seesion will not be set, and we check if there is a new TOS all the time
+     * Otherwise, the session will be set with accepted = false. We check no longer the now TOS, but we read the value from the session.
+     */
+    public function ifLastTOSAccepted()
+    {
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        if ($session->has(self::SESSION_KEY_TOS_ACCEPTED))
+        {
+            return; // already checked and not accepted
+        }
+
+        $token = $this->tokenStorage->getToken();
+        if ($token) {
+            $user = $token->getUser();
+            if ($user instanceof UserLender) {
+                /** @var \clients $client */
+                $client = $this->oEntityManager->getRepository('clients');
+                if ($client->get($user->getClientId())) {
+                    if (false === $this->isAcceptedCGV($client, $this->getLastTosId($client))) {
+                        $session->set(self::SESSION_KEY_TOS_ACCEPTED, false);
+                    }
+                }
+            }
+        }
+    }
+
+    public function getLastTosId(\clients $client)
+    {
+        /** @var \settings $settings */
+        $settings = $this->oEntityManager->getRepository('settings');
+        if (in_array($client->type, array(\clients::TYPE_LEGAL_ENTITY, \clients::TYPE_LEGAL_ENTITY_FOREIGNER))) {
+            $settingsType = 'Lien conditions generales inscription preteur societe';
+        } else {
+            $settingsType = 'Lien conditions generales inscription preteur particulier';
+        }
+        $settings->get($settingsType, 'type');
+
+        return $settings->value;
+    }
+
+    public function acceptLastTos(\clients $client)
+    {
+        if (false === empty($client->id_client)) {
+            /** @var \acceptations_legal_docs $tosAccepted */
+            $tosAccepted = $this->oEntityManager->getRepository('acceptations_legal_docs');
+            $tosAccepted->id_client = $client->id_client;
+            $tosAccepted->id_legal_doc = $this->getLastTosId($client);
+            $tosAccepted->create();
+
+            $session = $this->requestStack->getCurrentRequest()->getSession();
+            $session->remove(self::SESSION_KEY_TOS_ACCEPTED);
+        }
     }
 
     /**
