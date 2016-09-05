0397bf59c042d1e15805df91001da65fcfe88125
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-09-05 14:52:08 +0200

    Fix SourceMap generation. UTF-8 with BOM does not work correctly in Chrome

--- gulpfile.js/tasks/browserify.js
+++ gulpfile.js/tasks/browserify.js
@@ -34,7 +34,7 @@ var browserifyTask =  function(watchMode) {
         .pipe(buffer())
         .pipe(gulpIf(!global.production, sourcemaps.init({loadMaps: true})))
         // .pipe(gulpIf(!global.production, uglify()))
-        .pipe(gulpIf(!global.production, sourcemaps.write('./')))
+        .pipe(gulpIf(!global.production, sourcemaps.write('./', {charset: 'latin1'})))
         .pipe(gulp.dest(path.join(config.root.dest, bundleConfig.dest)))
         .on('end', function() {bundleLogger.end(bundleConfig.outputName)})
         .pipe(browserSync.reload({
