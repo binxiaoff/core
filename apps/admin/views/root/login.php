68c229436774ec8516dc676af994a5bc4e0da339
Clément Déniau <clement.deniau@unilend.fr>
2016-09-28 19:00:30 +0200

    TMA-979 Remove BO captcha

--- apps/admin/views/root/login.php
+++ apps/admin/views/root/login.php
@@ -47,10 +47,6 @@
                     <h2 style="margin-bottom:20px;" class="notice_error"><?= $this->error_login ?></h2>
                 <?php endif; ?>
 
-                <?php if (isset($this->displayCaptchaError) && $this->displayCaptchaError != "") : ?>
-                    <h2 style="margin-bottom:20px;" class="notice_error"><?= $this->displayCaptchaError ?></h2>
-                <?php endif; ?>
-
                 <?php if (isset($this->type_retour_new_pass) && $this->retour_new_pass != "") : ?>
                     <h2 style="margin-bottom:20px; color:<?= ($this->type_retour_new_pass == 0 ? 'red' : 'green') ?>;"><?= $this->retour_new_pass ?></h2>
                 <?php endif; ?>
@@ -66,19 +62,9 @@
                         </td>
                     </tr>
 
-                    <?php if (isset($_SESSION['login_user']) && $_SESSION['login_user']['nb_tentatives_precedentes'] > 5) : ?>
-                        <tr>
-                            <td>
-                                <img onclick="reloadCaptcha();" class="reloadCaptcha" src="<?= $this->surl ?>/images/default/Captcha-icon-reload.gif" alt="Reload captcha"/>
-                                <img id="phoca-captcha" src="<?= $this->url ?>/captcha/<?= time() ?>.jpg" alt="captcha"/>
-                            </td>
-                            <td><input type="text" name="captcha" id="captcha" class="input_login"/></td>
-                        </tr>
-                    <?php endif; ?>
-
                     <tr>
                         <td colspan="2" class="center">
-                            <?php if (isset($_SESSION['login_user']) && ($_SESSION['login_user']['nb_tentatives_precedentes'] > 1 && $_SESSION['login_user']['nb_tentatives_precedentes'] <= 5)): ?>
+                            <?php if (isset($_SESSION['login_user']) && ($_SESSION['login_user']['nb_tentatives_precedentes'] > 1)): ?>
                                 <div id="load_bloc">
                                     <img src="<?= $this->surl ?>/images/default/loading.gif" alt="loading...">
                                     <h2 style="margin-bottom:20px;" class="notice_error"><?= 'Par mesure de sécurité vous devez attendre ' . $_SESSION['login_user']['duree_waiting'] . ' secondes' ?></h2>
@@ -88,23 +74,13 @@
                             <div id="bloc_action">
                                 <input type="submit" value="Se connecter" title="Se connecter" name="connect" id="connect" class="btn"/>
                             </div>
-                            <?php if (isset($_SESSION['login_user']) && ($_SESSION['login_user']['nb_tentatives_precedentes'] > 1 && $_SESSION['login_user']['nb_tentatives_precedentes'] <= 5)) : ?>
+                            <?php if (isset($_SESSION['login_user']) && ($_SESSION['login_user']['nb_tentatives_precedentes'] > 1)) : ?>
                                 <script type="text/javascript">
                                     $("input").keypress(function (event) {
                                         if (event.keyCode == 13) {
                                             event.preventDefault();
                                         }
                                     });
-                                </script>
-                            <?php elseif (isset($_SESSION['login_user']) && $_SESSION['login_user']['nb_tentatives_precedentes'] > 5) : ?>
-                                <script type="text/javascript">
-                                    function reloadCaptcha() {
-                                        now = new Date();
-                                        var capObj = document.getElementById('phoca-captcha');
-                                        if (capObj) {
-                                            capObj.src = capObj.src + (capObj.src.indexOf('?') > -1 ? '&' : '?') + Math.ceil(Math.random() * (now.getTime()));
-                                        }
-                                    }
                                 </script>
                             <?php endif; ?>
                         </td>
@@ -115,6 +91,6 @@
     </div>
     <div id="footer_login"><?= $this->cms ?> 1.1 -
         <a href="http://www.equinoa.com" title="Agence Web Equinoa">Equinoa</a> &copy;<?= date('Y') ?></div>
-</div>
+    </div>
 </body>
 </html>
