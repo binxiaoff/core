46452ce5e07eaa5f8f42b1256308efb5b15b763d
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-09-28 13:58:05 +0200

    TMA-962 Cache footer content for 1 hour

--- src/Unilend/Bundle/FrontBundle/Controller/MainController.php
+++ src/Unilend/Bundle/FrontBundle/Controller/MainController.php
@@ -704,53 +704,69 @@ class MainController extends Controller
      */
     public function footerAction()
     {
-        /** @var EntityManager $entityManager */
-        $entityManager = $this->get('unilend.service.entity_manager');
-        /** @var \menus $menus */
-        $menus = $entityManager->getRepository('menus');
-        /** @var \tree_menu $subMenus */
-        $subMenus = $entityManager->getRepository('tree_menu');
-        /** @var \tree $page */
-        $page = $entityManager->getRepository('tree');
+        /** @var MemcacheCachePool $cachePool */
+        $cachePool  = $this->get('memcache.default');
+        $cachedItem = $cachePool->getItem('Footer');
 
-        $footerMenu = [];
-        foreach ($menus->select('status = 1', 'id_menu ASC') as $menu) {
-            $children = [];
-            foreach ($subMenus->select('status = 1 AND id_menu = ' . $menu['id_menu'], 'ordre ASC') as $subMenu) {
-                $children[] = [
-                    'title'  => $subMenu['nom'],
-                    'target' => $subMenu['target'],
-                    'url'    => ($subMenu['complement'] === 'L' && $page->get(['id_tree' => $subMenu['value']])) ? '/' . $page->slug : $subMenu['value']
+        if (false === $cachedItem->isHit()) {
+            /** @var EntityManager $entityManager */
+            $entityManager = $this->get('unilend.service.entity_manager');
+            /** @var \menus $menus */
+            $menus = $entityManager->getRepository('menus');
+            /** @var \tree_menu $subMenus */
+            $subMenus = $entityManager->getRepository('tree_menu');
+            /** @var \tree $page */
+            $page = $entityManager->getRepository('tree');
+
+            $footerMenu = [];
+            foreach ($menus->select('status = 1', 'id_menu ASC') as $menu) {
+                $children = [];
+                foreach ($subMenus->select('status = 1 AND id_menu = ' . $menu['id_menu'], 'ordre ASC') as $subMenu) {
+                    $children[] = [
+                        'title'  => $subMenu['nom'],
+                        'target' => $subMenu['target'],
+                        'url'    => ($subMenu['complement'] === 'L' && $page->get(['id_tree' => $subMenu['value']])) ? '/' . $page->slug : $subMenu['value']
+                    ];
+                }
+
+                $footerMenu[] = [
+                    'title'    => $menu['nom'],
+                    'children' => $children
                 ];
             }
 
-            $footerMenu[] = [
-                'title'    => $menu['nom'],
-                'children' => $children
-            ];
-        }
+            /** @var \blocs $block */
+            $block = $entityManager->getRepository('blocs');
+            /** @var \blocs_elements $blockElement */
+            $blockElement = $entityManager->getRepository('blocs_elements');
+            /** @var \elements $elements */
+            $elements = $entityManager->getRepository('elements');
 
-        /** @var \blocs $block */
-        $block = $entityManager->getRepository('blocs');
-        /** @var \blocs_elements $blockElement */
-        $blockElement = $entityManager->getRepository('blocs_elements');
-        /** @var \elements $elements */
-        $elements = $entityManager->getRepository('elements');
-
-        $partners = [];
-        if ($block->get('partenaires', 'slug')) {
-            $elementsId = array_column($elements->select('status = 1 AND id_bloc = ' . $block->id_bloc, 'ordre ASC'), 'id_element');
-            foreach ($blockElement->select('status = 1 AND id_bloc = ' . $block->id_bloc, 'FIELD(id_element, ' . implode(', ', $elementsId) . ') ASC') as $element) {
-                if (false === empty($element['value'])) {
-                    $partners[] = [
-                        'alt' => $element['complement'],
-                        'src' => $element['value']
-                    ];
+            $partners = [];
+            if ($block->get('partenaires', 'slug')) {
+                $elementsId = array_column($elements->select('status = 1 AND id_bloc = ' . $block->id_bloc, 'ordre ASC'), 'id_element');
+                foreach ($blockElement->select('status = 1 AND id_bloc = ' . $block->id_bloc, 'FIELD(id_element, ' . implode(', ', $elementsId) . ') ASC') as $element) {
+                    if (false === empty($element['value'])) {
+                        $partners[] = [
+                            'alt' => $element['complement'],
+                            'src' => $element['value']
+                        ];
+                    }
                 }
             }
+
+            $finalElements = [
+                'footerMenu' => $footerMenu,
+                'partners'   => $partners
+            ];
+
+            $cachedItem->set($finalElements)->expiresAfter(3600);
+            $cachePool->save($cachedItem);
+        } else {
+            $finalElements = $cachedItem->get();
         }
 
-        $response = $this->render('partials/site/footer.html.twig', ['menus' => $footerMenu, 'partners' => $partners]);
+        $response = $this->render('partials/site/footer.html.twig', ['menus' => $finalElements['footerMenu'], 'partners' => $finalElements['partners']]);
         $response->setSharedMaxAge(900);
 
         return $response;
