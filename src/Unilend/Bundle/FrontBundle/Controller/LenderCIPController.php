e4e54bd50bf018fbb9c7d757db64d52f59c2c563
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-10-10 11:27:38 +0200

    DEV-1040 Add controller/views for CIP

---
+++ src/Unilend/Bundle/FrontBundle/Controller/LenderCIPController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Unilend\Bundle\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LenderCIPController extends Controller
+{
+    /**
+     * @Route("/conseil-cip", name="cip_index")
+     * @Template("lender_cip/index.html.twig")
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/conseil-cip/oui-non", name="cip_boolean_question")
+     * @Template("lender_cip/boolean.html.twig")
+     *
+     * @return array
+     */
+    public function booleanQuestionAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/conseil-cip/valeur", name="cip_value_question")
+     * @Template("lender_cip/value.html.twig")
+     *
+     * @return array
+     */
+    public function valueQuestionAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/conseil-cip/conseil", name="cip_advice")
+     * @Template("lender_cip/advice.html.twig")
+     *
+     * @return array
+     */
+    public function adviceAction()
+    {
+        return [];
+    }
+}
