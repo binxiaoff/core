543c082d443e503ae0aa1998f6535334a5512751
Bin XIAO <bin.xiao@unilend.fr>
2016-11-10 15:15:45 +0100

    TECH-138 Newrelic - NR transactions mixed up  - Fix the naming issue for the legacy code.

--- public/admin/index.php
+++ public/admin/index.php
@@ -9,10 +9,6 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
-if (extension_loaded ('newrelic')) {
-    newrelic_set_appname ("Unilend-backoffice");
-}
-
 session_start();
 ini_set('session.gc_maxlifetime', 3600); // 1h la session
 
