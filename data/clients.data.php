746b41336b315095ba39572139025f8d6105759f
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-11-17 16:56:25 +0100

    TMA-1229 Fix lender bank transfer matching

--- data/clients.data.php
+++ data/clients.data.php
@@ -891,36 +891,16 @@ class clients extends clients_crud
 
     /**
      * Retrieve old pattern that lender must use in bank transfer label (with '?' or '' instead of accented characters)
-     * @param $sClientId
-     * @param $sMatchPattern
+     * @param int    $clientId
+     * @param string $pattern
      * @return bool
      */
-    public function isLenderPattern($sClientId, $sMatchPattern)
+    public function isLenderPattern($clientId, $pattern)
     {
-        $this->get($sClientId);
+        $pattern       = str_replace(' ', '', $pattern);
+        $lenderPattern = str_replace(' ', '', $this->getLenderPattern($clientId));
 
-        $aStrTrans = array(
-            'À' => '?', 'à' => '?', 'Á' => '?', 'á' => '?', 'Â' => '?', 'â' => '?', 'Ã' => '?', 'ã' => '?', 'Ä' => '?',
-            'ä' => '?', 'Å' => '?', 'å' => '?', 'Æ' => '?', 'æ' => '?', 'Ç' => '?', 'ç' => '?', 'È' => '?', 'è' => '?',
-            'É' => '?', 'é' => '?', 'Ê' => '?', 'ê' => '?', 'Ë' => '?', 'ë' => '?', 'Ì' => '?', 'ì' => '?', 'Í' => '?',
-            'í' => '?', 'Î' => '?', 'î' => '?', 'Ï' => '?', 'ï' => '?', 'Ñ' => '?', 'ñ' => '?', 'Ò' => '?', 'ò' => '?',
-            'Ó' => '?', 'ó' => '?', 'Ô' => '?', 'ô' => '?', 'Õ' => '?', 'õ' => '?', 'Ö' => '?', 'ö' => '?', 'Ø' => '?',
-            'ø' => '?', 'Œ' => '?', 'œ' => '?', 'ß' => '?', 'Ù' => '?', 'ù' => '?', 'Ú' => '?', 'ú' => '?',
-            'Û' => '?', 'û' => 'u', 'Ü' => '?', 'ü' => '?', 'Ý' => '?', 'ý' => '?', 'Ÿ' => '?', 'ÿ' => '?'
-        );
-
-        $sPattern = str_replace(' ', '',
-            str_pad($this->id_client, 6, 0, STR_PAD_LEFT)
-            . mb_strtoupper(
-                strtr(substr($this->prenom, 0, 1), $aStrTrans)
-                . strtr($this->nom, $aStrTrans)
-            ));
-
-        if (false !== strpos($sMatchPattern, $sPattern) || false !== strpos($sMatchPattern, str_replace('?', '', $sPattern))) {
-            return true;
-        } else {
-            return false;
-        }
+        return (false !== strpos($pattern, $lenderPattern));
     }
 
     public function getDuplicates($sLastName, $sFirstName, $sBirthdate)
