62e0cf4c04bf6f70ad1d5ba59cdd227874fbcd02
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-11-25 11:04:04 +0100

    TST-44 Change translation depending on type of lender

--- src/Unilend/Bundle/FrontBundle/Controller/LenderCIPController.php
+++ src/Unilend/Bundle/FrontBundle/Controller/LenderCIPController.php
@@ -127,6 +127,8 @@ class LenderCIPController extends Controller
             }
 
             if ($lastQuestion->isBooleanType($lastQuestion->type)) {
+                $template['lenderType'] = in_array($this->getClient()->type, [\clients::TYPE_PERSON, \clients::TYPE_PERSON_FOREIGNER]) ? 'physical' : 'moral';
+
                 if (\lender_questionnaire_question::TYPE_AWARE_DIVIDE_INVESTMENTS === $lastQuestion->type) {
                     $template['question']['valid_answer']   = \lender_questionnaire_question::VALUE_BOOLEAN_FALSE;
                     $template['question']['invalid_answer'] = \lender_questionnaire_question::VALUE_BOOLEAN_TRUE;
