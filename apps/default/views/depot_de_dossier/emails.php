009767b44a4376bd96f6b4e37e76a5955b4b51b9
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-12-01 16:48:26 +0100

    Delete old front office views

--- apps/default/views/depot_de_dossier/emails.php
+++
@@ -1,5 +0,0 @@
-<div class="main">
-    <div class="shell">
-        <p><?= $this->lng['depot-de-dossier']['contenu-stop-emails'] ?></p>
-    </div>
-</div>
--- apps/default/views/depot_de_dossier/fin.php
+++
@@ -1,8 +0,0 @@
-<div class="main">
-    <div class="shell">
-        <p><?= $this->sMessage ?></p>
-        <?php if ($this->bDisplayContact) { ?>
-            <?php $this->fireView(('../templates/contact_emprunteur')); ?>
-        <?php } ?>
-    </div>
-</div>
