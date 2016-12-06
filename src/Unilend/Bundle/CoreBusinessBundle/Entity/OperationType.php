5a033f1906b2c88aa0385ac0dfdaaa269a1b543c
Bin XIAO <bin.xiao@unilend.fr>
2016-12-06 11:53:30 +0100

    Add Doctrine ORM entities.

--- src/Unilend/Bundle/CoreBusinessBundle/Entity/OperationType.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Entity/OperationType.php
@@ -7,11 +7,20 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * OperationType
  *
- * @ORM\Table(name="operation_type", uniqueConstraints={@ORM\UniqueConstraint(name="label_UNIQUE", columns={"label"})}, indexes={@ORM\Index(name="fk_id_wallet_type_debtor_idx", columns={"id_wallet_type_debtor"}), @ORM\Index(name="fk_id_wallet_type_creditor_idx", columns={"id_wallet_type_creditor"})})
+ * @ORM\Table(name="operation_type", uniqueConstraints={@ORM\UniqueConstraint(name="label_UNIQUE", columns={"label"})})
  * @ORM\Entity
  */
 class OperationType
 {
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
     /**
      * @var string
      *
@@ -19,36 +28,16 @@ class OperationType
      */
     private $label;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
 
     /**
-     * @var \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType
+     * Get id
      *
-     * @ORM\ManyToOne(targetEntity="Unilend\Bundle\CoreBusinessBundle\Entity\WalletType")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="id_wallet_type_debtor", referencedColumnName="id")
-     * })
+     * @return integer
      */
-    private $idWalletTypeDebtor;
-
-    /**
-     * @var \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType
-     *
-     * @ORM\ManyToOne(targetEntity="Unilend\Bundle\CoreBusinessBundle\Entity\WalletType")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="id_wallet_type_creditor", referencedColumnName="id")
-     * })
-     */
-    private $idWalletTypeCreditor;
-
-
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * Set label
@@ -72,63 +61,5 @@ class OperationType
     public function getLabel()
     {
         return $this->label;
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set idWalletTypeDebtor
-     *
-     * @param \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType $idWalletTypeDebtor
-     *
-     * @return OperationType
-     */
-    public function setIdWalletTypeDebtor(\Unilend\Bundle\CoreBusinessBundle\Entity\WalletType $idWalletTypeDebtor = null)
-    {
-        $this->idWalletTypeDebtor = $idWalletTypeDebtor;
-
-        return $this;
-    }
-
-    /**
-     * Get idWalletTypeDebtor
-     *
-     * @return \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType
-     */
-    public function getIdWalletTypeDebtor()
-    {
-        return $this->idWalletTypeDebtor;
-    }
-
-    /**
-     * Set idWalletTypeCreditor
-     *
-     * @param \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType $idWalletTypeCreditor
-     *
-     * @return OperationType
-     */
-    public function setIdWalletTypeCreditor(\Unilend\Bundle\CoreBusinessBundle\Entity\WalletType $idWalletTypeCreditor = null)
-    {
-        $this->idWalletTypeCreditor = $idWalletTypeCreditor;
-
-        return $this;
-    }
-
-    /**
-     * Get idWalletTypeCreditor
-     *
-     * @return \Unilend\Bundle\CoreBusinessBundle\Entity\WalletType
-     */
-    public function getIdWalletTypeCreditor()
-    {
-        return $this->idWalletTypeCreditor;
     }
 }
