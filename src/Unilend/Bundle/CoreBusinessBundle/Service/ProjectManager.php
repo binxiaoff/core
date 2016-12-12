b8eec6facc31aea73fb9f7ecc9bc14721d9bd0b8
Antoine Préveaux <antoine.preveaux@unilend.fr>
2016-12-12 12:21:59 +0100

    TMA-623 Remove debug log

--- src/Unilend/Bundle/CoreBusinessBundle/Service/ProjectManager.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Service/ProjectManager.php
@@ -722,10 +722,6 @@ class ProjectManager
         $oProjectStatus = $this->oEntityManager->getRepository('projects_status');
         $oProjectStatus->get($iProjectStatus, 'status');
 
-        if ($this->oLogger instanceof LoggerInterface && empty($oProjectStatus->id_project_status)) {
-            $this->oLogger->error('Trying to insert empty status for project ' . $oProject->id_project . ' - Trace: ' . serialize(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)), array('id_project' => $oProject->id_project));
-        }
-
         $oProjectsStatusHistory->id_project        = $oProject->id_project;
         $oProjectsStatusHistory->id_project_status = $oProjectStatus->id_project_status;
         $oProjectsStatusHistory->id_user           = $iUserId;
