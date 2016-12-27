13a27036e4c1f265224a5a3c008c598557abd026
Bin XIAO <bin.xiao@unilend.fr>
2016-12-27 11:30:22 +0100

    Remove the check on date in cron

--- src/Unilend/Bundle/CommandBundle/Command/UnilendBankTransfertCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/UnilendBankTransfertCommand.php
@@ -25,54 +25,48 @@ class UnilendBankTransfertCommand extends ContainerAwareCommand
     {
         /** @var EntityManager $entityManager */
         $entityManager = $this->getContainer()->get('unilend.service.entity_manager');
-        $entityManager->getRepository('transactions_types'); //load for constant use
 
-        $jour           = date('d');
-        $datesVirements = array(1, 15);
+        /** @var \platform_account_unilend $oAccountUnilend */
+        $oAccountUnilend = $entityManager->getRepository('platform_account_unilend');
+        $total           = $oAccountUnilend->getBalance();
 
-        if (in_array($jour, $datesVirements)) {
-            /** @var \platform_account_unilend $oAccountUnilend */
-            $oAccountUnilend = $entityManager->getRepository('platform_account_unilend');
-            $total           = $oAccountUnilend->getBalance();
+        if ($total > 0) {
+            /** @var \virements $virements */
+            $virements = $entityManager->getRepository('virements');
+            /** @var \transactions $transactions */
+            $transactions = $entityManager->getRepository('transactions');
+            /** @var \bank_unilend $bank_unilend */
+            $bank_unilend = $entityManager->getRepository('bank_unilend');
 
-            if ($total > 0) {
-                /** @var \virements $virements */
-                $virements    = $entityManager->getRepository('virements');
-                /** @var \transactions $transactions */
-                $transactions = $entityManager->getRepository('transactions');
-                /** @var \bank_unilend $bank_unilend */
-                $bank_unilend = $entityManager->getRepository('bank_unilend');
+            $transactions->id_client        = 0;
+            $transactions->montant          = $total;
+            $transactions->id_langue        = 'fr';
+            $transactions->date_transaction = date('Y-m-d H:i:s');
+            $transactions->status           = \transactions::STATUS_VALID;
+            $transactions->type_transaction = \transactions_types::TYPE_UNILEND_BANK_TRANSFER;
+            $transactions->create();
 
-                $transactions->id_client        = 0;
-                $transactions->montant          = $total;
-                $transactions->id_langue        = 'fr';
-                $transactions->date_transaction = date('Y-m-d H:i:s');
-                $transactions->status           = \transactions::STATUS_VALID;
-                $transactions->type_transaction = \transactions_types::TYPE_UNILEND_BANK_TRANSFER;
-                $transactions->create();
+            $virements->id_client      = 0;
+            $virements->id_project     = 0;
+            $virements->id_transaction = $transactions->id_transaction;
+            $virements->montant        = $total;
+            $virements->motif          = 'UNILEND_' . date('dmY');
+            $virements->type           = \virements::TYPE_UNILEND;
+            $virements->status         = \virements::STATUS_PENDING;
+            $virements->create();
 
-                $virements->id_client      = 0;
-                $virements->id_project     = 0;
-                $virements->id_transaction = $transactions->id_transaction;
-                $virements->montant        = $total;
-                $virements->motif          = 'UNILEND_' . date('dmY');
-                $virements->type           = \virements::TYPE_UNILEND;
-                $virements->status         = \virements::STATUS_PENDING;
-                $virements->create();
+            $bank_unilend->id_transaction         = $transactions->id_transaction;
+            $bank_unilend->id_echeance_emprunteur = 0;
+            $bank_unilend->id_project             = 0;
+            $bank_unilend->montant                = '-' . $total;
+            $bank_unilend->type                   = \bank_unilend::TYPE_DEBIT_UNILEND;
+            $bank_unilend->status                 = 3;
+            $bank_unilend->create();
 
-                $bank_unilend->id_transaction         = $transactions->id_transaction;
-                $bank_unilend->id_echeance_emprunteur = 0;
-                $bank_unilend->id_project             = 0;
-                $bank_unilend->montant                = '-' . $total;
-                $bank_unilend->type                   = \bank_unilend::TYPE_DEBIT_UNILEND;
-                $bank_unilend->status                 = 3;
-                $bank_unilend->create();
-
-                $oAccountUnilend->id_transaction = $transactions->id_transaction;
-                $oAccountUnilend->type           = \platform_account_unilend::TYPE_WITHDRAW;
-                $oAccountUnilend->amount         = - $total;
-                $oAccountUnilend->create();
-            }
+            $oAccountUnilend->id_transaction = $transactions->id_transaction;
+            $oAccountUnilend->type           = \platform_account_unilend::TYPE_WITHDRAW;
+            $oAccountUnilend->amount         = -$total;
+            $oAccountUnilend->create();
         }
     }
 }
