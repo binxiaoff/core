2842c4cc898478ec48f019081bbd5ea1328fb8df
Bin XIAO <bin.xiao@unilend.fr>
2017-01-12 11:31:06 +0100

    Extends SymfonyFrameworkBundle application and remove unnecessary code

--- src/Unilend/Bundle/CommandBundle/Bridge/Application.php
+++ src/Unilend/Bundle/CommandBundle/Bridge/Application.php
@@ -2,66 +2,33 @@
 namespace Unilend\Bundle\CommandBundle\Bridge;
 
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Bundle\FrameworkBundle\Console\Application as FrameworkBundleApplication;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Filesystem\LockHandler;
 
-class Application extends BaseApplication
+class Application extends FrameworkBundleApplication
 {
-    /** @var KernelInterface  */
-    private $kernel;
-    /** @var bool  */
-    private $commandsRegistered = false;
-
     /**
-     * Application constructor.
-     *
-     * @param KernelInterface $kernel
-     */
+    * @inheritDoc
+    */
     public function __construct(KernelInterface $kernel)
     {
-        $this->kernel = $kernel;
-        parent::__construct('Unilend Console', '1.0');
+        parent::__construct($kernel);
 
-        $this->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', $kernel->getEnvironment()));
-        $this->getDefinition()->addOption(new InputOption('--no-debug', null, InputOption::VALUE_NONE, 'Switches off debug mode.'));
         $this->getDefinition()->addOption(new InputOption('--multi-process', '-m', InputOption::VALUE_NONE, 'This is a multi process or a single process.'));
     }
 
     /**
-     * Gets the Kernel associated with this Console.
-     *
-     * @return KernelInterface A KernelInterface instance
-     */
-    public function getKernel()
-    {
-        return $this->kernel;
-    }
-
-    /**
-     * Runs the current application.
-     *
-     * @param InputInterface  $input  An Input instance
-     * @param OutputInterface $output An Output instance
-     *
-     * @return int 0 if everything went fine, or an error code
+     * @inheritDoc
      */
     public function doRun(InputInterface $input, OutputInterface $output)
     {
-        $this->kernel->boot();
+        $this->getKernel()->boot();
 
-        if (! $this->commandsRegistered) {
-            $this->registerCommands();
-
-            $this->commandsRegistered = true;
-        }
-        /** @var ContainerBuilder $container */
-        $container = $this->kernel->getContainer();
+        $container = $this->getKernel()->getContainer();
 
         foreach ($this->all() as $command) {
             if ($command instanceof ContainerAwareInterface) {
@@ -74,8 +41,6 @@ class Application extends BaseApplication
         if (! $input->hasParameterOption(array('--multi-process', '-m'), false)) {
             $lock = new LockHandler($this->getCommandName($input));
             if (! $lock->lock()) {
-                /** @var ContainerBuilder $container */
-                $container = $this->kernel->getContainer();
                 $container->get('monolog.logger.console')->warning('The command ' . $this->getCommandName($input) . ' is already running in another process.');
                 $output->writeln('The command ' . $this->getCommandName($input) . ' is already running in another process.');
                 return 0;
@@ -89,26 +54,5 @@ class Application extends BaseApplication
         }
 
         return $return;
-    }
-
-    /**
-     * Register all the commands declared as a service (that has the unilend.command tag.
-     */
-    protected function registerCommands()
-    {
-        /** @var ContainerBuilder $container */
-        $container = $this->kernel->getContainer();
-
-        foreach ($this->kernel->getBundles() as $bundle) {
-            if ($bundle instanceof Bundle) {
-                $bundle->registerCommands($this);
-            }
-        }
-
-        if ($container->hasParameter('console.command.ids')) {
-            foreach ($container->getParameter('console.command.ids') as $id) {
-                $this->add($container->get($id));
-            }
-        }
     }
 }
