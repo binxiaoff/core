18964f961d4a19b8dd041c1d83fb9efb4e2e7d90
Antoine Préveaux <antoine.preveaux@unilend.fr>
2017-01-25 17:49:24 +0100

    TECH-209 Add X-Frame-Options header

--- public/default/index_dev.php
+++ public/default/index_dev.php
@@ -1,18 +1,18 @@
 <?php
 
-if(getenv('SYMFONY_ENV') && 'prod' === getenv('SYMFONY_ENV')) {
-    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+if (getenv('SYMFONY_ENV') && 'prod' === getenv('SYMFONY_ENV')) {
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
     exit;
 }
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require __DIR__.'/../../app/autoload.php';
+$loader = require __DIR__ . '/../../app/autoload.php';
 include __DIR__ . '/../../core/controller.class.php';
 include __DIR__ . '/../../core/command.class.php';
 include __DIR__ . '/../../config.php';
-require_once __DIR__.'/../../app/AppKernel.php';
+require_once __DIR__ . '/../../app/AppKernel.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -34,8 +34,7 @@ if (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVE
 
 Debug::enable();
 
-$kernel = new AppKernel('dev', true);
-
+$kernel   = new AppKernel('dev', true);
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- public/default/index_preprod.php
+++ public/default/index_preprod.php
@@ -1,17 +1,17 @@
 <?php
 
-if(getenv('SYMFONY_ENV') && 'prod' === getenv('SYMFONY_ENV')) {
-    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+if (getenv('SYMFONY_ENV') && 'prod' === getenv('SYMFONY_ENV')) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
     exit;
 }
 
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require __DIR__.'/../../app/autoload.php';
+$loader = require __DIR__ . '/../../app/autoload.php';
 include __DIR__ . '/../../core/controller.class.php';
 include __DIR__ . '/../../core/command.class.php';
 include __DIR__ . '/../../config.php';
-require_once __DIR__.'/../../app/AppKernel.php';
+require_once __DIR__ . '/../../app/AppKernel.php';
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
 ini_set('display_errors', 0);
@@ -31,11 +31,7 @@ if (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVE
     );
 }
 
-session_start();
-ini_set('session.gc_maxlifetime', 3600); // 1h la session
-
-$kernel = new AppKernel('preprod', false);
-
+$kernel   = new AppKernel('preprod', false);
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
