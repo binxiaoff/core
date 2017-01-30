c111599c34531f943a857c0fbc5500b47b3ea310
Bin XIAO <bin.xiao@unilend.fr>
2017-01-30 11:10:10 +0100

    remove the env option from shell script

--- bin/migration_transaction.sh
+++ bin/migration_transaction.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-php /data/sites/unilend/bin/console dev:migrate:create_wallet 100 --env=prod
-php /data/sites/unilend/bin/console dev:migrate:transactions 20000 --env=prod
+php /data/sites/unilend/bin/console dev:migrate:create_wallet 100
+php /data/sites/unilend/bin/console dev:migrate:transactions 20000
