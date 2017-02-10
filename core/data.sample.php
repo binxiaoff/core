04eb05593c010e1742e007350a87e92c0fb31cb8
Antoine Préveaux <antoine.preveaux@unilend.fr>
2017-02-10 16:09:25 +0100

    Clean automatic crud/data generation

--- core/data.sample.php
+++ core/data.sample.php
@@ -1,30 +1,5 @@
 <?php
-// **************************************************************************************************** //
-// ***************************************    ASPARTAM    ********************************************* //
-// **************************************************************************************************** //
-//
-// Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies
-// or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
-// In no event shall the authors or copyright holders equinoa be liable for any claim,
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
-// out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising
-// or otherwise to promote the sale, use or other dealings in this Software without
-// prior written authorization from equinoa.
-//
-//  Version : 2.4.0
-//  Date : 21/03/2011
-//  Coupable : CM
-//
-// **************************************************************************************************** //
+
 class --classe-- extends --classe--_crud
 {
     public function __construct($bdd, $params = '')
@@ -42,11 +17,10 @@ class --classe-- extends --classe--_crud
             $order = ' ORDER BY ' . $order;
         }
 
-        $sql = 'SELECT * FROM `--table--`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
-
-        $result   = array();
-        $resultat = $this->bdd->query($sql);
-        while ($record = $this->bdd->fetch_assoc($resultat)) {
+        $result    = [];
+        $query     = 'SELECT * FROM `--table--`' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+        $statement = $this->bdd->query($query);
+        while ($record = $this->bdd->fetch_assoc($statement)) {
             $result[] = $record;
         }
         return $result;
--- core/data2.sample.php
+++ core/data2.sample.php
@@ -1,30 +1,5 @@
 <?php
-// **************************************************************************************************** //
-// ***************************************    ASPARTAM    ********************************************* //
-// **************************************************************************************************** //
-//
-// Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies
-// or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
-// In no event shall the authors or copyright holders equinoa be liable for any claim,
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
-// out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising
-// or otherwise to promote the sale, use or other dealings in this Software without
-// prior written authorization from equinoa.
-//
-//  Version : 2.4.0
-//  Date : 21/03/2011
-//  Coupable : CM
-//
-// **************************************************************************************************** //
+
 class --classe-- extends --classe--_crud
 {
     public function __construct($bdd, $params = '')
@@ -37,13 +12,15 @@ class --classe-- extends --classe--_crud
         if ($where != '') {
             $where = ' WHERE ' . $where;
         }
+
         if ($order != '') {
             $order = ' ORDER BY ' . $order;
         }
 
-        $result   = array();
-        $resultat = $this->bdd->query('SELECT * FROM --table--' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : '')));
-        while ($record = $this->bdd->fetch_assoc($resultat)) {
+        $result    = [];
+        $query     = 'SELECT * FROM --table--' . $where . $order . ($nb != '' && $start != '' ? ' LIMIT ' . $start . ',' . $nb : ($nb != '' ? ' LIMIT ' . $nb : ''));
+        $statement = $this->bdd->query($query);
+        while ($record = $this->bdd->fetch_assoc($statement)) {
             $result[] = $record;
         }
         return $result;
@@ -59,11 +36,11 @@ class --classe-- extends --classe--_crud
         return (int) $this->bdd->result($result);
     }
 
-    public function exist($list_field_value)
+    public function exist($fields)
     {
         $list = '';
-        foreach ($list_field_value as $champ => $valeur) {
-            $list .= ' AND ' . $champ . ' = "' . $valeur . '" ';
+        foreach ($fields as $field => $value) {
+            $list .= ' AND ' . $field . ' = "' . $value . '" ';
         }
 
         $result = $this->bdd->query('SELECT * FROM --table-- WHERE 1 = 1' . $list);
