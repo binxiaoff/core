96a0fbabaf227e787e45fe5f5ac3f067ca23dc68
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2017-03-02 12:49:57 +0100

    DEV-464 : Project LAB - Detect atypical operations  - CRON to check vigilance rules

--- src/Unilend/Bundle/CoreBusinessBundle/Repository/BidsRepository.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Repository/BidsRepository.php
@@ -24,4 +24,30 @@ class BidsRepository extends EntityRepository
         $query = $qb->getQuery();
         return $query->getSingleScalarResult();
     }
+
+    public function countByClientInPeriod(\DateTime $from, \DateTime $to, $clientId)
+    {
+        $query = '
+            SELECT COUNT(id_bid)
+            FROM bids
+            WHERE bids.added BETWEEN :from_date AND :to_date
+              AND bids.id_lender_account = (SELECT la.id_lender_account FROM lenders_accounts la WHERE la.id_client_owner = :id_client)
+        ';
+
+        return $this->getEntityManager()
+            ->getConnection()
+            ->executeQuery(
+                $query,
+                [
+                    'from_date' => $from->format('Y-m-d H:i:s'),
+                    'to_date'   => $to->format('Y-m-d H:i:s'),
+                    'id_client' => $clientId
+                ],
+                [
+                    'from_date' => \PDO::PARAM_STR,
+                    'to_date'   => \PDO::PARAM_STR,
+                    'id_client' => \PDO::PARAM_INT
+                ]
+            )->fetchColumn();
+    }
 }
