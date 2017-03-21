02e342025562f775c985921ce6ad87aba94adb38
Antoine Préveaux <antoine.preveaux@unilend.fr>
2017-03-21 10:46:34 +0100

    DEV-1258 Fix comity and analysis sections display

--- apps/admin/views/dossiers/blocs/etape7.php
+++ apps/admin/views/dossiers/blocs/etape7.php
@@ -1,8 +1,5 @@
-<div id="content_etape7">
-    <?php if (
-        $this->projects->status >= \projects_status::COMITY_REVIEW
-        || $this->projects_status_history->projectHasHadStatus($this->projects->id_project, \projects_status::COMITY_REVIEW)
-    ) : ?>
+<?php if ($this->projects->status >= \projects_status::COMITY_REVIEW || $this->projects_status_history->projectHasHadStatus($this->projects->id_project, \projects_status::COMITY_REVIEW)) : ?>
+    <div id="content_etape7">
         <?php
         $moyenne  = round($this->projects_notes->performance_fianciere_comite * 0.2 + $this->projects_notes->marche_opere_comite * 0.2 + $this->projects_notes->dirigeance_comite * 0.2 + $this->projects_notes->indicateur_risque_dynamique_comite * 0.4, 1);
         $start = '';
@@ -37,50 +34,75 @@
                     <th style="vertical-align:top;"><label for="marche_opere_comite">Marché opéré</label></th>
                     <td style="vertical-align:top;"><span id="marche_opere_comite"><?= $this->projects_notes->marche_opere_comite ?></span> / 10</td>
                     <th><label for="dirigeance_comite">Dirigeance</label></th>
-                    <td><input tabindex="14" id="dirigeance_comite" name="dirigeance_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->dirigeance_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
-                    <th><label for="indicateur_risque_dynamique_comite">Indicateur de risque dynamique</label></th>
-                    <td><input tabindex="15" id="indicateur_risque_dynamique_comite" name="indicateur_risque_dynamique_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->indicateur_risque_dynamique_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
+                    <td>
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="dirigeance_comite" name="dirigeance_comite" value="<?= $this->projects_notes->dirigeance_comite ?>" type="text" maxlength="4" tabindex="14" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->dirigeance_comite ?> / 10
+                        <?php endif; ?>
+                    </td>
+                    <th><label for="indicateur_risque_dynamique_comite">Indicateur risque dynamique</label></th>
+                    <td>
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="indicateur_risque_dynamique_comite" name="indicateur_risque_dynamique_comite" value="<?= $this->projects_notes->indicateur_risque_dynamique_comite ?>" type="text" maxlength="4" tabindex="15" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->indicateur_risque_dynamique_comite ?> / 10
+                        <?php endif; ?>
+                    </td>
                 </tr>
                 <tr>
-                    <td colspan="2" style="vertical-align:top;">
-                        <table>
-                            <tr>
-                                <th><label for="structure2">Structure</label></th>
-                                <td><input tabindex="9" id="structure_comite" name="structure_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->structure_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
-                            </tr>
-                            <tr>
-                                <th><label for="rentabilite2">Rentabilité</label></th>
-                                <td><input tabindex="10" id="rentabilite_comite" name="rentabilite_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->rentabilite_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
-                            </tr>
-                            <tr>
-                                <th><label for="tresorerie2">Trésorerie</label></th>
-                                <td>
-                                    <input tabindex="11" id="tresorerie_comite" name="tresorerie_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->tresorerie_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10
-                                </td>
-                            </tr>
-                        </table>
+                    <th><label for="structure_comite">Structure</label></th>
+                    <td>
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="structure_comite" name="structure_comite" value="<?= $this->projects_notes->structure_comite ?>" type="text" maxlength="4" tabindex="9" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->structure_comite ?> / 10
+                        <?php endif; ?>
                     </td>
-                    <td colspan="2" style="vertical-align:top;">
-                        <table>
-                            <tr>
-                                <th><label for="global2">Global</label></th>
-                                <td><input tabindex="12" id="global_comite" name="global_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->global_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
-                            </tr>
-                            <tr>
-                                <th><label for="individuel2">Individuel</label></th>
-                                <td><input tabindex="13" id="individuel_comite" name="individuel_comite" class="input_court cal_moyen" type="text" value="<?= $this->projects_notes->individuel_comite ?>" maxlength="4" onkeyup="nodizaines(this.value, this.id);"> / 10</td>
-                            </tr>
-                        </table>
+                    <th><label for="global_comite">Global</label></th>
+                    <td colspan="5">
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="global_comite" name="global_comite" value="<?= $this->projects_notes->global_comite ?>" type="text" maxlength="4" tabindex="12" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->global_comite ?> / 10
+                        <?php endif; ?>
                     </td>
-                    <td colspan="4"></td>
+                </tr>
+                <tr>
+                    <th><label for="rentabilite_comite">Rentabilité</label></th>
+                    <td>
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="rentabilite_comite" name="rentabilite_comite" value="<?= $this->projects_notes->rentabilite_comite ?>" type="text" maxlength="4" tabindex="10" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->rentabilite_comite ?> / 10
+                        <?php endif; ?>
+                    </td>
+                    <th><label for="individuel_comite">Individuel</label></th>
+                    <td colspan="5">
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="individuel_comite" name="individuel_comite" value="<?= $this->projects_notes->individuel_comite ?>" type="text" maxlength="4" tabindex="13" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->individuel_comite ?> / 10
+                        <?php endif; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="tresorerie_comite">Trésorerie</label></th>
+                    <td colspan="7">
+                        <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                            <input id="tresorerie_comite" name="tresorerie_comite" value="<?= $this->projects_notes->tresorerie_comite ?>" type="text" maxlength="4" tabindex="11" class="input_court cal_moyen" onkeyup="nodizaines(this.value, this.id);"> / 10
+                        <?php else : ?>
+                            <?= $this->projects_notes->tresorerie_comite ?> / 10
+                        <?php endif; ?>
+                    </td>
                 </tr>
                 <tr class="lanote">
                     <th colspan="8" style="text-align:center;">Note : <span class="moyenneNote_comite"><?= $moyenne ?> / 10 (soit <?= $start ?>)</span></th>
                 </tr>
                 <tr>
-                    <td colspan="8" style="text-align:center;">
+                    <td colspan="8">
                         <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
-                            <label for="avis_comite" style="text-align:left;display: block;">Avis comité :</label><br>
+                            <label for="avis_comite" style="text-align:left;display: block;">Avis comité</label><br>
                             <textarea tabindex="16" name="avis_comite" style="height:700px;" id="avis_comite" class="textarea_large avis_comite"><?= $this->projects_notes->avis_comite ?></textarea>
                             <script type="text/javascript">var ckedAvis_comite = CKEDITOR.replace('avis_comite', {height: 700});</script>
                         <?php else : ?>
@@ -89,18 +111,17 @@
                     </td>
                 </tr>
             </table>
-            <br><br>
-            <div id="valid_etape7" class="valid_etape">Données sauvegardées</div>
-            <div class="btnDroite">
-                <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+            <div id="valid_etape7" class="valid_etape"><br><br>Données sauvegardées</div>
+            <?php if ($this->projects->status == \projects_status::COMITY_REVIEW) : ?>
+                <div class="btnDroite">
                     <input id="min_rate" type="hidden" value="<?= isset($this->rate_min) ? $this->rate_min : '' ?>">
                     <input id="max_rate" type="hidden" value="<?= isset($this->rate_max) ? $this->rate_max : '' ?>">
                     <input type="button" onclick="valid_rejete_etape7(3, <?= $this->projects->id_project ?>)" class="btn" value="Sauvegarder">
                     <a href="<?= $this->lurl ?>/dossiers/comity_to_analysis/<?= $this->projects->id_project ?>" class="btn btn_link thickbox">Retour à l'analyse</a>
                     <a href="<?= $this->lurl ?>/dossiers/ajax_rejection/7/<?= $this->projects->id_project ?>" class="btn btnValid_rejet_etape7 btn_link thickbox" style="background:#CC0000;border-color:#CC0000;">Rejeter</a>
                     <input type="button" onclick="valid_rejete_etape7(1, <?= $this->projects->id_project ?>)" class="btn btnValid_rejet_etape7" style="background:#009933;border-color:#009933;" value="Valider">
-                <?php endif; ?>
-            </div>
+                </div>
+            <?php endif; ?>
         </div>
         <script type="text/javascript">
             $(".cal_moyen").keyup(function() {
@@ -157,5 +178,5 @@
                 $(".moyenneNote_comite").html(moyenne + " / 10" + ' (soit ' + start + ')');
             });
         </script>
-    <?php endif; ?>
-</div>
+    </div>
+<?php endif; ?>
