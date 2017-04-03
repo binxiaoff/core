0e044769f3bef3a7fcfedc535f4bd515e2befc44
Bin XIAO <bin.xiao@unilend.fr>
2017-04-03 16:35:39 +0200

    TMA-1684 Création de nouveau recouvreur Progéris

--- src/Unilend/Bundle/CommandBundle/Command/DevDebtCollectionCreationCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/DevDebtCollectionCreationCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Unilend\Bundle\CoreBusinessBundle\Entity\Clients;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Projects;
 use Unilend\Bundle\CoreBusinessBundle\Entity\ProjectsStatus;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Receptions;
@@ -14,6 +15,9 @@ use Unilend\Bundle\CoreBusinessBundle\Entity\WalletType;
 
 class DevDebtCollectionCreationCommand extends ContainerAwareCommand
 {
+    const CLIENT_HASH_MCS      = '2f9f590e-d689-11e6-b3d7-005056a378e2';
+    const CLIENT_HASH_PROGERIS = 'f12f0f5b-1867-11e7-a89f-0050569e51ae';
+
     /**
      * @see Command
      */
@@ -23,9 +27,10 @@ class DevDebtCollectionCreationCommand extends ContainerAwareCommand
             ->setName('unilend:dev_tools:debt_collection:create')
             ->setDescription('Import manually the debt collection repayment')
             ->addArgument('action', InputArgument::REQUIRED, 'Which action do you want to take?')
-            ->addOption('reception-id', null, InputOption::VALUE_OPTIONAL, 'Use with the action "provision". The reception id of the provision.')
-            ->addOption('project-id', null, InputOption::VALUE_OPTIONAL, 'Use with the action "provision" and "repayment". The project id of the debt collection.')
-            ->addOption('commission', null, InputOption::VALUE_OPTIONAL, 'Use with the action "provision" and "repayment". The commission for the debt collection.')
+            ->addOption('reception-id', null, InputOption::VALUE_REQUIRED, 'Use with the action "provision". The reception id of the provision.')
+            ->addOption('project-id', null, InputOption::VALUE_REQUIRED, 'Use with the action "provision" and "repayment". The project id of the debt collection.')
+            ->addOption('commission', null, InputOption::VALUE_REQUIRED, 'Use with the action "provision" and "repayment". The commission for the debt collection.')
+            ->addOption('collector', null, InputOption::VALUE_REQUIRED, 'Use with the action "provision" and "repayment". The debt collector for the debt collection.')
             ->setHelp(<<<EOF
 The <info>unilend:dev_tools:debt_collection:create</info> create the transactions and operations for the debt collection.
 <info>php bin/console unilend:dev_tools:debt_collection:create provision|repayment</info>
@@ -73,16 +78,26 @@ EOF
             $output->writeln('Client id: ' . $project->getIdCompany()->getIdClientOwner() . ' not found.');
             return;
         }
-        // We support only MCS as debt collector
-        $clientCollector = $clientRepo->findOneBy(['hash' => '2f9f590e-d689-11e6-b3d7-005056a378e2']);
+        $borrower = $walletRepo->getWalletByType($client->getIdClient(), WalletType::BORROWER);
+        if (null === $borrower) {
+            $output->writeln('Borrower with client id : ' . $project->getIdCompany()->getIdClientOwner() . ' not found.');
+            return;
+        }
+        $clientCollector = $this->getCollector($input, $output);
         if (null === $clientCollector) {
-            $output->writeln('Client hash: 2f9f590e-d689-11e6-b3d7-005056a378e2 not found.');
+            $output->writeln('Collector not found.');
+            return;
+        }
+        $collector = $walletRepo->getWalletByType($clientCollector->getIdClient(), WalletType::DEBT_COLLECTOR);
+        if (null === $collector) {
+            $output->writeln('Collector\'s wallet not found.');
             return;
         }
         if ($commission <= 0) {
             $output->writeln('Invalid commission');
             return;
         }
+
         $entityManager->getConnection()->beginTransaction();
         try {
             $reception->setTypeRemb(Receptions::REPAYMENT_TYPE_RECOVERY)
@@ -90,8 +105,6 @@ EOF
                       ->setIdClient($client)
                       ->setIdProject($project)
                       ->setAssignmentDate(new \DateTime());
-            $borrower  = $walletRepo->getWalletByType($client->getIdClient(), WalletType::BORROWER);
-            $collector = $walletRepo->getWalletByType($clientCollector->getIdClient(), WalletType::DEBT_COLLECTOR);
             $this->getContainer()->get('unilend.service.operation_manager')->provisionCollection($collector, $borrower, $reception, $commission);
 
             $entityManager->flush();
@@ -106,15 +119,40 @@ EOF
     {
         $entityManager    = $this->getContainer()->get('doctrine.orm.entity_manager');
         $operationManager = $this->getContainer()->get('unilend.service.operation_manager');
-        $projectRepo      = $entityManager->getRepository('UnilendCoreBusinessBundle:Projects');
         $walletRepo       = $entityManager->getRepository('UnilendCoreBusinessBundle:Wallet');
-        $clientRepo       = $entityManager->getRepository('UnilendCoreBusinessBundle:Clients');
         $projectId        = $input->getOption('project-id');
         $commission       = filter_var($input->getOption('commission'), FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $project          = $projectRepo->find($projectId);
 
+        $project = $entityManager->getRepository('UnilendCoreBusinessBundle:Projects')->find($projectId);
         if (null === $project) {
             $output->writeln('Project id: ' . $projectId . ' not found.');
+            return;
+        }
+        $borrower = $walletRepo->getWalletByType($project->getIdCompany()->getIdClientOwner(), WalletType::BORROWER);
+        if (null === $borrower) {
+            $output->writeln('Borrower with client id : ' . $project->getIdCompany()->getIdClientOwner() . ' not found.');
+            return;
+        }
+        $clientCollector = $this->getCollector($input, $output);
+        if (null === $clientCollector) {
+            $output->writeln('Collector not found.');
+            return;
+        }
+        $collector = $walletRepo->getWalletByType($clientCollector->getIdClient(), WalletType::DEBT_COLLECTOR);
+        if (null === $collector) {
+            $output->writeln('Collector\'s wallet not found.');
+            return;
+        }
+
+        $protectedDir = $this->getContainer()->getParameter('path.protected');
+        //Encode: UTF-8, new line : LF
+        $fileName = $protectedDir . 'import/' . 'recouvrement.csv';
+        if (false === file_exists($fileName)) {
+            $output->writeln($protectedDir . 'import/' . 'recouvrement.csv not found');
+            return;
+        }
+        if (false === ($rHandle = fopen($fileName, 'r'))) {
+            $output->writeln($protectedDir . 'import/' . 'recouvrement.csv cannot be opened');
             return;
         }
 
@@ -124,18 +162,6 @@ EOF
         $dateOfChange = new \DateTime(Projects::DEBT_COLLECTION_CONDITION_CHANGEMENT_DATE);
         $dateOfChange->setTime(0, 0, 0);
 
-        $clientCollector = $clientRepo->findOneBy(['hash' => '2f9f590e-d689-11e6-b3d7-005056a378e2']);
-        $borrower        = $walletRepo->getWalletByType($project->getIdCompany()->getIdClientOwner(), WalletType::BORROWER);
-        $collector       = $walletRepo->getWalletByType($clientCollector->getIdClient(), WalletType::DEBT_COLLECTOR);
-
-        if (null === $clientCollector) {
-            $output->writeln('Borrower with client id : ' . $project->getIdCompany()->getIdClientOwner() . ' not found.');
-            return;
-        }
-        if (null === $collector) {
-            $output->writeln('Collector with client hash : 2f9f590e-d689-11e6-b3d7-005056a378e2 not found.');
-            return;
-        }
         $entityManager->getConnection()->beginTransaction();
         try {
             if ($fundReleaseDate >= $dateOfChange) {
@@ -144,16 +170,6 @@ EOF
                 }
                 $operationManager->payCollectionCommissionByBorrower($borrower, $collector, $commission, $project);
             }
-
-            //Encode: UTF-8, new line : LF
-            $fileName = $this->getContainer()->getParameter('path.protected') . 'import/' . 'recouvrement.csv';
-            if (false === file_exists($fileName)) {
-                throw new \Exception($this->getContainer()->getParameter('path.protected') . 'import/' . 'recouvrement.csv not found');
-            }
-            if (false === ($rHandle = fopen($fileName, 'r'))) {
-                throw new \Exception($this->getContainer()->getParameter('path.protected') . 'import/' . 'recouvrement.csv cannot be opened');
-            }
-
             while (($aRow = fgetcsv($rHandle, 0, ';')) !== false) {
                 $clientId = $aRow[0];
                 $amount   = str_replace(',', '.', $aRow[1]);
@@ -169,11 +185,37 @@ EOF
                     }
                 }
             }
-            fclose($rHandle);
             $entityManager->getConnection()->commit();
         } catch (\Exception $e) {
             $entityManager->getConnection()->rollBack();
             $output->writeln('Transaction rollbacked. Error : ' . $e->getMessage());
         }
+        fclose($rHandle);
+    }
+
+    /**
+     * We support only MCS or Progéris as debt collector
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return null|Clients
+     */
+    private function getCollector(InputInterface $input, OutputInterface $output)
+    {
+        $clientRepo    = $this->getContainer()->get('doctrine.orm.entity_manager')->getRepository('UnilendCoreBusinessBundle:Clients');
+        $collectorName = $input->getOption('collector');
+        switch (strtoupper($collectorName)) {
+            case 'MCS':
+                $clientHash = self::CLIENT_HASH_MCS;
+                break;
+            case 'PROGERIS':
+                $clientHash = self::CLIENT_HASH_PROGERIS;
+                break;
+            default:
+                $output->writeln('Debt collector : ' . $collectorName . 'is not supported');
+                return null;
+        }
+        return $clientRepo->findOneBy(['hash' => $clientHash]);
     }
 }
