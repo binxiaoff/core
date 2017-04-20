3dbc7e023f14415c73e55eec80fad59363820a68
Anna Breyer <anna.breyer@unilend.fr>
2017-04-20 11:58:26 +0200

    correct merge error

--- src/Unilend/Bundle/CoreBusinessBundle/Repository/OperationRepository.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Repository/OperationRepository.php
@@ -7,7 +7,6 @@ use Doctrine\DBAL\Cache\QueryCacheProfile;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
-use Unilend\Bridge\Doctrine\DBAL\Connection;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Operation;
 use Unilend\Bundle\CoreBusinessBundle\Entity\OperationType;
 use Unilend\Bundle\CoreBusinessBundle\Entity\PaysV2;
