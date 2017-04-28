61f787b150bf1bf43d81ec537f50b174788b4bb4
Antoine Préveaux <antoine.preveaux@unilend.fr>
2017-04-28 15:50:42 +0200

    DEV-1305 Dynamize partner project request

--- src/Unilend/Bundle/FrontBundle/Controller/PartnerAccountController.php
+++ src/Unilend/Bundle/FrontBundle/Controller/PartnerAccountController.php
@@ -9,11 +9,17 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Unilend\Bundle\CoreBusinessBundle\Entity\Clients;
+use Unilend\Bundle\CoreBusinessBundle\Entity\ClientsAdresses;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Companies;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Projects;
 use Unilend\Bundle\CoreBusinessBundle\Entity\ProjectsComments;
 use Unilend\Bundle\CoreBusinessBundle\Entity\TemporaryLinksLogin;
+use Unilend\Bundle\CoreBusinessBundle\Entity\Users;
+use Unilend\Bundle\CoreBusinessBundle\Entity\WalletType;
 use Unilend\Bundle\FrontBundle\Security\User\UserPartner;
+use Unilend\Bundle\FrontBundle\Service\DataLayerCollector;
+use Unilend\Bundle\FrontBundle\Service\SourceManager;
 use Unilend\core\Loader;
 
 class PartnerAccountController extends Controller
@@ -23,11 +29,26 @@ class PartnerAccountController extends Controller
      * @Method("GET")
      * @Security("has_role('ROLE_PARTNER')")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function projectRequestAction()
+    public function projectRequestAction(Request $request)
     {
-        return $this->render('/partner_account/project_request.html.twig');
+        $projectManager  = $this->get('unilend.service.project_manager');
+        $borrowingMotive = $this->get('doctrine.orm.entity_manager')->getRepository('UnilendCoreBusinessBundle:BorrowingMotive');
+
+        $template = [
+            'loanPeriods'      => $projectManager->getPossibleProjectPeriods(),
+            'projectAmountMin' => $projectManager->getMinProjectAmount(),
+            'projectAmountMax' => $projectManager->getMaxProjectAmount(),
+            'borrowingMotives' => $borrowingMotive->findBy([], ['rank' => 'ASC']),
+            'form'             => $request->getSession()->get('partnerProjectRequest', [])
+        ];
+
+        $request->getSession()->remove('partnerProjectRequest');
+
+        return $this->render('/partner_account/project_request.html.twig', $template);
     }
 
     /**
@@ -35,11 +56,185 @@ class PartnerAccountController extends Controller
      * @Method("POST")
      * @Security("has_role('ROLE_PARTNER')")
      *
+     * @param Request $request
+     *
      * @return RedirectResponse
      */
-    public function projectRequestFormAction()
+    public function projectRequestFormAction(Request $request)
     {
-        // Add the project to Vos Prospects with status simulation ( if the prospect is eligible )
+        $translator     = $this->get('translator');
+        $projectManager = $this->get('unilend.service.project_manager');
+        $entityManager  = $this->get('doctrine.orm.entity_manager');
+
+        $amount   = null;
+        $motive   = null;
+        $duration = null;
+        $siren    = null;
+        $email    = null;
+
+        try {
+            if (empty($request->request->get('esim')) || false === is_array($request->request->get('esim'))) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            $formData = $request->request->get('esim');
+
+            if (empty($formData['amount']) || empty($formData['motive']) || empty($formData['duration']) || empty($formData['siren']) || empty($formData['email'])) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            // Amount
+            $minimumAmount = $projectManager->getMinProjectAmount();
+            $maximumAmount = $projectManager->getMaxProjectAmount();
+            $amount        = str_replace([' ', '€'], '', $formData['amount']);
+
+            if (false === filter_var($amount, FILTER_VALIDATE_INT)) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            if ($amount < $minimumAmount || $amount > $maximumAmount) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_amount-value-error'));
+            }
+
+            // Motive
+            if (false === filter_var($formData['motive'], FILTER_VALIDATE_INT)) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            $motive = $formData['motive'];
+
+            // Duration
+            if (false === filter_var($formData['duration'], FILTER_VALIDATE_INT)) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            $duration = $formData['duration'];
+
+            // SIREN
+            $siren       = str_replace(' ', '', $formData['siren']);
+            $sirenLength = strlen($siren);
+
+            if (
+                1 !== preg_match('/^[0-9]*$/', $siren)
+                || false === in_array($sirenLength, [9, 14]) // SIRET numbers also allowed
+            ) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            $siren = substr($siren, 0, 9);
+
+            // Email
+            if (false === filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) {
+                throw new \InvalidArgumentException($translator->trans('partner-project-request_required-fields-error'));
+            }
+
+            $email = $formData['email'];
+        } catch (\InvalidArgumentException $exception) {
+            $this->addFlash('partnerProjectRequestErrors', $exception->getMessage());
+
+            $request->getSession()->set('partnerProjectRequest', [
+                'values' => [
+                    'amount'   => $amount,
+                    'siren'    => $siren,
+                    'email'    => $email,
+                    'motive'   => $motive,
+                    'duration' => $duration
+                ]
+            ]);
+
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+        $sourceManager = $this->get('unilend.frontbundle.service.source_manager');
+
+        if ($entityManager->getRepository('UnilendCoreBusinessBundle:Clients')->existEmail($email)) {
+            $email .= '-' . time();
+        }
+
+        $client = new Clients();
+        $client
+            ->setEmail($email)
+            ->setIdLangue('fr')
+            ->setStatus(Clients::STATUS_ONLINE)
+            ->setSource($sourceManager->getSource(SourceManager::SOURCE1))
+            ->setSource2($sourceManager->getSource(SourceManager::SOURCE2))
+            ->setSource3($sourceManager->getSource(SourceManager::SOURCE3))
+            ->setSlugOrigine($sourceManager->getSource(SourceManager::ENTRY_SLUG));
+
+        $siret   = $sirenLength === 14 ? str_replace(' ', '', $formData['siren']) : '';
+        $company = new Companies();
+        $company->setSiren($siren)
+            ->setSiret($siret)
+            ->setStatusAdresseCorrespondance(1)
+            ->setEmailDirigeant($email)
+            ->setEmailFacture($email);
+
+        $entityManager->beginTransaction();
+        try {
+            $entityManager->persist($client);
+
+            $clientAddress = new ClientsAdresses();
+            $clientAddress->setIdClient($client);
+
+            $entityManager->persist($clientAddress);
+            $entityManager->flush($clientAddress);
+
+            $company->setIdClientOwner($client->getIdClient());
+
+            $entityManager->persist($company);
+            $entityManager->flush($company);
+
+            $this->get('unilend.service.wallet_creation_manager')->createWallet($client, WalletType::PARTNER);
+
+            $entityManager->commit();
+        } catch (\Exception $exception) {
+            $entityManager->getConnection()->rollBack();
+            $this->get('logger')->error('An error occurred while creating client: ' . $exception->getMessage(), [['class' => __CLASS__, 'function' => __FUNCTION__]]);
+        }
+
+        if (empty($client->getIdClient())) {
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+//        $request->getSession()->set(DataLayerCollector::SESSION_KEY_CLIENT_EMAIL, $client->getEmail());
+//        $request->getSession()->set(DataLayerCollector::SESSION_KEY_BORROWER_CLIENT_ID, $client->getIdClient());
+
+        /** @var UserPartner $user */
+        $user        = $this->getUser();
+        $rootCompany = $user->getCompany();
+
+        while ($rootCompany->getIdParentCompany()) {
+            $rootCompany = $rootCompany->getIdParentCompany();
+        }
+
+        $project                                       = $this->get('unilend.service.entity_manager')->getRepository('projects');
+        $project->id_company                           = $company->getIdCompany();
+        $project->amount                               = $amount;
+        $project->period                               = $duration;
+        $project->id_borrowing_motive                  = $motive;
+        $project->ca_declara_client                    = 0;
+        $project->resultat_exploitation_declara_client = 0;
+        $project->fonds_propres_declara_client         = 0;
+        $project->status                               = \projects_status::SIMULATION;
+        $project->id_partner                           = $entityManager->getRepository('UnilendCoreBusinessBundle:Partner')->findOneBy(['idCompany' => $rootCompany->getIdCompany()])->getId();
+        $project->commission_rate_funds                = \projects::DEFAULT_COMMISSION_RATE_FUNDS;
+        $project->commission_rate_repayment            = \projects::DEFAULT_COMMISSION_RATE_REPAYMENT;
+        $project->id_company_submitter                 = $user->getCompany()->getIdCompany();
+        $project->id_client_submitter                  = $user->getClientId();
+        $project->create();
+
+        $projectManager->addProjectStatus(Users::USER_ID_FRONT, \projects_status::SIMULATION, $project);
+
+        $request->getSession()->set('partnerProjectRequest', [
+            'values' => [
+                'amount'   => $amount,
+                'siren'    => $siren,
+                'email'    => $email,
+                'motive'   => $motive,
+                'duration' => $duration
+            ]
+        ]);
+
         return $this->redirectToRoute('partner_project_request_eligibility_test');
     }
 
@@ -316,7 +511,7 @@ class PartnerAccountController extends Controller
      *
      * @return array
      */
-    private function formatNotes(array $notes)
+    private function formatNotes($notes)
     {
         $display = [];
 
