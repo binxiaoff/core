61bf7eef457bd346124c5bfacb4bb15b1e674246
Dimitar Slaev <dimitar.slaev@unilend.fr>
2017-05-03 10:34:25 +0200

    DEV-1228 - Revise Vos Emprunteurs

--- app/Resources/assets/js/app/controllers/PartnerDashboard.js
+++ app/Resources/assets/js/app/controllers/PartnerDashboard.js
@@ -9,86 +9,25 @@ var Utility = require('Utility')
 // Partner Prospects Table
 $doc
     .on(Utility.clickEvent, '.table-prospects [data-action]', function() {
-        // TODO - ADD AJAX URL FOR DELETING A PROSPECT
         var $prospect = $(this).closest('tr')
         var action = $(this).data('action')
         var $modal = $('#modal-partner-prospect-' + action)
 
-        // Add prospect id for further actions (abandon / submit)
-        $modal.data('prospect-id', $prospect.attr('id'))
+        // Fill in the hidden fields
+        if (action === 'submit') {
+            var company = $prospect.data('sortable-borrower')
+            var amount = $prospect.data('sortable-amount')
+            var duration = $prospect.data('sortable-duration')
+            var motif = $prospect.data('sortable-motif')
+
+            var $form = $modal.find('form')
+            $form.find('[name="simulator[company]"]').val(company)
+            $form.find('[name="simulator[amount]"]').val(amount)
+            $form.find('[name="simulator[duration]"]').val(duration)
+            $form.find('[name="simulator[motif]"]').val(motif)
+        }
+
         // Insert the company name inside the modal text and Show the popup
         $modal.find('.ui-modal-output-company').html($prospect.data('sortable-borrower'))
         $modal.uiModal('open')
-    })
-    .on(Utility.clickEvent, '#modal-partner-prospect-submit [data-modal-doactionsubmit]', function() {
-        var $modal = $('#modal-partner-prospect-submit')
-        var $prospect = $('#' + $modal.data('prospect-id'))
-        var $form = $('#submit-partner-prospect')
-        var siren = $prospect.data('sortable-siren')
-        var company = $prospect.data('sortable-borrower')
-        var amount = $prospect.data('sortable-amount')
-        var duration = $prospect.data('sortable-duration')
-        var motif = $prospect.data('sortable-motif')
-
-        // @Debug data
-        console.log('siren: ' + siren + ' | company: ' + company + ' | amount: ' + amount + ' | duration: ' + duration + ' | motif: ' + motif)
-
-        $form.find('[name="esim[siren]"]').val(siren)
-        $form.find('[name="esim[company]"]').val(company)
-        $form.find('[name="esim[amount]"]').val(amount)
-        $form.find('[name="esim[duration]"]').val(duration)
-        $form.find('[name="esim[motif]"]').val(motif)
-        $form.submit();
-
-        $modal.uiModal('close')
-
-        console.log('Submit prospect ' + $prospect.attr('id'))
-    })
-    .on(Utility.clickEvent, '#modal-partner-prospect-cancel [data-modal-doactionsubmit]', function() {
-        var $modal = $(this).closest('[data-modal]')
-        var $prospect = $('#' + $modal.data('prospect-id'))
-
-        var $select = $modal.find('#prospect-cancel-motif')
-        if ($select.val() !== '0') {
-            // TODO - Remove lines below and Uncomment Ajax
-
-            $modal.uiModal('close')
-            $prospect.remove()
-            if (!$('.table-prospects-item').length) {
-                $('#partner-prospects-panel .table-scroll').remove()
-                $('#partner-prospects-panel .message-info').show()
-            }
-
-            // var formData = {
-            //   prospectId : $prospect.attr('id'),
-            //   motif : $select.val()
-            // }
-            // // console.log(formData)
-            // $.ajax({
-            //   type: 'POST',
-            //   url: '',
-            //   data: formData,
-            //   success: function(response) {
-            //     if (response.text === 'OK') {
-            //       $modal.uiModal('close')
-            //       $prospect.remove()
-            //       if (!$('.table-prospects-item').length) {
-            //         $('#partner-prospects-panel .table-scroll').remove()
-            //         $('#partner-prospects-panel .message-info').show()
-            //       }
-            //     }
-            //   },
-            //   error: function() {
-            //     console.log("error retrieving data");
-            //   }
-            // })
-            // TODO END
-        } else {
-            $select.parent().addClass('ui-formvalidation-error')
-            $select.change(function(){
-                if ($(this).val() !== 0) {
-                    $(this).parent().removeClass('ui-formvalidation-error')
-                }
-            })
-        }
     })
