95438a0dec711049b1757d4c24783fac6741eaa4
Dimitar Slaev <dimitar.slaev@unilend.fr>
2017-05-09 10:37:13 +0200

    DEV-1340 - Improve UI for sending project notifications

--- apps/admin/views/preteurs/notifications.php
+++ apps/admin/views/preteurs/notifications.php
@@ -1,12 +1,51 @@
+<style>
+    form {
+        margin-bottom: 30px;
+    }
+    form label {
+        display: block;
+        margin-bottom: 5px;
+    }
+    .table-clickable tr td {
+        cursor: pointer;
+    }
+    .table-hover tr:hover > td {
+        background: #cecece;
+    }
+    #add_notification_form {
+        display: none;
+    }
+    .send_project {
+        display: inline-block;
+        font-size: 12px;
+        margin-left: 104px;
+        color: #75757d;
+    }
+</style>
 <script>
     $(function () {
-        displayNotificationForm = function (projectId) {
-            $('#selectedProjectId').val(projectId)
-            $('#add_notifiaction').show();
-        }
 
-        $('#addNotification').click(function (event) {
-            event.preventDefault();
+        $("#notificationDate").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: '<?=(date('Y') - 1)?>:<?=(date('Y') + 16)?>'
+        });
+        $("#notificationDate").datepicker('setDate', new Date());
+
+        $('#project_list table tr').on('click', function () {
+            var projectName = $(this).data('project-name')
+            var projectId = $(this).data('project-id')
+            var form = $('#add_notification_form')
+            $('#selectedProjectId').val(projectId)
+            form.find('.send_project .name').text(projectName)
+            form.find('.send_project .id').text(projectId)
+            form.show();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 450);
+        });
+        $('#addNotification').on('click', function (e) {
+            e.preventDefault();
             var form = $('#add_notification_form');
             $.ajax({
                 method: form.attr('method'),
@@ -27,24 +66,63 @@
 </script>
 <div id="contenu">
     <div id="search_project">
-        <h2>Choisir un projet</h2>
-        <form method="post" name="project_form" id="project_form" enctype="multipart/form-data" action="<?= $this->lurl ?>/preteurs/notifications" target="_parent">
-            <fieldset name="project">
-                <p><label for="projectId">ID projet</label></p>
-                <input type="text" name="projectId" id="projectId" class="input_large">
-                <br>
-                <p><label for="projectTitle">Raison sociale</label></p>
-                <input type="text" name="projectTitle" id="projectTitle" class="input_large">
-            </fieldset>
-            <br>
-            <fieldset>
-                <input type="submit" title="valider" value="Valider" id="searchProject" name="searchProject" class="btn">
-            </fieldset>
-        </form>
+        <table style="width: 100%">
+            <tr>
+                <td style="width: 33%">
+                    <h2>Choisir un projet</h2>
+                    <br>
+                    <form method="post" name="project_form" id="project_form" enctype="multipart/form-data" action="<?= $this->lurl ?>/preteurs/notifications" target="_parent">
+                        <fieldset name="project">
+                            <p>
+                                <label for="projectId">ID projet</label>
+                                <input type="text" name="projectId" id="projectId" class="input_large">
+                            </p>
+                            <p>
+                                <label for="projectTitle">Raison sociale</label>
+                                <input type="text" name="projectTitle" id="projectTitle" class="input_large">
+                            </p>
+                        </fieldset>
+                        <fieldset>
+                            <input type="submit" title="valider" value="Valider" id="searchProject" name="searchProject" class="btn">
+                        </fieldset>
+                    </form>
+                </td>
+                <td style="width: 66%">
+                    <form method="post" name="add_notification_form" id="add_notification_form" enctype="multipart/form-data" action="<?= $this->lurl ?>/preteurs/addNotification" target="_parent">
+                        <h2>Détails de la notification <div class="send_project"><span class="name"></span>  <span class="id"></span></div></h2>
+                        <br>
+                        <table style="width: 100%">
+                            <tr>
+                                <td style="width: 33%" class="add_notification">
+                                    <p>
+                                        <label for="notificationDate">Date de la notification</label>
+                                        <input type="text" id="notificationDate" class="input_large" name="notificationDate" value="">
+                                    </p>
+                                    <p>
+                                        <label for="notificationSubject">Sujet</label>
+                                        <input type="text" id="notificationSubject" class="input_large" name="notificationSubject" value="">
+                                    </p>
+                                </td>
+                                <td style="width: 63%; padding-left: 3%; padding-right: 10px">
+                                    <p>
+                                        <label for="notificationContent">Contenu de la notification</label>
+                                        <textarea id="notificationContent" name="notificationContent" class="input_large" style="height: 87px; width: 100%;"></textarea>
+                                        <input type="hidden" id="selectedProjectId" name="selectedProjectId" value="">
+                                    </p>
+                                    <div>
+                                        <div id="add_notification_result" style="float: left;"></div>
+                                        <input type="submit" title="envoyer" value="Envoyer" id="addNotification" name="addNotification" class="btn" style="float: right;">
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                 </td>
+            </tr>
+        </table>
         <?php if (false === empty($this->projectList)) : ?>
-            <br>
             <div id="project_list">
-                <table class="tablesorter">
+                <table class="tablesorter table-clickable table-hover">
                     <thead>
                     <tr>
                         <th class="header">ID</th>
@@ -55,7 +133,7 @@
                     <tbody>
                     <?php $i = 1; ?>
                     <?php foreach ($this->projectList as $project) : ?>
-                        <tr <?= ($i % 2 === 1 ? '' : ' class="odd"') ?> onclick="displayNotificationForm(<?= $project['id_project'] ?>)">
+                        <tr <?= ($i % 2 === 1 ? '' : ' class="odd"') ?> data-project-id="<?= $project['id_project'] ?>"  data-project-name="<?= $project['name'] ?>">
                             <td><?= $project['id_project'] ?></td>
                             <td><?= $project['name'] ?></td>
                             <td><?= $project['label'] ?></td>
@@ -65,30 +143,5 @@
                 </table>
             </div>
         <? endif; ?>
-    </div>
-    <br>
-    <div id="add_notifiaction" style="display: none">
-        <h2>Détails de la notification</h2>
-        <form method="post" name="add_notification_form" id="add_notification_form" enctype="multipart/form-data" action="<?= $this->lurl ?>/preteurs/addNotification" target="_parent">
-            <fieldset name="notifications">
-                <p><label for="notificationDate">Date de la notification</label></p>
-                <input type="text" id="notificationDate" class="input_large" name="notificationDate" value="">
-                <br/>
-                <p><label for="notificationSubject">Sujet</label></p>
-                <input type="text" id="notificationSubject" class="input_large" name="notificationSubject" value="">
-                <br>
-                <p><label for="notificationContent">Contenu de la notification</label></p>
-                <textarea id="notificationContent" name="notificationContent"></textarea>
-
-                <input type="hidden" id="selectedProjectId" name="selectedProjectId" value="">
-            </fieldset>
-            <fieldset>
-                <input type="submit" title="valider" value="Valider" id="addNotification" name="addNotification" class="btn">
-            </fieldset>
-        </form>
-    </div>
-    <br>
-    <div id="add_notification_result">
-
     </div>
 </div>
