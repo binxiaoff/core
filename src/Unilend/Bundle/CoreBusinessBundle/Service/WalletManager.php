2858f9e3254bd0758178bde37d26b796acecd008
Bin XIAO <bin.xiao@unilend.fr>
2017-05-15 17:42:27 +0200

    DEV-1145 Legacy transaction cleaning including wallet line, bank line and bank_unilend, tax

--- src/Unilend/Bundle/CoreBusinessBundle/Service/WalletManager.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Service/WalletManager.php
@@ -68,7 +68,6 @@ class WalletManager
     {
         $this->entityManager->getConnection()->beginTransaction();
         try {
-            $this->legacyCommitBalance($wallet->getIdClient()->getIdClient(), $amount, $bid);
             if (-1 === bccomp($wallet->getAvailableBalance(), $amount)) {
                 //throw new \DomainException('The available balance for wallet id : ' . $wallet->getId() . ' must not be lower than zero');
             }
@@ -78,6 +77,7 @@ class WalletManager
             $wallet->setAvailableBalance($availableBalance);
             $wallet->setCommittedBalance($committedBalance);
             $this->entityManager->flush($wallet);
+
             $this->snap($wallet, $bid);
             $this->entityManager->getConnection()->commit();
         } catch (\Exception $e) {
@@ -91,18 +91,12 @@ class WalletManager
      * @param        $amount
      * @param        $origin
      *
-     * @return null|\transactions
      * @throws \Exception
      */
     public function releaseBalance(Wallet $wallet, $amount, $origin)
     {
         $this->entityManager->getConnection()->beginTransaction();
         try {
-            $transaction = null;
-            if ($origin instanceof Bids) {
-                $transaction = $this->legacyReleaseBalance($wallet->getIdClient()->getIdClient(), $amount, $origin);
-            }
-
             if (-1 === bccomp($wallet->getCommittedBalance(), $amount)) {
                 //throw new \DomainException('The committed balance for wallet id : ' . $wallet->getId() . ' must not be lower than zero');
             }
@@ -112,10 +106,9 @@ class WalletManager
             $wallet->setAvailableBalance($availableBalance);
             $wallet->setCommittedBalance($committedBalance);
             $this->entityManager->flush($wallet);
+
             $this->snap($wallet, $origin);
             $this->entityManager->getConnection()->commit();
-
-            return $transaction; //compatibility legacy
         } catch (\Exception $e) {
             $this->entityManager->getConnection()->rollBack();
             throw $e;
@@ -123,84 +116,8 @@ class WalletManager
     }
 
     /**
-     * @param      $clientId
-     * @param      $amount
-     * @param Bids $bid
-     */
-    private function legacyCommitBalance($clientId, $amount, Bids $bid)
-    {
-        /** @var \transactions $transaction */
-        $transaction = $this->legacyEntityManager->getRepository('transactions');
-        /** @var \wallets_lines $walletLine */
-        $walletLine = $this->legacyEntityManager->getRepository('wallets_lines');
-
-        $amountInCent = bcmul($amount, 100);
-
-        $transaction->id_client        = $clientId;
-        $transaction->montant          = -$amountInCent;
-        $transaction->id_langue        = 'fr';
-        $transaction->date_transaction = date('Y-m-d H:i:s');
-        $transaction->status           = \transactions::STATUS_VALID;
-        $transaction->id_project       = $bid->getProject()->getIdProject();
-        $transaction->type_transaction = \transactions_types::TYPE_LENDER_LOAN;
-        $transaction->ip_client        = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-        $transaction->create();
-
-        $walletLine->id_lender                = $bid->getIdLenderAccount()->getId();
-        $walletLine->type_financial_operation = \wallets_lines::TYPE_BID;
-        $walletLine->id_transaction           = $transaction->id_transaction;
-        $walletLine->status                   = \wallets_lines::STATUS_VALID;
-        $walletLine->type                     = \wallets_lines::VIRTUAL;
-        $walletLine->amount                   = -$amountInCent;
-        $walletLine->id_project               = $bid->getProject()->getIdProject();
-        $walletLine->create();
-
-        $bid->setIdLenderWalletLine($walletLine->id_wallet_line);
-    }
-
-    /**
-     * @param      $clientId
-     * @param      $amount
-     * @param Bids $bid
-     *
-     * @return \transactions
-     */
-    private function legacyReleaseBalance($clientId, $amount, Bids $bid)
-    {
-        /** @var \transactions $transaction */
-        $transaction = $this->legacyEntityManager->getRepository('transactions');
-        /** @var \wallets_lines $walletLine */
-        $walletLine = $this->legacyEntityManager->getRepository('wallets_lines');
-
-        $amountInCent = bcmul($amount, 100);
-
-        $transaction->id_client        = $clientId;
-        $transaction->montant          = $amountInCent;
-        $transaction->id_langue        = 'fr';
-        $transaction->date_transaction = date('Y-m-d H:i:s');
-        $transaction->status           = \transactions::STATUS_VALID;
-        $transaction->id_project       = $bid->getProject()->getIdProject();
-        $transaction->ip_client        = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-        $transaction->id_bid_remb      = $bid->getIdBid();
-        $transaction->type_transaction = \transactions_types::TYPE_LENDER_LOAN;
-        $transaction->create();
-
-        $walletLine->id_lender                = $bid->getIdLenderAccount()->getId();
-        $walletLine->type_financial_operation = \wallets_lines::TYPE_BID;
-        $walletLine->id_transaction           = $transaction->id_transaction;
-        $walletLine->status                   = \wallets_lines::STATUS_VALID;
-        $walletLine->type                     = \wallets_lines::VIRTUAL;
-        $walletLine->id_bid_remb              = $bid->getIdBid();
-        $walletLine->amount                   = $amountInCent;
-        $walletLine->id_project               = $bid->getProject()->getIdProject();
-        $walletLine->create();
-
-        return $transaction;
-    }
-
-    /**
-     * @param Operation   $operation
-     * @param Wallet $creditor
+     * @param Operation $operation
+     * @param Wallet    $creditor
      */
     private function credit(Operation $operation, Wallet $creditor)
     {
@@ -216,8 +133,8 @@ class WalletManager
     }
 
     /**
-     * @param Operation   $operation
-     * @param Wallet $debtor
+     * @param Operation $operation
+     * @param Wallet    $debtor
      */
     private function debit(Operation $operation, Wallet $debtor)
     {
@@ -250,8 +167,8 @@ class WalletManager
     {
         $walletSnap = new WalletBalanceHistory();
         $walletSnap->setIdWallet($wallet)
-                   ->setAvailableBalance($wallet->getAvailableBalance())
-                   ->setCommittedBalance($wallet->getCommittedBalance());
+            ->setAvailableBalance($wallet->getAvailableBalance())
+            ->setCommittedBalance($wallet->getCommittedBalance());
 
         if (false === is_array($parameters)) {
             $parameters = [$parameters];
