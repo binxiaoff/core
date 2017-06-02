5aed5871297007dcfe75a62d3d3c415bd252757a
Dimitar Slaev <dimitar.slaev@unilend.fr>
2017-06-02 11:05:05 +0200

    TMA-1857 - Fix a typo in admin nav

--- apps/admin/views/header.php
+++ apps/admin/views/header.php
@@ -126,7 +126,7 @@
                     <li><a href="<?= $this->lurl ?>/transferts/deblocage" title="Déblocage">Déblocage des fonds</a></li>
                     <li><a href="<?= $this->lurl ?>/transferts/succession" title="Succession">Succession (Transfert de solde et prêts)</a></li>
                     <li><a href="<?= $this->lurl ?>/client_atypical_operation" title="Lutte Anti-Balanchiment">Opérations atypiques</a></li>
-                    <li><a href="<?= $this->lurl ?>/transferts/virement_emprunteur" title="Transfert des fons">Transfert des fonds</a></li>
+                    <li><a href="<?= $this->lurl ?>/transferts/virement_emprunteur" title="Transfert des fonds">Transfert des fonds</a></li>
                 </ul>
             </li>
         <?php endif; ?>
