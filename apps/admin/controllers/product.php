3eb55fac7cdb7b81cbd0fa2cd3a1842d6acf8e5d
Bin XIAO <bin.xiao@unilend.fr>
2017-06-28 11:21:53 +0200

    Modify product attribute label.

--- apps/admin/controllers/product.php
+++ apps/admin/controllers/product.php
@@ -18,7 +18,7 @@ class productController extends bootstrap
     public function _default()
     {
         /** @var \product $product */
-        $product = $this->loadData('product');
+        $product           = $this->loadData('product');
         $this->productList = $product->select();
     }
 
@@ -41,14 +41,14 @@ class productController extends bootstrap
 
         // max / min duration
         /** @var \Unilend\Bundle\CoreBusinessBundle\Service\Product\ProductManager $productManager */
-        $productManager = $this->get('unilend.service_product.product_manager');
+        $productManager        = $this->get('unilend.service_product.product_manager');
         $this->duration['min'] = $productManager->getAttributesByType($this->product, ProductAttributeType::MIN_LOAN_DURATION_IN_MONTH);
         $this->duration['max'] = $productManager->getAttributesByType($this->product, ProductAttributeType::MAX_LOAN_DURATION_IN_MONTH);
 
         // motivation
         $borrowerMotives = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_BORROWING_MOTIVE);
         /** @var \borrowing_motive $motive */
-        $motive = $this->loadData('borrowing_motive');
+        $motive                = $this->loadData('borrowing_motive');
         $this->borrowerMotives = [];
         if (false === empty($borrowerMotives)) {
             foreach ($borrowerMotives as $borrowerMotive) {
@@ -57,19 +57,19 @@ class productController extends bootstrap
             }
         }
 
-        $this->creationDaysMin          = $productManager->getAttributesByType($this->product, ProductAttributeType::MIN_CREATION_DAYS);
-        $this->rcs                      = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_BORROWER_COMPANY_RCS);
-        $this->nafcodes                 = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_BORROWER_COMPANY_NAF_CODE);
-        $this->lenderId                 = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_CLIENT_ID);
-        $this->lenderType               = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_CLIENT_TYPE);
-        $this->checkExcludedLocation    = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_EXCLUDED_HEADQUARTERS_LOCATION);
-        $this->maxXerfiScore            = $productManager->getAttributesByType($this->product, ProductAttributeType::MAX_XERFI_SCORE);
+        $this->creationDaysMin       = $productManager->getAttributesByType($this->product, ProductAttributeType::MIN_CREATION_DAYS);
+        $this->rcs                   = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_BORROWER_COMPANY_RCS);
+        $this->nafcodes              = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_BORROWER_COMPANY_NAF_CODE);
+        $this->lenderId              = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_CLIENT_ID);
+        $this->lenderType            = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_CLIENT_TYPE);
+        $this->checkExcludedLocation = $productManager->getAttributesByType($this->product, ProductAttributeType::ELIGIBLE_EXCLUDED_HEADQUARTERS_LOCATION);
+        $this->maxXerfiScore         = $productManager->getAttributesByType($this->product, ProductAttributeType::MAX_XERFI_SCORE);
         $this->noBlendDays           = $productManager->getAttributesByType($this->product, ProductAttributeType::NO_IN_PROGRESS_BLEND_PROJECT_DAYS);
         $this->noIncidentBlendDays   = $productManager->getAttributesByType($this->product, ProductAttributeType::NO_INCIDENT_BLEND_PROJECT_DAYS);
         $this->noIncidentUnilendDays = $productManager->getAttributesByType($this->product, ProductAttributeType::NO_INCIDENT_UNILEND_PROJECT_DAYS);
-        $this->checkOwner               = $productManager->getAttributesByType($this->product, ProductAttributeType::VERIFICATION_REQUESTER_IS_ONE_OF_THE_DIRECTOR);
-        $this->minPreScore              = $productManager->getAttributesByType($this->product, ProductAttributeType::MIN_PRE_SCORE);
-        $this->maxPreScore              = $productManager->getAttributesByType($this->product, ProductAttributeType::MAX_PRE_SCORE);
+        $this->checkOwner            = $productManager->getAttributesByType($this->product, ProductAttributeType::VERIFICATION_REQUESTER_IS_ONE_OF_THE_DIRECTOR);
+        $this->minPreScore           = $productManager->getAttributesByType($this->product, ProductAttributeType::MIN_PRE_SCORE);
+        $this->maxPreScore           = $productManager->getAttributesByType($this->product, ProductAttributeType::MAX_PRE_SCORE);
     }
 
     public function _contract_details()
