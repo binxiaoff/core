e36685abcc178fd148cb73fb360df97ff007cb41
Dimitar Slaev <dimitar.slaev@unilend.fr>
2017-07-28 17:37:40 +0200

    TMA-2054 - Front modifications

--- apps/admin/views/stats/reporting_sfpmei.php
+++ apps/admin/views/stats/reporting_sfpmei.php
@@ -1,3 +1,25 @@
+<script>
+    $(function(){
+        $.tablesorter.addParser({
+            id: 'dataAttr',
+            is: function(s) {
+                return false;
+            },
+            format: function(s, table, cell) {
+                return $(cell).attr('data-sort-value');
+            },
+            type: 'numeric'
+        });
+
+        $('.tablesorter').tablesorter({
+            headers : {
+                0 : { sorter: 'dataAttr' },
+                2 : { sorter: false }
+            }
+        });
+    })
+</script>
+
 <div id="contenu">
     <h1>Reporting Mensuel SFPMEI</h1>
     <?php if (0 < count($this->reportingList)) : ?>
@@ -12,7 +34,7 @@
             <tbody>
             <?php foreach ($this->reportingList as $year => $reportings) : ?>
                 <?php foreach ($reportings as $index => $reporting) : ?>
-                    <tr <?= ($index % 2 == 1 ? '' : ' class="odd"') ?>>
+                    <tr>
                         <td><?= $year ?></td>
                         <td><?= $reporting['month'] ?></td>
                         <td><a title="Télécharger" href="<?= $reporting['link'] ?>"><?= $reporting['name'] ?></a></td>
@@ -23,34 +45,38 @@
         </table>
     <?php else : ?>
         Aucun fichier trouvé.
-        <table class="tablesorter">
+        <table class="table table-striped table-hover tablesorter">
             <thead>
             <tr>
-                <th>Annee</th>
-                <th>Mois</th>
-                <th>Lien de téléchargement</th>
+                <th style="width: 12%">Date</th >
+                <th colspan="2">Fichier</th>
             </tr>
             </thead>
             <tbody>
             <tr>
-                <td>2017</td>
-                <td>mars</td>
-                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx">reporting_mensuel_sfpmei_20170331.xlsx</a></td>
-            </tr>
-            <tr class="odd">
-                <td>2017</td>
-                <td>avril</td>
-                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170405.xlsx">reporting_mensuel_sfpmei_20170405.xlsx</a></td>
-            </tr>
-            <tr class="odd">
-                <td>2017</td>
-                <td>juin</td>
-                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_comp_20170630.xlsx">reporting_mensuel_sfpmei_comp_20170630.xlsx</a></td>
+                <td data-sort-value="201701">Janvier 2017</td>
+                <td>reporting_mensuel_sfpmei_20170231.xlsx</td>
+                <td class="text-right"><a href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx" class="btn-primary btn-sm">Télécharger</a></td>
             </tr>
             <tr>
-                <td>2017</td>
-                <td>juillet</td>
-                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170727.xlsx">reporting_mensuel_sfpmei_20170727.xlsx</a></td>
+                <td data-sort-value="201702">Fevrier 2017</td>
+                <td>reporting_mensuel_sfpmei_20170631.xlsx</td>
+                <td class="text-right"><a href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx" class="btn-primary btn-sm">Télécharger</a></td>
+            </tr>
+            <tr>
+                <td data-sort-value="201601">Janvier 2016</td>
+                <td>reporting_mensuel_sfpmei_20170331.xlsx</td>
+                <td class="text-right"><a href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx" class="btn-primary btn-sm">Télécharger</a></td>
+            </tr>
+            <tr>
+                <td data-sort-value="201602">Fevrier 2016</td>
+                <td>reporting_mensuel_sfpmei_20170431.xlsx</td>
+                <td class="text-right"><a href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx" class="btn-primary btn-sm">Télécharger</a></td>
+            </tr>
+            <tr>
+                <td data-sort-value="201604">Avril 2016</td>
+                <td>reporting_mensuel_sfpmei_20170331.xlsx</td>
+                <td class="text-right"><a href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx" class="btn-primary btn-sm">Télécharger</a></td>
             </tr>
             </tbody>
         </table>
