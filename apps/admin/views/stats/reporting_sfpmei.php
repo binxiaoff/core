10cc2790bab27a7d6a2d971b93af6f51e68b247b
Anna Breyer <anna.breyer@unilend.fr>
2017-07-28 15:04:43 +0200

    Add fake data in view

--- apps/admin/views/stats/reporting_sfpmei.php
+++ apps/admin/views/stats/reporting_sfpmei.php
@@ -23,5 +23,36 @@
         </table>
     <?php else : ?>
         Aucun fichier trouvé.
+        <table class="tablesorter">
+            <thead>
+            <tr>
+                <th>Annee</th>
+                <th>Mois</th>
+                <th>Lien de téléchargement</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>2017</td>
+                <td>mars</td>
+                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170331.xlsx">reporting_mensuel_sfpmei_20170331.xlsx</a></td>
+            </tr>
+            <tr class="odd">
+                <td>2017</td>
+                <td>avril</td>
+                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170405.xlsx">reporting_mensuel_sfpmei_20170405.xlsx</a></td>
+            </tr>
+            <tr class="odd">
+                <td>2017</td>
+                <td>juin</td>
+                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_comp_20170630.xlsx">reporting_mensuel_sfpmei_comp_20170630.xlsx</a></td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>juillet</td>
+                <td><a title="Télécharger" href="/stats/reporting_sfpmei/file/reporting_mensuel_sfpmei_20170727.xlsx">reporting_mensuel_sfpmei_20170727.xlsx</a></td>
+            </tr>
+            </tbody>
+        </table>
     <?php endif ?>
 </div>
