f941bb155039cc28c7f0eedb016601fe1d542b72
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2017-08-17 17:38:47 +0200

    DEV-1535 : Intégration de la page détails des impayés (recouvrements et retards)

---
+++ apps/admin/views/emprunteurs/details_impayes.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="block block-rounded">
+            <div class="block-header">
+                <div class="pull-left">
+                    <a href="/emprunteurs/projets_avec_retard" class="text-muted"><span class="fa fa-arrow-left"></span> Retards et recouvrements</a>
+                </div>
+                <div class="pull-right">
+                    <small>Siren: 429892899</small> <small>|</small> <small>ID Projet: 429892</small>
+                    <span class="label label-info push-10-l">Remboursement</span>
+                </div>
+            </div>
+            <div class="block-content block-content-full">
+                <h2 class="h3 push-30">
+                    Europe Metal Concept <br>
+                    <small class="font-w400">Ingenerie, Etudes, Techniques</small>
+                </h2>
+                <div class="row push-30">
+                    <div class="col-md-3">
+                        <div class="font-s12 font-w600 text-uppercase">Restant à recouvrer</div>
+                        <a class="h2 font-w300 text-primary">17 000 €</a>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="font-s12 font-w600 text-uppercase">Confié au recouvreur</div>
+                        <a class="h2 font-w300 text-primary">0%</a>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="font-s12 font-w600 text-uppercase">Réceptions à traiter</div>
+                        <a class="h2 font-w300 text-primary">5</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="retards" class="block block-rounded">
+            <?php $this->fireView('details_impayes/retards'); ?>
+        </div>
+        <div id="debt-colletion" class="block block-rounded">
+            <?php $this->fireView('details_impayes/recouvrements'); ?>
+        </div>
+        <div id="fees-honoraries" class="block block-rounded">
+            <?php $this->fireView('details_impayes/frais_honoraires') ?>
+        </div>
+        <div id="receptions" class="block block-rounded">
+            <?php $this->fireView('details_impayes/receptions') ?>
+        </div>
+    </div>
+</div>
---
+++ apps/admin/views/emprunteurs/details_impayes/frais_honoraires.php
@@ -0,0 +1,111 @@
+<div class="block-content block-content-full">
+    <h3 class="h4 push-20">Frais et Honoraires</h3>
+    <table class="table table-bordered table-hover table-header-bg">
+        <thead>
+        <tr>
+            <th style="width: 25%">
+                Date
+            </th>
+            <th style="width: 25%">
+                Type
+            </th>
+            <th style="width: 25%">
+                Mode de réglement
+            </th>
+            <th style="width: 25%">
+                Régularisé par réception
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>
+                14/04/2017
+            </td>
+            <td>
+                Honoraire
+            </td>
+            <td>
+                Retenu par recouvreur
+            </td>
+            <td>
+                N/A
+            </td>
+        </tr>
+        <tr>
+            <td>
+                14/08/2017
+            </td>
+            <td>
+                Frais visite
+            </td>
+            <td>
+                Avancé par Unilend
+            </td>
+            <td>
+                200 € - 14/07/2017
+            </td>
+        </tr>
+        <tr>
+            <td>
+                14/04/2017
+            </td>
+            <td>
+                Frais visite
+            </td>
+            <td>
+                Avancé par Unilend
+            </td>
+            <td>
+                Non régularisé
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="text-right">
+        <a role="button" class="btn btn-primary" data-toggle="modal" data-target="#additional-fees">Ajouter des frais</a>
+    </div>
+    <!-- Start MODALS -->
+    <div class="modal fade in" id="additional-fees">
+        <form class="modal-dialog js-validation" action="/recouvrements/details/429892" method="post">
+            <div class="modal-content">
+                <div class="block block-themed remove-margin-b">
+                    <div class="block-header bg-primary">
+                        <h5 class="block-title">Ajout de frais avancés par Unilend</h5>
+                    </div>
+                    <div class="block-content">
+                        <div class="form-group">
+                            <label>Type de frais</label>
+                            <select class="form-control required" name="fee-type">
+                                <option value="0">Selectionner</option>
+                                <option value="1">Clause pénale</option>
+                                <option value="2">Frais de visite domiciliare</option>
+                                <option value="3">Injonction de payer</option>
+                                <option value="4">Référé provision</option>
+                                <option value="5">Saisie conservatoire</option>
+                                <option value="6">Assignation au fond</option>
+                                <option value="7">Assignation en procédure collective</option>
+                                <option value="8">Procédure d'appel</option>
+                                <option value="9">Opposition sur vente de fonds de commerce</option>
+                                <option value="10">Frais de déclarations de créance</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Montant</label>
+                            <input class="form-control required" type="number" name="fee-amount">
+                        </div>
+                        <div class="form-group">
+                            <label>Date de facture</label>
+                            <input class="form-control js-datepicker required" type="text" name="fee-invoice-date" data-date-format="mm/dd/yy" placeholder="mm/dd/yy">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-sm btn-default" type="button" data-dismiss="modal">Annuler</button>
+                    <button class="btn btn-sm btn-primary" type="submit">Valider</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- End MODALS -->
+</div>
