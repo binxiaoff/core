bb10d06470c5a2e36635999a4b6a7de3c9f040d1
Bin XIAO <bin.xiao@unilend.fr>
2017-10-24 18:45:25 +0200

    TMA-1701 ELS 10M > Erreur MongoDB  - add MongoDB connection test

---
+++ src/Unilend/Bundle/CommandBundle/Command/DevMongoDBTestCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Unilend\Bundle\CommandBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DevMongoDBTestCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('unilend:dev_tools:mongodb:test')
+            ->setDescription('Mongodb connection test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $documentManager = $this->getContainer()->get('doctrine_mongodb')->getManager();
+        $connection      = $this->getContainer()->get('doctrine_mongodb')->getConnection();
+
+        $documentManager->getRepository('UnilendStoreBundle:WsCall')->findOneBy(['provider' => 'altares']);
+        $connection->close();
+    }
+}
