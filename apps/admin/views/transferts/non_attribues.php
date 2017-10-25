78e57bce50bb064d39052d6dfd3623d35e558130
Dimitar Slaev <dimitar.slaev@unilend.fr>
2017-10-25 18:17:32 +0200

    TMA-2291 - Pagination on /transferts/non_attribues

--- apps/admin/views/transferts/non_attribues.php
+++ apps/admin/views/transferts/non_attribues.php
@@ -15,66 +15,115 @@
     }
 </style>
 <script>
-  $(function () {
-    var dt = $('#receptions-table').DataTable({
-      serverSide: true,
-      processing: true,
-      columnDefs: [
-        {orderable: false, targets: [1, 4, 5]},
-        {searchable: false, targets: [1, 2, 3, 4, 5, 6]},
-        {visible: false, targets: [6]},
-        {name: "idReception", "targets": 0},
-        {name: "motif", "targets": 1},
-        {name: "montant", "targets": 2},
-        {name: "added", "targets": 3},
-        {name: "code", "targets": 4}
-      ],
-      order: [[0, "desc"]],
-      ajax: '/transferts/get_non_attribues',
-      language: {
-        url: '<?= $this->lurl ?>/oneui/js/plugins/datatables/localisation/fr_FR.json'
-      }
+    $(function () {
+        var dt = $('#receptions-table').DataTable({
+            serverSide: true,
+            processing: true,
+            columnDefs: [
+                {orderable: false, targets: [1, 4, 5]},
+                {searchable: false, targets: [1, 2, 3, 4, 5, 6]},
+                {visible: false, targets: [6]},
+                {name: "idReception", "targets": 0},
+                {name: "motif", "targets": 1},
+                {name: "montant", "targets": 2},
+                {name: "added", "targets": 3},
+                {name: "code", "targets": 4}
+            ],
+            order: [[0, "desc"]],
+            ajax: '/transferts/get_non_attribues',
+            language: {
+                url: '<?= $this->lurl ?>/oneui/js/plugins/datatables/localisation/fr_FR.json'
+            },
+            createdRow: function (row, data, index) {
+                console.log(data)
+                var $row = $(row)
+                var receptionId = data[0]
+                var comment = data[5]
+                var line = data[6]
+                var amount = data[2]
+                var negative = (amount.replace(',', '.').replace(/[^\d\-\.]/g, '') < 0) ? true : false
+
+                var attachReceptionBtn = '<a class="attach-reception thickbox" href="<?= $this->lurl ?>/transferts/attribution/'+ receptionId +'" title="Attribuer l\'operation">' +
+                    '<span class="fa fa-check"></span>' +
+                    '</a>'
+                var ignoreReceptionBtn = '<a class="ignore-reception table-action" data-reception-id="' + receptionId + '" title="Ignorer l\'operation">' +
+                    '<span class="fa fa-close"></span>' +
+                    '</a>'
+                var showReceptionBtn = '<a class="show-reception table-action" data-reception-id="' + receptionId + '" data-line="' + line + '" title="Afficher l\'opération">' +
+                    '<span class="fa fa-eye"></span>' +
+                    '</a>'
+                var addCommentBtn = '<a class="add-comment table-action" data-reception-id="' + receptionId + '" data-comment="' + comment + '" title="Commenter l\'opération">' +
+                    '<span class="fa fa-pencil"></span>' +
+                    '</a>'
+
+                if (negative) {
+                    $row.css('background', '#f9adb3')
+                }
+                if (comment !== null && comment !== '') {
+                    addCommentBtn = '<a class="add-comment modify-comment table-action" data-reception-id="' + receptionId + '" data-comment="' + comment + '" title="Modifier le commentaire">' +
+                        '<span class="fa fa-pencil-square"></span>' +
+                        '</a>'
+                    if (!negative)
+                        $row.css('background', '#fdeec6')
+                }
+                $row.attr('data-reception-id', receptionId)
+                $row.find('td:last-child').append(attachReceptionBtn + ignoreReceptionBtn + showReceptionBtn + addCommentBtn)
+                $row.find('.thickbox').colorbox()
+            }
+        })
+
+        $(document).on('click', '.table-action', function(){
+            var $modal
+            var receptionId = $(this).data('reception-id')
+
+            if ($(this).is('.ignore-reception')) {
+                $modal = $('#modal-ignore-reception')
+                $modal.find('[name=reception]').val(receptionId)
+            }
+
+            if ($(this).is('.add-comment') || $(this).is('.modify-comment')) {
+                $modal = $('#modal-add-modify-comment')
+                var comment = $(this).data('comment')
+                $modal.find('[name=comment]').html(comment)
+                $modal.find('[name=reception]').val(receptionId)
+                if ($(this).is('.modify-comment')) {
+                    $modal.find('h1').text('Modifier le commentaire')
+                } else {
+                    $modal.find('h1').text('Ajouter un commentaire')
+                }
+            }
+
+            if ($(this).is('.show-reception')) {
+                $modal = $('#modal-show-reception')
+                $modal.find('.line').html($(this).data('line'))
+            }
+
+            $.colorbox({html:$modal.html(), width: '50%'})
+        })
+
+        $(document).on('submit', '#modal-ignore-reception-form', function(e){
+            e.preventDefault()
+            var $form = $(this)
+            var $reception = $form.find('[name=reception]')
+            $.ajax({
+                url: $form.attr('action'),
+                method: $form.attr('method'),
+                data: $form.serialize(),
+                success: function (response) {
+                    $.colorbox.close()
+                    if (response === 'ok') {
+                        $('tr[data-reception-id=' + $reception.val() + ']').fadeOut()
+                    } else {
+                        alert('Une erreur est survenue')
+                    }
+                },
+                error: function () {
+                    $.colorbox.close()
+                    alert('Une erreur est survenue')
+                }
+            })
+        })
     })
-
-    $('.ignore-line-form').on('submit', function (event) {
-      event.preventDefault()
-
-      var $form = $(this)
-      var $reception = $form.find('[name=reception]')
-
-      $.ajax({
-        url: $form.attr('action'),
-        method: $form.attr('method'),
-        data: $form.serialize(),
-        success: function (response) {
-          $.colorbox.close()
-          if (response === 'ok') {
-            $('tr[data-reception-id=' + $reception.val() + ']').fadeOut()
-          } else {
-            alert('Une erreur est survenue')
-          }
-        },
-        error: function () {
-          $.colorbox.close()
-          alert('Une erreur est survenue')
-        }
-      })
-    })
-
-    $('.reception-line').tooltip({
-      position: {my: 'left top', at: 'right top'},
-      content: function () {
-        return $(this).prop('title')
-      }
-    })
-
-    $('#receptions-table > thead > tr > th').tooltip({
-      position: {my: 'top', at: 'bottom'}
-    })
-
-    $('.inline').tooltip({disabled: true})
-    $('.inline').colorbox({inline: true})
-  });
 </script>
 <div id="contenu">
     <h1>Opérations non affectées</h1>
@@ -86,7 +135,7 @@
             <th>Montant</th>
             <th>Date</th>
             <th title="Code interbancaire (Cfonb120)">Code</th>
-            <th>&nbsp;</th>
+            <th style="width: 120px">&nbsp;</th>
         </tr>
         </thead>
         <tfoot>
@@ -99,41 +148,43 @@
             <th>&nbsp;</th>
         </tr>
         </tfoot>
-
     </table>
     <div class="hidden">
-        <?php foreach ($this->nonAttributedReceptions as $reception) : ?>
-            <div id="content-line-<?= $reception->getIdReception() ?>" style="white-space: nowrap; padding: 10px;"><?= $reception->getLigne() ?></div>
-            <div id="comment-line-<?= $reception->getIdReception() ?>" style="padding: 10px; min-width: 500px;">
-                <form id="comment-line-form-<?= $reception->getIdReception() ?>" class="comment-line-form" method="POST" action="<?= $this->lurl ?>/transferts/comment">
-                    <input type="hidden" name="reception" value="<?= $reception->getIdReception() ?>">
-                    <input type="hidden" name="referer" value="<?= $_SERVER['REQUEST_URI'] ?>">
-                    <h1>Commenter une opération</h1>
-                    <div class="form-group">
-                        <label for="comment-line-comment-<?= $reception->getIdReception() ?>" class="sr-only">Commentaire</label>
-                        <textarea id="comment-line-comment-<?= $reception->getIdReception() ?>" name="comment" rows="5" class="form-control"><?= $reception->getComment() ?></textarea>
-                    </div>
-                    <div class="text-right">
-                        <button type="button" class="btn-default" onclick="$.fn.colorbox.close()">Annuler</button>
-                        <button type="submit" class="btn-primary">Valider</button>
-                    </div>
-                </form>
+        <div id="modal-show-reception" style="padding: 10px; min-width: 500px;">
+            <div class="line"></div>
+            <div class="text-right">
+                <button type="button" class="btn-default" onclick="$.fn.colorbox.close()">OK</button>
             </div>
-            <div id="ignore-line-<?= $reception->getIdReception() ?>" style="padding: 10px; min-width: 500px;">
-                <form id="ignore-line-form-<?= $reception->getIdReception() ?>" class="ignore-line-form" method="POST" action="<?= $this->lurl ?>/transferts/ignore">
-                    <input type="hidden" name="reception" value="<?= $reception->getIdReception() ?>">
-                    <h1>Ignorer une opération</h1>
-                    <div class="form-group">
-                        <label for="ignore-line-comment-<?= $reception->getIdReception() ?>">Commentaire</label>
-                        <textarea id="ignore-line-comment-<?= $reception->getIdReception() ?>" name="comment" rows="5" class="form-control"><?= $reception->getComment() ?></textarea>
-                    </div>
-                    <div class="text-right">
-                        <button type="button" class="btn-default" onclick="$.fn.colorbox.close()">Annuler</button>
-                        <button type="submit" class="btn-primary">Valider</button>
-                    </div>
-                </form>
-            </div>
-            <div id="comment-<?= $reception->getIdReception() ?>"><?= nl2br($reception->getComment()) ?></div>
-        <?php endforeach; ?>
+        </div>
+        <div id="modal-add-modify-comment" style="padding: 10px; min-width: 500px;">
+            <form method="POST" action="<?= $this->lurl ?>/transferts/comment">
+                <input type="hidden" name="reception">
+                <input type="hidden" name="referer" value="<?= $_SERVER['REQUEST_URI'] ?>">
+                <h1>Commenter l'opération</h1>
+                <div class="form-group">
+                    <label class="sr-only">Commentaire</label>
+                    <textarea name="comment" rows="5" class="form-control"></textarea>
+                </div>
+                <div class="text-right">
+                    <button type="button" class="btn-default" onclick="$.fn.colorbox.close()">Annuler</button>
+                    <button type="submit" class="btn-primary">Valider</button>
+                </div>
+            </form>
+        </div>
+        <div id="modal-ignore-reception" style="padding: 10px; min-width: 500px;">
+            <form id="modal-ignore-reception-form" method="POST" action="<?= $this->lurl ?>/transferts/ignore">
+                <input type="hidden" name="reception">
+                <h1>Ignorer l'operation</h1>
+                <div class="form-group">
+                    <label>Commentaire</label>
+                    <textarea name="comment" rows="5" class="form-control"></textarea>
+                </div>
+                <div class="text-right">
+                    <button type="button" class="btn-default" onclick="$.fn.colorbox.close()">Annuler</button>
+                    <button type="submit" class="btn-primary">Valider</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
+
