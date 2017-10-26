15b891238fe7908c34dc6dc7f4b0d033d1c39c86
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2017-10-26 14:13:20 +0200

    TST-266 : Fix Unilend and lender IRR calculation

--- src/Unilend/Bundle/CoreBusinessBundle/Service/IRRManager.php
+++ src/Unilend/Bundle/CoreBusinessBundle/Service/IRRManager.php
@@ -167,8 +167,6 @@ class IRRManager
         $lenderStat->setValue($lenderIRR);
         $this->entityManager->persist($lenderStat);
         $this->entityManager->flush($lenderStat);
-
-        var_dump($wallet->getIdClient()->getIdClient(), $lenderIRR);
     }
 
     /**
