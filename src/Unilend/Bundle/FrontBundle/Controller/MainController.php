66417e945a0f57538cf905d5d099710579c9a361
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2017-11-16 10:36:53 +0100

    TECH-332 - use constant

--- src/Unilend/Bundle/FrontBundle/Controller/MainController.php
+++ src/Unilend/Bundle/FrontBundle/Controller/MainController.php
@@ -20,6 +20,7 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Translation\TranslatorInterface;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Clients;
 use Unilend\Bundle\CoreBusinessBundle\Entity\OffresBienvenues;
+use Unilend\Bundle\CoreBusinessBundle\Entity\Tree;
 use Unilend\Bundle\CoreBusinessBundle\Entity\Users;
 use Unilend\Bundle\CoreBusinessBundle\Entity\WalletType;
 use Unilend\Bundle\CoreBusinessBundle\Service\ProjectManager;
@@ -280,7 +281,7 @@ class MainController extends Controller
         /** @var \tree $tree */
         $tree = $entityManager->getRepository('tree');
 
-        if (false === $tree->get(['slug' => $slug, 'status' => 1])) {
+        if (false === $tree->get(['slug' => $slug, 'status' => Tree::STATUS_ONLINE])) {
             throw new NotFoundHttpException('Page with slug ' . $slug . ' could not be found');
         }
 
