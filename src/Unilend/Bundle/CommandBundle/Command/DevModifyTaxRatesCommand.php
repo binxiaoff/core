aa91ba3716e4aa164c3db3e0f8fccc5e31c0547c
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2017-12-07 10:37:06 +0100

    TMA-2472 : CR feedback - Add slack notification

--- src/Unilend/Bundle/CommandBundle/Command/DevModifyTaxRatesCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/DevModifyTaxRatesCommand.php
@@ -25,22 +25,25 @@ class DevModifyTaxRatesCommand extends ContainerAwareCommand
         try {
             $csg->setRate(9.90);
             $entityManager->flush($csg);
+            $csgMessage = ':white_check_mark: *CSG* rate was successfully updated. New rate value: ' . $csg->getRate() . ' %.';
         } catch (\Exception $exception) {
+            $csgMessage = ':warning: Could not update the *CSG* tax rate: ' . $exception->getMessage();
             $this->getContainer()->get(
-                'monolog.logger.console')->err('Could not update the "CSG" tax rate ' . $exception->getMessage(),
-                ['method' => __METHOD__, 'file' => $exception->getFile(), 'line' => $exception->getLine()]
-            );
+                'monolog.logger.console')->error($csgMessage, ['method' => __METHOD__, 'file' => $exception->getFile(), 'line' => $exception->getLine()]);
         }
 
         $prelevementsObligatoires = $taxTypeRepository->find(TaxType::TYPE_STATUTORY_CONTRIBUTIONS);
         try {
             $prelevementsObligatoires->setRate(12.80);
             $entityManager->flush($prelevementsObligatoires);
+            $POMessage = ' :white_check_mark: *Prélèvements Obligatoire* rate was successfully updated. New rate value: ' . $prelevementsObligatoires->getRate() . ' %.';
         } catch (\Exception $exception) {
+            $POMessage = ' :warning: Could not update the *Prélèvements obligatoires* tax rate: ' . $exception->getMessage();
             $this->getContainer()->get(
-                'monolog.logger.console')->err('Could not update the "Prélèvements obligatoires" tax rate ' . $exception->getMessage(),
-                ['method' => __METHOD__, 'file' => $exception->getFile(), 'line' => $exception->getLine()]
-            );
+                'monolog.logger.console')->error($POMessage, ['method' => __METHOD__, 'file' => $exception->getFile(), 'line' => $exception->getLine()]);
         }
+
+        $slackManager = $this->getContainer()->get('unilend.service.slack_manager');
+        $slackManager->sendMessage($csgMessage . $POMessage, '#itprivate');
     }
 }
