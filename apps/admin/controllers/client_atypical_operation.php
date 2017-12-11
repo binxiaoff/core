f9f672adcbe0c0873524f837247609abf199808e
Bin XIAO <bin.xiao@unilend.fr>
2017-12-11 18:29:26 +0100

    TMA-2542  Erreurs sur userEntity

--- apps/admin/controllers/client_atypical_operation.php
+++ apps/admin/controllers/client_atypical_operation.php
@@ -51,7 +51,6 @@ class client_atypical_operationController extends bootstrap
 
         $this->showActions                  = true;
         $this->showUpdated                  = false;
-        $this->userEntity                   = $entityManager->getRepository('UnilendCoreBusinessBundle:Users')->find($_SESSION['user']['id_user']);
         $this->clientVigilanceStatusHistory = $entityManager->getRepository('UnilendCoreBusinessBundle:ClientVigilanceStatusHistory');
     }
 
