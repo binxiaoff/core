847fcfcbc629b8920570601a7b739a553f33312e
Bin XIAO <bin.xiao@unilend.fr>
2017-12-13 15:50:00 +0100

    TMA-2555 Correction requête IFU 2017 - bénéficiaires TMA-2548 Evolution requête IFU 2017 - Revenus

--- src/Unilend/Bundle/CommandBundle/Command/QueriesLenderRevenueCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/QueriesLenderRevenueCommand.php
@@ -68,7 +68,6 @@ EOF
         $activeSheet->setCellValueByColumnAndRow(3, $row, 'Date');
         $activeSheet->setCellValueByColumnAndRow(4, $row, 'Montant');
         $activeSheet->setCellValueByColumnAndRow(5, $row, 'Monnaie');
-        $activeSheet->setCellValueByColumnAndRow(6, $row, 'idClient');
         $row++;
 
         $operationRepository  = $this->getContainer()->get('doctrine.orm.entity_manager')->getRepository('UnilendCoreBusinessBundle:Operation');
@@ -80,37 +79,37 @@ EOF
             if ($sumLoans > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '117');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($sumLoans, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, round($sumLoans));
                 $row += 1;
             }
 
             $grossInterest               = $operationRepository->sumCreditOperationsByTypeAndYear($wallet, [OperationType::GROSS_INTEREST_REPAYMENT], null, $year);
             $grossInterestRegularization = $operationRepository->sumDebitOperationsByTypeAndYear($wallet, [OperationType::GROSS_INTEREST_REPAYMENT_REGULARIZATION], null, $year);
-            $grossInterest               = round(bcsub($grossInterest, $grossInterestRegularization, 4), 2);
+            $grossInterest               = round(bcsub($grossInterest, $grossInterestRegularization, 4));
             if ($grossInterest > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '53');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($grossInterest, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, $grossInterest);
                 $row += 1;
             }
 
             $deductedAtSource               = $operationRepository->sumDebitOperationsByTypeAndYear($wallet, [OperationType::TAX_FR_RETENUES_A_LA_SOURCE], null, $year);
             $deductedAtSourceRegularization = $operationRepository->sumCreditOperationsByTypeAndYear($wallet, [OperationType::TAX_FR_RETENUES_A_LA_SOURCE_REGULARIZATION], null, $year);
-            $deductedAtSource               = round(bcsub($deductedAtSource, $deductedAtSourceRegularization, 4), 2);
+            $deductedAtSource               = round(bcsub($deductedAtSource, $deductedAtSourceRegularization, 4));
             if ($deductedAtSource > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '2');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($deductedAtSource, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, $deductedAtSource);
                 $row += 1;
             }
 
             $statutoryContributions               = $operationRepository->sumDebitOperationsByTypeAndYear($wallet, [OperationType::TAX_FR_PRELEVEMENTS_OBLIGATOIRES], null, $year);
             $statutoryContributionsRegularization = $operationRepository->sumCreditOperationsByTypeAndYear($wallet, [OperationType::TAX_FR_PRELEVEMENTS_OBLIGATOIRES_REGULARIZATION], null, $year);
-            $statutoryContributions               = round(bcsub($statutoryContributions, $statutoryContributionsRegularization, 4), 2);
+            $statutoryContributions               = round(bcsub($statutoryContributions, $statutoryContributionsRegularization, 4));
             if ($statutoryContributions > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '54');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($statutoryContributions, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, $statutoryContributions);
                 $row += 1;
             }
 
@@ -120,28 +119,28 @@ EOF
             if ($capitalRepayments > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '118');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($capitalRepayments, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, $capitalRepayments);
                 $row += 1;
             }
 
             $interestWhileInFrance            = $operationRepository->getGrossInterestPaymentsInFrance($wallet, $year);
             $regularizedInterestWhileInFrance = $operationRepository->getRegularizedGrossInterestPaymentsInFrance($wallet, $year);
-            $interestWhileInFrance            = round(bcsub($interestWhileInFrance, $regularizedInterestWhileInFrance, 4), 2);
+            $interestWhileInFrance            = round(bcsub($interestWhileInFrance, $regularizedInterestWhileInFrance, 4));
             if ($interestWhileInFrance > 0) {
                 $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                 $activeSheet->setCellValueByColumnAndRow(2, $row, '66');
-                $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($interestWhileInFrance, 2, ',', ''));
+                $activeSheet->setCellValueByColumnAndRow(4, $row, $interestWhileInFrance);
                 $row += 1;
             }
 
             if (in_array($wallet->getIdClient()->getType(), [Clients::TYPE_PERSON, Clients::TYPE_PERSON_FOREIGNER])) {
                 $netInterestWhileInEEA            = $operationRepository->sumNetInterestRepaymentsNotInEeaExceptFrance($wallet, $year);
                 $regularizedNetInterestWhileInEEA = $operationRepository->sumRegularizedNetInterestRepaymentsNotInEeaExceptFrance($wallet, $year);
-                $netInterestWhileInEEA            = round(bcsub($netInterestWhileInEEA, $regularizedNetInterestWhileInEEA, 4), 2);
+                $netInterestWhileInEEA            = round(bcsub($netInterestWhileInEEA, $regularizedNetInterestWhileInEEA, 4));
                 if ($netInterestWhileInEEA > 0) {
                     $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                     $activeSheet->setCellValueByColumnAndRow(2, $row, '81');
-                    $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($netInterestWhileInEEA, 2, ',', ''));
+                    $activeSheet->setCellValueByColumnAndRow(4, $row, $netInterestWhileInEEA);
                     $row += 1;
                 }
             }
@@ -149,11 +148,11 @@ EOF
             if (in_array($wallet->getIdClient()->getType(), [Clients::TYPE_PERSON, Clients::TYPE_PERSON_FOREIGNER])) {
                 $capitalWhileInEEA            = $operationRepository->sumRegularizedCapitalRepaymentsInEeaExceptFrance($wallet, $year);
                 $regularizedCapitalWhileInEEA = $operationRepository->sumCapitalRepaymentsInEeaExceptFrance($wallet, $year);
-                $capitalWhileInEEA            = round(bcsub($capitalWhileInEEA, $regularizedCapitalWhileInEEA, 4), 2);
+                $capitalWhileInEEA            = round(bcsub($capitalWhileInEEA, $regularizedCapitalWhileInEEA, 4));
                 if ($capitalWhileInEEA > 0) {
                     $this->addCommonCellValues($activeSheet, $row, $year, $wallet);
                     $activeSheet->setCellValueByColumnAndRow(2, $row, '82');
-                    $activeSheet->setCellValueByColumnAndRow(4, $row, number_format($capitalWhileInEEA, 2, ',', ''));
+                    $activeSheet->setCellValueByColumnAndRow(4, $row, $capitalWhileInEEA);
                     $row += 1;
                 }
             }
@@ -176,13 +175,12 @@ EOF
         $commonValues = [
             'CodeEntreprise' => 1, //official code of SFPMEI
             'Date'           => '31/12/' . $year,
-            'Monnaie'        => 'EURO',
+            'Monnaie'        => 'EUR',
         ];
 
         $activeSheet->setCellValueByColumnAndRow(0, $row, $commonValues['CodeEntreprise']);
         $activeSheet->setCellValueByColumnAndRow(1, $row, $wallet->getWireTransferPattern());
         $activeSheet->setCellValueByColumnAndRow(3, $row, $commonValues['Date']);
         $activeSheet->setCellValueByColumnAndRow(5, $row, $commonValues['Monnaie']);
-        $activeSheet->setCellValueByColumnAndRow(6, $row, $wallet->getIdClient()->getIdClient());
     }
 }
