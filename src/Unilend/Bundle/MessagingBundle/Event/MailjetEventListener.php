3f85d0606e549d2794788e5d6b00a8696ed042a8
Antoine Préveaux <antoine.preveaux@unilend.fr>
2018-01-15 14:46:58 +0100

    TECH-361 Use new Mailjet event tracking API
    - Use forked version of event tracking bundle
    - Migrate to Doctrine

--- src/Unilend/Bundle/MessagingBundle/Event/MailjetEventListener.php
+++ src/Unilend/Bundle/MessagingBundle/Event/MailjetEventListener.php
@@ -2,163 +2,242 @@
 
 namespace Unilend\Bundle\MessagingBundle\Event;
 
-use Knp\Bundle\MailjetBundle\Event\Adapter\BlockedEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\BounceEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\ClickEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\OpenEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\SpamEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\TypofixEvent;
-use Knp\Bundle\MailjetBundle\Event\Adapter\UnsubEvent;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
+use Knp\Bundle\MailjetBundle\Event\Adapter\{
+    BlockedEvent,
+    BounceEvent,
+    ClickEvent,
+    OpenEvent,
+    SentEvent,
+    SpamEvent,
+    UnsubEvent
+};
 use Knp\Bundle\MailjetBundle\Event\Listener\EventListenerInterface;
-use Unilend\Bundle\CoreBusinessBundle\Service\Simulator\EntityManager;
+use Psr\Log\LoggerInterface;
+use Unilend\Bundle\CoreBusinessBundle\Entity\{
+    MailjetEventBlocked,
+    MailjetEventBounce,
+    MailjetEventClick,
+    MailjetEventOpen,
+    MailjetEventSpam,
+    MailjetEventUnsub
+};
 
 /**
  * @package Unilend\Bundle\MessagingBundle\Event
- * https://dev.mailjet.com/guides/#event-api-real-time-notifications
+ * @link    https://dev.mailjet.com/guides/#event-api-real-time-notifications
  */
 class MailjetEventListener implements EventListenerInterface
 {
     /** @var EntityManager */
     private $entityManager;
+    /** @var LoggerInterface */
+    private $logger;
 
-    public function __construct(EntityManager $entityManager)
+    /**
+     * @param EntityManager   $entityManager
+     * @param LoggerInterface $logger
+     */
+    public function __construct(EntityManager $entityManager, LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
+        $this->logger        = $logger;
     }
 
+    /**
+     * @param SentEvent $event
+     */
+    public function onSentEvent(SentEvent $event)
+    {
+        // "sent" event is not interesting for us as we can use `mail_queue`
+    }
+
+    /**
+     * @param OpenEvent $event
+     */
     public function onOpenEvent(OpenEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_open $openEvent */
-        $openEvent                  = $this->entityManager->getRepository('mailjet_event_open');
-        $openEvent->time            = $data['time'];
-        $openEvent->email           = $data['email'];
-        $openEvent->campaign_id     = $data['mj_campaign_id'];
-        $openEvent->contact_id      = $data['mj_contact_id'];
-        $openEvent->custom_campaign = $data['customcampaign'];
-        $openEvent->message_id      = $data['MessageID'];
-        $openEvent->custom_id       = $data['CustomID'];
-        $openEvent->payload         = $data['Payload'];
-        $openEvent->ip              = $data['ip'];
-        $openEvent->geo             = $data['geo'];
-        $openEvent->agent           = $data['agent'];
-        $openEvent->create();
+        $openEvent = new MailjetEventOpen();
+        $openEvent
+            ->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->setIp($event->getIp())
+            ->setGeo($event->getGeo())
+            ->setAgent($event->getUserAgent());
+
+        try {
+            $this->entityManager->persist($openEvent);
+            $this->entityManager->flush($openEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 
+    /**
+     * @param SpamEvent $event
+     */
     public function onSpamEvent(SpamEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_spam $spamEvent */
-        $spamEvent = $this->entityManager->getRepository('mailjet_event_spam');
-        $spamEvent->time            = $data['time'];
-        $spamEvent->email           = $data['email'];
-        $spamEvent->campaign_id     = $data['mj_campaign_id'];
-        $spamEvent->contact_id      = $data['mj_contact_id'];
-        $spamEvent->custom_campaign = $data['customcampaign'];
-        $spamEvent->message_id      = $data['MessageID'];
-        $spamEvent->custom_id       = $data['CustomID'];
-        $spamEvent->payload         = $data['Payload'];
-        $spamEvent->source          = $data['source'];
-        $spamEvent->create();
+        $spamEvent = new MailjetEventSpam();
+        $spamEvent
+            ->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->setSource($event->getSource());
+
+        try {
+            $this->entityManager->persist($spamEvent);
+            $this->entityManager->flush($spamEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 
+    /**
+     * @param ClickEvent $event
+     */
     public function onClickEvent(ClickEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_click $clickEvent */
-        $clickEvent                  = $this->entityManager->getRepository('mailjet_event_click');
-        $clickEvent->time            = $data['time'];
-        $clickEvent->email           = $data['email'];
-        $clickEvent->campaign_id     = $data['mj_campaign_id'];
-        $clickEvent->contact_id      = $data['mj_contact_id'];
-        $clickEvent->custom_campaign = $data['customcampaign'];
-        $clickEvent->message_id      = $data['MessageID'];
-        $clickEvent->custom_id       = $data['CustomID'];
-        $clickEvent->payload         = $data['Payload'];
-        $clickEvent->url             = $data['url'];
-        $clickEvent->create();
+        $clickEvent = new MailjetEventClick();
+        $clickEvent
+            ->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->seturl($event->geturl());
+
+        try {
+            $this->entityManager->persist($clickEvent);
+            $this->entityManager->flush($clickEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 
+    /**
+     * @param UnsubEvent $event
+     */
     public function onUnsubEvent(UnsubEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_unsub $unsubEvent */
-        $unsubEvent                  = $this->entityManager->getRepository('mailjet_event_unsub');
-        $unsubEvent->time            = $data['time'];
-        $unsubEvent->email           = $data['email'];
-        $unsubEvent->campaign_id     = $data['mj_campaign_id'];
-        $unsubEvent->contact_id      = $data['mj_contact_id'];
-        $unsubEvent->custom_campaign = $data['customcampaign'];
-        $unsubEvent->message_id      = $data['MessageID'];
-        $unsubEvent->custom_id       = $data['CustomID'];
-        $unsubEvent->payload         = $data['Payload'];
-        $unsubEvent->list_id         = $data['mj_list_id'];
-        $unsubEvent->ip              = $data['ip'];
-        $unsubEvent->geo             = $data['geo'];
-        $unsubEvent->agent           = $data['agent'];
-        $unsubEvent->create();
+        $unsubEvent = new MailjetEventUnsub();
+        $unsubEvent
+            ->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->setListId($event->getListId())
+            ->setIp($event->getIp())
+            ->setGeo($event->getGeo())
+            ->setAgent($event->getUserAgent());
+
+        try {
+            $this->entityManager->persist($unsubEvent);
+            $this->entityManager->flush($unsubEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 
+    /**
+     * @param BounceEvent $event
+     */
     public function onBounceEvent(BounceEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_bounce $bounceEvent */
-        $bounceEvent                   = $this->entityManager->getRepository('mailjet_event_bounce');
-        $bounceEvent->time             = $data['time'];
-        $bounceEvent->email            = $data['email'];
-        $bounceEvent->campaign_id      = $data['mj_campaign_id'];
-        $bounceEvent->contact_id       = $data['mj_contact_id'];
-        $bounceEvent->custom_campaign  = $data['customcampaign'];
-        $bounceEvent->message_id       = $data['MessageID'];
-        $bounceEvent->custom_id        = $data['CustomID'];
-        $bounceEvent->payload          = $data['Payload'];
-        $bounceEvent->blocked          = $data['blocked'];
-        $bounceEvent->hard_bounce      = $data['hard_bounce'];
-        $bounceEvent->error_related_to = $data['error_related_to'];
-        $bounceEvent->error            = $data['error'];
-        $bounceEvent->create();
+        $bounceEvent = new MailjetEventBounce();
+        $bounceEvent
+            ->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->setBlocked($event->isBlocked())
+            ->setHardBounce($event->isHardBounce())
+            ->setErrorRelatedTo($event->getErrorExplanation())
+            ->setError($event->getError());
+
+        try {
+            $this->entityManager->persist($bounceEvent);
+            $this->entityManager->flush($bounceEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 
+    /**
+     * @param BlockedEvent $event
+     */
     public function onBlockedEvent(BlockedEvent $event)
     {
-        $data = $event->getData();
+        $event = $event->getEvent();
 
-        /** @var \mailjet_event_blocked $blockedEvent */
-        $blockedEvent                   = $this->entityManager->getRepository('mailjet_event_blocked');
-        $blockedEvent->time             = $data['time'];
-        $blockedEvent->email            = $data['email'];
-        $blockedEvent->campaign_id      = $data['mj_campaign_id'];
-        $blockedEvent->contact_id       = $data['mj_contact_id'];
-        $blockedEvent->custom_campaign  = $data['customcampaign'];
-        $blockedEvent->message_id       = $data['MessageID'];
-        $blockedEvent->custom_id        = $data['CustomID'];
-        $blockedEvent->payload          = $data['Payload'];
-        $blockedEvent->error_related_to = $data['error_related_to'];
-        $blockedEvent->error            = $data['error'];
-        $blockedEvent->create();
-    }
+        $blockedEvent = new MailjetEventBlocked();
+        $blockedEvent->setTime($event->getTime())
+            ->setEmail($event->getEmail())
+            ->setCampaignId($event->getCampaignId())
+            ->setContactId($event->getContactId())
+            ->setCustomCampaign($event->getCustomCampaign())
+            ->setMessageId($event->getMessageId())
+            ->setCustomId($event->getCustomId())
+            ->setPayload($event->getPayload())
+            ->setErrorRelatedTo($event->getErrorExplanation())
+            ->setError($event->getError());
 
-    public function onTypofixEvent(TypofixEvent $event)
-    {
-        $data = $event->getData();
-
-        /** @var \mailjet_event_typofix $typofixEvent */
-        $typofixEvent                   = $this->entityManager->getRepository('mailjet_event_typofix');
-        $typofixEvent->time             = $data['time'];
-        $typofixEvent->email            = $data['email'];
-        $typofixEvent->campaign_id      = $data['mj_campaign_id'];
-        $typofixEvent->contact_id       = $data['mj_contact_id'];
-        $typofixEvent->custom_campaign  = $data['customcampaign'];
-        $typofixEvent->message_id       = $data['MessageID'];
-        $typofixEvent->custom_id        = $data['CustomID'];
-        $typofixEvent->payload          = $data['Payload'];
-        $typofixEvent->original_address = $data['original_address'];
-        $typofixEvent->new_address      = $data['new_address'];
-        $typofixEvent->create();
+        try {
+            $this->entityManager->persist($blockedEvent);
+            $this->entityManager->flush($blockedEvent);
+        } catch (ORMException $exception) {
+            $this->logger->error(
+                'Could not save Mailjet "open" event',
+                ['file' => $exception->getFile(), 'line' => $exception->getLine()]
+            );
+        }
     }
 }
