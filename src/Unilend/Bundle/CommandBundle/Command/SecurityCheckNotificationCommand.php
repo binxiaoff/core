44c16c82aff2ea2ef9b1f767e78b5b4b7287ab3e
Bin XIAO <bin.xiao@unilend.fr>
2018-03-19 16:42:58 +0100

    TECH-321 Improve code style

--- src/Unilend/Bundle/CommandBundle/Command/SecurityCheckNotificationCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/SecurityCheckNotificationCommand.php
@@ -3,25 +3,23 @@
 namespace Unilend\Bundle\CommandBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\{
+    ArrayInput, InputInterface
+};
+use Symfony\Component\Console\Output\{
+    BufferedOutput, OutputInterface
+};
 
 class SecurityCheckNotificationCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('unilend:security:check_result:check_notify')
+        $this->setName('unilend:security:checker:notify')
             ->setDescription('Check for known security issues for the packages managed by composer via SensioLabs Security Advisories Checker and notify the IT when finding vulnerabilities)');
     }
 
     /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return int|null|void
-     * @throws \Exception
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -41,25 +39,20 @@ class SecurityCheckNotificationCommand extends ContainerAwareCommand
             $slackManager = $this->getContainer()->get('unilend.service.slack_manager');
             $slackManager->sendMessage($content, '#it-monitoring');
 
-            $varMail = array(
-                'content' => nl2br($content)
-            );
-
             $addressEmail = $this->getContainer()->get('doctrine.orm.entity_manager')->getRepository('UnilendCoreBusinessBundle:Settings')->findOneBy(['type' => 'Adresse email IT']);
 
             if ($addressEmail) {
-                $message = $this->getContainer()->get('unilend.swiftmailer.message_provider')->newMessage('notification-security-check', $varMail);
+                $message = $this->getContainer()->get('unilend.swiftmailer.message_provider')->newMessage('notification-security-check', ['content' => nl2br($content)]);
                 try {
                     $message->setTo($addressEmail->getValue());
                     $mailer = $this->getContainer()->get('mailer');
-                    $result = $mailer->send($message);
-                    echo $result;
+                    $mailer->send($message);
                 } catch (\Exception $exception) {
                     $this->getContainer()->get('monolog.logger.console')->error(
                         'Could not send email : notification-security-check - Exception: ' . $exception->getMessage(),
                         [
                             'id_mail_template' => $message->getTemplateId(),
-                            'email address'    => $addressEmail->getVaue(),
+                            'email'            => $addressEmail->getVaue(),
                             'class'            => __CLASS__,
                             'function'         => __FUNCTION__,
                             'file'             => $exception->getFile(),
