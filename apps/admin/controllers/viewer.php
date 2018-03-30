c48aff3477d01eccd7d9d10bdbda08282f8bef1b
Matt Scheurich <matt@lvl99.com>
2018-03-30 16:31:39 +0200

    * Refactored document viewer frontend code * Connected document viewer to backend * Added "Voir" link when viewing Emprunteur documents (admin: dossiers/edit) which displays the document viewer within a colorbox iframe modal

--- apps/admin/controllers/viewer.php
+++ apps/admin/controllers/viewer.php
@@ -11,6 +11,37 @@ class viewerController extends bootstrap
 
     public function _default()
     {
-        $this->render();
+        /** @var \Doctrine\ORM\EntityManager $entityManager */
+        $entityManager = $this->get('doctrine.orm.entity_manager');
+
+        $this->projects = $this->loadData('projects');
+
+        if (isset($this->params[0]) && $this->projects->get($this->params[0], 'id_project')) {
+            $this->projectEntity = $entityManager->getRepository('UnilendCoreBusinessBundle:Projects')->find($this->projects->id_project);
+            $this->attachments = $this->projectEntity->getAttachments();
+
+            $this->documents = [];
+            foreach ( $this->attachments as $attachment )
+            {
+                /** @var \Unilend\Bundle\CoreBusinessBundle\Entity\ProjectAttachment $attachment */
+                $currentAttachment = $attachment->getAttachment();
+
+                $document = [
+                    'id' => $currentAttachment->getId(),
+                    'name' => empty($currentAttachment->getOriginalName()) ? $currentAttachment->getPath() : $currentAttachment->getOriginalName(),
+                    'url' => $this->url . '/attachment/download/id/' . $currentAttachment->getId() . '/file/' . urlencode($currentAttachment->getPath()),
+                    'isSelected' => $this->params[1] == $currentAttachment->getId(),
+                ];
+
+                $this->documents[] = $document;
+            }
+
+            $this->render(null, [
+                'documents' => $this->documents,
+            ]);
+        } else {
+            header('Location: ' . $this->lurl);
+            die;
+        }
     }
 }
