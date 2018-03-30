7d74467fa93d7d9c19761d639bfdfa362ddd834c
Mesbah Zitouni <mesbah.zitouni@unilend.fr>
2018-03-30 12:01:04 +0200

    RUN-2887 : Fix Autobid first activation notification/email sending
    - Delete unused constants from `clients_gestion_type_notif`
    - Always insert notification and send email on Autobid first activation,
    regardless on any notification setting

--- data/clients_gestion_type_notif.data.php
+++ data/clients_gestion_type_notif.data.php
@@ -1,30 +1,4 @@
 <?php
-// **************************************************************************************************** //
-// ***************************************    ASPARTAM    ********************************************* //
-// **************************************************************************************************** //
-//
-// Copyright (c) 2008-2011, equinoa
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies
-// or substantial portions of the Software.
-// The Software is provided "as is", without warranty of any kind, express or implied, including but
-// not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
-// In no event shall the authors or copyright holders equinoa be liable for any claim,
-// damages or other liability, whether in an action of contract, tort or otherwise, arising from,
-// out of or in connection with the software or the use or other dealings in the Software.
-// Except as contained in this notice, the name of equinoa shall not be used in advertising
-// or otherwise to promote the sale, use or other dealings in this Software without
-// prior written authorization from equinoa.
-//
-//  Version : 2.4.0
-//  Date : 21/03/2011
-//  Coupable : CM
-//
-// **************************************************************************************************** //
 
 class clients_gestion_type_notif extends clients_gestion_type_notif_crud
 {
@@ -38,14 +12,7 @@ class clients_gestion_type_notif extends clients_gestion_type_notif_crud
     const TYPE_DEBIT                         = 8;
     const TYPE_PROJECT_PROBLEM               = 9;
     const TYPE_AUTOBID_BALANCE_LOW           = 10;
-    const TYPE_AUTOBID_BALANCE_INSUFFICIENT  = 11; // @deprecated
-    const TYPE_AUTOBID_FIRST_ACTIVATION      = 12; // @deprecated
     const TYPE_AUTOBID_ACCEPTED_REJECTED_BID = 13;
-
-    public function __construct($bdd, $params = '')
-    {
-        parent::__construct($bdd, $params);
-    }
 
     public function select($where = '', $order = '', $start = '', $nb = '')
     {
