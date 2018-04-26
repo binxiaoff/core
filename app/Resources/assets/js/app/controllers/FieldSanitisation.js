de5f8a30ec7a3b72daf7b35ba30d7e0e286b78d7
Matt Scheurich <matt@lvl99.com>
2018-04-26 19:11:42 +0200

    BLD-232 testing simple text sanitisation for raison sociale

--- app/Resources/assets/js/app/controllers/FieldSanitisation.js
+++ app/Resources/assets/js/app/controllers/FieldSanitisation.js
@@ -10,15 +10,23 @@ var sanity = require('Sanity')
 var $doc = $(document)
 
 $doc
-  // Title
+  // Sanitise Title (e.g. Raison Sociale)
   .on('FormValidation:validateInput:beforeValidate', 'input[data-formvalidation-type="title"]', function (event, FormValidation, inputValidation) {
     $(this).val(sanity($(this).val()).sanitiseSimpleText())
   })
-  // Siret
+
+  // Sanitise Siret
   .on('FormValidation:validateInput:beforeValidate', 'input[data-formvalidation-type="siret"]', function (event, FormValidation, inputValidation) {
     $(this).val(sanity($(this).val()).sanitiseSiret())
   })
-  // Siret
-  .on('FormValidation:validateInput:beforeValidate', 'input[data-formvalidation-type="title"]', function (event, FormValidation, inputValidation) {
-    $(this).val(sanity($(this).val()).sanitiseSimpleText())
+
+  // Sanitise Siren
+  .on('FormValidation:validateInput:beforeValidate', 'input[data-formvalidation-type="siren"]', function (event, FormValidation, inputValidation) {
+    $(this).val(sanity($(this).val()).sanitiseSiren())
   })
+
+  // Sanitise Phone/Mobile
+  .on('FormValidation:validateInput:beforeValidate', 'input[data-formvalidation-type="tel"], input[data-formvalidation-type="phone"], input[data-formvalidation-type="telephone"], input[data-formvalidation-type="mobile"]', function (event, FormValidation, inputValidation) {
+    $(this).val(sanity($(this).val()).sanitisePhone())
+  })
+
