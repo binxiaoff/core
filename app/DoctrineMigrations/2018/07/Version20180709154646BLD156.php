88d506ce51a8128df0107af20a48622eb7f3321d
Bin XIAO <bin.xiao@unilend.fr>
2018-07-09 18:06:17 +0200

    BLD-156 Propose to the lenders to re-do the CIP survey if it's expired

---
+++ app/DoctrineMigrations/2018/07/Version20180709154646BLD156.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20180709154646BLD156 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE translations SET translation = \'Votre capacité à prêter est actuellement limitée. Pour prêter comme vous le souhaitez et bénéficier de conseils personnalisés, nous vous proposons de répondre au questionnaire d’évaluation.\' WHERE section = \'lender-evaluation\' AND name = \'index-no-ongoing-evaluation-text\'');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE translations SET translation = \'Votre capacité à prêter est actuellement limitée car nous n\'\'avons pas pu vous donner de conseil personnalisé. Pour prêter comme vous le souhaitez et bénéficier de conseils personnalisés, nous vous proposons de répondre au questionnaire d\'\'évaluation.\' WHERE section = \'lender-evaluation\' AND name = \'index-no-ongoing-evaluation-text\'');
+    }
+}
