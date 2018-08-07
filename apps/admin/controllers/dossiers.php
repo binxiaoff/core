f5a11f146448a92adc7cfc3f301a8f2c5eaca1ba
Antoine Préveaux <antoine.preveaux@unilend.fr>
2018-08-07 12:01:47 +0200

    TECH-279 Update borrower sources list

--- apps/admin/controllers/dossiers.php
+++ apps/admin/controllers/dossiers.php
@@ -1,7 +1,9 @@
 <?php
 
 use Psr\Log\LoggerInterface;
-use Unilend\Bundle\CoreBusinessBundle\Entity\{AddressType, AttachmentType, BorrowingMotive, Companies, CompanyAddress, CompanyStatus, Echeanciers, Loans, Partner, PartnerProjectAttachment, Prelevements, ProjectAbandonReason, ProjectNotification, ProjectRejectionReason, ProjectRepaymentTask, Projects, ProjectsComments, ProjectsPouvoir, ProjectsStatus, Users, UsersTypes, Virements, WalletType, Zones};
+use Unilend\Bundle\CoreBusinessBundle\Entity\{AddressType, AttachmentType, BorrowingMotive, Companies, CompanyAddress, CompanyStatus, Echeanciers, Loans, Partner, PartnerProjectAttachment,
+    Prelevements, ProjectAbandonReason, ProjectNotification, ProjectRejectionReason, ProjectRepaymentTask, Projects, ProjectsComments, ProjectsPouvoir, ProjectsStatus, Users, UsersTypes, Virements,
+    WalletType, Zones};
 use Unilend\Bundle\CoreBusinessBundle\Service\{BackOfficeUserManager, ProjectManager, ProjectRequestManager, TermsOfSaleManager, WireTransferOutManager};
 use Unilend\Bundle\WSClientBundle\Entity\Altares\EstablishmentIdentityDetail;
 
@@ -684,7 +686,7 @@ class dossiersController extends bootstrap
 
             $this->xerfi                       = $this->loadData('xerfi');
             $this->sectors                     = $this->loadData('company_sector')->select();
-            $this->sources                     = array_column($this->clients->select('source NOT LIKE "http%" AND source NOT IN ("", "1") GROUP BY source'), 'source');
+            $this->sources                     = $this->getSourcesList();
             $this->ratings                     = $this->loadRatings($this->companies, $this->projects->id_company_rating_history, $this->xerfi);
             $this->ratings['unilend_prescore'] = $this->addUnilendPrescoring($this->projects_notes);
             $this->aCompanyProjects            = $this->companies->getProjectsBySIREN();
@@ -1410,7 +1412,7 @@ class dossiersController extends bootstrap
         $this->settings->get('Durée des prêts autorisées', 'type');
         $this->dureePossible = explode(',', $this->settings->value);
 
-        $this->sources = array_column($this->clients->select('source NOT LIKE "http%" AND source NOT IN ("", "1") GROUP BY source'), 'source');
+        $this->sources = $this->getSourcesList();
     }
 
     public function _funding()
@@ -2928,5 +2930,18 @@ class dossiersController extends bootstrap
             'action' => 'Test d&#39éligibilité',
             'user'   => ''
         ];
-        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getSourcesList(): array
+    {
+        return [
+            'Commercial Courtier',
+            'Commercial Direct',
+            'Franchise',
+            'Test'
+        ];
+    }
 }
