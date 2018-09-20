c5227adeb4544b5f8829b382b9606760a5c59e8f
annabreyer <anna.breyer@unilend.fr>
2018-09-20 14:22:09 +0200

    BLD-320: rename already existing TOS files
    - change correspondance between old and new id_legal_doc following BLD-319
    - iterate folders instead of clients from DB
    - add handling of exception in already existing file name

--- src/Unilend/Bundle/CommandBundle/Command/Dev/RenameExistingLenderTosFilesCommand.php
+++ src/Unilend/Bundle/CommandBundle/Command/Dev/RenameExistingLenderTosFilesCommand.php
@@ -12,6 +12,19 @@ use Unilend\Bundle\CoreBusinessBundle\Service\Document\LenderTermsOfSaleGenerato
 
 class RenameExistingLenderTosFilesCommand extends Command
 {
+    CONST ID_LEGAL_DOC_CORRESPONDANCE_TABLE = [
+        92  => 44,
+        93  => 45,
+        95  => 46,
+        254 => 47,
+        255 => 48,
+        301 => 49,
+        443 => 50,
+        444 => 51,
+        473 => 52,
+        474 => 53
+    ];
+
     /** @var Filesystem */
     private $filesystem;
     /** @var string */
@@ -59,8 +72,7 @@ class RenameExistingLenderTosFilesCommand extends Command
 The <info>unilend:dev_tools:lender:legacy_terms_of_sale:rename</info> command renames the already existing pdf version of accepted lenders terms of sale and saves it in the database.
 <info>php bin/console unilend:dev_tools:lender:legacy_terms_of_sale:rename</info>
 EOF
-            )
-            ->addOption('limit', 'l', InputOption::VALUE_REQUIRED, 'Number of clients to process');
+            );
     }
 
     /**
@@ -68,78 +80,68 @@ EOF
      */
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $limit = $input->getOption('limit');
-        $limit = $limit ? $limit : 1000;
-
-        // idLegalDoc before migration => idLegalDoc now (DEV-827)
-        $idLegalDocCorrespondanceTable = [
-            92  => 43,
-            93  => 44,
-            95  => 45,
-            254 => 46,
-            255 => 47,
-            300 => 48,
-            301 => 49,
-            443 => 50,
-            444 => 51,
-            473 => 52,
-            474 => 53
-        ];
-
         $clientRepository            = $this->entityManager->getRepository('UnilendCoreBusinessBundle:Clients');
         $acceptedLegalDocsRepository = $this->entityManager->getRepository('UnilendCoreBusinessBundle:AcceptationsLegalDocs');
-
-        $clientWithoutPdfName = $this->entityManager
-            ->getRepository('UnilendCoreBusinessBundle:AcceptationsLegalDocs')
-            ->getLenderWithAcceptedIdLegalDocAndNoPdfValue($limit);
 
         $renamedPdfs   = 0;
         $savedPdfNames = 0;
 
-        foreach ($clientWithoutPdfName as $clientWithTos) {
-            try {
-                $directoryName = $this->protectedPath . LenderTermsOfSaleGenerator::PATH . DIRECTORY_SEPARATOR . $clientWithTos['idClient'];
-                if (false === $this->filesystem->exists($directoryName)) {
-                    continue;
-                }
+        $directoryFinder = new Finder();
+        $directoryFinder->directories()->in($this->protectedPath . LenderTermsOfSaleGenerator::PATH);
 
+        foreach ($directoryFinder as $directory) {
+            $clientId = $directory->getFilename();
+
+            try {
                 $finder = new Finder();
-                $finder->files()->in($directoryName);
+                $finder->files()->in($directory->getPathname());
 
                 foreach ($finder as $file) {
                     $hash               = null;
                     $idLegalDocFromName = null;
                     $fileName           = $file->getFilename();
 
-                    if ('cgv_preteurs' !== substr($fileName, 0, 12)) {
-                        $this->logger->warning($directoryName . ' does contain a file which does not seem to be a lender terms of sale. Filename: ' . $fileName, [
+                    if (false === $this->checkFileName($fileName)){
+                        $this->logger->warning($directory->getPathname() . ' does contain a file which does not seem to be a lender terms of sale. Filename: ' . $fileName, [
                             'class'          => __CLASS__,
                             'function'       => __FUNCTION__
                         ]);
                         continue;
                     }
 
+                    if ('CGV-UNILEND-PRETEUR' === substr($fileName, 0, 19)) {
+                        $normalizedName = str_replace('CGV-UNILEND-PRETEUR', 'cgv_preteurs', $file->getPathname());
+
+                        if (false === $this->filesystem->exists($normalizedName)) {
+                            $this->filesystem->rename($file->getPathname(), $normalizedName);
+                            $fileName = $file->getFilename();
+                        } else {
+                            $this->filesystem->remove($file);
+                            continue;
+                        }
+                    }
+
                     $hash   = $this->getHashFromFileName($fileName);
                     $client = $clientRepository->findOneBy(['hash' => $hash]);
-                    if (null === $client || $clientWithTos['idClient'] != $client->getIdClient()) {
+                    if (null === $client || $clientId != $client->getIdClient()) {
                         $this->logger->warning('Folder client id and client from hash do not match. Filename: ' . $fileName, [
                             'class'     => __CLASS__,
                             'function'  => __FUNCTION__,
-                            'id_client' => $clientWithTos['idClient'],
+                            'id_client' => $clientId,
                             'hash'      => $hash,
                         ]);
                         continue;
                     }
 
                     $idLegalDocFromName = $this->getIdLegalDocFromFileName($fileName);
-                    $idLegalDoc         = isset($idLegalDocCorrespondanceTable[$idLegalDocFromName]) ? $idLegalDocCorrespondanceTable[$idLegalDocFromName] : $idLegalDocFromName;
-                    $accepted           = $acceptedLegalDocsRepository->findOneBy(['idClient' => $clientWithTos['idClient'], 'idLegalDoc' => $idLegalDoc]);
+                    $idLegalDoc         = isset(self::ID_LEGAL_DOC_CORRESPONDANCE_TABLE[$idLegalDocFromName]) ? self::ID_LEGAL_DOC_CORRESPONDANCE_TABLE[$idLegalDocFromName] : $idLegalDocFromName;
+                    $accepted           = $acceptedLegalDocsRepository->findOneBy(['idClient' => $clientId, 'idLegalDoc' => $idLegalDoc]);
 
                     if (null === $accepted) {
                         $this->logger->warning('There is nor entry in acceptations_legal_docs for this client and legal doc. Filename: ' . $fileName, [
                             'class'        => __CLASS__,
                             'function'     => __FUNCTION__,
-                            'id_client'    => $clientWithTos['idClient'],
+                            'id_client'    => $clientId,
                             'id_legal_doc' => $idLegalDoc
                         ]);
                         continue;
@@ -150,10 +152,12 @@ EOF
                         $renamedPdfs++;
                     }
 
-                    $accepted->setPdfName($this->lenderTermsOfSaleGenerator->getName($accepted));
+                    if (null === $accepted->getPdfName()) {
+                        $accepted->setPdfName($this->lenderTermsOfSaleGenerator->getName($accepted));
 
-                    $this->entityManager->flush();
-                    $savedPdfNames++;
+                        $this->entityManager->flush();
+                        $savedPdfNames++;
+                    }
                 }
             } catch (\Exception $exception) {
                 $this->logger->error('An error occurred while renaming lender terms of sale pdf. Message: ' . $exception->getMessage(), [
@@ -161,12 +165,12 @@ EOF
                     'function'  => __FUNCTION__,
                     'file'      => $exception->getFile(),
                     'line'      => $exception->getLine(),
-                    'id_client' => $clientWithTos['idClient']
+                    'id_client' => $clientId
                 ]);
             }
         }
 
-        $output->writeln('For ' . count($clientWithoutPdfName) . ' clients ' . $savedPdfNames . ' pdf names have been saved, and ' . $renamedPdfs . ' files have been renamed.');
+        $output->writeln($savedPdfNames . ' pdf names have been saved, and ' . $renamedPdfs . ' files have been renamed.');
     }
 
     /**
@@ -176,7 +180,6 @@ EOF
      */
     private function getHashFromFileName(string $filename): string
     {
-
         $filename   = str_replace('cgv_preteurs-', '', $filename);
         $filename   = str_replace('.pdf', '', $filename);
         $endOfHash  = strrpos($filename, '-');
@@ -198,5 +201,15 @@ EOF
         $idLegalDoc = substr($filename, $separator + 1);
 
         return (int)$idLegalDoc;
+    }
+
+    /**
+     * @param string $fileName
+     *
+     * @return bool
+     */
+    private function checkFileName(string $fileName): bool
+    {
+        return 'cgv_preteurs' === substr($fileName, 0, 12) || 'CGV-UNILEND-PRETEUR' === substr($fileName, 0, 19);
     }
 }
