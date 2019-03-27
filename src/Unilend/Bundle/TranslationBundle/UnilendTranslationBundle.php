8d9339b9a2e540de91a9abf2adfcdb64ecf81b72
binxiao <bin.xiao@lafabriquebyca.com>
2019-03-27 11:22:43 +0100

    Let service sonata.cache.symfony be public Update outdated dependencies in composer.json

--- src/Unilend/Bundle/TranslationBundle/UnilendTranslationBundle.php
+++ src/Unilend/Bundle/TranslationBundle/UnilendTranslationBundle.php
@@ -2,8 +2,16 @@
 
 namespace Unilend\Bundle\TranslationBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Unilend\Bundle\TranslationBundle\DependencyInjection\Compiler\MakeSonataCacheSymfonyPublicPass;
 
 class UnilendTranslationBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new MakeSonataCacheSymfonyPublicPass());
+    }
 }
