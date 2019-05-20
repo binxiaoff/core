93934d555cc12f22efff4fec9b234f5b1c34553d
Bin XIAO <bin.xiao@lafabriquebyca.com>
2019-05-20 10:19:26 +0200

    CALS-111 Update project form's translations

---
+++ src/Migrations/2019/Version20190520081257.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190520081257 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'CALS-111 Update project form translations';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('INSERT IGNORE INTO translations (locale, section, name, translation, added) VALUES
+                        (\'fr_FR\', \'project-form\', \'lead_manager\', \'Chef de file\', NOW())');
+        $this->addSql('UPDATE translations set translation = \'RUN emprunteur\' WHERE section = \'project-form\' AND name = \'run\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        /// this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('DELETE FROM translations WHERE section = \'project-form\' AND name = \'lead_manager\'');
+        $this->addSql('UPDATE translations set translation = \'RUN\' WHERE section = \'project-form\' AND name = \'run\'');
+    }
+}
