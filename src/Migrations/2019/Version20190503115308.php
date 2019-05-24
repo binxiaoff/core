8a0cc2ee514c05927a5e1567cafa52b889dc5f79
Bin XIAO <bin.xiao@lafabriquebyca.com>
2019-05-24 15:45:04 +0200

    Correct a migration scripte

--- src/Migrations/2019/Version20190503115308.php
+++ src/Migrations/2019/Version20190503115308.php
@@ -22,12 +22,14 @@ final class Version20190503115308 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE attachment DROP FOREIGN KEY FK_795FD9BBE173B1B8');
         $this->addSql('ALTER TABLE attachment ADD id_client_owner INT DEFAULT NULL, ADD id_company_owner INT DEFAULT NULL, ADD description VARCHAR(191) DEFAULT NULL, ADD archived_by INT DEFAULT NULL, ADD updated_by INT DEFAULT NULL, CHANGE original_name original_name VARCHAR(191) NOT NULL, CHANGE archived archived DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE id_client added_by INT NOT NULL');
         $this->addSql('ALTER TABLE attachment RENAME INDEX id_client TO IDX_795FD9BB699B6BAF');
         $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BB7E3C61F9 FOREIGN KEY (id_client_owner) REFERENCES clients (id_client)');
         $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BB66AB7494 FOREIGN KEY (id_company_owner) REFERENCES companies (id_company)');
         $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BB141E829E FOREIGN KEY (archived_by) REFERENCES clients (id_client)');
         $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BBE8DE7170 FOREIGN KEY (updated_by) REFERENCES clients (id_client)');
+        $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BBE7CA843C FOREIGN KEY (added_by) REFERENCES clients (id_client)');
         $this->addSql('CREATE INDEX IDX_795FD9BB4FCC0FB9 ON attachment (id_client_owner)');
         $this->addSql('CREATE INDEX IDX_795FD9BB36B9957C ON attachment (id_company_owner)');
         $this->addSql('CREATE INDEX IDX_795FD9BB51B07D6D ON attachment (archived_by)');
