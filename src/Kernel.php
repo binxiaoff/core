80abdebbe305b58dc150d9e4c2b9eccbf906d20a
Bin XIAO <bin.xiao@lafabriquebyca.com>
2019-06-03 18:29:31 +0200

    Upgrade to Symfony 4.3.0 stable version Apply the last flex recipes changes Continue to use symfony/doctrine-bridge 4.2.8, till this pull request will be merged: https://github.com/symfony/symfony/pull/31811

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,29 +8,20 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
-use Unilend\DependencyInjection\Compiler\AddDbalCacheConfigurationPass;
-use Unilend\DependencyInjection\Compiler\MakeSonataCacheSymfonyPublicPass;
-use Unilend\DependencyInjection\Compiler\OverrideNewRelicSettingsForConsolePass;
+use Unilend\DependencyInjection\Compiler\{AddDbalCacheConfigurationPass, MakeSonataCacheSymfonyPublicPass, OverrideNewRelicSettingsForConsolePass};
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    public function getCacheDir()
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles(): iterable
     {
-        return $this->getProjectDir().'/var/cache/'.$this->environment;
-    }
-
-    public function getLogDir()
-    {
-        return $this->getProjectDir().'/var/log';
-    }
-
-    public function registerBundles()
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
+        $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
@@ -38,29 +29,17 @@ class Kernel extends BaseKernel
         }
     }
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    /**
+     * {@inheritdoc}
+     */
+    public function getProjectDir(): string
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
-
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services_legacy}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{zombies}'.self::CONFIG_EXTS, 'glob');
+        return \dirname(__DIR__);
     }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes)
-    {
-        $confDir = $this->getProjectDir().'/config';
-
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
@@ -68,5 +47,34 @@ class Kernel extends BaseKernel
         $container->addCompilerPass(new OverrideNewRelicSettingsForConsolePass());
         $container->addCompilerPass(new MakeSonataCacheSymfonyPublicPass());
         $container->addCompilerPass(new AddDbalCacheConfigurationPass());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', true);
+        $confDir = $this->getProjectDir() . '/config';
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services_legacy}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{zombies}' . self::CONFIG_EXTS, 'glob');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        $confDir = $this->getProjectDir() . '/config';
+
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
     }
 }
