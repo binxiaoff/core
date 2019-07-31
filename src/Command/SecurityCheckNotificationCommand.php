ea68845871b7975c3c06154dd6a867b0609fcb6c
Antoine Préveaux <antoine.preveaux@ca-lendingservices.com>
2019-07-31 14:38:24 +0200

    TECH-72 Remove Unilend commands

--- src/Command/SecurityCheckNotificationCommand.php
+++ src/Command/SecurityCheckNotificationCommand.php
@@ -2,18 +2,76 @@
 
 namespace Unilend\Command;
 
+use Exception;
+use Psr\Log\LoggerInterface;
 use SensioLabs\Security\SecurityChecker;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Swift_Mailer;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Unilend\Entity\Settings;
+use Unilend\Repository\SettingsRepository;
+use Unilend\Service\SlackManager;
+use Unilend\SwiftMailer\TemplateMessageProvider;
 
-class SecurityCheckNotificationCommand extends ContainerAwareCommand
+/**
+ * @todo In order to make it work, some configuration is needed
+ *     - Slack manager must be configured
+ *     - Slack channel must be created and configured
+ *     - "SECURITY_CHECK_NOTIFICATION_EMAIL" setting must be defined
+ *     - "notification-security-check" email must be created
+ */
+class SecurityCheckNotificationCommand extends Command
 {
+    private const SLACK_CHANNEL = '#to-be-configured';
+
+    /** @var string */
+    private $projectDirectory;
+    /** @var TemplateMessageProvider */
+    private $templateMessageProvider;
+    /** @var Swift_Mailer */
+    private $mailer;
+    /** @var SlackManager */
+    private $slackManager;
+    /** @var SettingsRepository */
+    private $settingsRepository;
+    /** @var LoggerInterface */
+    private $logger;
+
+    /**
+     * @param string                  $projectDirectory
+     * @param TemplateMessageProvider $templateMessageProvider
+     * @param Swift_Mailer            $mailer
+     * @param SlackManager            $slackManager
+     * @param SettingsRepository      $settingsRepository
+     * @param LoggerInterface         $consoleLogger
+     */
+    public function __construct(
+        string $projectDirectory,
+        TemplateMessageProvider $templateMessageProvider,
+        Swift_Mailer $mailer,
+        SlackManager $slackManager,
+        SettingsRepository $settingsRepository,
+        LoggerInterface $consoleLogger
+    ) {
+        $this->projectDirectory        = $projectDirectory;
+        $this->templateMessageProvider = $templateMessageProvider;
+        $this->mailer                  = $mailer;
+        $this->slackManager            = $slackManager;
+        $this->settingsRepository      = $settingsRepository;
+        $this->logger                  = $consoleLogger;
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
-        $this->setName('unilend:security:checker:notify')
-            ->setDescription('Check for known security issues for the packages managed by composer via SensioLabs Security Advisories Checker and notify the IT when finding vulnerabilities)');
+        $this
+            ->setName('unilend:security:checker:notify')
+            ->setDescription('Check known security issues in the packages managed by Composer using SensioLabs Security Advisories Checker')
+        ;
     }
 
     /**
@@ -21,47 +79,50 @@ class SecurityCheckNotificationCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $checker         = new SecurityChecker();
-        $lockFile        = $this->getContainer()->get('kernel')->getProjectDir() . DIRECTORY_SEPARATOR . 'composer.lock';
-        $vulnerabilities = $checker->check($lockFile);
+        $checker     = new SecurityChecker();
+        $lockFile    = $this->projectDirectory . DIRECTORY_SEPARATOR . 'composer.lock';
+        $checkResult = $checker->check($lockFile);
 
-        if ($checker->getLastVulnerabilityCount() > 0) {
-            $content = 'Vulnerabilities detected in current composer.lock:' . PHP_EOL;
-            foreach ($vulnerabilities as $packageName => $vulnerability) {
-                $content .= $packageName . ': ';
-                foreach ($vulnerability['advisories'] as $advisory) {
-                    $content .= $advisory['title'] . '(' . $advisory['cve'] . '). ';
-                }
-                $content .= PHP_EOL;
-            }
-            $slackManager = $this->getContainer()->get('unilend.service.slack_manager');
-            $slackManager->sendMessage($content, '#it-monitoring');
-
-            $addressEmail = $this->getContainer()->get('doctrine.orm.entity_manager')->getRepository(Settings::class)->findOneBy(['type' => 'Adresse email IT']);
-
-            if ($addressEmail) {
-                $message = $this->getContainer()->get('unilend.swiftmailer.message_provider')->newMessage('notification-security-check', ['content' => nl2br($content)]);
-                try {
-                    $message->setTo($addressEmail->getValue());
-                    $mailer = $this->getContainer()->get('mailer');
-                    $mailer->send($message);
-                } catch (\Exception $exception) {
-                    $this->getContainer()->get('monolog.logger.console')->error(
-                        'Could not send email : notification-security-check - Exception: ' . $exception->getMessage(),
-                        [
-                            'id_mail_template' => $message->getTemplateId(),
-                            'email'            => $addressEmail->getVaue(),
-                            'class'            => __CLASS__,
-                            'function'         => __FUNCTION__,
-                            'file'             => $exception->getFile(),
-                            'line'             => $exception->getLine()
-                        ]
-                    );
-                }
-            }
-            $output->writeln('Vulnerabilities detected. See Slack or email for more details.');
-        } else {
+        if (0 === $checkResult->count()) {
             $output->writeln('No vulnerability detected');
+
+            return;
         }
+
+        $content         = 'Vulnerabilities detected in current composer.lock:' . PHP_EOL;
+        $vulnerabilities = json_decode($checkResult, true);
+
+        foreach ($vulnerabilities as $packageName => $vulnerability) {
+            $content .= $packageName . ': ';
+            foreach ($vulnerability['advisories'] as $advisory) {
+                $content .= $advisory['title'] . '(' . $advisory['cve'] . '). ';
+            }
+            $content .= PHP_EOL;
+        }
+
+        $this->slackManager->sendMessage($content, self::SLACK_CHANNEL);
+
+        $emailSetting = $this->settingsRepository->findOneBy(['type' => 'SECURITY_CHECK_NOTIFICATION_EMAIL']);
+
+        if ($emailSetting) {
+            $recipient = $emailSetting->getValue();
+            $message   = $this->templateMessageProvider->newMessage('notification-security-check', ['content' => nl2br($content)]);
+
+            try {
+                $message->setTo($recipient);
+                $this->mailer->send($message);
+            } catch (Exception $exception) {
+                $this->logger->error(sprintf('Could not send email "notification-security-check". Error: %s', $exception->getMessage()), [
+                    'id_mail_template' => $message->getTemplateId(),
+                    'email'            => $recipient,
+                    'class'            => __CLASS__,
+                    'function'         => __FUNCTION__,
+                    'file'             => $exception->getFile(),
+                    'line'             => $exception->getLine(),
+                ]);
+            }
+        }
+
+        $output->writeln('Vulnerabilities detected. See Slack or email for more details.');
     }
 }
--- src/Command/SendMailCommand.php
+++ src/Command/SendMailCommand.php
@@ -1,16 +1,36 @@
 <?php
+
 namespace Unilend\Command;
 
+use Psr\Log\LoggerInterface;
+use Swift_Mailer;
+use Swift_Transport_SpoolTransport;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\{InputInterface, InputOption};
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Unilend\SwiftMailer\DatabaseSpool;
 
 class SendMailCommand extends ContainerAwareCommand
 {
+    /** @var Swift_Mailer */
+    private $mailer;
+    /** @var LoggerInterface */
+    private $logger;
+
     /**
-     * @see Command
+     * @param Swift_Mailer    $mailer
+     * @param LoggerInterface $consoleLogger
+     */
+    public function __construct(Swift_Mailer $mailer, LoggerInterface $consoleLogger)
+    {
+        $this->mailer = $mailer;
+        $this->logger = $consoleLogger;
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
      */
     protected function configure()
     {
@@ -19,11 +39,13 @@ class SendMailCommand extends ContainerAwareCommand
             ->setDescription('Sends emails from the spool')
             ->addOption('message-limit', null, InputOption::VALUE_OPTIONAL, 'The maximum number of messages to send.')
             ->addOption('time-limit', null, InputOption::VALUE_OPTIONAL, 'The time limit for sending messages (in seconds).')
-            ->setHelp(<<<EOF
+            ->setHelp(
+                <<<'EOF'
 The <info>mailer:spool:send</info> command sends all emails from the spool.
 <info>php bin/console mailer:spool:send --message-limit=10 --time-limit=10</info>
 EOF
-            );
+            )
+        ;
     }
 
     /**
@@ -31,9 +53,9 @@ EOF
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $mailer    = $this->getContainer()->get('mailer');
-        $transport = $mailer->getTransport();
-        if ($transport instanceof \Swift_Transport_SpoolTransport) {
+        $transport = $this->mailer->getTransport();
+
+        if ($transport instanceof Swift_Transport_SpoolTransport) {
             $spool = $transport->getSpool();
 
             if ($spool instanceof DatabaseSpool) {
