4a0908b8ade7f201f2ade500eac5833b9379e279
Antoine Préveaux <antoine.preveaux@ca-lendingservices.com>
2019-08-21 12:33:48 +0200

    TECH-80 Delete CMS

--- apps/admin/bootstrap.php
+++ apps/admin/bootstrap.php
@@ -2,17 +2,11 @@
 
 class bootstrap extends Controller
 {
-    private const ZONE_LABEL_CONFIGURATION = 'configuration';
-    private const ZONE_LABEL_EDITION       = 'edition';
-
-    /** @var string */
-    protected $staticsKey;
-
     public function initialize()
     {
         parent::initialize();
 
-        $this->staticsKey = (string) filemtime(__FILE__);
+        $staticsKey = (string) filemtime(__FILE__);
 
         $this->loadJs('jquery');
         $this->loadJs('jquery-ui/jquery-ui.min');
@@ -21,14 +15,14 @@ class bootstrap extends Controller
         $this->loadJs('colorbox/jquery.colorbox-min');
         $this->loadJs('tablesorter/jquery.tablesorter.min');
         $this->loadJs('tablesorter/jquery.tablesorter.pager');
-        $this->loadJs('main', $this->staticsKey);
+        $this->loadJs('main', $staticsKey);
 
         $this->loadCss('bootstrap');
         $this->loadCss('../scripts/freeow/freeow');
         $this->loadCss('../scripts/colorbox/colorbox');
         $this->loadCss('../scripts/tablesorter/style');
         $this->loadCss('../scripts/jquery-ui/jquery-ui.min');
-        $this->loadCss('main', $this->staticsKey);
+        $this->loadCss('main', $staticsKey);
 
         $this->nb_lignes = 100;
     }
