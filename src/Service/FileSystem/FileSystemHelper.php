4f465f7f24ff3dfdc2516bd31da227de971f599f
Bin XIAO <bin.xiao@ca-lendingservices.com>
2019-09-11 15:33:43 +0200

    CALS-336 Install and configure Flysystem AWS S3 adapter

--- src/Service/FileSystem/FileSystemHelper.php
+++ src/Service/FileSystem/FileSystemHelper.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Unilend\Service\FileSystem;
 
-use League\Flysystem\FileExistsException;
-use League\Flysystem\FilesystemInterface;
+use League\Flysystem\{FileExistsException, FileNotFoundException, FilesystemInterface};
 use RuntimeException;
+use Symfony\Component\HttpFoundation\{ResponseHeaderBag, StreamedResponse};
+use URLify;
 
 class FileSystemHelper
 {
@@ -31,5 +32,27 @@ class FileSystemHelper
         }
 
         unlink($srcFilePath);
+    }
+
+    /**
+     * @param FilesystemInterface $filesystem
+     * @param string              $filePath
+     * @param string|null         $fileName
+     *
+     * @throws FileNotFoundException
+     *
+     * @return StreamedResponse
+     */
+    public function download(FilesystemInterface $filesystem, string $filePath, ?string $fileName = null): StreamedResponse
+    {
+        $response = new StreamedResponse(static function () use ($filePath, $filesystem) {
+            stream_copy_to_stream($filesystem->readStream($filePath), fopen('php://output', 'w+b'));
+        });
+
+        $contentDisposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, URLify::downcode($fileName ?? pathinfo($filePath, PATHINFO_FILENAME)));
+        $response->headers->set('Content-Disposition', $contentDisposition);
+        $response->headers->set('Content-Type', $filesystem->getMimetype($filePath) ?: 'application/octet-stream');
+
+        return $response;
     }
 }
