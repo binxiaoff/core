5c1fb5168c2fa663d0d7add14a5fab6c038953f9
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2019-09-23 09:50:51 +0200

    CALS-287 Code review feedback Delete MailerManager & EntityManager from ClientsListener

--- src/Listener/Entity/ClientsListener.php
+++ src/Listener/Entity/ClientsListener.php
@@ -4,37 +4,27 @@ declare(strict_types=1);
 
 namespace Unilend\Listener\Entity;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Unilend\Entity\Clients;
 use Unilend\Message\Client\ClientUpdated;
-use Unilend\Service\MailerManager;
 
 class ClientsListener
 {
-    /** @var MailerManager */
-    private $mailerManager;
-    /** @var EntityManagerInterface */
-    private $entityManager;
     /** @var MessageBusInterface */
     private $messageBus;
     /** @var TranslatorInterface */
     private $translator;
 
     /**
-     * @param MailerManager          $mailerManager
-     * @param EntityManagerInterface $entityManager
-     * @param MessageBusInterface    $messageBus
-     * @param TranslatorInterface    $translator
+     * @param MessageBusInterface $messageBus
+     * @param TranslatorInterface $translator
      */
-    public function __construct(MailerManager $mailerManager, EntityManagerInterface $entityManager, MessageBusInterface $messageBus, TranslatorInterface $translator)
+    public function __construct(MessageBusInterface $messageBus, TranslatorInterface $translator)
     {
-        $this->mailerManager = $mailerManager;
-        $this->entityManager = $entityManager;
-        $this->messageBus    = $messageBus;
-        $this->translator    = $translator;
+        $this->messageBus = $messageBus;
+        $this->translator = $translator;
     }
 
     /**
