5f21fea12e6b2f130b3d7287c11ea2c7846f3aca
Bin Xiao <bin.xiao@ca-lendingservices.com>
2019-09-30 10:24:00 +0200

    CALS-363 Improve the comparaison of phone number fields

--- src/Listener/Entity/ClientsListener.php
+++ src/Listener/Entity/ClientsListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Unilend\Listener\Entity;
 
 use Doctrine\ORM\Event\PreUpdateEventArgs;
+use libphonenumber\PhoneNumber;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Unilend\Entity\Clients;
 use Unilend\Message\Client\ClientUpdated;
@@ -36,12 +37,13 @@ class ClientsListener
      */
     public function preUpdate(Clients $client, PreUpdateEventArgs $args)
     {
-        $changeSet   = $args->getEntityChangeSet();
-        $phoneFields = ['mobile', 'phone'];
+        $changeSet = $args->getEntityChangeSet();
 
         foreach ($changeSet as $updatedField => $newValue) {
-            if (false === in_array($updatedField, self::MONITORED_FIELDS)
-                || (in_array($updatedField, $phoneFields) && $args->getOldValue($updatedField)->equals($args->getNewValue($updatedField)))) {
+            if (false === in_array($updatedField, self::MONITORED_FIELDS, true)
+                || ($args->getOldValue($updatedField) instanceof PhoneNumber
+                    && $args->getNewValue($updatedField) instanceof PhoneNumber
+                    && $args->getOldValue($updatedField)->equals($args->getNewValue($updatedField)))) {
                 unset($changeSet[$updatedField]);
             }
         }
