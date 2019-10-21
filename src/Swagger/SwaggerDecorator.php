afe5c33ae5ddbebcc60bafe4c9e490b115af1447
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2019-10-21 13:57:08 +0200

    TECH-166 Add authentication and refresh token route in documentation

--- src/Swagger/SwaggerDecorator.php
+++ src/Swagger/SwaggerDecorator.php
@@ -36,6 +36,111 @@ class SwaggerDecorator implements NormalizerInterface
         $docs['info']['title']   = 'KLS';
         $docs['info']['version'] = '1.0.0';
 
+        $docs['paths']['/authentication_token'] = [
+            'post' => [
+                'tags'        => ['Authentication'],
+                'operationId' => 'postAuthenticationToken',
+                'summary'     => 'Log in and get an authentication token',
+                'requestBody' => [
+                    'content' => [
+                        'application/json' => [
+                            'schema' => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'username' => [
+                                        'description' => 'Username of the user',
+                                        'type'        => 'string',
+                                        'format'      => 'email',
+                                    ],
+                                    'password' => [
+                                        'description' => 'Password of the user',
+                                        'type'        => 'string',
+                                    ],
+                                ],
+                                'required' => ['username', 'password'],
+                            ],
+                        ],
+                    ],
+                ],
+                'responses' => [
+                    '200' => [
+                        'description' => 'Login succeded',
+                        'content'     => [
+                            'application/json' => [
+                                'schema' => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'token' => [
+                                            'description' => 'JWT Token',
+                                            'type'        => 'string',
+                                        ],
+                                        'refresh_token' => [
+                                            'description' => 'Refresh Token',
+                                            'type'        => 'string',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    '401' => [
+                        'description' => 'Bad credentials',
+                        'content'     => [
+                            'application/json' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $docs['paths']['/token/refresh'] = [
+            'post' => [
+                'tags'        => ['Authentication'],
+                'operationId' => 'postRefreshToken',
+                'summary'     => 'Refresh valid token',
+                'requestBody' => [
+                    'content' => [
+                        'application/x-www-form-urlencoded' => [
+                            'schema' => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'refresh_token' => [
+                                        'description' => 'JWT Refresh Token',
+                                        'type'        => 'string',
+                                    ],
+                                ],
+                                'required' => ['refresh_token'],
+                            ],
+                        ],
+                    ],
+                ],
+                'responses' => [
+                    '200' => [
+                        'description' => 'Refresh succeded',
+                        'content'     => [
+                            'application/x-form-' => [
+                                'schema' => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'refresh_token' => [
+                                            'description' => 'JWT Refresh Token',
+                                            'type'        => 'string',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    '401' => [
+                        'description' => 'Refresh token don\'t exist',
+                        'content'     => [
+                            'application/json' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
         return $docs;
     }
 
