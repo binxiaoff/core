bc459d79fdf652f597f4d6ef800e610237b340fd
Bin Xiao <bin.xiao@ca-lendingservices.com>
2019-12-03 16:23:18 +0100

    Remove unused "use" statement

--- src/EventSubscriber/JWT/JWTCreatedSubscriber.php
+++ src/EventSubscriber/JWT/JWTCreatedSubscriber.php
@@ -8,7 +8,6 @@ use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Events as JwtEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Unilend\Entity\Clients;
 use Unilend\Repository\ClientsRepository;
@@ -58,7 +57,8 @@ class JWTCreatedSubscriber implements EventSubscriberInterface
             $payload['hash']      = $user->getHash();
             $payload['firstName'] = $user->getFirstName();
             $payload['lastName']  = $user->getLastName();
-            $payload['company']   = $this->serializer->normalize($user->getStaff()->getCompany());
+            //todo: put the exact fields
+            $payload['company'] = $this->serializer->normalize($user->getStaff()->getCompany());
             $event->setData($payload);
         }
     }
