e5370e57d4e5b53919ceab7ef5a43c28d3575766
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2020-01-16 10:31:35 +0100

    CALS-751 Apply codereview feedback

--- src/Migrations/2020/Version20200116092533.php
+++ src/Migrations/2020/Version20200116092533.php
@@ -7,21 +7,18 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20200116092533 extends AbstractMigration
 {
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
-        return '';
+        return 'CALS-751 Log attachment downloads';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('CREATE TABLE attachment_download (id INT AUTO_INCREMENT NOT NULL, id_attachment INT NOT NULL, id_client INT NOT NULL, added DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7C093130DCD5596C (id_attachment), INDEX IDX_7C093130E173B1B8 (id_client), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE attachment_download ADD CONSTRAINT FK_7C093130DCD5596C FOREIGN KEY (id_attachment) REFERENCES attachment (id)');
         $this->addSql('ALTER TABLE attachment_download ADD CONSTRAINT FK_7C093130E173B1B8 FOREIGN KEY (id_client) REFERENCES clients (id_client)');
@@ -29,9 +26,6 @@ final class Version20200116092533 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP TABLE attachment_download');
     }
 }
