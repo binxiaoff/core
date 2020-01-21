69712ee89196ba5cbf718e5bd7f4df3afe7a6ded
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-01-21 15:00:47 +0100

    CALS-549 Add a user of an entity

--- src/Security/Voter/StaffVoter.php
+++ src/Security/Voter/StaffVoter.php
@@ -75,7 +75,7 @@ class StaffVoter extends Voter
      */
     private function canCreate(Staff $subject, Staff $submitterStaff): bool
     {
-        // A manager cannot create a user with markets other than is own
+        // A manager cannot create a staff with markets other than is own. But we can create a staff without market segment (used for invitation via email)
         return 0 === $subject->getMarketSegments()->count()
             || $subject->getMarketSegments()->forAll(static function ($key, MarketSegment $marketSegment) use ($submitterStaff) {
                 return $submitterStaff->getMarketSegments()->contains($marketSegment);
