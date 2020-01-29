92077e36a56cceca9e0a28e423af63bce5409d5a
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-01-29 18:41:21 +0100

    Put CALS as signed company

--- src/Migrations/2020/Version20200129143138.php
+++ src/Migrations/2020/Version20200129143138.php
@@ -17,7 +17,7 @@ final class Version20200129143138 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE company_status (id INT AUTO_INCREMENT NOT NULL, id_company INT NOT NULL, status SMALLINT NOT NULL, added DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX idx_company_status_id_client (id_company), INDEX idx_company_status_status (status), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('INSERT INTO company_status(id_company, status, added) SELECT id, 10, NOW() FROM companies WHERE id_parent_company = 1');
+        $this->addSql('INSERT INTO company_status(id_company, status, added) SELECT id, 10, NOW() FROM companies WHERE id_parent_company = 1 OR id = 1');
         $this->addSql('ALTER TABLE company_status ADD CONSTRAINT FK_469F01699122A03F FOREIGN KEY (id_company) REFERENCES companies (id)');
         $this->addSql('ALTER TABLE companies ADD id_current_status INT DEFAULT NULL');
         $this->addSql('ALTER TABLE companies ADD CONSTRAINT FK_8244AA3A41AF0274 FOREIGN KEY (id_current_status) REFERENCES company_status (id)');
