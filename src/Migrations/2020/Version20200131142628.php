9208314e6044e9f7ce3c484eda45314dc5091f41
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-01-31 16:20:02 +0100

    CALS-853 Change tranche_offer floor type to nullable

---
+++ src/Migrations/2020/Version20200131142628.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200131142628 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'CALS-853 change the rate_floor_type in tranche_offer to be nullable';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tranche_offer CHANGE rate_floor_type rate_floor_type VARCHAR(20) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tranche_offer CHANGE rate_floor_type rate_floor_type VARCHAR(20) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
