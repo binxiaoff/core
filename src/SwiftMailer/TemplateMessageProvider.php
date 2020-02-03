fb59bf0dc36d7056e752a002c022d31ff12299fb
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-02-03 18:59:41 +0100

    Change mjml renderer

--- src/SwiftMailer/TemplateMessageProvider.php
+++ src/SwiftMailer/TemplateMessageProvider.php
@@ -6,6 +6,7 @@ namespace Unilend\SwiftMailer;
 
 use Doctrine\ORM\EntityManagerInterface;
 use InvalidArgumentException;
+use NotFloran\MjmlBundle\Renderer\BinaryRenderer;
 use Psr\Log\LoggerInterface;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -24,20 +25,25 @@ class TemplateMessageProvider
     private $logger;
     /** @var Environment */
     private $twig;
+    /** @var BinaryRenderer */
+    private $mjmlRenderer;
 
     /**
      * @param MailTemplateRepository $mailTemplateRepository
+     * @param BinaryRenderer         $mjmlRenderer
      * @param Environment            $twig
      * @param string                 $defaultLocale
      */
     public function __construct(
         MailTemplateRepository $mailTemplateRepository,
+        BinaryRenderer $mjmlRenderer,
         Environment $twig,
         string $defaultLocale
     ) {
         $this->mailTemplateRepository = $mailTemplateRepository;
         $this->defaultLocale          = $defaultLocale;
         $this->twig                   = $twig;
+        $this->mjmlRenderer           = $mjmlRenderer;
     }
 
     /**
@@ -104,7 +110,7 @@ class TemplateMessageProvider
     {
         $senderName = $this->twig->createTemplate($mailTemplate->getSenderName())->render($context);
         $subject    = $this->twig->createTemplate($mailTemplate->getSubject())->render($context);
-        $body       = $this->twig->render($mailTemplate->getName(), $context);
+        $body       = $this->mjmlRenderer->render($this->twig->render($mailTemplate->getName(), $context));
 
         /** @var TemplateMessage $message */
         $message = new TemplateMessage($mailTemplate);
@@ -113,7 +119,7 @@ class TemplateMessageProvider
             ->setFrom($mailTemplate->getSenderEmail(), $senderName)
             ->setReplyTo($mailTemplate->getSenderEmail(), $senderName)
             ->setSubject($subject)
-            ->setBody($body, 'text/mjml')
+            ->setBody($body, 'text/html')
         ;
 
         if ($this->logger instanceof LoggerInterface) {
