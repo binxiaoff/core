c0ab9439a45e400bfd480caecb38a7d129b3fda6
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2020-02-12 15:18:19 +0100

    CALS-797 CR

--- src/Service/Project/ProjectNotifier.php
+++ src/Service/Project/ProjectNotifier.php
@@ -4,49 +4,20 @@ declare(strict_types=1);
 
 namespace Unilend\Service\Project;
 
-use Doctrine\ORM\{ORMException, OptimisticLockException};
-use Exception;
 use Psr\Log\{LogLevel, LoggerInterface};
-use Unilend\Entity\{Project, ProjectStatus};
-use Unilend\Service\ProjectParticipation\ProjectParticipationNotifier;
+use Unilend\Entity\{Project};
 
 class ProjectNotifier
 {
-    /** @var ProjectParticipationNotifier */
-    private $projectParticipationNotifier;
     /** @var LoggerInterface */
     private $supportLogger;
 
     /**
-     * @param ProjectParticipationNotifier $projectParticipationNotifier
-     * @param LoggerInterface              $supportLogger
+     * @param LoggerInterface $supportLogger
      */
-    public function __construct(ProjectParticipationNotifier $projectParticipationNotifier, LoggerInterface $supportLogger)
+    public function __construct(LoggerInterface $supportLogger)
     {
-        $this->projectParticipationNotifier = $projectParticipationNotifier;
-        $this->supportLogger                = $supportLogger;
-    }
-
-    /**
-     * @param Project $project
-     *
-     * @throws ORMException
-     * @throws OptimisticLockException
-     * @throws Exception
-     *
-     * @return int
-     */
-    public function notifyProjectPublished(Project $project): int
-    {
-        $sent = 0;
-        if (ProjectStatus::STATUS_PUBLISHED === $project->getCurrentStatus()->getStatus()) {
-            $participants = $project->getProjectParticipations();
-            foreach ($participants as $participant) {
-                $sent += $this->projectParticipationNotifier->notifyParticipantInvited($participant);
-            }
-        }
-
-        return $sent;
+        $this->supportLogger = $supportLogger;
     }
 
     /**
