0eb3291807cad04a6b19d8f91ba0593093d38a36
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2020-02-14 10:42:28 +0100

    CALS-973 Fix migration down

--- src/Migrations/2020/Version20200213135717.php
+++ src/Migrations/2020/Version20200213135717.php
@@ -32,10 +32,10 @@ final class Version20200213135717 extends AbstractMigration
     {
         $this->addSql('RENAME TABLE company TO companies');
 
-        $this->addSql('ALTER TABLE company RENAME INDEX idx_4fbf094f91c00f TO IDX_8244AA3A91C00F');
-        $this->addSql('ALTER TABLE company RENAME INDEX uniq_4fbf094f17d2fe0d TO UNIQ_8244AA3A17D2FE0D');
-        $this->addSql('ALTER TABLE company RENAME INDEX uniq_4fbf094f41af0274 TO UNIQ_8244AA3A41AF0274');
-        $this->addSql('ALTER TABLE company RENAME INDEX uniq_4fbf094fb5b48b91 TO UNIQ_8244AA3AB5B48B91');
-        $this->addSql('ALTER TABLE company RENAME INDEX uniq_4fbf094fda33cdfb TO UNIQ_8244AA3ADA33CDFB');
+        $this->addSql('ALTER TABLE companies RENAME INDEX idx_4fbf094f91c00f TO IDX_8244AA3A91C00F');
+        $this->addSql('ALTER TABLE companies RENAME INDEX uniq_4fbf094f17d2fe0d TO UNIQ_8244AA3A17D2FE0D');
+        $this->addSql('ALTER TABLE companies RENAME INDEX uniq_4fbf094f41af0274 TO UNIQ_8244AA3A41AF0274');
+        $this->addSql('ALTER TABLE companies RENAME INDEX uniq_4fbf094fb5b48b91 TO UNIQ_8244AA3AB5B48B91');
+        $this->addSql('ALTER TABLE companies RENAME INDEX uniq_4fbf094fda33cdfb TO UNIQ_8244AA3ADA33CDFB');
     }
 }
