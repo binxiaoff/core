1bf2a96c08e60477c10b8181d2b91bd8219b73c3
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-02-26 17:23:52 +0100

    CALS-1187 Add XML Signer

---
+++ src/Service/PSN/XmlSigner.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Service\PSN;
+
+use DOMDocument;
+use Exception;
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+
+class XmlSigner
+{
+    private const CANONICAL_METHOD      = XMLSecurityDSig::EXC_C14N;
+    private const DIGEST_METHOD         = XMLSecurityDSig::SHA256;
+    private const PRIVATE_KEY_ALGORITHM = XMLSecurityKey::RSA_SHA256;
+
+    /**
+     * @param string $xmlSource
+     * @param string $privateKey
+     * @param string $publicKey
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    public function signe(string $xmlSource, string $privateKey, string $publicKey): string
+    {
+        $xml = new DOMDocument();
+        $xml->loadXML($xmlSource);
+
+        $xmlDSig = new XMLSecurityDSig();
+        $xmlDSig->setCanonicalMethod(self::CANONICAL_METHOD);
+        $xmlDSig->addReference($xml, self::DIGEST_METHOD, ['http://www.w3.org/2000/09/xmldsig#enveloped-signature']);
+
+        $xmlDSigKey = new XMLSecurityKey(self::PRIVATE_KEY_ALGORITHM, ['type' => 'private']);
+        $xmlDSigKey->loadKey($privateKey, true);
+
+        $xmlDSig->sign($xmlDSigKey);
+        $xmlDSig->add509Cert($publicKey);
+
+        $xmlDSig->appendSignature($xml->documentElement);
+
+        return $xml->saveXML();
+    }
+}
