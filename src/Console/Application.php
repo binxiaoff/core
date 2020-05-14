68ae283a4518b239291ce4f69e137a05b98044be
Bin Xiao <bin.xiao@ca-lendingservices.com>
2020-05-14 12:20:58 +0200

    CALS-954 Remove the shortcut of multi-process option since it has conflit with an option of the messenger:consume command (+ some code cleanup)

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -1,42 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Unilend\Console;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application as FrameworkBundleApplication;
 use Symfony\Component\Console\{Input\InputInterface, Input\InputOption, Output\OutputInterface};
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Lock\{Factory, Lock, Store\SemaphoreStore};
+use Symfony\Component\Lock\{Lock, LockFactory, Store\SemaphoreStore};
 
 class Application extends FrameworkBundleApplication
 {
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
     public function __construct(KernelInterface $kernel)
     {
         parent::__construct($kernel);
 
-        $this->getDefinition()->addOption(new InputOption('--multi-process', '-m', InputOption::VALUE_NONE, 'This is a multi process or a single process.'));
+        $this->getDefinition()->addOption(new InputOption('--multi-process', null, InputOption::VALUE_NONE, 'This is a multi process or a single process.'));
     }
 
     /**
-     * @inheritDoc
+     * {@inheritdoc}
      */
-    public function doRun(InputInterface $input, OutputInterface $output)
+    public function doRun(InputInterface $input, OutputInterface $output): int
     {
         $this->getKernel()->boot();
 
-        if (false === $input->hasParameterOption(['--multi-process', '-m'], false)) {
+        if (false === $input->hasParameterOption(['--multi-process'], false)) {
             $semaphoreStore = new SemaphoreStore();
-            $factory        = new Factory($semaphoreStore);
+            $factory        = new LockFactory($semaphoreStore);
             $commandName    = $this->getCommandName($input);
 
             if ($commandName) {
                 $lock = $factory->createLock($commandName);
 
                 if (false === $lock->acquire()) {
-                    $this->getKernel()->getContainer()->get('monolog.logger.console')->warning('The command ' . $this->getCommandName($input) . ' is already running in another process.');
-                    $output->writeln('The command ' . $this->getCommandName($input) . ' is already running in another process.');
+                    $logger  = $this->getKernel()->getContainer()->get('monolog.logger.console');
+                    $message = sprintf('The command %s is already running in another process.', $this->getCommandName($input));
+                    if ($logger) {
+                        $logger->warning($message);
+                    }
+                    $output->writeln($message);
 
                     return 0;
                 }
