f762cf328bb9a7d03a31028b7e00f502c7a6f9db
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2020-06-11 16:15:46 +0200

    Fix migration

--- src/Migrations/2020/Version20200611130530.php
+++ src/Migrations/2020/Version20200611130530.php
@@ -17,9 +17,9 @@ final class Version20200611130530 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql("UPDATE project SET target_arranger_participation_money_currency = NULL WHERE target_arranger_participation_money_currency = ''");
-        $this->addSql("UPDATE project SET target_arranger_participation_money_amount = NULL WHERE target_arranger_participation_money_amount = ''");
+        $this->addSql("UPDATE project SET target_arranger_participation_money_amount = NULL WHERE target_arranger_participation_money_amount = CAST('' AS DECIMAL)");
         $this->addSql("UPDATE project SET arrangement_commission_money_currency = NULL WHERE arrangement_commission_money_currency = ''");
-        $this->addSql("UPDATE project SET arrangement_commission_money_amount = NULL WHERE arrangement_commission_money_amount = ''");
+        $this->addSql("UPDATE project SET arrangement_commission_money_amount = NULL WHERE arrangement_commission_money_amount = CAST('' AS DECIMAL)");
     }
 
     public function down(Schema $schema): void
