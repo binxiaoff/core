5bcbb81d5a7c59a28bb2241ac1499d4e3aea51e0
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2020-06-16 10:19:39 +0200

    CALS-1537 Improve ParticipationContactVoter

--- src/Security/Voter/ProjectParticipationContactVoter.php
+++ src/Security/Voter/ProjectParticipationContactVoter.php
@@ -4,34 +4,39 @@ declare(strict_types=1);
 
 namespace Unilend\Security\Voter;
 
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Unilend\Entity\Clients;
-use Unilend\Entity\ProjectParticipation;
 use Unilend\Entity\ProjectParticipationContact;
 use Unilend\Entity\ProjectStatus;
 use Unilend\Repository\ProjectParticipationContactRepository;
+use Unilend\Service\ProjectParticipation\ProjectParticipationManager;
 
 class ProjectParticipationContactVoter extends AbstractEntityVoter
 {
     public const ATTRIBUTE_VIEW   = 'view';
     public const ATTRIBUTE_EDIT   = 'edit';
     public const ATTRIBUTE_CREATE = 'create';
-    /**
-     * @var ProjectParticipationContactRepository
-     */
+
+    /** @var ProjectParticipationContactRepository */
     private $projectParticipationContactRepository;
+    /** @var ProjectParticipationManager */
+    private $projectParticipationManager;
 
     /**
-     * ProjectParticipationContactVoter constructor.
-     *
      * @param AuthorizationCheckerInterface         $authorizationChecker
      * @param ProjectParticipationContactRepository $projectParticipationContactRepository
+     * @param ProjectParticipationManager           $projectParticipationManager
      */
-    public function __construct(AuthorizationCheckerInterface $authorizationChecker, ProjectParticipationContactRepository $projectParticipationContactRepository)
-    {
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        ProjectParticipationContactRepository $projectParticipationContactRepository,
+        ProjectParticipationManager $projectParticipationManager
+    ) {
         parent::__construct($authorizationChecker);
         $this->authorizationChecker                  = $authorizationChecker;
         $this->projectParticipationContactRepository = $projectParticipationContactRepository;
+        $this->projectParticipationManager           = $projectParticipationManager;
     }
 
     /**
@@ -60,36 +65,38 @@ class ProjectParticipationContactVoter extends AbstractEntityVoter
      * @param ProjectParticipationContact $subject
      * @param Clients                     $user
      *
+     * @throws NonUniqueResultException
+     *
      * @return bool
      */
     protected function canEdit(ProjectParticipationContact $subject, Clients $user)
     {
-        return $subject->getClient() === $user
-            || (
-                $this->isValidProjectParticipationContact($subject->getProjectParticipation(), $user)
-                && false === $subject->isArchived()
-            );
+        return $this->isParticipant($subject, $user);
     }
 
     /**
      * @param ProjectParticipationContact $subject
      * @param Clients                     $user
      *
+     * @throws NonUniqueResultException
+     *
      * @return bool
      */
     protected function canCreate(ProjectParticipationContact $subject, Clients $user)
     {
-        return $this->isValidProjectParticipationContact($subject->getProjectParticipation(), $user);
+        return $this->isParticipant($subject, $user);
     }
 
     /**
-     * @param ProjectParticipation $projectParticipation
-     * @param Clients              $user
+     * @param ProjectParticipationContact $subject
+     * @param Clients                     $user
+     *
+     * @throws NonUniqueResultException
      *
      * @return bool
      */
-    private function isValidProjectParticipationContact(ProjectParticipation $projectParticipation, Clients $user)
+    private function isParticipant(ProjectParticipationContact $subject, Clients $user)
     {
-        return null !== $this->projectParticipationContactRepository->findOneBy(['projectParticipation' => $projectParticipation, 'client' => $user, 'archived' => null]);
+        return $this->projectParticipationManager->isParticipant($user->getCurrentStaff(), $subject->getProjectParticipation()->getProject());
     }
 }
