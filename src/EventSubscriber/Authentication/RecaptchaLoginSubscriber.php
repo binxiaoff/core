47610e2109185107de5c6f7dc219fad0cf2890e6
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2020-07-10 18:55:21 +0200

    CALS-1313 Apply codereview feedback

--- src/EventSubscriber/Authentication/RecaptchaLoginSubscriber.php
+++ src/EventSubscriber/Authentication/RecaptchaLoginSubscriber.php
@@ -11,38 +11,26 @@ use Symfony\Component\Security\Http\SecurityEvents;
 use Unilend\Exception\Authentication\RecaptchaChallengeFailedException;
 use Unilend\Service\GoogleRecaptchaManager;
 
-class RecaptchaLoginListener implements EventSubscriberInterface
+class RecaptchaLoginSubscriber implements EventSubscriberInterface
 {
     private const CAPTCHA_VALUE_REQUEST_KEY = 'captchaValue';
     private GoogleRecaptchaManager $recaptchaManager;
-    private string $environment;
 
     /**
      * @param GoogleRecaptchaManager $recaptchaManager
-     * @param string                 $environment
      */
-    public function __construct(
-        GoogleRecaptchaManager $recaptchaManager,
-        string $environment
-    ) {
+    public function __construct(GoogleRecaptchaManager $recaptchaManager)
+    {
         $this->recaptchaManager = $recaptchaManager;
-        $this->environment      = $environment;
     }
 
     /**
      * @param InteractiveLoginEvent $event
-     *
-     * @return bool
      */
-    public function verifyCaptcha(InteractiveLoginEvent $event)
+    public function verifyCaptcha(InteractiveLoginEvent $event): void
     {
-        // Condition to allow to test in development environment with postman without having to have a captcha token
-        if ('development' === $this->environment) {
-            return true;
-        }
-
         $request      = $event->getRequest();
-        $captchaValue = $request->get(static::CAPTCHA_VALUE_REQUEST_KEY);
+        $captchaValue = json_decode($request->getContent(), true)[static::CAPTCHA_VALUE_REQUEST_KEY] ?? null;
 
         try {
             if (false === $this->recaptchaManager->isValid($captchaValue)) {
@@ -51,14 +39,12 @@ class RecaptchaLoginListener implements EventSubscriberInterface
         } catch (JsonException $exception) {
             throw new RecaptchaChallengeFailedException('The captcha challenge has failed', 0, $exception);
         }
-
-        return true;
     }
 
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             SecurityEvents::INTERACTIVE_LOGIN => 'verifyCaptcha',
--- src/Service/GoogleRecaptchaManager.php
+++ src/Service/GoogleRecaptchaManager.php
@@ -19,52 +19,66 @@ class GoogleRecaptchaManager
     /** @var LoggerInterface */
     private LoggerInterface $logger;
 
+    /** @var bool */
+    private bool $debug;
+
     /**
      * @param Client          $client
      * @param string          $secret
      * @param LoggerInterface $logger
+     * @param bool            $debug
      */
-    public function __construct(Client $client, string $secret, LoggerInterface $logger)
+    public function __construct(Client $client, string $secret, LoggerInterface $logger, bool $debug = false)
     {
         $this->client = $client;
         $this->secret = $secret;
         $this->logger = $logger;
+        $this->debug  = $debug;
     }
 
     /**
-     * @param string      $captchaResponse
+     * @param string|null $captchaResponse
      * @param string|null $remoteIp
      *
      * @throws JsonException
      *
      * @return bool
      */
-    public function isValid(string $captchaResponse, ?string $remoteIp = null): bool
+    public function isValid(?string $captchaResponse, ?string $remoteIp = null): bool
     {
-        $response = $this->verify($captchaResponse, $remoteIp);
-
-        if (200 !== $response->getStatusCode()) {
-            $this->logger->warning('Unable to check Google reCAPTCHA - Invalid response code: ' . $response->getStatusCode());
-
+        // Condition to allow to test in development environment with postman without having to have a captcha token
+        if ($this->debug) {
             return true;
         }
 
-        $content = $response->getBody()->getContents();
-        $content = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+        if ($captchaResponse) {
+            $response = $this->verify($captchaResponse, $remoteIp);
 
-        if (null === $content) {
-            $this->logger->warning('Unable to check Google reCAPTCHA - JSON could not be decoded');
+            if (200 !== $response->getStatusCode()) {
+                $this->logger->warning('Unable to check Google reCAPTCHA - Invalid response code: ' . $response->getStatusCode());
 
-            return true;
+                return true;
+            }
+
+            $content = $response->getBody()->getContents();
+            $content = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+            if (null === $content) {
+                $this->logger->warning('Unable to check Google reCAPTCHA - JSON could not be decoded');
+
+                return true;
+            }
+
+            if (false === isset($content['success'])) {
+                $this->logger->warning('Unable to check Google reCAPTCHA - "success" key not found');
+
+                return true;
+            }
+
+            return $content['success'];
         }
 
-        if (false === isset($content['success'])) {
-            $this->logger->warning('Unable to check Google reCAPTCHA - "success" key not found');
-
-            return true;
-        }
-
-        return $content['success'];
+        return false;
     }
 
     /**
