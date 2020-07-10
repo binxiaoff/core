04947ad1190b1d55e60e53d8ea2f92cacdd7625d
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2020-07-10 14:59:04 +0200

    CALS-1912 Update comment in migration

--- src/Migrations/2020/Version20200710131714.php
+++ src/Migrations/2020/Version20200710131714.php
@@ -782,22 +782,12 @@ final class Version20200710131714 extends AbstractMigration
         $this->addSql('ALTER TABLE company CHANGE short_code short_code VARCHAR(10) DEFAULT NULL');
 
         foreach (static::DATA as $datum) {
-            extract($datum, EXTR_OVERWRITE);
-            /**
-             * @var string $name
-             */
-            /**
-             * @var string $groupName
-             */
-            /**
-             * @var string $shortCode
-             */
-            /**
-             * @var string $bankCode
-             */
-            /**
-             * @var string $applicableVat
-             */
+            $name          = $datum['name'];
+            $groupName     = $datum['groupName'];
+            $shortCode     = $datum['shortCode'];
+            $bankCode      = $datum['bankCode'];
+            $applicableVat = $datum['applicableVat'];
+
             $id = null;
 
             if ($shortCode) {
