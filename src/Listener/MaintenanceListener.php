4ff8d25a9d617b1fc4ccba9c1614b33c00272dc5
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-09-03 17:17:24 +0200

    CALS-1185 Maintenance mode

---
+++ src/Listener/MaintenanceListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Unilend\Listener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class MaintenanceListener
+{
+
+    private bool $enabled;
+
+    public function __construct(bool $enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        if ($this->enabled) {
+            $event->setResponse(
+                new Response(
+                    'site is in maintenance mode',
+                    Response::HTTP_SERVICE_UNAVAILABLE
+                )
+            );
+            $event->stopPropagation();
+        }
+    }
+
+}
