723590071f1d2e9c9eaec2beb8740340351fed33
Jonathan Boyer <boyer.jonathan@gmail.com>
2020-09-24 16:16:39 +0200

    CALS-2447 Fix permission comment

--- src/Entity/ProjectParticipation.php
+++ src/Entity/ProjectParticipation.php
@@ -242,7 +242,8 @@ class ProjectParticipation implements TraceableStatusAwareInterface
      *
      * @Groups({
      *     ProjectParticipation::SERIALIZER_GROUP_SENSITIVE_READ,
-     *     ProjectParticipation::SERIALIZER_GROUP_PARTICIPATION_OWNER_OFFER_NEGOTIATION_WRITE
+     *     ProjectParticipation::SERIALIZER_GROUP_PARTICIPATION_OWNER_OFFER_NEGOTIATION_WRITE,
+     *     ProjectParticipation::SERIALIZER_GROUP_ARRANGER_ALLOCATION_WRITE
      * })
      */
     private ?string $committeeComment = null;
