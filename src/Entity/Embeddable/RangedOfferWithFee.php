48d154965d10093c19b90b5cd52a0bc6981780bc
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2020-10-16 17:47:27 +0200

    CALS-2703 Rename interest_request_min_money_amount into interest_request_max_money_amount

--- src/Entity/Embeddable/RangedOfferWithFee.php
+++ src/Entity/Embeddable/RangedOfferWithFee.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Unilend\Entity\Embeddable;
 
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Serializer\Annotation\Groups;
 
@@ -14,24 +15,24 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class RangedOfferWithFee extends OfferWithFee
 {
     /**
-     * @var NullableMoney
-     *
      * @ORM\Embedded(class="Unilend\Entity\Embeddable\NullableMoney")
      *
      * @Gedmo\Versioned
      *
      * @Groups({"rangedOfferWithFee:read", "rangedOfferWithFee:write"})
      */
-    private $minMoney;
+    private NullableMoney $maxMoney;
 
     /**
      * @param NullableMoney|null $money
      * @param string|null        $feeRate
-     * @param NullableMoney|null $minMoney
+     * @param NullableMoney|null $maxMoney
+     *
+     * @throws Exception
      */
-    public function __construct(?NullableMoney $money = null, ?string $feeRate = null, ?NullableMoney $minMoney = null)
+    public function __construct(?NullableMoney $money = null, ?string $feeRate = null, ?NullableMoney $maxMoney = null)
     {
-        $this->minMoney = $minMoney ?? new NullableMoney();
+        $this->maxMoney = $maxMoney ?? new NullableMoney();
 
         parent::__construct($money, $feeRate);
     }
@@ -39,8 +40,8 @@ class RangedOfferWithFee extends OfferWithFee
     /**
      * @return NullableMoney
      */
-    public function getMinMoney(): NullableMoney
+    public function getMaxMoney(): NullableMoney
     {
-        return $this->minMoney;
+        return $this->maxMoney;
     }
 }
