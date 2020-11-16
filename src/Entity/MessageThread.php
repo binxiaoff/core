982af6bd4485f437bcc147823d58873461a7754a
Julien <julien.oberlin.pro@gmail.com>
2020-11-16 09:07:00 +0100

    CALS-2846 : messaging v2 data model first draft

---
+++ src/Entity/MessageThread.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\{ArrayCollection, Collection};
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Unilend\Entity\Traits\PublicizeIdentityTrait;
+use Unilend\Entity\Traits\TimestampableAddedOnlyTrait;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *  name="message_thread",
+ *  indexes={
+ *     @ORM\Index(name="idx_added", columns={"added"})
+ *  }
+ * )
+ */
+class MessageThread
+{
+    use PublicizeIdentityTrait;
+    use TimestampableAddedOnlyTrait;
+
+    /**
+     * @var ArrayCollection|Collection
+     * @ORM\OneToMany(targetEntity="Unilend\Entity\Message", mappedBy="messageThread")
+     */
+    private Collection $messages;
+
+    /**
+     * Thread constructor.
+     */
+    public function __construct()
+    {
+        $this->added = new \DateTimeImmutable();
+        $this->messages = new ArrayCollection();
+    }
+
+    /**
+     * @param Collection|null $messages
+     * @return MessageThread
+     */
+    public function setMessages(?Collection $messages): MessageThread
+    {
+        $this->messages = $messages;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * @param Message $message
+     * @return MessageThread
+     */
+    public function addMessage(Message $message): MessageThread
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages->add($message);
+        }
+        return $this;
+    }
+
+    /**
+     * @param Message $message
+     * @return MessageThread
+     */
+    public function removeMessage(Message $message): MessageThread
+    {
+        if ($this->messages->contains($message)) {
+            $this->messages->remove($message);
+        }
+        return $this;
+    }
+}
