f9a605df97f1c684a6b0553e829badd1b0cc73f5
Julien <julien.oberlin.pro@gmail.com>
2020-11-16 13:31:15 +0100

    CALS-2846 : add MessageFile entity + message* repositories

---
+++ src/Repository/MessageFileRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Unilend\Repository;
+
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Unilend\Entity\MessageFile;
+
+/**
+ * @method MessageFile|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MessageFile|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MessageFile[]    findAll()
+ * @method MessageFile[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MessageFileRepository extends ServiceEntityRepository
+{
+    /**
+     * MessageFileRepository constructor.
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MessageFile::class);
+    }
+}
