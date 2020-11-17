c1a5d566d8bc58a89358ef5560cc733b0ba8683d
Julien <julien.oberlin.pro@gmail.com>
2020-11-17 17:55:10 +0100

    CALS-2846 : updates on data model

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -7,21 +7,16 @@ namespace Unilend\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\{ArrayCollection, Collection};
-
 use Unilend\Entity\Traits\PublicizeIdentityTrait;
 use Unilend\Entity\Traits\TimestampableAddedOnlyTrait;
 
 /**
  * @ORM\Entity
  * @ORM\Table(
- *  name="message",
  *  indexes={
- *      @ORM\Index(name="idx_sender", columns={"sender"}),
  *      @ORM\Index(name="idx_added", columns={"added"}),
- *      @ORM\Index(name="idx_thread_message", columns={"id_message_thread"}),
  *  }
  * )
- * @ORM\Entity(repositoryClass="Unilend\Repository\MessageRepository")
  * @ORM\HasLifecycleCallbacks
  */
 class Message
@@ -31,42 +26,36 @@ class Message
 
     /**
      * @var MessageThread
+     *
      * @ORM\ManyToOne(targetEntity="Unilend\Entity\MessageThread", inversedBy="messages")
-     * @ORM\JoinColumn(name="id_message_thread", referencedColumnName="id")
+     * @ORM\JoinColumn(name="id_message_thread", nullable=false)
      */
     private MessageThread $messageThread;
 
     /**
-     * @var Clients
+     * @var Staff
      *
-     * @ORM\ManyToOne(targetEntity="Unilend\Entity\Clients")
-     * @ORM\JoinColumn(name="sender", referencedColumnName="id")
+     * @ORM\ManyToOne(targetEntity="Unilend\Entity\Staff")
+     * @ORM\JoinColumn(name="id_sender", nullable=false)
      *
      * @Assert\NotBlank
      * @Assert\Valid
      */
-    private Clients $sender;
+    private Staff $sender;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="body", type="text", length=16777215, nullable=false)
+     * @ORM\Column(type="text", length=16777215, nullable=false)
      *
      * @Assert\NotBlank
      */
     protected string $body;
 
     /**
-     * @var MessageStatus
-     *
-     * @ORM\OneToMany(targetEntity="Unilend\Entity\MessageStatus", mappedBy="message")
-     */
-    private Collection $statuses;
-
-    /**
      * @var MessageFile[]|Collection
      *
-     * @ORM\OneToMany(targetEntity="MessageFile", mappedBy="message", cascade={"persist"}, orphanRemoval=true)
+     * @ORM\OneToMany(targetEntity="Unilend\Entity\MessageFile", mappedBy="message", cascade={"persist"}, orphanRemoval=true)
      */
     private Collection $messageFiles;
 
@@ -75,8 +64,7 @@ class Message
      */
     public function __construct()
     {
-        $this->statuses = new ArrayCollection();
-        $this->messageFiles = new ArrayCollection();
+        $this->added = new \DateTimeImmutable();
     }
 
     /**
@@ -100,6 +88,7 @@ class Message
 
     /**
      * @param MessageThread|null $messageThread
+     *
      * @return Message
      */
     public function setMessageThread(?MessageThread $messageThread): Message
@@ -119,6 +108,7 @@ class Message
 
     /**
      * @param string|null $body
+     *
      * @return Message
      */
     public function setBody(?string $body): Message
@@ -137,60 +127,6 @@ class Message
     }
 
     /**
-     * @param Collection|null $statuses
-     * @return Message
-     */
-    public function setMessages(?Collection $statuses): Message
-    {
-        $this->statuses = $statuses;
-
-        return $this;
-    }
-
-    /**
-     * @return ArrayCollection
-     */
-    public function getStatuses()
-    {
-        return $this->statuses;
-    }
-
-    /**
-     * @param MessageStatus $status
-     * @return Message
-     */
-    public function addStatus(MessageStatus $status): Message
-    {
-        if (!$this->statuses->contains($status)) {
-            $this->statuses->add($status);
-        }
-        return $this;
-    }
-
-    /**
-     * @param MessageStatus $status
-     * @return Message
-     */
-    public function removeStatus(MessageStatus $status): Message
-    {
-        if ($this->statuses->contains($status)) {
-            $this->statuses->remove($status);
-        }
-        return $this;
-    }
-
-    /**
-     * @param Collection|null $messageFiles
-     * @return $this
-     */
-    public function setMessageFiles(?Collection $messageFiles): Message
-    {
-        $this->messageFiles = $messageFiles;
-
-        return $this;
-    }
-
-    /**
      * @return MessageFile[]|Collection
      */
     public function getMessageFiles(): Collection
@@ -206,19 +142,6 @@ class Message
     {
         if (!$this->messageFiles->contains($messageFile)) {
             $this->messageFiles->add($messageFile);
-        }
-        return $this;
-    }
-
-    /**
-     * @param MessageFile $messageFile
-     *
-     * @return Message
-     */
-    public function removeMessageFile(MessageFile $messageFile): Message
-    {
-        if ($this->messageFiles->contains($messageFile)) {
-            $this->messageFiles->remove($messageFile);
         }
         return $this;
     }
