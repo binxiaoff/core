1a71d95e993161a3199d3b8458870a9f90ff516e
Julien <julien.oberlin.pro@gmail.com>
2020-11-23 10:36:22 +0100

    CALS-2897 : add projectParticpationMembers random messages send + random message status for messaging v2 fixtures

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -6,13 +6,14 @@ namespace Unilend\DataFixtures;
 
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Unilend\Entity\ProjectParticipation;
 use Unilend\Entity\Staff;
 use Unilend\Entity\Project;
 use Unilend\Entity\Message;
 use Unilend\Entity\MessageStatus;
 use Unilend\Entity\MessageThread;
+use Unilend\Entity\ProjectParticipation;
 
 class MessageFixtures extends AbstractFixtures implements DependentFixtureInterface
 {
@@ -26,14 +27,16 @@ class MessageFixtures extends AbstractFixtures implements DependentFixtureInterf
      */
     public function load(ObjectManager $manager): void
     {
+        $this->manager = $manager;
         $projectsWithParticipations = $this->getReferences(ProjectFixtures::PROJECTS_WITH_PARTICIPATION);
 
         foreach ($projectsWithParticipations as $key => $projectWithParticipation) {
             if ($projectWithParticipation->getOrganizers()->count() > 0) {
-                $projectOrganizerStaff = $projectWithParticipation->getOrganizers()->first()->getAddedBy();
-                $projectParticipations = $projectWithParticipation->getProjectParticipations();
                 // Create a projectOrganizer.staff message to each projectParticipationMember.staff
-                $this->createMessagesByProjectParticipation($projectOrganizerStaff, $projectWithParticipation, $projectParticipations, $manager);
+                $projectParticipationsWithMembers = $this->createMessagesForProjectParticipations($projectWithParticipation, $projectWithParticipation->getProjectParticipations(), $projectWithParticipation->getOrganizers()->first()->getAddedBy());
+
+                // Create a random projectParticipation.member.staff message to each projectParticipationMember.staff
+                $this->createMessagesForProjectParticipations($projectWithParticipation, $projectParticipationsWithMembers);
             }
         }
     }
@@ -51,50 +54,63 @@ class MessageFixtures extends AbstractFixtures implements DependentFixtureInterf
     }
 
     /**
-     * @param Staff         $projectOrganizerStaff
-     * @param Project       $project
-     * @param Collection    $projectParticipations
-     * @param ObjectManager $manager
-     */
-    private function createMessagesByProjectParticipation(Staff $projectOrganizerStaff, Project $project, Collection $projectParticipations, ObjectManager $manager)
-    {
-        foreach ($projectParticipations as $i => $projectParticipation) {
-            if ($projectParticipation->getProjectParticipationMembers()->count() > 0) {
-                $messageThread = $this->createMessageThreadForProjectParticipation($projectParticipation, $manager);
-
-                $message = (new Message($projectOrganizerStaff, $messageThread, sprintf(
-                    'Message from project "%s" organizer "%s" to company "%s" member\'s',
-                    $project->getTitle(),
-                    $projectOrganizerStaff->getClient()->getEmail(),
-                    $projectParticipation->getParticipant()->getDisplayName()
-                )));
-                $manager->persist($message);
-                // Add an message status for each recipient projectParticipationMember
-                foreach ($projectParticipation->getProjectParticipationMembers() as $projectParticipationMember) {
-                    // Avoid project organisator
-                    if ($projectParticipationMember->getStaff() !== $projectOrganizerStaff) {
-                        $messageStatus = (new MessageStatus(MessageStatus::STATUS_UNREAD, $message, $projectParticipationMember->getStaff()));
-                        $manager->persist($messageStatus);
-                    }
-                }
-                $projectParticipation->setMessageThread($messageThread);
-            }
-        }
-        $manager->flush();
-    }
-
-    /**
      * @param ProjectParticipation $projectParticipation
-     * @param ObjectManager        $manager
      *
      * @return MessageThread
      */
-    private function createMessageThreadForProjectParticipation(ProjectParticipation $projectParticipation, ObjectManager $manager)
+    private function getMessageThreadForProjectParticipation(ProjectParticipation $projectParticipation)
     {
+        if ($projectParticipation->getMessageThread() instanceof MessageThread) {
+            return $projectParticipation->getMessageThread();
+        }
         $messageThread = (new MessageThread())->setPublicId();
-        $manager->persist($messageThread);
-        $manager->flush();
+        $this->manager->persist($messageThread);
+        $projectParticipation->setMessageThread($messageThread);
+        $this->manager->flush();
 
         return $messageThread;
+    }
+
+    /**
+     * @param Project    $project
+     * @param Collection $projectParticipations
+     * @param Staff|null $sender
+     *
+     * @return ArrayCollection
+     */
+    private function createMessagesForProjectParticipations(Project $project, Collection $projectParticipations, Staff $sender = null)
+    {
+        $projectParticipationsWithMembers = new ArrayCollection();
+        foreach ($projectParticipations as $i => $projectParticipation) {
+            if ($projectParticipation->getProjectParticipationMembers()->count() > 0) {
+                $messageThread = $this->getMessageThreadForProjectParticipation($projectParticipation);
+                $projectParticipationMembers = $projectParticipation->getProjectParticipationMembers()->toArray();
+
+                // If sender not set, pick one of projectParticipationMembers as a message sender
+                $sender = $sender ?: $projectParticipationMembers[array_rand($projectParticipationMembers, 1)]->getStaff();
+
+                $message = (new Message($sender, $messageThread, sprintf(
+                    'Message from project "%s" organizer "%s" to company "%s" member\'s',
+                    $project->getTitle(),
+                    $sender->getClient()->getEmail(),
+                    $projectParticipation->getParticipant()->getDisplayName()
+                )));
+                $this->manager->persist($message);
+
+                // Add an message status for each recipient projectParticipationMember
+                foreach ($projectParticipation->getProjectParticipationMembers() as $projectParticipationMember) {
+                    // Avoid to send to sender
+                    if ($sender !== $projectParticipationMember->getStaff()) {
+                        $messagePossibleStatuses = MessageStatus::getPossibleStatuses();
+                        $messageStatus = (new MessageStatus($messagePossibleStatuses[array_rand($messagePossibleStatuses)], $message, $projectParticipationMember->getStaff()));
+                        $this->manager->persist($messageStatus);
+                    }
+                }
+                $projectParticipationsWithMembers->add($projectParticipation);
+            }
+        }
+        $this->manager->flush();
+
+        return $projectParticipationsWithMembers;
     }
 }
