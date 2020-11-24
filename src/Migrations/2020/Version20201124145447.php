d6b3df8d69184b9211fc109977b956eaf33ff423
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2020-11-24 16:28:58 +0100

    CALS-2691 Remove LegalDocument unused fields

--- src/Migrations/2020/Version20201124145447.php
+++ src/Migrations/2020/Version20201124145447.php
@@ -19,6 +19,7 @@ final class Version20201124145447 extends AbstractMigration
         $this->addSql('ALTER TABLE client_status DROP content');
         $this->addSql('ALTER TABLE clients DROP id_language, DROP title, DROP slug, DROP mobile');
         $this->addSql('ALTER TABLE file DROP description');
+        $this->addSql('ALTER TABLE legal_document DROP first_time_instruction, DROP differential_instruction, DROP updated');
     }
 
     public function down(Schema $schema) : void
@@ -26,5 +27,6 @@ final class Version20201124145447 extends AbstractMigration
         $this->addSql('ALTER TABLE client_status ADD content MEDIUMTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE clients ADD id_language VARCHAR(2) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD title VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD slug VARCHAR(191) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, ADD mobile VARCHAR(35) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE file ADD description VARCHAR(191) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE legal_document ADD first_time_instruction MEDIUMTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD differential_instruction MEDIUMTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD updated DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 }
