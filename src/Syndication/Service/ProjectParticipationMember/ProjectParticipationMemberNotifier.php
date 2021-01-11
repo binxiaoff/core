1453daa9027c0a68c2ebcdb736555e026916925a
Julien Oberlin <julien.oberlin@kls-platform.com>
2021-01-11 15:23:57 +0000

    CALS-3108 : add forgottent vraiable for mailjet template

--- src/Syndication/Service/ProjectParticipationMember/ProjectParticipationMemberNotifier.php
+++ src/Syndication/Service/ProjectParticipationMember/ProjectParticipationMemberNotifier.php
@@ -7,7 +7,7 @@ namespace Unilend\Syndication\Service\ProjectParticipationMember;
 use Exception;
 use Swift_Mailer;
 use Symfony\Component\Routing\RouterInterface;
-use Unilend\Core\Entity\Staff;
+use Unilend\Core\Entity\{Staff, TemporaryToken};
 use Unilend\Core\Service\TemporaryTokenGenerator;
 use Unilend\Core\SwiftMailer\MailjetMessage;
 use Unilend\Syndication\Entity\{Project, ProjectParticipationMember, ProjectStatus};
@@ -70,6 +70,7 @@ class ProjectParticipationMemberNotifier
         }
 
         $context = [
+            'temporaryToken_token' => ($temporaryToken instanceof TemporaryToken) ? $temporaryToken->getToken() : null,
             'front_viewParticipation_URL' => $this->router->generate(
                 'front_viewParticipation',
                 [
@@ -77,7 +78,7 @@ class ProjectParticipationMemberNotifier
                 ],
                 RouterInterface::ABSOLUTE_URL
             ),
-            'front_initialAccount_URL' => $temporaryToken ? $this->router->generate(
+            'front_initialAccount_URL' => ($temporaryToken instanceof TemporaryToken) ? $this->router->generate(
                 'front_initialAccount',
                 [
                     'temporaryTokenPublicId' => $temporaryToken->getToken(),
