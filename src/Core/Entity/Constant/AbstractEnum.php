b9d044c6daed4452828e6560cb7d42bc58df57c2
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-01-14 16:41:40 +0100

    CALS-3152 Create AbstractEnum.php class
    This class is the basis for all enum classes

---
+++ src/Core/Entity/Constant/AbstractEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Core\Entity\Constant;
+
+use Unilend\Core\Traits\ConstantsAwareTrait;
+
+abstract class AbstractEnum
+{
+    use ConstantsAwareTrait;
+
+    /**
+     * Is private to forbid instantiation
+     */
+    final private function __construct()
+    {
+    }
+
+    /**
+     * @return string[]|array
+     */
+    final public static function getConstList(): array
+    {
+        return static::getConstants();
+    }
+}
