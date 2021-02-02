85016c6098d7c0863b9bfc0b651b5271e2d6416a
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-02-02 09:41:45 +0100

    CALS-3228 Apply codereview feedback
    Update description to remove SensioLabs usage
    Add help text to better describe usage

--- src/Core/Command/SecurityCheckCommand.php
+++ src/Core/Command/SecurityCheckCommand.php
@@ -41,7 +41,14 @@ class SecurityCheckCommand extends Command
     {
         $this
             ->setName('kls:security:check')
-            ->setDescription('Check known security issues in the packages managed by Composer using SensioLabs Security Advisories Checker')
+            ->setDescription('Check known security issues in the packages managed by Composer')
+            ->setHelp(<<<HELP
+The command is successful if no installed packages (at their currently installed version) 
+are present in the security advisories database <href=https://github.com/FriendsOfPHP/security-advisories>PHP Security Advisories Database</>
+
+It uses <href=https://github.com/enlightn/security-checker>Enlightn Security Checker</>
+HELP
+            )
         ;
     }
 
