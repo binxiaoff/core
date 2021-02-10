6b47b83b7b7700dec7b790cd1fd72db3fb633e9a
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-02-10 12:57:28 +0100

    CALS-3362 code style improvements
     - Add IdentityTrait (to use later)
     - Add type hint

---
+++ src/Core/Entity/Traits/IdentityTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Core\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait IdentityTrait
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private ?int $id;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
