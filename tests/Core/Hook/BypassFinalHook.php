d15139f32c9ec9f4e289b534ea8de18df76df437
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-02-12 17:17:55 +0100

    Update test

---
+++ tests/Core/Hook/BypassFinalHook.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Test\Core\Hook;
+
+use DG\BypassFinals;
+use PHPUnit\Runner\BeforeTestHook;
+
+final class BypassFinalHook implements BeforeTestHook
+{
+    /**
+     * @param string $test
+     */
+    public function executeBeforeTest(string $test): void
+    {
+        BypassFinals::enable();
+    }
+}
