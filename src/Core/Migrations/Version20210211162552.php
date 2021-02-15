0d79a349db3969e97d4a4108fed7a5fb7d53d69b
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2021-02-15 16:49:53 +0100

    CALS-3299 Update migration

--- src/Core/Migrations/Version20210211162552.php
+++ src/Core/Migrations/Version20210211162552.php
@@ -17,11 +17,11 @@ final class Version20210211162552 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql('CREATE TABLE agency_margin_impact (id INT AUTO_INCREMENT NOT NULL, id_margin_rule INT DEFAULT NULL, id_tranche INT DEFAULT NULL, margin NUMERIC(5, 4) NOT NULL, public_id VARCHAR(36) NOT NULL, UNIQUE INDEX UNIQ_BE66DFA3B5B48B91 (public_id), INDEX IDX_BE66DFA32384C64D (id_margin_rule), INDEX IDX_BE66DFA3B8FAF130 (id_tranche), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE agency_margin_rule (id INT AUTO_INCREMENT NOT NULL, id_covenant INT DEFAULT NULL, public_id VARCHAR(36) NOT NULL, expression_operator VARCHAR(2) NOT NULL, expression_value NUMERIC(5, 4) NOT NULL, expression_max_value NUMERIC(5, 4) DEFAULT NULL, UNIQUE INDEX UNIQ_383340BB5B48B91 (public_id), INDEX IDX_383340BA4306C62 (id_covenant), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE agency_margin_rule (id INT AUTO_INCREMENT NOT NULL, id_covenant INT DEFAULT NULL, public_id VARCHAR(36) NOT NULL, inequality_operator VARCHAR(2) NOT NULL, inequality_min_value NUMERIC(65, 4) NOT NULL, inequality_max_value NUMERIC(65, 4) DEFAULT NULL, UNIQUE INDEX UNIQ_383340BB5B48B91 (public_id), INDEX IDX_383340BA4306C62 (id_covenant), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE agency_margin_impact ADD CONSTRAINT FK_BE66DFA32384C64D FOREIGN KEY (id_margin_rule) REFERENCES agency_margin_rule (id)');
         $this->addSql('ALTER TABLE agency_margin_impact ADD CONSTRAINT FK_BE66DFA3B8FAF130 FOREIGN KEY (id_tranche) REFERENCES agency_tranche (id)');
         $this->addSql('ALTER TABLE agency_margin_rule ADD CONSTRAINT FK_383340BA4306C62 FOREIGN KEY (id_covenant) REFERENCES agency_covenant (id)');
-        $this->addSql('ALTER TABLE agency_covenant_rule ADD expression_operator VARCHAR(2) NOT NULL, ADD expression_value NUMERIC(5, 4) NOT NULL, ADD expression_max_value NUMERIC(5, 4) DEFAULT NULL, DROP expression, CHANGE year year INT NOT NULL');
+        $this->addSql('ALTER TABLE agency_covenant_rule ADD inequality_operator VARCHAR(2) NOT NULL, ADD inequality_min_value NUMERIC(65, 4) NOT NULL, ADD inequality_max_value NUMERIC(65, 4) DEFAULT NULL, DROP expression, CHANGE year year INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -29,6 +29,6 @@ final class Version20210211162552 extends AbstractMigration
         $this->addSql('ALTER TABLE agency_margin_impact DROP FOREIGN KEY FK_BE66DFA32384C64D');
         $this->addSql('DROP TABLE agency_margin_impact');
         $this->addSql('DROP TABLE agency_margin_rule');
-        $this->addSql('ALTER TABLE agency_covenant_rule ADD expression VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP expression_operator, DROP expression_value, DROP expression_max_value, CHANGE year year VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE agency_covenant_rule ADD expression VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP inequality_operator, DROP inequality_value, DROP inequality_max_value, CHANGE year year VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
