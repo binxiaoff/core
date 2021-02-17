6d874a3f81a20a69a4990b0593163317c479793a
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-02-18 00:09:58 +0100

    CALS-3414 Simplify and base the validator on the property

--- src/Core/Validator/Constraints/MoneyGreaterThanOrEqual.php
+++ src/Core/Validator/Constraints/MoneyGreaterThanOrEqual.php
@@ -12,29 +12,4 @@ use Symfony\Component\Validator\Constraint;
 class MoneyGreaterThanOrEqual extends Constraint
 {
     public string $message = 'The money amount is not greater than the previous one.';
-    public string $field = '';
-
-    /**
-     * @inheritDoc
-     */
-    public function getTargets(): string
-    {
-        return self::CLASS_CONSTRAINT;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getRequiredOptions(): array
-    {
-        return ['field'];
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getDefaultOption(): string
-    {
-        return 'field';
-    }
 }
--- src/Core/Validator/Constraints/MoneyGreaterThanOrEqualValidator.php
+++ src/Core/Validator/Constraints/MoneyGreaterThanOrEqualValidator.php
@@ -7,7 +7,7 @@ namespace Unilend\Core\Validator\Constraints;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Validator\{Constraint, ConstraintValidator, Exception\ConstraintDefinitionException, Exception\UnexpectedTypeException};
-use Unilend\Core\Entity\Embeddable\Money;
+use Unilend\Core\Entity\Interfaces\MoneyInterface;
 use Unilend\Core\Service\MoneyCalculator;
 
 class MoneyGreaterThanOrEqualValidator extends ConstraintValidator
@@ -26,53 +26,37 @@ class MoneyGreaterThanOrEqualValidator extends ConstraintValidator
     /**
      * @inheritDoc
      */
-    public function validate($entity, Constraint $constraint): void
+    public function validate($value, Constraint $constraint): void
     {
         if (false === $constraint instanceof MoneyGreaterThanOrEqual) {
             throw new UnexpectedTypeException($constraint, MoneyGreaterThanOrEqual::class);
         }
 
-        $fieldName = $constraint->field;
-
-        if (false === \is_string($fieldName)) {
-            throw new UnexpectedTypeException($fieldName, 'string');
+        if (false === $value instanceof MoneyInterface) {
+            throw new UnexpectedTypeException($value, MoneyInterface::class);
         }
 
-        if (empty($fieldName)) {
-            throw new ConstraintDefinitionException('The money field has to be specified.');
-        }
+        $entity = $this->context->getObject();
 
         if (null === $entity) {
             return;
         }
 
         $fullyQualifiedName = \get_class($entity);
-        /** @var EntityManagerInterface $entityManager */
-        $entityManager = $this->managerRegistry->getManagerForClass($fullyQualifiedName);
+        $entityManager      = $this->managerRegistry->getManagerForClass($fullyQualifiedName);
 
-        if (!$entityManager) {
-            throw new ConstraintDefinitionException(sprintf('Unable to find the object manager associated with an entity of class "%s".', $fullyQualifiedName));
-        }
-
-        $class = $entityManager->getClassMetadata($fullyQualifiedName);
-
-        if (false === $class->hasField($fieldName) && false === $class->hasAssociation($fieldName)) {
-            throw new ConstraintDefinitionException(sprintf('The field "%s" is not mapped by Doctrine, so it cannot be validated.', $fieldName));
-        }
-
-        $money = $class->reflFields[$fieldName]->getValue($entity);
-
-        if (false === $money instanceof Money) {
-            throw new UnexpectedTypeException($money, Money::class);
+        if (false === $entityManager instanceof EntityManagerInterface) {
+            throw new ConstraintDefinitionException(sprintf('Unable to find the entity manager associated with an entity of class "%s".', $fullyQualifiedName));
         }
 
         $previousEntity = $entityManager->getUnitOfWork()->getOriginalEntityData($entity);
+        $propertyPath   = $this->context->getPropertyPath();
+        $moneyClass     = \get_class($value);
+        $previousMoney  = new $moneyClass($previousEntity[$propertyPath . '.currency'], $previousEntity[$propertyPath . '.amount']);
 
-        $previousMoney = new Money($previousEntity[$fieldName . '.currency'], $previousEntity[$fieldName . '.amount']);
-
-        if (-1 === MoneyCalculator::compare($money, $previousMoney)) {
+        if (-1 === MoneyCalculator::compare($value, $previousMoney)) {
             $this->context->buildViolation($constraint->message)
-                ->atPath($fieldName)
+                ->atPath($propertyPath)
                 ->addViolation()
             ;
         }
