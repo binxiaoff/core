661ac98efbbf31c59a7a278a409b2189ce1af4e8
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-02-23 10:13:25 +0100

    Remove useless imports

--- src/Syndication/Security/Voter/ProjectParticipationTrancheVoter.php
+++ src/Syndication/Security/Voter/ProjectParticipationTrancheVoter.php
@@ -4,29 +4,15 @@ declare(strict_types=1);
 
 namespace Unilend\Syndication\Security\Voter;
 
-use Doctrine\ORM\NonUniqueResultException;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Unilend\Core\Entity\User;
 use Unilend\Core\Security\Voter\AbstractEntityVoter;
-use Unilend\Syndication\Entity\{Project,
-    ProjectParticipationMember,
-    ProjectParticipationStatus,
-    ProjectParticipationTranche};
-use Unilend\Syndication\Service\ProjectParticipation\ProjectParticipationManager;
+use Unilend\Syndication\Entity\{ProjectParticipationStatus, ProjectParticipationTranche};
 
 class ProjectParticipationTrancheVoter extends AbstractEntityVoter
 {
     public const ATTRIBUTE_CREATE           = 'create';
     public const ATTRIBUTE_EDIT             = 'edit';
     public const ATTRIBUTE_SENSITIVE_VIEW   = 'sensitive_view';
-
-    /**
-     * @param AuthorizationCheckerInterface $authorizationChecker
-     */
-    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
-    {
-        parent::__construct($authorizationChecker);
-    }
 
     /**
      * @param ProjectParticipationTranche $projectParticipationTranche
