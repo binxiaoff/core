8665eac0ffb8018eca9a93d3511018db9c32e290
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-02-26 19:07:56 +0100

    Update dataroom schema
    use a root folder
    remove abstractFileContainer (now useless)
    add some utility method
    abandon use of arrayAccess

--- src/Core/Entity/Drive.php
+++ src/Core/Entity/Drive.php
@@ -7,17 +7,29 @@ namespace Unilend\Core\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use InvalidArgumentException;
 
 /**
+ * Represent a collection of folders akin to a filesystem with a base folder root who has path /.
+ *
+ * Mainly used in dataroom
+ *
  * @ORM\Entity
  * @ORM\Table(name="core_drive")
  */
-class Drive extends AbstractFileContainer
+class Drive
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private ?int $id = null;
+
     /**
      * @var Folder[]|Collection
      *
-     * @ORM\OneToMany(targetEntity=Folder::class, indexBy="path", mappedBy="drive")
+     * @ORM\OneToMany(targetEntity=Folder::class, indexBy="path", mappedBy="drive", cascade={"persist", "remove"})
      */
     private Collection $folders;
 
@@ -26,23 +38,28 @@ class Drive extends AbstractFileContainer
      */
     public function __construct()
     {
-        parent::__construct();
-        $this->folders = new ArrayCollection();
+        $this->folders = new ArrayCollection(['/' => new Folder('root', $this, '/')]);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
     /**
      * @return Collection|Folder[]
      */
-    public function getFolders()
+    public function getFolders(): Collection
     {
         return $this->folders;
     }
 
-    /**
-     * @return Folder
-     */
-    public function createFolder(string $name, ?string $path = null)
+    public function mkFolder(string $name, string $path = '/'): Folder
     {
+        if ($this->get($path)) {
+            throw new InvalidArgumentException('Path already exist');
+        }
+
         $folder = new Folder($name, $this, $path);
 
         $this->folders[$folder->getPath()] = $folder;
@@ -51,11 +68,45 @@ class Drive extends AbstractFileContainer
     }
 
     /**
+     * @param Folder|string $path
+     */
+    public function rmFolder($path): Drive
+    {
+        if ($path instanceof Folder) {
+            if ($path->getDrive() !== $this) {
+                throw new InvalidArgumentException('The given folder drive is not $this');
+            }
+
+            $path = $path->getPath();
+        }
+
+        if ('/' === $path) {
+            throw new InvalidArgumentException('Please delete the drive instead');
+        }
+
+        foreach ($this->folders as $folder) {
+            if (0 === mb_strpos($path, $path->getPath())) {
+                unset($this->folders[$folder->getPath()]);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
      * @return $this
      */
-    public function addFile(File $file): Drive
+    public function rmFile(File $file, string $path = '/'): Drive
     {
-        $this->files[] = $file;
+        if ($path instanceof Folder) {
+            $path = $path->getPath();
+        }
+
+        $folder = $this->folders[$path];
+
+        if ($folder) {
+            $folder->rmFile($file);
+        }
 
         return $this;
     }
@@ -63,5 +114,29 @@ class Drive extends AbstractFileContainer
     public function isPathPresent(string $path): bool
     {
         return '/' === $path || isset($this->folders[$path]);
+    }
+
+    public function getFolder(string $path): ?Folder
+    {
+        return $this->folders[$path];
+    }
+
+    public function getFile(string $path): ?File
+    {
+        $parent = $this->getFolder(\dirname($path));
+
+        if ($parent) {
+            return $parent->getFile(basename($path));
+        }
+
+        return null;
+    }
+
+    /**
+     * @return File|Folder|null
+     */
+    public function get(string $path)
+    {
+        return $this->getFolder($path) ?? $this->getFile($path);
     }
 }
--- src/Core/Entity/Folder.php
+++ src/Core/Entity/Folder.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Unilend\Core\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use InvalidArgumentException;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 use Symfony\Component\Validator\Constraints as Assert;
+use Unilend\Core\Entity\Traits\PublicizeIdentityTrait;
 use Unilend\Core\Entity\Traits\TimestampableAddedOnlyTrait;
 
 /**
@@ -18,10 +24,22 @@ use Unilend\Core\Entity\Traits\TimestampableAddedOnlyTrait;
  * })
  *
  * @UniqueEntity(fields={"drive", "path"})
+ *
+ * @ApiResource(
+ *     itemOperations={
+ *         "get": {
+ *             "controller": "ApiPlatform\Core\Action\NotFoundAction",
+ *             "read": false,
+ *             "output": false,
+ *         },
+ *     },
+ *     collectionOperations={}
+ * )
  */
-class Folder extends AbstractFileContainer
+class Folder
 {
     use TimestampableAddedOnlyTrait;
+    use PublicizeIdentityTrait;
 
     /**
      * @ORM\Column(type="text", nullable=false)
@@ -42,24 +60,38 @@ class Folder extends AbstractFileContainer
      * @Assert\Length(max="100")
      *
      * @ORM\Column(type="string", length=50, nullable=false)
+     *
+     * @Groups({"folder:read"})
      */
     private string $name;
 
     /**
      * @Assert\NotBlank
      *
-     * @ORM\ManyToOne(targetEntity=Drive::class, inversedBy="folders")
+     * @ORM\ManyToOne(targetEntity=Drive::class, inversedBy="folders", cascade={"persist"})
      * @ORM\JoinColumn(name="id_drive", nullable=false)
      */
     private Drive $drive;
 
+    /**
+     * @var File[]|Collection
+     *
+     * This is a OneToMany unidirectionnal relation : Folder <-× File
+     * https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/reference/association-mapping.html#one-to-many-unidirectional-with-join-table
+     *
+     * @ORM\ManyToMany(targetEntity=File::class, cascade={"persist", "remove"}, indexBy="publicId")
+     * @ORM\JoinTable(
+     *      inverseJoinColumns={@ORM\JoinColumn(name="file_id", referencedColumnName="id", unique=true)}
+     * )
+     */
+    protected Collection $files;
+
     public function __construct(string $name, Drive $drive, ?string $path = null)
     {
-        parent::__construct();
         $this->drive = $drive;
         $path        = '/' === $path ? null : $path;
 
-        if ($path && false === $this->drive->isPathPresent($path)) {
+        if ('/' !== $path && null === $this->drive->getFolder($path)) {
             throw new InvalidArgumentException(sprintf('Given path %s does not exist in drive', $path));
         }
 
@@ -67,6 +99,8 @@ class Folder extends AbstractFileContainer
         $this->path     = $path . DIRECTORY_SEPARATOR . $name;
         $this->pathHash = hash('crc32b', $this->path);
         $this->added    = new DateTimeImmutable();
+        $this->files    = new ArrayCollection();
+        $this->setPublicId();
     }
 
     public function getPath(): string
@@ -85,10 +119,45 @@ class Folder extends AbstractFileContainer
     }
 
     /**
-     * @return iterable|File[]
+     * @return Collection|Folder[]
      */
-    public function getFiles(): iterable
+    public function getChildrenFolders(): Collection
     {
-        return $this->files;
+        return $this->drive->getFolders()->filter(function (Folder $folder) {
+            return 0 === mb_strpos($folder->getPath(), $this->getPath());
+        });
+    }
+
+    /**
+     * @Groups({"folder:read"})
+     *
+     * @MaxDepth(1)
+     */
+    public function getContent(): Collection
+    {
+        return new ArrayCollection([...array_values($this->getChildrenFolders()->toArray()), ...array_values($this->files->toArray())]);
+    }
+
+    /**
+     * @param $name
+     */
+    public function mkdir($name): Folder
+    {
+        return $this->drive->mkFolder($name, $this->getPath());
+    }
+
+    public function rmFile(File $file)
+    {
+        $this->files->removeElement($file);
+    }
+
+    public function rmFolder(string $relativePath)
+    {
+        $this->drive->rmFolder($this->path . DIRECTORY_SEPARATOR . $relativePath);
+    }
+
+    public function getFile(string $publicId): ?File
+    {
+        return $this->files[$publicId];
     }
 }
