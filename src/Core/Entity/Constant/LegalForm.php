e95461c0d2e4d5cd3dde5cf9d3fdf643f01e8016
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-03-09 23:34:47 +0100

    Add constants for legal forms

---
+++ src/Core/Entity/Constant/LegalForm.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Core\Entity\Constant;
+
+class LegalForm extends AbstractEnum
+{
+    public const SARL = 'SARL';
+    public const SAS  = 'SAS';
+    public const SASU = 'SASU';
+    public const EURL = 'EURL';
+}
