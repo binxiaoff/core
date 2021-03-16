31873ec0ace267d1704570185408e86d1c47f02d
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-03-16 10:58:56 +0100

    Fix TermRepositoryHistory : Change ManagerRegistry import

--- src/Agency/Repository/TermHistoryRepository.php
+++ src/Agency/Repository/TermHistoryRepository.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Unilend\Agency\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
 use Unilend\Agency\Entity\Term;
 use Unilend\Agency\Entity\TermHistory;
 
@@ -18,7 +18,6 @@ use Unilend\Agency\Entity\TermHistory;
  */
 class TermHistoryRepository extends ServiceEntityRepository
 {
-
     /**
      * @param ManagerRegistry $managerRegistry
      */
