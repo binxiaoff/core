3ce163be34c1bc51f658ca4d9c22282684de87ed
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-03-19 00:56:39 +0100

    Fix repository imports

--- src/Core/Repository/CompanyGroupTagRepository.php
+++ src/Core/Repository/CompanyGroupTagRepository.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Unilend\Core\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Unilend\Core\Entity\CompanyGroupTag;
 
 /**
