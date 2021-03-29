729b501f70402e157db25cad290291724ad62087
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-03-29 17:38:18 +0200

    Initialize some users

--- tests/Core/DataFixtures/UserFixture.php
+++ tests/Core/DataFixtures/UserFixture.php
@@ -6,12 +6,27 @@ namespace Unilend\Test\Core\DataFixtures;
 
 use Doctrine\Persistence\ObjectManager;
 use Exception;
-use Faker\Provider\Internet;
+use Faker\Provider\fr_FR\Person;
 use ReflectionException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Unilend\Core\Entity\User;
+use Unilend\Core\Entity\UserStatus;
 
 class UserFixture extends AbstractFixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    /**
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @inheritDoc
      *
@@ -22,13 +37,17 @@ class UserFixture extends AbstractFixture
     {
         $manager->getConnection()->getConfiguration()->setSQLLogger(null);
         foreach (range(1, 20) as $index) {
-            $user = new User('user' . $index . '@' . Internet::safeEmailDomain());
+            $user = new User('user' . $index . '@test.com');
 
             $reference = 'user/' . $index;
 
             $this->addReference($reference, $user);
 
             $this->setPublicId($user, $reference);
+
+            if ($index <= 10) {
+                $this->initialize($user);
+            }
 
             $manager->persist($user);
         }
@@ -44,5 +63,20 @@ class UserFixture extends AbstractFixture
     public static function getReferenceName(User $user)
     {
         return $user->getPublicId();
+    }
+
+    /**
+     * @param User $user
+     *
+     * @throws Exception
+     */
+    private function initialize(User $user)
+    {
+        $user->setFirstName(Person::firstNameMale());
+        $user->setLastName(Person::firstNameFemale());
+        $user->setPhone('+33600000000');
+        $user->setJobFunction('job');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, '0000'));
+        $user->setCurrentStatus(new UserStatus($user, UserStatus::STATUS_CREATED));
     }
 }
