1971250b7def3cc8fe98238fd48b37da6fdcc085
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-03-29 12:00:37 +0200

    CALS-3616 Add CA regional bank filter

--- src/Core/Entity/Constant/CARegionalBank.php
+++ src/Core/Entity/Constant/CARegionalBank.php
@@ -7,8 +7,9 @@ namespace Unilend\Core\Entity\Constant;
 class CARegionalBank
 {
     public const REGIONAL_BANKS = [
-        'CAPR', 'ALVO', 'ANMA', 'AQTN', 'ATVD', 'BRPI', 'CEST', 'CENF', 'CENL', 'COUE', 'CHBO', 'CM2SE', 'CHPE', 'CORS', 'CODA', 'NORM', 'SAVO', 'FINI',
-        'FRAC', 'GUAD', 'ILVI', 'LANG', 'L&HL', 'LORR', 'MART', 'MORB', 'NORF', 'NEST', 'NMPY', 'NORS', 'IDFR', 'PRCA', 'PYGA', 'REUN', 'SRAL', 'SMED',
-        'TOUL', 'TPOI', 'VALF',
+        'CAPR', 'ALVO', 'ANMA', 'AQTN', 'ATVD', 'BRPI', 'CEST', 'CENF', 'CENL', 'COUE',
+        'CHBO', 'CM2SE', 'CHPE', 'CORS', 'CODA', 'NORM', 'SAVO', 'FINI', 'FRAC', 'GUAD',
+        'ILVI', 'LANG', 'L&HL', 'LORR', 'MART', 'MORB', 'NORF', 'NEST', 'NMPY', 'NORS',
+        'IDFR', 'PRCA', 'PYGA', 'REUN', 'SRAL', 'SMED', 'TOUL', 'TPOI', 'VALF',
     ];
 }
