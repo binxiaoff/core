e08e87c8b2b5dc88d2282d8ae01bd17489b0e3ce
Benjamin Gras <benjamin.gras@kls-platform.com>
2021-04-14 16:48:19 +0200

    fix(agency): manage foreign key constraint in migration
    Drop and re-create the migration because a foreign key will
    prevent a change in the associated columns.

--- src/Core/Migrations/Version20210406165003.php
+++ src/Core/Migrations/Version20210406165003.php
@@ -9,29 +9,28 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20210406165003 extends AbstractMigration
 {
-    /**
-     * @return string
-     */
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'CALS-3057 Add missing nullable=false in JoinColumn annotation';
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
+        $this->addSql('ALTER TABLE agency_borrower_member DROP CONSTRAINT FK_5B36A3AA8B4BA121');
         $this->addSql('ALTER TABLE agency_borrower_member CHANGE id_borrower id_borrower INT NOT NULL');
+        $this->addSql('ALTER TABLE agency_borrower_member ADD CONSTRAINT FK_5B36A3AA8B4BA121 FOREIGN KEY (id_borrower) REFERENCES agency_borrower (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE agency_participation_member DROP CONSTRAINT FK_D4BCDFFB157D332A');
         $this->addSql('ALTER TABLE agency_participation_member CHANGE id_participation id_participation INT NOT NULL');
+        $this->addSql('ALTER TABLE agency_participation_member ADD CONSTRAINT FK_D4BCDFFB157D332A FOREIGN KEY (id_participation) REFERENCES agency_participation (id) ON DELETE CASCADE');
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
+        $this->addSql('ALTER TABLE agency_borrower_member DROP CONSTRAINT FK_5B36A3AA8B4BA121');
         $this->addSql('ALTER TABLE agency_borrower_member CHANGE id_borrower id_borrower INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE agency_borrower_member ADD CONSTRAINT FK_5B36A3AA8B4BA121 FOREIGN KEY (id_borrower) REFERENCES agency_borrower (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE agency_participation_member DROP CONSTRAINT FK_D4BCDFFB157D332A');
         $this->addSql('ALTER TABLE agency_participation_member CHANGE id_participation id_participation INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE agency_participation_member ADD CONSTRAINT FK_D4BCDFFB157D332A FOREIGN KEY (id_participation) REFERENCES agency_participation (id) ON DELETE CASCADE');
     }
 }
