050a19f0019076c3ca137dff05c428e2b2879cda
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2021-04-15 16:03:04 +0200

    feat(agency):CALS-3084 Add security check

--- src/Agency/Controller/Participation/Get.php
+++ src/Agency/Controller/Participation/Get.php
@@ -4,23 +4,40 @@ declare(strict_types=1);
 
 namespace Unilend\Agency\Controller\Participation;
 
+use _HumbugBox373c0874430e\Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Security;
 use Unilend\Agency\Entity\Participation;
-use Unilend\Agency\Entity\Project;
+use Unilend\Agency\Security\Voter\ParticipationVoter;
+use Unilend\Core\Entity\Folder;
 
 class Get
 {
+    /** @var Security  */
+    private Security $security;
+
+    /**
+     * @param Security $security
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @param Participation $data
      * @param Request       $request
      *
-     * @return Project
+     * @return Folder
      */
     public function __invoke(Participation $data, Request $request)
     {
-        $drive = $data->getPersonalDrive();
+        if (false === $this->security->isGranted(ParticipationVoter::ATTRIBUTE_VIEW_DRIVE, $data)) {
+            throw new AccessDeniedException();
+        }
 
+        $drive  = $data->getPersonalDrive();
         $return = $drive->get($request->get('path', '/') ?? '/');
 
         if ($return) {
