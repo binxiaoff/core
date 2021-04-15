8bdc8fa4ebf9652d0c7ff32645ac0f74888d51f6
rebindaine <morgane.rebindaine@ca-lendingservices.com>
2021-04-15 11:07:05 +0200

    feat(agency):CALS-3078 Add security check

--- src/Agency/Controller/Project/Post.php
+++ src/Agency/Controller/Project/Post.php
@@ -6,21 +6,26 @@ namespace Unilend\Agency\Controller\Project;
 
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Unilend\Agency\Entity\Project;
 use Unilend\Agency\Repository\ProjectRepository;
+use Unilend\Agency\Security\Voter\ProjectVoter;
 use Unilend\Core\Entity\Drive;
 use Unilend\Core\Entity\Folder;
 
 class Post
 {
     private ProjectRepository $projectRepository;
+    private Security          $security;
 
     /**
      * @param ProjectRepository $projectRepository
+     * @param Security          $security
      */
-    public function __construct(ProjectRepository $projectRepository)
+    public function __construct(ProjectRepository $projectRepository, Security $security)
     {
         $this->projectRepository = $projectRepository;
+        $this->security          = $security;
     }
 
     /**
@@ -33,20 +38,24 @@ class Post
     {
         $property = $request->attributes->get('sharedDrive');
         $drive    = null;
+        $project  = $this->projectRepository->find($data->getId());
 
-        if ($property) {
-            $project = $this->projectRepository->find($data->getId());
-            $method  = 'get' . ucfirst($property) . 'Drive';
+        if ($project && $property) {
+            $method = 'get' . ucfirst($property) . 'Drive';
 
-            if (null === $project || false === method_exists($project, $method)) {
-                throw new BadRequestException('Method not exists');
+            if (false === method_exists($project, $method)) {
+                throw new BadRequestException();
             }
 
             $drive = $project->{$method}();
         }
 
         if (false === $drive instanceof Drive) {
-            throw new BadRequestException('No drive');
+            throw new BadRequestException();
+        }
+
+        if (false === $this->security->isGranted(ProjectVoter::ATTRIBUTE_CREATE_FOLDER, $project)) {
+            throw new BadRequestException();
         }
 
         $content = $request->toArray();
@@ -54,14 +63,14 @@ class Post
 
         // throw Exception if name contains other character than letters or spaces
         if (false === ctype_alpha(str_replace(' ', '', $name))) {
-            throw new BadRequestException('Name is invalid');
+            throw new BadRequestException();
         }
 
         $path         = $request->attributes->get('path');
         $parentFolder = $drive->get($path);
 
         if (false === $parentFolder instanceof Folder) {
-            throw new BadRequestException('Parent folder does not exists');
+            throw new BadRequestException();
         }
 
         return new Folder($name, $drive, $parentFolder->getPath());
