3ae670ff4682c29a4b8516613b151d7a62833129
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-04-27 11:43:14 +0200

    bugfix(agency): Remove useless declaration of nullability on Project::companyGroupTag JoinColumn (it is the default value)

--- src/Agency/Entity/Project.php
+++ src/Agency/Entity/Project.php
@@ -410,7 +410,7 @@ class Project
     /**
      * @ORM\ManyToOne(targetEntity=CompanyGroupTag::class)
      * @ORM\JoinColumns({
-     *     @ORM\JoinColumn(name="id_company_group_tag", referencedColumnName="id", nullable=true)
+     *     @ORM\JoinColumn(name="id_company_group_tag", referencedColumnName="id")
      * })
      *
      * Remove assertion for external banks (they may have no companyGroupTag)
