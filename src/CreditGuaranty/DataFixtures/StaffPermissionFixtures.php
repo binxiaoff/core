00dd863d0775724328d5c4bb70e1806c4200b0df
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-05-14 15:28:21 +0200

    feat(fei): Add voter for C&G admin staff for granting permission to other staff
    Closes CALS-3769

--- src/CreditGuaranty/DataFixtures/StaffPermissionFixtures.php
+++ src/CreditGuaranty/DataFixtures/StaffPermissionFixtures.php
@@ -11,20 +11,56 @@ use Unilend\Core\DataFixtures\CompanyFixtures;
 use Unilend\Core\DataFixtures\StaffFixtures;
 use Unilend\Core\DataFixtures\UserFixtures;
 use Unilend\Core\Entity\Company;
+use Unilend\Core\Model\Bitmask;
 use Unilend\CreditGuaranty\Entity\StaffPermission;
 
 class StaffPermissionFixtures extends AbstractFixtures implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        // Managing company (CASA)
         /** @var Company $company */
         $company       = $this->getReference(CompanyFixtures::CASA);
         $referenceUser = $this->getReference(UserFixtures::ADMIN);
 
         foreach ($company->getStaff() as $staff) {
-            $staffPermission = new StaffPermission($staff, StaffPermission::PERMISSION_READ_PROGRAM);
+            $staffPermission = new StaffPermission(
+                $staff,
+                new Bitmask(StaffPermission::PERMISSION_READ_PROGRAM
+                    | StaffPermission::PERMISSION_EDIT_PROGRAM
+                    | StaffPermission::PERMISSION_CREATE_PROGRAM
+                | StaffPermission::PERMISSION_READ_RESERVATION)
+            );
             if ($staff->getUser() === $referenceUser) {
-                $staffPermission->addPermission(StaffPermission::PERMISSION_EDIT_PROGRAM | StaffPermission::PERMISSION_CREATE_PROGRAM);
+                $staffPermission->setGrantPermissions(
+                    StaffPermission::PERMISSION_GRANT_READ_PROGRAM
+                    | StaffPermission::PERMISSION_GRANT_CREATE_PROGRAM
+                    | StaffPermission::PERMISSION_GRANT_EDIT_PROGRAM
+                    | StaffPermission::PERMISSION_GRANT_READ_RESERVATION
+                );
+            }
+
+            $manager->persist($staffPermission);
+        }
+
+        // participant (CR)
+        /** @var Company $company */
+        $company = $this->getReference(CompanyFixtures::COMPANY_MANY_STAFF);
+        foreach ($company->getStaff() as $staff) {
+            $staffPermission = new StaffPermission(
+                $staff,
+                new Bitmask(StaffPermission::PERMISSION_READ_RESERVATION
+                    | StaffPermission::PERMISSION_EDIT_RESERVATION
+                    | StaffPermission::PERMISSION_CREATE_RESERVATION
+                | StaffPermission::PERMISSION_READ_PROGRAM)
+            );
+            if ($staff->getUser() === $referenceUser) {
+                $staffPermission->setGrantPermissions(
+                    StaffPermission::PERMISSION_GRANT_READ_RESERVATION
+                    | StaffPermission::PERMISSION_GRANT_CREATE_RESERVATION
+                    | StaffPermission::PERMISSION_GRANT_EDIT_RESERVATION
+                    | StaffPermission::PERMISSION_GRANT_READ_PROGRAM
+                );
             }
 
             $manager->persist($staffPermission);
--- src/CreditGuaranty/Service/StaffPermissionManager.php
+++ src/CreditGuaranty/Service/StaffPermissionManager.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Unilend\CreditGuaranty\Service;
 
 use Unilend\Core\Entity\Staff;
+use Unilend\Core\Model\Bitmask;
 use Unilend\CreditGuaranty\Repository\StaffPermissionRepository;
 
 class StaffPermissionManager
@@ -18,8 +19,7 @@ class StaffPermissionManager
 
     public function hasPermissions(Staff $staff, int $permissions): bool
     {
-        //todo: use the new getInheritedRightStaff() method
-        $staffToCheck = $staff->isManager() ? $staff->getManagedStaff() : [$staff];
+        $staffToCheck = $staff->getManagedStaff();
 
         foreach ($staffToCheck as $managedStaff) {
             $staffPermission = $this->staffPermissionRepository->findOneBy(['staff' => $managedStaff]);
@@ -29,5 +29,12 @@ class StaffPermissionManager
         }
 
         return false;
+    }
+
+    public function canGrant(Staff $staff, Bitmask $permissions): bool
+    {
+        $staffPermission = $this->staffPermissionRepository->findOneBy(['staff' => $staff]);
+        //Since the "grant permissions" have the same positon as "permissions" that a user can grant, we can check it by using has().
+        return $staffPermission && $staffPermission->getGrantPermissions()->has($permissions);
     }
 }
