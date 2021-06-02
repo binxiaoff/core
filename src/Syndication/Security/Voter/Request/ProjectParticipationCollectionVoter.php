48ce94c2fbc90b5da8e240b51bb7022f940b42f5
elisatheng <elisa.theng@gmail.com>
2021-06-02 14:33:07 +0200

    fix(syndication): move ProjectParticipationCollectionVoter in request folder

--- src/Syndication/Security/Voter/Request/ProjectParticipationCollectionVoter.php
+++ src/Syndication/Security/Voter/Request/ProjectParticipationCollectionVoter.php
@@ -2,22 +2,16 @@
 
 declare(strict_types=1);
 
-namespace Unilend\Syndication\Security\Voter;
+namespace Unilend\Syndication\Security\Voter\Request;
 
 use Unilend\Core\Security\Voter\AbstractEntityVoter;
 use Unilend\Syndication\Entity\Request\ProjectParticipationCollection;
+use Unilend\Syndication\Security\Voter\ProjectVoter;
 
 class ProjectParticipationCollectionVoter extends AbstractEntityVoter
 {
     public const ATTRIBUTE_CREATE = 'create';
 
-    protected const UNILEND_ENTITY_NAMESPACE = 'Unilend\\Syndication\\Entity\\Request\\';
-
-    /**
-     * @param ProjectParticipationCollection $projectParticipationCollection
-     *
-     * @return bool
-     */
     protected function canCreate(ProjectParticipationCollection $projectParticipationCollection): bool
     {
         return $this->authorizationChecker->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $projectParticipationCollection->getProject());
