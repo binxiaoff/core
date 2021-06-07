233270bd55d6e2ea17c06b4fdb9d0bd867399dc7
elisatheng <elisa.theng@gmail.com>
2021-06-07 16:57:31 +0200

    fix(fei): code review feedbacks

--- src/CreditGuaranty/Validator/Constraints/ReservationEligible.php
+++ src/CreditGuaranty/Validator/Constraints/ReservationEligible.php
@@ -13,11 +13,6 @@ class ReservationEligible extends Constraint
 {
     public string $message = 'The reservation is ineligible.';
 
-    public function validatedBy(): string
-    {
-        return self::class . 'Validator';
-    }
-
     public function getTargets(): string
     {
         return self::CLASS_CONSTRAINT;
--- src/CreditGuaranty/Validator/Constraints/ReservationEligibleValidator.php
+++ src/CreditGuaranty/Validator/Constraints/ReservationEligibleValidator.php
@@ -6,19 +6,22 @@ namespace Unilend\CreditGuaranty\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Unilend\CreditGuaranty\Entity\Borrower;
 use Unilend\CreditGuaranty\Entity\BorrowerBusinessActivity;
+use Unilend\CreditGuaranty\Entity\Project;
 use Unilend\CreditGuaranty\Entity\ReservationStatus;
+use Unilend\CreditGuaranty\Repository\ProgramEligibilityRepository;
 use Unilend\CreditGuaranty\Service\EligibilityChecker;
 
 class ReservationEligibleValidator extends ConstraintValidator
 {
-    private const FIELD_CATEGORIES = ['general', 'profile', 'activity', 'project', 'loan'];
-
+    private ProgramEligibilityRepository $programEligibilityRepository;
     private EligibilityChecker $eligibilityChecker;
 
-    public function __construct(EligibilityChecker $eligibilityChecker)
+    public function __construct(ProgramEligibilityRepository $programEligibilityRepository, EligibilityChecker $eligibilityChecker)
     {
-        $this->eligibilityChecker = $eligibilityChecker;
+        $this->programEligibilityRepository = $programEligibilityRepository;
+        $this->eligibilityChecker           = $eligibilityChecker;
     }
 
     /**
@@ -27,11 +30,20 @@ class ReservationEligibleValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint): void
     {
-        if (ReservationStatus::STATUS_SENT !== $value->getStatus()) {
+        if (ReservationStatus::STATUS_SENT !== $value->getStatus() || null !== $value->getId()) {
             return;
         }
 
         $reservation = $value->getReservation();
+
+        if (false === ($reservation->getBorrower() instanceof Borrower)) {
+            $this->context->buildViolation('CreditGuaranty.Reservation.borrower.missing')
+                ->atPath('reservation.borrower')
+                ->addViolation()
+            ;
+
+            return;
+        }
 
         if (false === ($reservation->getBorrowerBusinessActivity() instanceof BorrowerBusinessActivity)) {
             $this->context->buildViolation('CreditGuaranty.Reservation.borrowerBusinessActivity.missing')
@@ -42,7 +54,14 @@ class ReservationEligibleValidator extends ConstraintValidator
             return;
         }
 
-        // no need to check project because it is done in ReservationStatus
+        if (false === ($reservation->getProject() instanceof Project)) {
+            $this->context->buildViolation('CreditGuaranty.Reservation.project.missing')
+                ->atPath('reservation.project')
+                ->addViolation()
+            ;
+
+            return;
+        }
 
         if (0 === $reservation->getFinancingObjects()->count()) {
             $this->context->buildViolation('CreditGuaranty.Reservation.financingObject.missing')
@@ -53,8 +72,8 @@ class ReservationEligibleValidator extends ConstraintValidator
             return;
         }
 
-        foreach (self::FIELD_CATEGORIES as $category) {
-            if (false === $this->eligibilityChecker->check($reservation, $category)) {
+        foreach ($this->programEligibilityRepository->findFieldCategoriesByProgram($reservation->getProgram()) as $category) {
+            if (false === $this->eligibilityChecker->checkByCategory($reservation, $category)) {
                 $this->context->buildViolation('CreditGuaranty.Reservation.ineligibleCategory')
                     ->setParameter('{{ category }}', $category)
                     ->atPath('reservation')
