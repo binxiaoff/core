e5d86e39764e947d462ce676fd74e5cc14752356
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-06-21 23:56:59 +0200

    feat(agency): CALS-3958 Give the "project creation" permission to root manager when activating the Agency module
    Closes CALS-3958

---
+++ src/Agency/Listener/Doctrine/AgencyModuleActivatedListener.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Agency\Listener\Doctrine;
+
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Unilend\Core\Entity\CompanyModule;
+use Unilend\Core\Entity\Staff;
+
+class AgencyModuleActivatedListener
+{
+    public function onFlush(OnFlushEventArgs $args): void
+    {
+        $em  = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityUpdates() as $companyModule) {
+            if (
+                $companyModule instanceof CompanyModule
+                && array_key_exists(CompanyModule::PROPERTY_ACTIVATED, $uow->getEntityChangeSet($companyModule))
+                && $companyModule->isActivated()
+            ) {
+                // put it in foreach, because it will pass here only once in the most case and it's hard to meet the conditions of the above if.
+                $classMetadata = $em->getClassMetadata(Staff::class);
+
+                foreach ($companyModule->getCompany()->getRootTeam()->getStaff() as $staff) {
+                    if (false === $staff->isManager()) {
+                        continue;
+                    }
+
+                    $staff->setAgencyProjectCreationPermission(true);
+                    $uow->computeChangeSet($classMetadata, $staff);
+                }
+            }
+        }
+    }
+}
