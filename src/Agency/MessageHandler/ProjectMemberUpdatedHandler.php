b4a866027b89f4ac3ab1d6f4d6e6aff7d40972e3
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-07-18 11:02:00 +0200

    fix(agency): CALS-4166 Send email when AbstractProjectMember::user is changed

---
+++ src/Agency/MessageHandler/ProjectMemberUpdatedHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unilend\Agency\MessageHandler;
+
+use Doctrine\Persistence\ManagerRegistry;
+use InvalidArgumentException;
+use JsonException;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Unilend\Agency\Entity\AbstractProjectMember;
+use Unilend\Agency\Message\ProjectMemberUpdated;
+use Unilend\Agency\Notifier\ProjectMemberNotifier;
+
+class ProjectMemberUpdatedHandler implements MessageHandlerInterface
+{
+    private ManagerRegistry       $registry;
+    private ProjectMemberNotifier $projectMemberNotifier;
+
+    public function __construct(ManagerRegistry $registry, ProjectMemberNotifier $projectMemberNotifier)
+    {
+        $this->registry              = $registry;
+        $this->projectMemberNotifier = $projectMemberNotifier;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function __invoke(ProjectMemberUpdated $projectMemberUpdated)
+    {
+        if (false === isset($projectMemberUpdated->getChangeSet()['user'])) {
+            return;
+        }
+
+        $manager = $this->registry->getManagerForClass($projectMemberUpdated->getProjectMemberClass());
+
+        if (null === $manager) {
+            throw new InvalidArgumentException(\sprintf('Missing manager for %s', $projectMemberUpdated->getProjectMemberClass()));
+        }
+
+        /** @var AbstractProjectMember $projectMember */
+        $projectMember = $manager->find($projectMemberUpdated->getProjectMemberClass(), $projectMemberUpdated->getProjectMemberId());
+
+        if (null === $projectMember) {
+            throw new InvalidArgumentException(
+                \sprintf("Project member of class %s with id %d doesn't exist", $projectMemberUpdated->getProjectMemberClass(), $projectMemberUpdated->getProjectMemberId())
+            );
+        }
+
+        if ($projectMember && $projectMember->getProject()->isPublished()) {
+            $this->projectMemberNotifier->notifyProjectPublication($projectMember);
+        }
+    }
+}
