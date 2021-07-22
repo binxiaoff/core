c76abe4a91281f656c446f5ac882f95c5cfc1c0e
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-07-22 15:21:12 +0200

    feature(core): CALS-4051 Add new loan types

--- src/Core/Entity/Constant/Tranche/LoanType.php
+++ src/Core/Entity/Constant/Tranche/LoanType.php
@@ -10,6 +10,13 @@ final class LoanType extends AbstractEnum
 {
     public const TERM_LOAN            = 'term_loan';
     public const SHORT_TERM           = 'short_term';
+    public const LONG_TERM            = 'long_term';
+    public const ACTION               = 'action';
+    public const OBLIGATION           = 'obligation';
+    public const OBSA                 = 'obsa';
+    public const OCA                  = 'oca';
+    public const CROWD_FUNDING        = 'crowd_funding';
+    public const MEZZANINE            = 'mezzanine';
     public const REVOLVING_CREDIT     = 'revolving_credit';
     public const STAND_BY             = 'stand_by';
     public const SIGNATURE_COMMITMENT = 'signature_commitment';
