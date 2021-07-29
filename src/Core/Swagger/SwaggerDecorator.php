3e587cb54377a082ea25cc407b931f39fe11f644
elisatheng <elisa.theng@gmail.com>
2021-07-29 17:59:04 +0200

    feat(core): CALS-4272 add authentication_token path in swagger

--- src/Core/Swagger/SwaggerDecorator.php
+++ src/Core/Swagger/SwaggerDecorator.php
@@ -20,6 +20,14 @@ class SwaggerDecorator implements NormalizerInterface
     }
 
     /**
+     * @param mixed $data
+     */
+    public function supportsNormalization($data, ?string $format = null): bool
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    /**
      * @param mixed $object
      *
      * @throws ExceptionInterface
@@ -29,55 +37,23 @@ class SwaggerDecorator implements NormalizerInterface
         /** @var array $docs */
         $docs = $this->decorated->normalize($object, $format, $context);
 
-        $docs['paths'][$this->router->generate('gesdinet_jwt_refresh_token')] = [
-            'post' => [
-                'tags'        => ['Authentication'],
-                'operationId' => 'postRefreshToken',
-                'summary'     => 'Refresh valid token',
-                'requestBody' => [
-                    'content' => [
-                        'application/x-www-form-urlencoded' => [
-                            'schema' => [
-                                'type'       => 'object',
-                                'properties' => [
-                                    'refresh_token' => [
-                                        'description' => 'JWT Refresh Token',
-                                        'type'        => 'string',
-                                    ],
-                                ],
-                                'required' => ['refresh_token'],
-                            ],
-                        ],
-                    ],
-                ],
-                'responses' => [
-                    '200' => [
-                        'description' => 'Refresh succeded',
-                        'content'     => [
-                            'application/x-form-' => [
-                                'schema' => [
-                                    'type'       => 'object',
-                                    'properties' => [
-                                        'refresh_token' => [
-                                            'description' => 'JWT Refresh Token',
-                                            'type'        => 'string',
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                    '401' => [
-                        'description' => 'Refresh token don\'t exist',
-                        'content'     => [
-                            'application/json' => [],
-                        ],
-                    ],
-                ],
+        // Definitions
+        $docs['definitions']['Folder'] = [
+            'properties' => [
+                'name' => ['type' => 'string'],
             ],
         ];
 
-        // Remove unused routes (ItemOperations are necessary for APIPlatform but we don't expose those endpoints)
+        // Paths
+        $authenticationPaths = [
+            $this->router->generate('authentication_token')       => $this->generateAuthenticationTokenPath(),
+            $this->router->generate('gesdinet_jwt_refresh_token') => $this->generateRefreshTokenPath(),
+        ];
+        // put authentication paths at top of the list
+        $docs['paths'] = \array_merge($authenticationPaths, $docs['paths']);
+
+        // @todo to refactorize in CALS-4262
+        // remove unused routes (ItemOperations are necessary for APIPlatform but we don't expose those endpoints)
         $removedGetRoutes = [
             '/acceptations_legal_docs/{id}',
             '/company_modules/{id}',
@@ -100,20 +76,124 @@ class SwaggerDecorator implements NormalizerInterface
             }
         }
 
-        $docs['definitions']['Folder'] = [
-            'properties' => [
-                'name' => ['type' => 'string'],
-            ],
-        ];
-
         return $docs;
     }
 
-    /**
-     * @param mixed $data
-     */
-    public function supportsNormalization($data, ?string $format = null): bool
+    private function generateAuthenticationTokenPath(): array
     {
-        return $this->decorated->supportsNormalization($data, $format);
+        return [
+            'post' => [
+                'tags'        => ['Authentication'],
+                'operationId' => 'postAuthenticationToken',
+                'summary'     => 'Retrieve authentication tokens',
+                'requestBody' => [
+                    'content' => [
+                        'application/x-www-form-urlencoded' => [
+                            'schema' => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'username' => [
+                                        'description' => 'Email',
+                                        'type'        => 'string',
+                                    ],
+                                    'password' => [
+                                        'description' => 'Password',
+                                        'type'        => 'string',
+                                    ],
+                                    'captchaValue' => [
+                                        'description' => 'Captcha value',
+                                        'type'        => 'string',
+                                    ],
+                                ],
+                                'required' => ['username', 'password', 'captchaValue'],
+                            ],
+                        ],
+                    ],
+                ],
+                'responses' => [
+                    '200' => [
+                        'description' => 'Retrieving succeeded',
+                        'content'     => [
+                            'application/x-form-' => [
+                                'schema' => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'username' => [
+                                            'description' => 'Email',
+                                            'type'        => 'string',
+                                        ],
+                                        'password' => [
+                                            'description' => 'Password',
+                                            'type'        => 'string',
+                                        ],
+                                        'captchaValue' => [
+                                            'description' => 'Captcha value',
+                                            'type'        => 'string',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    '401' => [
+                        'description' => 'Invalid credentials',
+                        'content'     => [
+                            'application/json' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    private function generateRefreshTokenPath(): array
+    {
+        return [
+            'post' => [
+                'tags'        => ['Authentication'],
+                'operationId' => 'postRefreshToken',
+                'summary'     => 'Refresh valid token',
+                'requestBody' => [
+                    'content' => [
+                        'application/x-www-form-urlencoded' => [
+                            'schema' => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'refresh_token' => [
+                                        'description' => 'JWT Refresh Token',
+                                        'type'        => 'string',
+                                    ],
+                                ],
+                                'required' => ['refresh_token'],
+                            ],
+                        ],
+                    ],
+                ],
+                'responses' => [
+                    '200' => [
+                        'description' => 'Refresh succeeded',
+                        'content'     => [
+                            'application/x-form-' => [
+                                'schema' => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'refresh_token' => [
+                                            'description' => 'JWT Refresh Token',
+                                            'type'        => 'string',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    '401' => [
+                        'description' => 'Refresh token does not exist',
+                        'content'     => [
+                            'application/json' => [],
+                        ],
+                    ],
+                ],
+            ],
+        ];
     }
 }
