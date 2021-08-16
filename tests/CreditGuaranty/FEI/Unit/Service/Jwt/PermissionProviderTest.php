24b38228ec95c9ddb936cac6818e6d9f78bbcfed
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-08-16 15:34:48 +0200

    tests(core):  CALS-4388 Update test for new PermissionProviderInterface

--- tests/CreditGuaranty/FEI/Unit/Service/Jwt/PermissionProviderTest.php
+++ tests/CreditGuaranty/FEI/Unit/Service/Jwt/PermissionProviderTest.php
@@ -7,14 +7,12 @@ namespace KLS\Test\CreditGuaranty\FEI\Unit\Service\Jwt;
 use Exception;
 use KLS\Core\Entity\Company;
 use KLS\Core\Entity\Staff;
-use KLS\Core\Entity\Team;
 use KLS\Core\Entity\User;
 use KLS\Core\Model\Bitmask;
 use KLS\CreditGuaranty\FEI\Entity\StaffPermission;
 use KLS\CreditGuaranty\FEI\Repository\StaffPermissionRepository;
 use KLS\CreditGuaranty\FEI\Service\Jwt\PermissionProvider;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 
 /**
@@ -37,99 +35,136 @@ class PermissionProviderTest extends TestCase
         $this->staffPermissionRepository = null;
     }
 
+    public function providerGetGrantPermissions()
+    {
+        $staff = $this->createStaff();
+
+        yield from $this->commonProviderPermission();
+
+        yield 'staff with cg permissions (no grant permission)' => [
+            $staff->getUser(),
+            $staff,
+            0,
+            new StaffPermission($staff, new Bitmask(StaffPermission::MANAGING_COMPANY_ADMIN_PERMISSIONS)),
+        ];
+        yield 'staff with cg permissions and same grant_permissions' => [
+            $staff->getUser(),
+            $staff,
+            StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS,
+            (new StaffPermission($staff, new Bitmask(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS)))
+                ->setGrantPermissions(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS),
+        ];
+        yield 'staff with cg permissions and different grant permission' => [
+            $staff->getUser(),
+            $staff,
+            StaffPermission::PERMISSION_GRANT_EDIT_PROGRAM,
+            (new StaffPermission($staff, new Bitmask(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS)))
+                ->setGrantPermissions(StaffPermission::PERMISSION_GRANT_EDIT_PROGRAM),
+        ];
+    }
+
+    public function providerGetPermissions(): iterable
+    {
+        $staff = $this->createStaff();
+
+        yield from $this->commonProviderPermission();
+
+        yield 'staff with cg permissions (no grant permission)' => [
+            $staff->getUser(),
+            $staff,
+            StaffPermission::MANAGING_COMPANY_ADMIN_PERMISSIONS,
+            new StaffPermission($staff, new Bitmask(StaffPermission::MANAGING_COMPANY_ADMIN_PERMISSIONS)),
+        ];
+        yield 'staff with cg permissions and grant_permissions' => [
+            $staff->getUser(),
+            $staff,
+            StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS,
+            (new StaffPermission($staff, new Bitmask(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS)))
+                ->setGrantPermissions(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS),
+        ];
+        yield 'staff with cg permissions and different grant permission' => [
+            $staff->getUser(),
+            $staff,
+            StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS,
+            (new StaffPermission($staff, new Bitmask(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS)))
+                ->setGrantPermissions(StaffPermission::PERMISSION_GRANT_EDIT_PROGRAM),
+        ];
+    }
+
     /**
      * @covers ::getProductName
      */
-    public function testGetName(): void
+    public function testGetProductName(): void
     {
         static::assertSame('credit_guaranty', $this->createTestObject()->getProductName());
     }
 
     /**
-     * @covers ::provide
-     *
-     * @dataProvider staffProvider
+     * @covers ::getServiceName
      */
-    public function testProvide(Staff $staff, array $expected, ?StaffPermission $staffPermission = null): void
+    public function testGetServiceName(): void
     {
-        $this->staffPermissionRepository->findOneBy(['staff' => $staff])->shouldBeCalledTimes(2)->willReturn($staffPermission);
-
-        $permissionProvider = $this->createTestObject();
-        $result             = [
-            $permissionProvider->getServiceName() => [
-                'permissions'       => $permissionProvider->getPermissions($staff->getUser(), $staff),
-                'grant_permissions' => $permissionProvider->getGrantPermission($staff->getUser(), $staff),
-            ],
-        ];
-
-        static::assertSame($expected, $result);
+        static::assertSame('fei', $this->createTestObject()->getServiceName());
     }
 
     /**
-     * @throws Exception
+     * @covers ::getPermissions
+     *
+     * @dataProvider providerGetPermissions
      */
-    public function staffProvider(): iterable
+    public function testGetPermissions(?User $user, ?Staff $staff, int $expected, ?StaffPermission $staffPermission): void
+    {
+        $this->staffPermissionRepository->findOneBy(['staff' => $staff])->willReturn($staffPermission);
+
+        $permissionProvider = $this->createTestObject();
+
+        static::assertSame($expected, $permissionProvider->getPermissions($user, $staff));
+    }
+
+    /**
+     * @covers ::getPermissions
+     *
+     * @dataProvider providerGetGrantPermissions
+     *
+     * @param mixed $expected
+     */
+    public function testGetGrantPermissions(?User $user, ?Staff $staff, $expected, ?StaffPermission $staffPermission)
+    {
+        $this->staffPermissionRepository->findOneBy(['staff' => $staff])->willReturn($staffPermission);
+
+        $permissionProvider = $this->createTestObject();
+
+        static::assertSame($expected, $permissionProvider->getGrantPermission($user, $staff));
+    }
+
+    private function commonProviderPermission(): iterable
     {
         $staff = $this->createStaff();
 
-        yield 'staff without cg permissions' => [
-            $staff,
-            ['fei' => ['permissions' => 0, 'grant_permissions' => 0]],
+        yield 'no staff and no user' => [
+            new User('test@test.com'),
+            null,
+            0,
             null,
         ];
-        yield 'staff with cg permissions' => [
+        yield 'no user and staff' => [
+            null,
             $staff,
-            ['fei' => ['permissions' => StaffPermission::MANAGING_COMPANY_ADMIN_PERMISSIONS, 'grant_permissions' => 0]],
-            new StaffPermission($staff, new Bitmask(StaffPermission::MANAGING_COMPANY_ADMIN_PERMISSIONS)),
+            0,
+            null,
         ];
-        yield 'staff with cg permissions and grant_permissions' => [
+        yield 'user and no staff' => [
+            $staff->getUser(),
+            null,
+            0,
+            null,
+        ];
+        yield 'staff without cg permissions' => [
+            $staff->getUser(),
             $staff,
-            ['fei' => ['permissions' => StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS, 'grant_permissions' => StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS]],
-            (new StaffPermission($staff, new Bitmask(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS)))
-                ->setGrantPermissions(StaffPermission::PARTICIPANT_ADMIN_PERMISSIONS),
+            0,
+            null,
         ];
-    }
-
-    /**
-     * @covers ::provide
-     */
-    public function testProvideWithoutStaff(): void
-    {
-        $user = new User('user@mail.com');
-
-        $this->staffPermissionRepository->findOneBy(['staff' => Argument::any()])->shouldNotBeCalled();
-
-        $permissionProvider = $this->createTestObject();
-        $result             = [
-            $permissionProvider->getServiceName() => [
-                'permissions'       => $permissionProvider->getPermissions($user, null),
-                'grant_permissions' => $permissionProvider->getGrantPermission($user, null),
-            ],
-        ];
-
-        static::assertSame(['fei' => ['permissions' => 0, 'grant_permissions' => 0]], $result);
-    }
-
-    /**
-     * @covers ::provide
-     *
-     * @throws Exception
-     */
-    public function testProvideWithoutStaffPermission(): void
-    {
-        $staff = $this->createStaff();
-
-        $this->staffPermissionRepository->findOneBy(['staff' => $staff])->shouldBeCalledTimes(2)->willReturn(null);
-
-        $permissionProvider = $this->createTestObject();
-        $result             = [
-            $permissionProvider->getServiceName() => [
-                'permissions'       => $permissionProvider->getPermissions($staff->getUser(), $staff),
-                'grant_permissions' => $permissionProvider->getGrantPermission($staff->getUser(), $staff),
-            ],
-        ];
-
-        static::assertSame(['fei' => ['permissions' => 0, 'grant_permissions' => 0]], $result);
     }
 
     /**
@@ -137,10 +172,9 @@ class PermissionProviderTest extends TestCase
      */
     private function createStaff(): Staff
     {
-        $teamRoot = Team::createRootTeam(new Company('Company', 'Company', ''));
-        $team     = Team::createTeam('Team', $teamRoot);
+        $company = new Company('Company', 'Company', '');
 
-        return new Staff(new User('user@mail.com'), $team);
+        return new Staff(new User('user@mail.com'), $company->getRootTeam());
     }
 
     private function createTestObject(): PermissionProvider
