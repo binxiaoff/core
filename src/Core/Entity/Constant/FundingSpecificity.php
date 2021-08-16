116869a5d9f736df9004421b5bb80534a67a3397
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-08-16 17:54:19 +0200

    feature(core): CALS-3912 Add new funding specificity

--- src/Core/Entity/Constant/FundingSpecificity.php
+++ src/Core/Entity/Constant/FundingSpecificity.php
@@ -6,6 +6,12 @@ namespace KLS\Core\Entity\Constant;
 
 final class FundingSpecificity extends AbstractEnum
 {
-    public const FSA = 'FSA';
-    public const LBO = 'LBO';
+    public const FSA                 = 'FSA';
+    public const LBO                 = 'LBO';
+    public const PPR                 = 'PPR';
+    public const CORPORATE_FINANCING = 'corporate_financing';
+    public const PROJECT_FINANCING   = 'project_financing';
+    public const PROPERTY_LEASE      = 'property_lease'; // Crédit-bail immobilier
+    public const TOP_BALANCE_SHEET   = 'top_balance_sheet'; // Haut de bilan
+    public const EQUITY              = 'equity';
 }
