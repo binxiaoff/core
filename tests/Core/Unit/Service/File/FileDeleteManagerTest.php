8b0046a14a53a5c8386b140c5f98bd26d3729a8e
elisatheng <elisa.theng@gmail.com>
2021-08-17 18:27:36 +0200

    refactor(core): CALS-3219 split FileDeleteManager

--- tests/Core/Unit/Service/File/FileDeleteManagerTest.php
+++ tests/Core/Unit/Service/File/FileDeleteManagerTest.php
@@ -4,26 +4,16 @@ declare(strict_types=1);
 
 namespace KLS\Test\Core\Unit\Service\File;
 
-use Exception;
-use KLS\Core\Entity\Company;
-use KLS\Core\Entity\Embeddable\Money;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use KLS\Core\Entity\File;
-use KLS\Core\Entity\Staff;
-use KLS\Core\Entity\Team;
-use KLS\Core\Entity\User;
+use KLS\Core\Service\File\FileDeleteInterface;
 use KLS\Core\Service\File\FileDeleteManager;
-use KLS\Syndication\Arrangement\Entity\Project;
 use KLS\Syndication\Arrangement\Entity\ProjectFile;
-use KLS\Syndication\Arrangement\Repository\ProjectFileRepository;
-use KLS\Syndication\Arrangement\Repository\ProjectRepository;
-use KLS\Syndication\Arrangement\Security\Voter\ProjectFileVoter;
-use KLS\Syndication\Arrangement\Security\Voter\ProjectVoter;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * @coversDefaultClass \KLS\Core\Service\File\FileDeleteManager
@@ -32,210 +22,76 @@ use Symfony\Component\Security\Core\Security;
  */
 class FileDeleteManagerTest extends TestCase
 {
-    /** @var Security|ObjectProphecy */
-    private $security;
-
-    /** @var ProjectRepository|ObjectProphecy */
-    private $projectRepository;
-
-    /** @var ProjectFileRepository|ObjectProphecy */
-    private $projectFileRepository;
-
-    protected function setUp(): void
-    {
-        $this->security              = $this->prophesize(Security::class);
-        $this->projectRepository     = $this->prophesize(ProjectRepository::class);
-        $this->projectFileRepository = $this->prophesize(ProjectFileRepository::class);
-    }
-
-    protected function tearDown(): void
-    {
-        $this->security              = null;
-        $this->projectRepository     = null;
-        $this->projectFileRepository = null;
-    }
-
     /**
      * @covers ::delete
-     *
-     * @dataProvider projectFileProvider
      */
-    public function testDeleteForProjectFile(File $file, string $type): void
+    public function testDelete(): void
     {
-        $staff       = $this->createStaff();
-        $project     = $this->createProject($staff);
-        $projectFile = new ProjectFile($type, $file, $project, $staff);
+        /** @var FileDeleteInterface|ObjectProphecy $manager1 */
+        $manager1 = $this->prophesize(FileDeleteInterface::class);
+        /** @var FileDeleteInterface|ObjectProphecy $manager2 */
+        $manager2 = $this->prophesize(FileDeleteInterface::class);
 
-        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn($projectFile);
-        $this->security->isGranted(ProjectFileVoter::ATTRIBUTE_DELETE, $projectFile)->shouldBeCalledOnce()->willReturn(true);
-        $this->projectFileRepository->remove($projectFile)->shouldBeCalledOnce();
-        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldNotBeCalled();
-        $this->projectRepository->flush()->shouldNotBeCalled();
-
-        $fileDeleteManager = $this->createTestObject();
-        $fileDeleteManager->delete($file, $type);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function projectFileProvider(): array
-    {
         $file = new File();
         $file->setPublicId();
-
-        return [
-            ProjectFile::PROJECT_FILE_TYPE_GENERAL              => [$file, ProjectFile::PROJECT_FILE_TYPE_GENERAL],
-            ProjectFile::PROJECT_FILE_TYPE_ACCOUNTING_FINANCIAL => [$file, ProjectFile::PROJECT_FILE_TYPE_ACCOUNTING_FINANCIAL],
-            ProjectFile::PROJECT_FILE_TYPE_LEGAL                => [$file, ProjectFile::PROJECT_FILE_TYPE_LEGAL],
-            ProjectFile::PROJECT_FILE_TYPE_KYC                  => [$file, ProjectFile::PROJECT_FILE_TYPE_KYC],
-        ];
-    }
-
-    /**
-     * @covers ::delete
-     */
-    public function testDeleteForProjectFileExceptionNotFound(): void
-    {
-        $file = new File();
-        $file->setPublicId();
-
-        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn(null);
-        $this->security->isGranted(Argument::cetera())->shouldNotBeCalled();
-        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->flush()->shouldNotBeCalled();
-
-        static::expectException(NotFoundHttpException::class);
-
-        $fileDeleteManager = $this->createTestObject();
-        $fileDeleteManager->delete($file, ProjectFile::PROJECT_FILE_TYPE_GENERAL);
-    }
-
-    /**
-     * @covers ::delete
-     */
-    public function testDeleteForProjectFileExceptionAccessDenied(): void
-    {
         $type = ProjectFile::PROJECT_FILE_TYPE_GENERAL;
+
+        $manager1->supports($type)->shouldBeCalledOnce()->willReturn(true);
+        $manager1->delete($file, $type)->shouldBeCalledOnce();
+        $manager2->supports($type)->shouldNotBeCalled();
+        $manager2->delete($file, $type)->shouldNotBeCalled();
+
+        $fileDeleteManager = new FileDeleteManager([$manager1->reveal(), $manager2->reveal()]);
+        $fileDeleteManager->delete($file, $type);
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteWithNothingSupports(): void
+    {
+        /** @var FileDeleteInterface|ObjectProphecy $manager1 */
+        $manager1 = $this->prophesize(FileDeleteInterface::class);
+
         $file = new File();
         $file->setPublicId();
-        $staff       = $this->createStaff();
-        $project     = $this->createProject($staff);
-        $projectFile = new ProjectFile($type, $file, $project, $staff);
+        $type = 'general';
 
-        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn($projectFile);
-        $this->security->isGranted(ProjectFileVoter::ATTRIBUTE_DELETE, $projectFile)->shouldBeCalledOnce()->willReturn(false);
-        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->flush()->shouldNotBeCalled();
+        $manager1->supports($type)->shouldBeCalledOnce()->willReturn(false);
+        $manager1->delete(Argument::cetera())->shouldNotBeCalled();
 
-        static::expectException(AccessDeniedException::class);
+        static::expectException(NotFoundHttpException::class);
 
-        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager = new FileDeleteManager([$manager1->reveal()]);
         $fileDeleteManager->delete($file, $type);
     }
 
     /**
      * @covers ::delete
      *
-     * @dataProvider projectProvider
+     * @dataProvider exceptionProvider
      */
-    public function testDeleteProject(File $file, string $type, string $field): void
+    public function testDeleteException(string $exceptionClass): void
     {
-        $project = $this->createProject($this->createStaff());
+        /** @var FileDeleteInterface|ObjectProphecy $manager1 */
+        $manager1 = $this->prophesize(FileDeleteInterface::class);
 
-        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->findOneBy([$field => $file])->shouldBeCalledOnce()->willReturn($project);
-        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldBeCalledOnce()->willReturn(true);
-        $this->projectRepository->flush()->shouldBeCalledOnce();
+        $file = new File();
+        $file->setPublicId();
+        $type = ProjectFile::PROJECT_FILE_TYPE_GENERAL;
 
-        $fileDeleteManager = $this->createTestObject();
+        $manager1->supports($type)->shouldBeCalledOnce()->willReturn(true);
+        $manager1->delete($file, $type)->shouldBeCalledOnce()->willThrow($exceptionClass);
+
+        static::expectException($exceptionClass);
+
+        $fileDeleteManager = new FileDeleteManager([$manager1->reveal()]);
         $fileDeleteManager->delete($file, $type);
     }
 
-    /**
-     * @throws Exception
-     */
-    public function projectProvider(): array
+    public function exceptionProvider(): iterable
     {
-        $file = new File();
-        $file->setPublicId();
-
-        return [
-            Project::PROJECT_FILE_TYPE_DESCRIPTION => [$file, Project::PROJECT_FILE_TYPE_DESCRIPTION, 'termSheet'],
-            Project::PROJECT_FILE_TYPE_NDA         => [$file, Project::PROJECT_FILE_TYPE_NDA, 'nda'],
-        ];
-    }
-
-    /**
-     * @covers ::delete
-     */
-    public function testDeleteForProjectExceptionNotFound(): void
-    {
-        $file = new File();
-        $file->setPublicId();
-
-        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->findOneBy(['termSheet' => $file])->shouldBeCalledOnce()->willReturn(null);
-        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->flush()->shouldNotBeCalled();
-
-        static::expectException(NotFoundHttpException::class);
-
-        $fileDeleteManager = $this->createTestObject();
-        $fileDeleteManager->delete($file, Project::PROJECT_FILE_TYPE_DESCRIPTION);
-    }
-
-    /**
-     * @covers ::delete
-     */
-    public function testDeleteForProjectExceptionAccessDenied(): void
-    {
-        $type = Project::PROJECT_FILE_TYPE_NDA;
-        $file = new File();
-        $file->setPublicId();
-        $staff   = $this->createStaff();
-        $project = $this->createProject($staff);
-
-        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
-        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
-        $this->projectRepository->findOneBy(['nda' => $file])->shouldBeCalledOnce()->willReturn($project);
-        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldBeCalledOnce()->willReturn(false);
-        $this->projectRepository->flush()->shouldNotBeCalled();
-
-        static::expectException(AccessDeniedException::class);
-
-        $fileDeleteManager = $this->createTestObject();
-        $fileDeleteManager->delete($file, $type);
-    }
-
-    private function createProject(Staff $staff): Project
-    {
-        return new Project(
-            $staff,
-            'RISK-GROUP-42',
-            new Money('EUR', '42')
-        );
-    }
-
-    private function createStaff(): Staff
-    {
-        $teamRoot = Team::createRootTeam(new Company('Company', 'Company', ''));
-        $team     = Team::createTeam('Team', $teamRoot);
-
-        return new Staff(new User('user@mail.com'), $team);
-    }
-
-    private function createTestObject(): FileDeleteManager
-    {
-        return new FileDeleteManager(
-            $this->security->reveal(),
-            $this->projectRepository->reveal(),
-            $this->projectFileRepository->reveal(),
-        );
+        yield ORMException::class => [ORMException::class];
+        yield OptimisticLockException::class => [OptimisticLockException::class];
     }
 }
---
+++ tests/Syndication/Arrangement/Unit/Service/File/FileDeleteManagerTest.php
@@ -0,0 +1,282 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KLS\Test\Syndication\Arrangement\Unit\Service\File;
+
+use Exception;
+use KLS\Core\Entity\Company;
+use KLS\Core\Entity\Embeddable\Money;
+use KLS\Core\Entity\File;
+use KLS\Core\Entity\Staff;
+use KLS\Core\Entity\Team;
+use KLS\Core\Entity\User;
+use KLS\Syndication\Arrangement\Entity\Project;
+use KLS\Syndication\Arrangement\Entity\ProjectFile;
+use KLS\Syndication\Arrangement\Repository\ProjectFileRepository;
+use KLS\Syndication\Arrangement\Repository\ProjectRepository;
+use KLS\Syndication\Arrangement\Security\Voter\ProjectFileVoter;
+use KLS\Syndication\Arrangement\Security\Voter\ProjectVoter;
+use KLS\Syndication\Arrangement\Service\File\FileDeleteManager;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * @coversDefaultClass \KLS\Syndication\Arrangement\Service\File\FileDeleteManager
+ *
+ * @internal
+ */
+class FileDeleteManagerTest extends TestCase
+{
+    /** @var Security|ObjectProphecy */
+    private $security;
+
+    /** @var ProjectRepository|ObjectProphecy */
+    private $projectRepository;
+
+    /** @var ProjectFileRepository|ObjectProphecy */
+    private $projectFileRepository;
+
+    protected function setUp(): void
+    {
+        $this->security              = $this->prophesize(Security::class);
+        $this->projectRepository     = $this->prophesize(ProjectRepository::class);
+        $this->projectFileRepository = $this->prophesize(ProjectFileRepository::class);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->security              = null;
+        $this->projectRepository     = null;
+        $this->projectFileRepository = null;
+    }
+
+    /**
+     * @covers ::supports
+     *
+     * @dataProvider validTypesProvider
+     */
+    public function testSupports(string $type): void
+    {
+        $fileDeleteManager = $this->createTestObject();
+        static::assertTrue($fileDeleteManager->supports($type));
+    }
+
+    public function validTypesProvider(): iterable
+    {
+        foreach (ProjectFile::getProjectFileTypes() as $type) {
+            yield $type => [$type];
+        }
+        foreach (Project::getProjectFileTypes() as $type) {
+            yield $type => [$type];
+        }
+    }
+
+    /**
+     * @covers ::supports
+     *
+     * @dataProvider invalidTypesProvider
+     */
+    public function testNotSupports(string $type): void
+    {
+        $fileDeleteManager = $this->createTestObject();
+        static::assertFalse($fileDeleteManager->supports($type));
+    }
+
+    public function invalidTypesProvider(): iterable
+    {
+        $types = ['general', 'accounting_financial', 'legal', 'kyc', 'description', 'nda'];
+
+        foreach ($types as $type) {
+            yield $type => [$type];
+        }
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @dataProvider projectFileProvider
+     */
+    public function testDeleteForProjectFile(File $file, string $type): void
+    {
+        $staff       = $this->createStaff();
+        $project     = $this->createProject($staff);
+        $projectFile = new ProjectFile($type, $file, $project, $staff);
+
+        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn($projectFile);
+        $this->security->isGranted(ProjectFileVoter::ATTRIBUTE_DELETE, $projectFile)->shouldBeCalledOnce()->willReturn(true);
+        $this->projectFileRepository->remove($projectFile)->shouldBeCalledOnce();
+        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldNotBeCalled();
+        $this->projectRepository->flush()->shouldNotBeCalled();
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, $type);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function projectFileProvider(): array
+    {
+        $file = new File();
+        $file->setPublicId();
+
+        return [
+            ProjectFile::PROJECT_FILE_TYPE_GENERAL              => [$file, ProjectFile::PROJECT_FILE_TYPE_GENERAL],
+            ProjectFile::PROJECT_FILE_TYPE_ACCOUNTING_FINANCIAL => [$file, ProjectFile::PROJECT_FILE_TYPE_ACCOUNTING_FINANCIAL],
+            ProjectFile::PROJECT_FILE_TYPE_LEGAL                => [$file, ProjectFile::PROJECT_FILE_TYPE_LEGAL],
+            ProjectFile::PROJECT_FILE_TYPE_KYC                  => [$file, ProjectFile::PROJECT_FILE_TYPE_KYC],
+        ];
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteForProjectFileExceptionNotFound(): void
+    {
+        $file = new File();
+        $file->setPublicId();
+
+        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn(null);
+        $this->security->isGranted(Argument::cetera())->shouldNotBeCalled();
+        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->flush()->shouldNotBeCalled();
+
+        static::expectException(NotFoundHttpException::class);
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, ProjectFile::PROJECT_FILE_TYPE_GENERAL);
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteForProjectFileExceptionAccessDenied(): void
+    {
+        $type = ProjectFile::PROJECT_FILE_TYPE_GENERAL;
+        $file = new File();
+        $file->setPublicId();
+        $staff       = $this->createStaff();
+        $project     = $this->createProject($staff);
+        $projectFile = new ProjectFile($type, $file, $project, $staff);
+
+        $this->projectFileRepository->findOneBy(['file' => $file])->shouldBeCalledOnce()->willReturn($projectFile);
+        $this->security->isGranted(ProjectFileVoter::ATTRIBUTE_DELETE, $projectFile)->shouldBeCalledOnce()->willReturn(false);
+        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->flush()->shouldNotBeCalled();
+
+        static::expectException(AccessDeniedException::class);
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, $type);
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @dataProvider projectProvider
+     */
+    public function testDeleteProject(File $file, string $type, string $field): void
+    {
+        $project = $this->createProject($this->createStaff());
+
+        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->findOneBy([$field => $file])->shouldBeCalledOnce()->willReturn($project);
+        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldBeCalledOnce()->willReturn(true);
+        $this->projectRepository->flush()->shouldBeCalledOnce();
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, $type);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function projectProvider(): array
+    {
+        $file = new File();
+        $file->setPublicId();
+
+        return [
+            Project::PROJECT_FILE_TYPE_DESCRIPTION => [$file, Project::PROJECT_FILE_TYPE_DESCRIPTION, 'termSheet'],
+            Project::PROJECT_FILE_TYPE_NDA         => [$file, Project::PROJECT_FILE_TYPE_NDA, 'nda'],
+        ];
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteForProjectExceptionNotFound(): void
+    {
+        $file = new File();
+        $file->setPublicId();
+
+        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->findOneBy(['termSheet' => $file])->shouldBeCalledOnce()->willReturn(null);
+        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->flush()->shouldNotBeCalled();
+
+        static::expectException(NotFoundHttpException::class);
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, Project::PROJECT_FILE_TYPE_DESCRIPTION);
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteForProjectExceptionAccessDenied(): void
+    {
+        $type = Project::PROJECT_FILE_TYPE_NDA;
+        $file = new File();
+        $file->setPublicId();
+        $staff   = $this->createStaff();
+        $project = $this->createProject($staff);
+
+        $this->projectFileRepository->findOneBy(Argument::any())->shouldNotBeCalled();
+        $this->projectFileRepository->remove(Argument::any())->shouldNotBeCalled();
+        $this->projectRepository->findOneBy(['nda' => $file])->shouldBeCalledOnce()->willReturn($project);
+        $this->security->isGranted(ProjectVoter::ATTRIBUTE_EDIT, $project)->shouldBeCalledOnce()->willReturn(false);
+        $this->projectRepository->flush()->shouldNotBeCalled();
+
+        static::expectException(AccessDeniedException::class);
+
+        $fileDeleteManager = $this->createTestObject();
+        $fileDeleteManager->delete($file, $type);
+    }
+
+    private function createProject(Staff $staff): Project
+    {
+        return new Project(
+            $staff,
+            'RISK-GROUP-42',
+            new Money('EUR', '42')
+        );
+    }
+
+    private function createStaff(): Staff
+    {
+        $teamRoot = Team::createRootTeam(new Company('Company', 'Company', ''));
+        $team     = Team::createTeam('Team', $teamRoot);
+
+        return new Staff(new User('user@mail.com'), $team);
+    }
+
+    private function createTestObject(): FileDeleteManager
+    {
+        return new FileDeleteManager(
+            $this->security->reveal(),
+            $this->projectRepository->reveal(),
+            $this->projectFileRepository->reveal(),
+        );
+    }
+}
