1843e974a73865428211fb09c6177036be0a4348
ivkouCALS <ivanis.kouame@ca-lendingservices.com>
2021-08-19 15:36:43 +0200

    feature(arrangement): CALS-4130 Finalize slack notification for project status update

--- src/Syndication/Arrangement/Service/Project/SlackNotifier/ProjectUpdateNotifier.php
+++ src/Syndication/Arrangement/Service/Project/SlackNotifier/ProjectUpdateNotifier.php
@@ -7,6 +7,7 @@ namespace KLS\Syndication\Arrangement\Service\Project\SlackNotifier;
 use InvalidArgumentException;
 use KLS\Syndication\Arrangement\Entity\Project;
 use KLS\Syndication\Arrangement\Entity\ProjectParticipation;
+use KLS\Syndication\Arrangement\Entity\ProjectParticipationStatus;
 use KLS\Syndication\Arrangement\Entity\ProjectStatus;
 use Nexy\Slack\Attachment;
 use Nexy\Slack\AttachmentField;
@@ -75,18 +76,24 @@ class ProjectUpdateNotifier implements ProjectNotifierInterface
             /** @var ProjectParticipation $participation */
             foreach ($project->getProjectParticipations() as $participation) {
                 if ($participation->isActive()) {
-                    $participants[] = $participation->getParticipant()->getDisplayName() . ': ' .
-                        $participation->getInterestRequest()->getMoney()->getAmount() . $participation->getInterestRequest()->getMoney()->getCurrency();
+                    $money          = $this->getRelevantParticipationDataByProjectStatus($project, $participation);
+                    $participants[] = $participation->getParticipant()->getDisplayName() . ': ' . $money;
 
                     continue;
                 }
                 $archivedParticipants[] = $participation->getParticipant()->getDisplayName() . ': ' .
-                    $this->translator->trans('project-participation-status.' . $participation->getCurrentStatus()->getStatus());
+                    $this->translator->trans($this->getArchivedProjectParticipationStatusTranslationKey($participation));
             }
             $message->attach(
                 (new Attachment())
-                    ->addField(new AttachmentField('Participants', \implode(', ', $participants), true))
-                    ->addField(new AttachmentField('Participants archivés', \implode(', ', $archivedParticipants), true))
+                    ->addField(new AttachmentField('Participants', false === empty($archivedParticipants) ? '• ' . \implode(PHP_EOL . '• ', $participants) : '', true))
+                    ->addField(
+                        new AttachmentField(
+                            'Participants archivés',
+                            false === empty($archivedParticipants) ? '• ' . \implode(PHP_EOL . '• ', $archivedParticipants) : '',
+                            true
+                        )
+                    )
             );
         }
 
@@ -113,5 +120,56 @@ class ProjectUpdateNotifier implements ProjectNotifierInterface
         }
 
         throw new InvalidArgumentException('The project is in an unknown status');
+    }
+
+    private function getRelevantParticipationDataByProjectStatus(Project $project, ProjectParticipation $participation): string
+    {
+        $projectCurrentStatus = $project->getCurrentStatus();
+
+        if ($projectCurrentStatus) {
+            switch ($projectCurrentStatus->getStatus()) {
+                case ProjectStatus::STATUS_INTEREST_EXPRESSION:
+                    return \implode(' - ', \array_filter([$participation->getInterestRequest()->getMoney(), $participation->getInterestRequest()->getMaxMoney()]));
+
+                case ProjectStatus::STATUS_PARTICIPANT_REPLY:
+                    return (string) $participation->getInvitationRequest()->getMoney();
+
+                case ProjectStatus::STATUS_ALLOCATION:
+                    return (string) $participation->getTotalInvitationReply();
+
+                case ProjectStatus::STATUS_SYNDICATION_FINISHED:
+                    return (string) $participation->getTotalAllocation();
+            }
+
+            throw new \UnexpectedValueException(
+                \sprintf('Unexpected %s status value for project %s', $projectCurrentStatus->getStatus(), $project->getPublicId())
+            );
+        }
+
+        throw new \UnexpectedValueException(\sprintf('No current status available for project %s', $project->getPublicId()));
+    }
+
+    private function getArchivedProjectParticipationStatusTranslationKey(ProjectParticipation $participation): string
+    {
+        $projectParticipationStatus = $participation->getCurrentStatus();
+
+        if ($projectParticipationStatus) {
+            switch ($projectParticipationStatus->getStatus()) {
+                case ProjectParticipationStatus::STATUS_ARCHIVED_BY_ARRANGER:
+                    return 'project-participation-status.-10';
+
+                case ProjectParticipationStatus::STATUS_ARCHIVED_BY_PARTICIPANT:
+                    return 'project-participation-status.-20';
+
+                case ProjectParticipationStatus::STATUS_COMMITTEE_REJECTED:
+                    return 'project-participation-status.-30';
+            }
+
+            throw new \UnexpectedValueException(
+                \sprintf('Unexpected %s status value for archived project participation %s', $projectParticipationStatus->getStatus(), $participation->getPublicId())
+            );
+        }
+
+        throw new \UnexpectedValueException(\sprintf('No current status available for projectParticipation %s', $participation->getPublicId()));
     }
 }
