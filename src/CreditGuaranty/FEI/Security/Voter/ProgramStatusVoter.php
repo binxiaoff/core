2767d0e45bf7ea766352e1802c59c6616ae328e7
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-09-20 18:19:14 +0200

    fix(fei): CALS-4673 Fix program status voter by removing program status check
    Closes CALS-4673

--- src/CreditGuaranty/FEI/Security/Voter/ProgramStatusVoter.php
+++ src/CreditGuaranty/FEI/Security/Voter/ProgramStatusVoter.php
@@ -7,14 +7,29 @@ namespace KLS\CreditGuaranty\FEI\Security\Voter;
 use KLS\Core\Entity\User;
 use KLS\Core\Security\Voter\AbstractEntityVoter;
 use KLS\CreditGuaranty\FEI\Entity\ProgramStatus;
+use KLS\CreditGuaranty\FEI\Entity\StaffPermission;
+use KLS\CreditGuaranty\FEI\Service\StaffPermissionManager;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class ProgramStatusVoter extends AbstractEntityVoter
 {
+    private StaffPermissionManager $staffPermissionManager;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker, StaffPermissionManager $staffPermissionManager)
+    {
+        parent::__construct($authorizationChecker);
+        $this->staffPermissionManager = $staffPermissionManager;
+    }
+
     /**
      * @param ProgramStatus $subject
      */
     protected function isGrantedAll($subject, User $user): bool
     {
-        return $this->authorizationChecker->isGranted(ProgramVoter::ATTRIBUTE_EDIT, $subject->getProgram());
+        $staff = $user->getCurrentStaff();
+
+        return $staff
+            && $this->authorizationChecker->isGranted(ProgramRoleVoter::ROLE_MANAGER, $subject->getProgram())
+            && $this->staffPermissionManager->hasPermissions($staff, StaffPermission::PERMISSION_EDIT_PROGRAM);
     }
 }
