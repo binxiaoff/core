6cf512973be3649ec86b34334d9a83ec26e3dc49
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-10-19 17:32:29 +0200

    feat(core): CALS-4792 Migrate TemporaryTokenAuthenticator to the new authenticator
    Closes CALS-4792

--- src/Core/Security/TemporaryTokenAuthenticator.php
+++ src/Core/Security/TemporaryTokenAuthenticator.php
@@ -8,36 +8,31 @@ use Exception;
 use KLS\Core\Event\TemporaryToken\TemporaryTokenAuthenticationEvents;
 use KLS\Core\Event\TemporaryToken\TemporaryTokenAuthenticationFailureEvent;
 use KLS\Core\Event\TemporaryToken\TemporaryTokenAuthenticationSuccessEvent;
-use KLS\Core\Exception\TemporaryToken\InvalidTemporaryTokenException;
 use KLS\Core\Repository\TemporaryTokenRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
-class TemporaryTokenAuthenticator extends AbstractGuardAuthenticator
+class TemporaryTokenAuthenticator extends AbstractAuthenticator
 {
     private TemporaryTokenRepository $temporaryTokenRepository;
     private EventDispatcherInterface $dispatcher;
 
-    public function __construct(TemporaryTokenRepository $temporaryTokenRepository, EventDispatcherInterface $dispatcher)
-    {
+    public function __construct(
+        TemporaryTokenRepository $temporaryTokenRepository,
+        EventDispatcherInterface $dispatcher
+    ) {
         $this->temporaryTokenRepository = $temporaryTokenRepository;
         $this->dispatcher               = $dispatcher;
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        $exception = new InvalidTemporaryTokenException('Temporary token is not found.', 0, $authException);
-        $event     = new TemporaryTokenAuthenticationFailureEvent($exception, $this->buildAuthenticationFailureResponse($exception->getMessage()));
-        $this->dispatcher->dispatch($event, TemporaryTokenAuthenticationEvents::AUTHENTICATION_FAILURE);
-
-        return $event->getResponse();
     }
 
     public function supports(Request $request): bool
@@ -45,30 +40,30 @@ class TemporaryTokenAuthenticator extends AbstractGuardAuthenticator
         return $request->headers->has('X-AUTH-TOKEN');
     }
 
-    public function getCredentials(Request $request)
+    public function authenticate(Request $request): PassportInterface
     {
-        return $request->headers->get('X-AUTH-TOKEN');
+        $token = $request->headers->get('X-AUTH-TOKEN');
+
+        if (empty($token)) {
+            throw new CustomUserMessageAuthenticationException('Temporary token is empty.');
+        }
+
+        return new SelfValidatingPassport(new UserBadge($token, [$this, 'getUser']));
     }
 
     /**
-     * @param mixed $credentials
-     *
      * @throws Exception
      */
-    public function getUser($credentials, UserProviderInterface $userProvider): UserInterface
+    public function getUser(string $token): UserInterface
     {
-        if (empty($credentials)) {
-            throw new InvalidTemporaryTokenException('Temporary token is empty.');
-        }
-
-        $temporaryToken = $this->temporaryTokenRepository->findOneBy(['token' => $credentials]);
+        $temporaryToken = $this->temporaryTokenRepository->findOneBy(['token' => $token]);
 
         if (null === $temporaryToken) {
-            throw new InvalidTemporaryTokenException('Temporary token is not found.');
+            throw new CustomUserMessageAuthenticationException('Temporary token is not found.');
         }
 
         if (false === $temporaryToken->isValid()) {
-            throw new InvalidTemporaryTokenException('Temporary token is not valid.');
+            throw new CustomUserMessageAuthenticationException('Temporary token is not valid.');
         }
 
         $temporaryToken->setAccessed();
@@ -77,37 +72,29 @@ class TemporaryTokenAuthenticator extends AbstractGuardAuthenticator
         return $temporaryToken->getUser();
     }
 
-    public function checkCredentials($credentials, UserInterface $user): bool
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return true;
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $authException): Response
-    {
-        $event = new TemporaryTokenAuthenticationFailureEvent($authException, $this->buildAuthenticationFailureResponse($authException->getMessage()));
+        $response = new JsonResponse(
+            [
+                'code'    => Response::HTTP_UNAUTHORIZED,
+                'message' => \strtr($exception->getMessageKey(), $exception->getMessageData()),
+            ],
+            Response::HTTP_UNAUTHORIZED
+        );
+        $event = new TemporaryTokenAuthenticationFailureEvent($exception, $response);
         $this->dispatcher->dispatch($event, TemporaryTokenAuthenticationEvents::AUTHENTICATION_FAILURE);
 
-        return $event->getResponse();
+        return $response;
     }
 
     /**
-     * @param mixed $providerKey
-     *
      * @throws Exception
      */
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         $event = new TemporaryTokenAuthenticationSuccessEvent($token->getUser());
         $this->dispatcher->dispatch($event, TemporaryTokenAuthenticationEvents::AUTHENTICATION_SUCCESS);
-    }
 
-    public function supportsRememberMe(): bool
-    {
-        return false;
-    }
-
-    private function buildAuthenticationFailureResponse(string $message): JsonResponse
-    {
-        return new JsonResponse(['code' => JsonResponse::HTTP_UNAUTHORIZED, 'message' => $message], JsonResponse::HTTP_UNAUTHORIZED);
+        return null;
     }
 }
