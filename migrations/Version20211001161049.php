cc1f95919e598dcba9717cab11a0642da0c6c89f
Bin Xiao <bin.xiao@ca-lendingservices.com>
2021-10-26 14:25:16 +0200

    fix(migration): Quick fix on the migration script problem in case of a re-execution (after a rollback)

--- migrations/Version20211001161049.php
+++ migrations/Version20211001161049.php
@@ -17,7 +17,7 @@ final class Version20211001161049 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE credit_guaranty_financing_object ADD loan_money_after_contractualisation_amount NUMERIC(15, 2) DEFAULT NULL, ADD loan_money_after_contractualisation_currency VARCHAR(3) DEFAULT NULL');
-        $this->addSql("INSERT INTO credit_guaranty_field (public_id, tag, category, type, field_alias, reservation_property_name, property_path, property_type, object_class, comparable, unit, predefined_items) VALUES ('91c636cc-cb6a-48f2-ad4a-589b588ad441', 'info', 'loan', 'other', 'loan_money_after_contractualisation', 'financingObjects', 'loanMoneyAfterContractualisation', 'NullableMoney', 'KLS\\\\CreditGuaranty\\\\FEI\\\\Entity\\\\FinancingObject', 0, NULL, NULL)");
+        $this->addSql("INSERT IGNORE INTO credit_guaranty_field (public_id, tag, category, type, field_alias, reservation_property_name, property_path, property_type, object_class, comparable, unit, predefined_items) VALUES ('91c636cc-cb6a-48f2-ad4a-589b588ad441', 'info', 'loan', 'other', 'loan_money_after_contractualisation', 'financingObjects', 'loanMoneyAfterContractualisation', 'NullableMoney', 'KLS\\\\CreditGuaranty\\\\FEI\\\\Entity\\\\FinancingObject', 0, NULL, NULL)");
     }
 
     public function down(Schema $schema): void
