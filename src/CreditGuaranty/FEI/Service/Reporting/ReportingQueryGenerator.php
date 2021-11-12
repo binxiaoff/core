6d3001d55b11ede6b2fd7ddb6232730a7756ee70
elisatheng <elisa.theng@gmail.com>
2021-11-12 14:26:15 +0100

    feat(fei): CALS-4484 clean query filters before processing

--- src/CreditGuaranty/FEI/Service/Reporting/ReportingQueryGenerator.php
+++ src/CreditGuaranty/FEI/Service/Reporting/ReportingQueryGenerator.php
@@ -6,14 +6,47 @@ namespace KLS\CreditGuaranty\FEI\Service\Reporting;
 
 use ArrayIterator;
 use Exception;
+use KLS\Core\Entity\Constant\MathOperator;
+use KLS\CreditGuaranty\FEI\Entity\Constant\FieldAlias;
 use KLS\CreditGuaranty\FEI\Entity\Field;
 use KLS\CreditGuaranty\FEI\Entity\ReportingTemplate;
 use KLS\CreditGuaranty\FEI\Repository\FieldRepository;
 
 class ReportingQueryGenerator
 {
-    private const FILTER_SEARCH = 'search';
-    private const FILTER_ORDER  = 'order';
+    private const FILTER_SEARCH          = 'search';
+    private const FILTER_ORDER           = 'order';
+    private const FILTER_REPORTING_DATES = 'reporting_dates';
+
+    private const DATE_FILTER_KEYS = [
+        self::FILTER_REPORTING_DATES,
+        FieldAlias::FIRST_RELEASE_DATE,
+        FieldAlias::RESERVATION_EXCLUSION_DATE,
+    ];
+
+    private const DURATION_FILTER_KEYS = [
+        FieldAlias::RESERVATION_SIGNING_DATE,
+    ];
+
+    private const ALLOWED_FILTER_KEYS = [
+        self::FILTER_SEARCH,
+        ...self::DATE_FILTER_KEYS,
+        ...self::DURATION_FILTER_KEYS,
+        self::FILTER_ORDER,
+    ];
+
+    private const ALLOWED_ORDER_VALUES = [
+        'asc',
+        'desc',
+    ];
+
+    private const MAPPING_OPERATORS = [
+        MathOperator::SUPERIOR          => '>',
+        MathOperator::SUPERIOR_OR_EQUAL => '>=',
+        MathOperator::INFERIOR          => '<',
+        MathOperator::INFERIOR_OR_EQUAL => '<=',
+        MathOperator::EQUAL             => '=',
+    ];
 
     private FieldRepository $fieldRepository;
     private ReportingQueryHelper $reportingQueryHelper;
@@ -71,6 +104,8 @@ class ReportingQueryGenerator
 
     private function generateFilters(array $fields, array $filters): array
     {
+        $this->cleanFilters($fields, $filters);
+
         $joins   = [];
         $clauses = [];
 
@@ -146,5 +181,76 @@ class ReportingQueryGenerator
         \array_walk_recursive($fields, fn (&$item) => $item = $item->getField());
 
         return $fields;
+    }
+
+    /**
+     * @param Field[]|array $fields
+     */
+    private function cleanFilters(array $fields, array &$filters): void
+    {
+        // field aliases of reportingTemplateFields
+        $fieldAliases = \array_map(static fn ($item) => $item->getFieldAlias(), $fields);
+
+        foreach ($filters as $filterKey => $filterValue) {
+            // we ignore non-existent filters like API Platform
+            if (false === \in_array($filterKey, self::ALLOWED_FILTER_KEYS)) {
+                unset($filters[$filterKey]);
+            }
+
+            // we ignore invalid values of existent filters like API Platform
+
+            if (self::FILTER_ORDER === $filterKey) {
+                if (false === \is_array($filterValue)) {
+                    unset($filters[$filterKey]);
+                }
+
+                foreach ($filterValue as $filterFieldAlias => $filterFieldValue) {
+                    // we remove the order filter item
+                    // if field_alias does not belong to reportingTemplateFields aliases and to reporting dates aliases
+                    // or if value is invalid
+                    if (
+                        false === \in_array($filterFieldAlias, $fieldAliases)
+                        && false === \in_array($filterFieldAlias, FieldAlias::REPORTING_DATE_FIELDS)
+                    ) {
+                        unset($filters[$filterKey][$filterFieldAlias]);
+                    }
+                    if (false === \in_array(\mb_strtolower($filterFieldValue), self::ALLOWED_ORDER_VALUES)) {
+                        unset($filters[$filterKey][$filterFieldAlias]);
+                    }
+                }
+            }
+
+            if (\in_array($filterKey, self::DATE_FILTER_KEYS)) {
+                if (false === \is_array($filterValue)) {
+                    unset($filters[$filterKey]);
+                }
+
+                foreach ($filterValue as $filterOperator => $filterOperatorValue) {
+                    if (empty(self::MAPPING_OPERATORS[$filterOperator])) {
+                        unset($filters[$filterKey]);
+                    }
+
+                    if (0 === \preg_match('/^(\d{4}\-\d{2}\-\d{2}|null)$/', $filterOperatorValue)) {
+                        unset($filters[$filterKey]);
+                    }
+                }
+            }
+
+            if (\in_array($filterKey, self::DURATION_FILTER_KEYS)) {
+                if (false === \is_array($filterValue)) {
+                    unset($filters[$filterKey]);
+                }
+
+                foreach ($filterValue as $filterOperator => $filterOperatorValue) {
+                    if (empty(self::MAPPING_OPERATORS[$filterOperator])) {
+                        unset($filters[$filterKey]);
+                    }
+
+                    if (0 === \preg_match('/^(\d+|null)$/', $filterOperatorValue)) {
+                        unset($filters[$filterKey]);
+                    }
+                }
+            }
+        }
     }
 }
