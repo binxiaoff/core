09cdd0a0ab668f44dcf3c8e9ed3c712f3b490d7b
Bin Xiao <bin.xiao@kls-platform.com>
2022-02-01 18:14:11 +0100

    fix(arrangement): CALS-4024 Fix the query extention of message thread to return the right threads

--- src/Core/Extension/MessageThread/ListExtension.php
+++ src/Core/Extension/MessageThread/ListExtension.php
@@ -23,8 +23,10 @@ class ListExtension implements QueryCollectionExtensionInterface
     private Security $security;
     private ProjectParticipationMemberRepository $projectParticipationMemberRepository;
 
-    public function __construct(Security $security, ProjectParticipationMemberRepository $projectParticipationMemberRepository)
-    {
+    public function __construct(
+        Security $security,
+        ProjectParticipationMemberRepository $projectParticipationMemberRepository
+    ) {
         $this->security                             = $security;
         $this->projectParticipationMemberRepository = $projectParticipationMemberRepository;
     }
@@ -54,27 +56,34 @@ class ListExtension implements QueryCollectionExtensionInterface
 
         $queryBuilder
             ->innerJoin(ProjectParticipation::class, 'pp', Join::WITH, $rootAlias . '.projectParticipation = pp.id')
-            ->leftJoin(ProjectParticipationMember::class, 'ppm', Join::WITH, 'ppm.projectParticipation = pp.id AND ppm.archived IS NULL')
+            ->leftJoin(ProjectParticipationMember::class, 'ppm', Join::WITH, 'ppm.projectParticipation = pp.id')
             ->innerJoin(Project::class, 'p', Join::WITH, 'p.id = pp.project')
             ->innerJoin(ProjectStatus::class, 'pst', Join::WITH, 'pst.project = p.id')
-            ->andWhere(
-                $queryBuilder->expr()->orX(
-                    // you are the arranger
-                    'p.submitterCompany = :company',
-                    // or you are member of participation OR you managed a member of a participation
-                    $queryBuilder->expr()->andX(
-                        'pst.status in (:displayableStatus)',
-                        $queryBuilder->expr()->orX(
-                            'ppm.staff = :staff',
-                            'ppm IN (:managedStaffMember)',
-                        )
+            ->andWhere($queryBuilder->expr()->orX(
+                // you are the project owner
+                'p.submitterUser = :user',
+                // or you fulfill the two following conditions :
+                $queryBuilder->expr()->andX(
+                    // 1. you are a non archived member of participation OR you managed a member of a participation
+                    $queryBuilder->expr()->orX(
+                        $queryBuilder->expr()->andX('ppm.staff = :staff', 'ppm.archived IS NULL'),
+                        'ppm IN (:managedStaffMember)',
+                    ),
+                    // 2. you are in arranger company OR your participant and the project is in displayable status
+                    $queryBuilder->expr()->orX(
+                        'p.submitterCompany = :company',
+                        'pst.status in (:displayableStatus)'
                     )
                 )
-            )
+            ))
             ->setParameter('company', $staff->getCompany())
             ->setParameter('displayableStatus', ProjectStatus::DISPLAYABLE_STATUSES)
             ->setParameter('staff', $staff)
-            ->setParameter('managedStaffMember', $this->projectParticipationMemberRepository->findActiveByManager($staff))
+            ->setParameter('user', $staff->getUser())
+            ->setParameter(
+                'managedStaffMember',
+                $this->projectParticipationMemberRepository->findActiveByManager($staff)
+            )
             ->orderBy('p.title', 'ASC')
         ;
     }
